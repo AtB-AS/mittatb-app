16f415da7873110ab58d11b2a297ab3c17dd4c8a
Christian Brevik <christian.brevik@gmail.com>
2020-02-18 13:38:38 +0100

    Test

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,3 +2,14 @@ cat <<EOT >> .env
 BUGSNAG_API_KEY=$BUGSNAG_API_KEY
 API_BASE_URL=$API_BASE_URL
 EOT
+
+brew tap wix/brew
+brew update
+brew install applesimutils
+
+echo "Install pods "
+cd ios; pod install; cd ..
+
+npx detox build
+
+npx detox test --cleanup
