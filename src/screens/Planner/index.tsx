ab925925d81006e6dae5ecbb25a20f99b51720b8
Mikael Brevik <mikaelbre@gmail.com>
2020-03-20 12:01:23 +0100

    Removes unused imports

--- src/screens/Planner/index.tsx
+++ src/screens/Planner/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {TripPattern} from '../../sdk';
 import Assistant from './Overview';
-import {Location} from '../../favorites/types';
 import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 import {LocationWithSearchMetadata} from '../../location-search';
 
@@ -10,7 +8,6 @@ export type PlannerStackParams = {
     fromLocation: LocationWithSearchMetadata;
     toLocation: LocationWithSearchMetadata;
   };
-  Detail: {tripPattern: TripPattern; from: Location; to: Location};
 };
 
 const Stack = createSharedElementStackNavigator<PlannerStackParams>();
