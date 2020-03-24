e5eeb00783a8bf4ff53e1c2f92c198c7c84aa130
Christian Brevik <christian.brevik@gmail.com>
2020-03-24 07:41:00 +0100

    Cannot use code bundle - it is CodePush specific

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -29,8 +29,7 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
             echo "Generating and uploading Android source maps"
             npx bugsnag-sourcemaps upload \
                 --api-key=$BUGSNAG_API_KEY \
-                --app-version=1.0 \
-                --code-bundle-id=$APPCENTER_BUILD_ID \
+                --app-version=$APPCENTER_BUILD_ID \
                 --minifiedFile=android/app/build/generated/assets/react/$APP_ENVIRONMENT/index.android.bundle \
                 --source-map=android/app/build/generated/sourcemaps/react/$APP_ENVIRONMENT/index.android.bundle.map \
                 --minified-url=index.android.bundle \
