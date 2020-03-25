302a128da2b5780fca0d29e7b289bc6454e6c6f1
Mikael Brevik <mikaelbre@gmail.com>
2020-03-25 22:58:10 +0100

    Adds lottie and new transport icon animation

---
+++ ios/enforce-bridge.swift
@@ -0,0 +1,9 @@
+//
+//  enforce-bridge.swift
+//  atb
+//
+//  Created by Mikael Brevik on 25/03/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/components/transportation-icon/index.tsx
+++ src/components/transportation-icon/index.tsx
@@ -1,25 +1,49 @@
 import {LegMode} from '../../sdk';
-import LegTransportMethodIcon, {
-  LegTransportMethodIconProps,
-} from './svg/LegTransportMethodIcon';
-import BusLegIcon from './svg/BusLegIcon';
-import TramLegIcon from './svg/TramLegIcon';
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import {View} from 'react-native';
+import {StyleSheet} from '../../theme';
+import BusFront from '../../assets/svg/BusFront';
+import TramFront from '../../assets/svg/TramFront';
 
-export type TransportationIconProps = LegTransportMethodIconProps & {
+export type TransportationIconProps = {
   mode: LegMode;
+  isLive?: boolean;
 };
 
 export default function TransportationIcon({
   mode,
-  ...props
+  isLive,
 }: TransportationIconProps) {
+  const styles = useStyle();
+  if (!isLive) {
+    return <InnerIcon mode={mode} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <LottieView source={require('./realtime-animation.json')} autoPlay loop />
+      <InnerIcon mode={mode} />
+    </View>
+  );
+}
+
+function InnerIcon({mode}: TransportationIconProps) {
   switch (mode) {
     case 'bus':
-      return <BusLegIcon {...props} />;
+      return <BusFront fill="black" />;
     case 'tram':
-      return <TramLegIcon {...props} />;
+      return <TramFront fill="black" />;
     default:
-      return <LegTransportMethodIcon {...props} />;
+      return null;
   }
 }
+
+const useStyle = StyleSheet.createThemeHook(theme => ({
+  container: {
+    width: 28,
+    height: 28,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
