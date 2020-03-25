17d9a79b6b9dc84241f3dcb36a120b087f8dcf35
Mikael Brevik <mikaelbre@gmail.com>
2020-03-25 12:57:11 +0100

    Removes debug logging

--- src/api/departures.ts
+++ src/api/departures.ts
@@ -5,7 +5,6 @@ export async function getNearestDepartures(
   coords: Coordinates,
 ): Promise<EstimatedCall[]> {
   let url = `v1/departures/nearest?lat=${coords.latitude}&lon=${coords.longitude}`;
-  console.log(url);
   const client = await getClient();
   const response = await client.get<EstimatedCall[]>(url);
   return response.data ?? [];
