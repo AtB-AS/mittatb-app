c4ca4fbb6bcafdc7b9e5eeee9565a8f9a62b96e0
Christian Brevik <christian.brevik@gmail.com>
2020-03-26 13:01:55 +0100

    Update src/favorites/storage.ts
    Co-Authored-By: Mikael Brevik <mikaelbre@gmail.com>

--- src/favorites/storage.ts
+++ src/favorites/storage.ts
@@ -45,7 +45,10 @@ export async function updateFavorite(
   return await setFavorites(favorites);
 }
 
-// TODO: Function to migrate old favorites to new favorites - remove when reasonable
+/*
+* @TODO: Function to migrate old favorites to new favorites - remove when reasonable
+* @deprecated
+*/
 export async function deprecated__ensureFavoritesHasIds(
   favorites: UserFavorites | null,
 ): Promise<UserFavorites | null> {
