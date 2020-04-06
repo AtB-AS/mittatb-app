76dced1b035b6365c8cbf4969cfb21f64c7316cd
Christian Brevik <christian.brevik@gmail.com>
2020-04-06 12:01:57 +0200

    Add release stage in env, set explicitly for config
    This is JavaScript-side only at the moment. Crashes that
    happens before JS bundle is loaded will not have correct
    release stage set.

--- src/diagnostics/bugsnag.ts
+++ src/diagnostics/bugsnag.ts
@@ -1,9 +1,10 @@
 import {Client} from 'bugsnag-react-native';
 import {getInstallId} from '../utils/installId';
-import {BUGSNAG_API_KEY, APP_VERSION} from 'react-native-dotenv';
+import {BUGSNAG_API_KEY, BUGSNAG_RELEASE_STAGE, APP_VERSION} from 'react-native-dotenv';
 
 const client = new Client(BUGSNAG_API_KEY);
 client.config.appVersion = APP_VERSION;
+client.config.releaseStage = BUGSNAG_RELEASE_STAGE;
 
 if (__DEV__) {
   client.config.registerBeforeSendCallback(function() {
