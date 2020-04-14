0c0d44201d8302bbee7c85f5a7d9bf6cfb763f38
Christian Brevik <christian.brevik@gmail.com>
2020-04-14 15:38:23 +0200

    Add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
   root: true,
-  extends: '@react-native-community',
+  extends: ['plugin:prettier/recommended', 'prettier/react'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-native'],
+  rules: {
+    // eslint
+    'no-console': 1,
+    'no-fallthrough': 2,
+
+    // React-Hooks Plugin
+    // The following rules are made available via `eslint-plugin-react-hooks`
+    'react-hooks/rules-of-hooks': 2, // early error
+
+    // https://github.com/Intellicode/eslint-plugin-react-native
+    'react-native/no-raw-text': 2, // rather early error than JS bundle crash
+    'react-native/no-single-element-style-arrays': 1,
+    'react-native/no-unused-styles': 1,
+  },
 };
