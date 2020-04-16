54ecafabcd60b0b57fa8df56d696d0235ba687d5
Mikael Brevik <mikaelbre@gmail.com>
2020-04-16 14:29:19 +0200

    chore: Adds confirm step to release tool (#181)

--- tools/release/release-draft.sh
+++ tools/release/release-draft.sh
@@ -4,11 +4,18 @@ if [[ "$branch" != "master" ]]; then
   exit 1;
 fi
 
+read -p "This will create a Pull Request on Github, are you sure you want to do release? (n/Y) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 0;
+fi
+
+
 echo 'Gathering commits and creating changelog'
 message=`yarn --silent conventional-changelog -p angular -n ./tools/release/changelog.config.js`
 
 if [ $? -eq 0 ]; then
-  echo 'Creating pull requests'
   gh pr create -B alpha-release -b "$message" -t '[Sync] New release to alpha channel'
 else
   echo 'Could not create changelog'
