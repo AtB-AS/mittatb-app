8f3b7c3ab0760e06cd429c193b88f53f47d8b2f9
Mikael Brevik <mikaelbre@gmail.com>
2020-04-21 15:16:08 +0200

    fix(overview): Fixes issue with overview showing wrong "to place" (#190)
    * fix(overview): Fixes issue with overview showing wrong "to place"
    
    Fixes #178
    
    There was an issue with async handling requests and race conditioning
    where if you searched for something before previous request was
    finished and the next request returned no result, it would show
    previous trip, but new to place. This is now fixed.
    
    This also technically improves performance somewhat, as we cancel
    previous requests as soon as we can, preventing further work and
    processing. These improvements were also added to search modal
    
    * Update src/location-search/useGeocoder.ts
    
    Co-Authored-By: Christian Brevik <christian.brevik@gmail.com>
    
    * Update src/location-search/useGeocoder.ts
    
    Co-Authored-By: Christian Brevik <christian.brevik@gmail.com>
    
    * Removes rethrowing error on non-cancellation errors for assistant trip patterns
    
    * Removes unnecessary destructuring in trip search api
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>

--- src/api/trips.ts
+++ src/api/trips.ts
@@ -1,22 +1,29 @@
 import {TripPattern} from '../sdk';
 import {getClient} from './client';
 import {Location} from '../favorites/types';
+import {AxiosRequestConfig} from 'axios';
 
-export default async function search(from: Location, to: Location) {
+export default async function search(
+  from: Location,
+  to: Location,
+  opts?: AxiosRequestConfig,
+) {
   const url = 'v1/journey/trip';
-  const {coordinates: fromCoordinates} = from;
-  const {coordinates: toCoordinates} = to;
   const client = await getClient();
-  return await client.post<TripPattern[]>(url, {
-    from: {
-      place: from.id,
-      name: from.name,
-      coordinates: fromCoordinates,
+  return await client.post<TripPattern[]>(
+    url,
+    {
+      from: {
+        place: from.id,
+        name: from.name,
+        coordinates: from.coordinates,
+      },
+      to: {
+        place: to.id,
+        name: to.name,
+        coordinates: to.coordinates,
+      },
     },
-    to: {
-      place: to.id,
-      name: to.name,
-      coordinates: toCoordinates,
-    },
-  });
+    opts,
+  );
 }
