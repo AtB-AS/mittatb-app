9e2bd0574cef15ff1d50619e8473200e86ed1952
Jørund Leknes <jorund.leknes@gmail.com>
2020-04-30 12:59:20 +0200

    fix: move fill color from path element to svg element for icons. eases changing the color.

--- src/assets/svg/ChevronDownIcon.tsx
+++ src/assets/svg/ChevronDownIcon.tsx
@@ -4,10 +4,7 @@ import Svg, {Path, SvgProps} from 'react-native-svg';
 function ChevronDownIcon(props: SvgProps) {
   return (
     <Svg width={20} height={20} viewBox="0 0 20 20" fill="black" {...props}>
-      <Path
-        d="M13.8281 7.14844L15 8.32031L10 13.3203L5 8.32031L6.17188 7.14844L10 10.9766L13.8281 7.14844Z"
-        fill="black"
-      />
+      <Path d="M13.8281 7.14844L15 8.32031L10 13.3203L5 8.32031L6.17188 7.14844L10 10.9766L13.8281 7.14844Z" />
     </Svg>
   );
 }
