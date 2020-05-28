df851caad6df05506eb8e0d43eb7f0e648e0e1b6
Christian Brevik <christian.brevik@gmail.com>
2020-05-28 14:29:26 +0200

    feat: Native splash screen
    Using react-native-bootsplash

--- ios/atb/AppDelegate.m
+++ ios/atb/AppDelegate.m
@@ -8,6 +8,8 @@
 #import <BugsnagConfiguration.h>
 #import <Firebase.h>
 
+#import "RNBootSplash.h"
+
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -59,6 +61,9 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
+  
   return YES;
 }
 
