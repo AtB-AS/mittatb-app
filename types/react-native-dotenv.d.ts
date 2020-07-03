1ce84f620066a116d369448d178ab74fb4c93741
Christian Brevik <christian.brevik@gmail.com>
2020-07-03 09:10:08 +0200

    feat: Extract product id to env file (#355)

--- types/react-native-dotenv.d.ts
+++ types/react-native-dotenv.d.ts
@@ -5,4 +5,5 @@ declare module 'react-native-dotenv' {
   export const APP_VERSION: string;
   export const IOS_BUNDLE_IDENTIFIER: string;
   export const PRIVACY_POLICY_URL: string;
+  export const SINGLE_TICKET_PRODUCT_ID: string;
 }
