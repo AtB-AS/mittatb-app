c937a8f7d6e002c6f1610873839c15e7d0811fee
Mikael Brevik <mikaelbre@gmail.com>
2020-07-27 08:37:57 +0200

    fix: increases icon size for tab bar

--- src/assets/svg/icons/tab-bar/Profile.tsx
+++ src/assets/svg/icons/tab-bar/Profile.tsx
@@ -3,10 +3,10 @@ import Svg, {Path, SvgProps} from 'react-native-svg';
 
 function SvgProfile(props: SvgProps) {
   return (
-    <Svg width={20} height={20} viewBox="0 0 20 20" fill="black" {...props}>
+    <Svg width={20} height={20} viewBox="0 0 16 16" fill="black" {...props}>
       <Path
         fillRule="evenodd"
-        d="M10 10a4 4 0 100-8 4 4 0 000 8zm-8 8a8 8 0 014.443-7.168A5.973 5.973 0 0010 12a5.973 5.973 0 003.557-1.168A8 8 0 0118 18H2z"
+        d="M8 8a4 4 0 100-8 4 4 0 000 8zm-8 8a8 8 0 014.443-7.168A5.973 5.973 0 008 10a5.973 5.973 0 003.557-1.168A8 8 0 0116 16H0z"
         clipRule="evenodd"
       />
     </Svg>
