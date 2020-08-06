4f76ed24bb7e0aad81f92b6fe6d53c32ed24a50c
Christian Brevik <christian.brevik@gmail.com>
2020-08-06 15:03:44 +0200

    chore: Upgrade to React Native 0.63.2

--- ios/atb/AppDelegate.m
+++ ios/atb/AppDelegate.m
@@ -13,7 +13,7 @@
 
 #import "RNBootSplash.h"
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -35,7 +35,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  #if DEBUG
+  #ifdef FB_SONARKIT_ENABLED
     InitializeFlipper(application);
   #endif
   NSString *intercomPath = [[NSBundle mainBundle] pathForResource:@"Intercom" ofType:@"plist"];
