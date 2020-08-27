a4b75cd785c0ca522927dbb6e3a4d48e461b33f0
Christian Brevik <christian.brevik@gmail.com>
2020-08-27 19:19:51 +0200

    Add shadow

--- src/location-search/map-selection/MapControls.tsx
+++ src/location-search/map-selection/MapControls.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
-import insets from '../../utils/insets';
+import {View, TouchableOpacity, ViewStyle} from 'react-native';
 import {CurrentLocationArrow} from '../../assets/svg/icons/places';
 import colors from '../../theme/colors';
+import {Add, Remove} from '../../assets/svg/icons/actions';
 
 export type Props = {
   flyToCurrentLocation(): void;
   zoomIn(): void;
   zoomOut(): void;
+};
+
+const shadows: ViewStyle = {
+  shadowColor: colors.general.gray200,
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowRadius: 8,
+  elevation: 8,
 };
 
 const MapControls: React.FC<Props> = ({
@@ -27,6 +37,7 @@ const MapControls: React.FC<Props> = ({
             justifyContent: 'center',
             alignItems: 'center',
             marginBottom: 8,
+            ...shadows,
           }}
         >
           <CurrentLocationArrow />
@@ -39,13 +50,13 @@ const MapControls: React.FC<Props> = ({
             borderTopLeftRadius: 5,
             borderTopRightRadius: 5,
             width: 36,
-            height: 28,
-
+            height: 36,
             justifyContent: 'center',
             alignItems: 'center',
+            ...shadows,
           }}
         >
-          <Text>+</Text>
+          <Add />
         </View>
       </TouchableOpacity>
       <TouchableOpacity onPress={zoomOut}>
@@ -55,15 +66,15 @@ const MapControls: React.FC<Props> = ({
             borderBottomLeftRadius: 5,
             borderBottomRightRadius: 5,
             width: 36,
-            height: 28,
+            height: 36,
             borderTopColor: colors.general.gray200,
             borderTopWidth: 0.5,
-
             justifyContent: 'center',
             alignItems: 'center',
+            ...shadows,
           }}
         >
-          <Text>-</Text>
+          <Remove />
         </View>
       </TouchableOpacity>
     </View>
