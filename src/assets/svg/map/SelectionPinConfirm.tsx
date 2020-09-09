843d7f6a6837885301c1e2768dba115d85689239
Christian Brevik <christian.brevik@gmail.com>
2020-09-09 14:12:09 +0200

    feat: Add pin SVGs

---
+++ src/assets/svg/map/SelectionPinConfirm.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Svg, {Path, SvgProps} from 'react-native-svg';
+
+function SvgSelectionPinConfirm(props: SvgProps) {
+  return (
+    <Svg width={28} height={37} fill="none" viewBox="0 0 28 37" {...props}>
+      <Path
+        fill="#fff"
+        d="M10.764 33.835a46.471 46.471 0 01-4.313-4.584C3.364 25.472 0 20.018 0 14 0 6.268 6.268 0 14 0s14 6.268 14 14c0 6.018-3.364 11.472-6.451 15.25a46.471 46.471 0 01-4.313 4.585C16.062 34.925 14 36.538 14 36.538s-2.062-1.613-3.236-2.703z"
+      />
+      <Path
+        fill="#37424A"
+        fillRule="evenodd"
+        d="M14 34s12-9.373 12-20c0-6.627-5.373-12-12-12S2 7.373 2 14c0 10.627 12 20 12 20z"
+        clipRule="evenodd"
+      />
+      <Path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M21.207 9.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414l3.293 3.293 7.293-7.293a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </Svg>
+  );
+}
+
+export default SvgSelectionPinConfirm;
