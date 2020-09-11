00ff0eac88660d7abc1625223b586f2633ac90f9
Christian Brevik <christian.brevik@gmail.com>
2020-09-11 13:22:11 +0200

    feat: Add new SVGs + animation shadow (#438)

--- src/assets/svg/map/index.tsx
+++ src/assets/svg/map/index.tsx
@@ -1,4 +1,4 @@
 export {default as SelectionPinConfirm} from './SelectionPinConfirm';
 export {default as SelectionPinMove} from './SelectionPinMove';
-export {default as SelectionPinMoveCircle} from './SelectionPinMoveCircle';
+export {default as SelectionPinShadow} from './SelectionPinShadow';
 export {default as SelectionPinUnknown} from './SelectionPinUnknown';
