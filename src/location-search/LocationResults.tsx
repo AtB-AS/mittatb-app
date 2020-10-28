3268ec6cc8db57a59c7fd932243420e11b8240e8
rikkekantega <rikke.rye@kantega.no>
2020-10-28 14:21:21 +0100

    fix: Correct naming on text component

--- src/location-search/LocationResults.tsx
+++ src/location-search/LocationResults.tsx
@@ -6,7 +6,7 @@ import insets from '../utils/insets';
 import {ArrowUpLeft} from '../assets/svg/icons/navigation';
 import {LocationSearchResult} from './types';
 import {FavoriteIcon} from '../favorites';
-import ThemedText from '../components/text';
+import Text from '../components/text';
 import ThemeIcon from '../components/theme-icon';
 
 type Props = {
@@ -27,7 +27,7 @@ const LocationResults: React.FC<Props> = ({
     <>
       {title && (
         <View accessibilityRole="header" style={styles.subHeader}>
-          <ThemedText style={styles.subLabel}>{title}</ThemedText>
+          <Text style={styles.subLabel}>{title}</Text>
           <View style={styles.subBar} />
         </View>
       )}
@@ -54,12 +54,8 @@ const LocationResults: React.FC<Props> = ({
                   )}
                 </View>
                 <View style={styles.locationTextContainer}>
-                  <ThemedText style={styles.locationName}>
-                    {searchResult.text}
-                  </ThemedText>
-                  <ThemedText style={styles.locality}>
-                    {searchResult.subtext}
-                  </ThemedText>
+                  <Text style={styles.locationName}>{searchResult.text}</Text>
+                  <Text style={styles.locality}>{searchResult.subtext}</Text>
                 </View>
               </TouchableOpacity>
             </View>
