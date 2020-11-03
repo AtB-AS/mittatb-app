2acab2e9cf27b59a8a63328845256b15e83134b7
Christian Brevik <christian.brevik@gmail.com>
2020-11-03 22:04:18 +0100

    fix: Create bundle folder if it doesn't exist

--- scripts/ios/create-sourcemaps.sh
+++ scripts/ios/create-sourcemaps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p bundle
+
 # iOS doesn't output source maps when bundling normally 
 npx react-native bundle --platform ios --dev false --reset-cache --entry-file index.js --bundle-output bundle/main.jsbundle --sourcemap-output bundle/main.jsbundle.map
