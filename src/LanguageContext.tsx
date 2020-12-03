e769a258dfc0f2d4ad0a9d884161a843d1983ddc
rikkekantega <rikke.rye@kantega.no>
2020-12-03 15:50:50 +0100

    fix: language hook call

--- src/LanguageContext.tsx
+++ src/LanguageContext.tsx
@@ -5,12 +5,11 @@ import {Language, lobot, DEFAULT_LANGUAGE} from './utils/language';
 import {useRemoteConfig} from './RemoteConfigContext';
 
 const AppLanguageProvider: React.FC = ({children}) => {
+  const currentLanguage = useLanguage();
   const {enable_i18n} = useRemoteConfig();
   if (!enable_i18n) {
     return <>{children}</>;
   }
-
-  const currentLanguage = useLanguage();
   return (
     <lobot.LanguageProvider value={currentLanguage}>
       {children}
