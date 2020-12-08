99bde7fc42abe7c6a10f4e819b3f93ca609f78f2
Gøran Dalum <goran.dalum@kantega.no>
2020-12-08 15:24:37 +0100

    Check enable_ticket flag

--- src/screens/Ticketing/index.tsx
+++ src/screens/Ticketing/index.tsx
@@ -6,5 +6,5 @@ import {useRemoteConfig} from '../../RemoteConfigContext';
 export default function Ticketing() {
   const {enable_ticketing} = useRemoteConfig();
 
-  return <Tickets />;
+  return !enable_ticketing ? <Splash /> : <Tickets />;
 }
