0f33702f585003a34019c2db3dcd68d9057d934a
rikkekantega <rikke.rye@kantega.no>
2020-12-11 10:49:58 +0100

    feat: Expand themed icon with text color type

--- src/components/theme-icon/index.tsx
+++ src/components/theme-icon/index.tsx
@@ -1,14 +1,16 @@
 import {SvgProps} from 'react-native-svg';
 import {useTheme} from '../../theme';
+import {TextColors} from '../../theme/colors';
 
 type ThemeIconProps = {
   svg(props: SvgProps): JSX.Element;
+  fill?: TextColors;
 } & SvgProps;
 
-const ThemeIcon = ({svg, ...props}: ThemeIconProps): JSX.Element => {
+const ThemeIcon = ({svg, fill, ...props}: ThemeIconProps): JSX.Element => {
   const {theme} = useTheme();
   const settings = {
-    fill: theme.text.colors.primary,
+    fill: theme.text.colors[fill ?? 'primary'],
     ...props,
   };
   return svg(settings);
