393b1b88793684bb229af6494e6a110d6e195916
Gøran Dalum <goran.dalum@kantega.no>
2020-12-18 13:09:47 +0100

    chore: Sort tariff zones for easier selection (#791)
    This will also make A the default tariff zone as it is the first tariff
    zone in the array.

--- src/api/tariffZones.ts
+++ src/api/tariffZones.ts
@@ -4,7 +4,9 @@ import client from './client';
 export async function list() {
   const url = 'reference-data/v1/ATB/tariff-zones';
   const response = await client.get<TariffZone[]>(url);
-  return response.data;
+  return response.data.sort((t1, t2) =>
+    t1.name.value.localeCompare(t2.name.value),
+  );
 }
 
 export type TariffZone = {
