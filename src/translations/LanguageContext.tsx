d8b1e38edd84013e47ff70af1deddbcf5e073e97
rikkekantega <61825573+rikkekantega@users.noreply.github.com>
2021-01-15 12:40:39 +0100

    fix: Broken logic on choosing language initial app install (#844)
    * fix: Broken logic on choosing language initial app install
    
    * fix: Even more simplified useLanguage logic

--- src/translations/LanguageContext.tsx
+++ src/translations/LanguageContext.tsx
@@ -24,33 +24,29 @@ function useLanguage() {
     preferences: {useSystemLanguage, language: userPreferencedLanguage},
   } = usePreferences();
 
-  // Usage of system setting not explicitly chosen, and user preferenced language has value
-  const shouldUseUserPreferenced =
-    !useSystemLanguage && !!userPreferencedLanguage;
+  useEffect(() => {
+    const onChange = () => {
+      setLocale(preferredLocale);
+    };
+    RNLocalize.addEventListener('change', onChange);
+    return () => {
+      RNLocalize.removeEventListener('change', onChange);
+    };
+  }, []);
 
   useEffect(() => {
-    if (shouldUseUserPreferenced) {
-      const newLanguage = getAsAppLanguage(userPreferencedLanguage);
-      setCurrentLanguage(newLanguage);
-    } else {
-      const onChange = () => {
-        setLocale(preferredLocale);
-      };
-      RNLocalize.addEventListener('change', onChange);
-      return () => {
-        RNLocalize.removeEventListener('change', onChange);
-      };
-    }
-  }, [useSystemLanguage, userPreferencedLanguage]);
-
-  useEffect(() => {
-    if (!shouldUseUserPreferenced) {
+    if (useSystemLanguage) {
       const language = locale
         ? getAsAppLanguage(locale.languageCode)
         : DEFAULT_LANGUAGE;
       setCurrentLanguage(language);
+    } else {
+      const newLanguage = getAsAppLanguage(
+        userPreferencedLanguage ?? DEFAULT_LANGUAGE,
+      );
+      setCurrentLanguage(newLanguage);
     }
-  }, [locale, useSystemLanguage]);
+  }, [userPreferencedLanguage, useSystemLanguage]);
 
   return currentLanguage;
 }
