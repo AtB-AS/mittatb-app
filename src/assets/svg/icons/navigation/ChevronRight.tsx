9768c693603bc84d820f958b2dc44c1672563c71
Mikael Brevik <mikaelbre@gmail.com>
2021-02-17 09:40:04 +0100

    refactor: fixes svg generation (#931)

--- src/assets/svg/icons/navigation/ChevronRight.tsx
+++ src/assets/svg/icons/navigation/ChevronRight.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Svg, {Path, SvgProps} from 'react-native-svg';
+import Svg, {SvgProps, Path} from 'react-native-svg';
 
 function SvgChevronRight(props: SvgProps) {
   return (
