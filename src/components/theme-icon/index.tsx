26041d6f38e9759d8da2bce4c4eeee12420e43f0
Mikael Brevik <mikaelbre@gmail.com>
2021-02-17 12:04:51 +0100

    Restructures navigation icon

--- src/components/theme-icon/index.tsx
+++ src/components/theme-icon/index.tsx
@@ -1,25 +1,4 @@
-import {useTheme} from '@atb/theme';
-import {iconSizes, TextColor} from '@atb/theme/colors';
-import {SvgProps} from 'react-native-svg';
+export {default as default} from './theme-icon';
 
-type ThemeIconProps = {
-  svg(props: SvgProps): JSX.Element;
-  colorType?: TextColor;
-  size?: keyof typeof iconSizes;
-} & SvgProps;
-
-const ThemeIcon = ({
-  svg,
-  colorType,
-  size = 'normal',
-  ...props
-}: ThemeIconProps): JSX.Element => {
-  const {theme} = useTheme();
-  const settings = {
-    fill: theme.text.colors[colorType ?? 'primary'],
-    height: theme.icon.size[size],
-    ...props,
-  };
-  return svg(settings);
-};
-export default ThemeIcon;
+export * from './navigation-icon';
+export {default as NavigationIcon} from './navigation-icon';
--- src/components/theme-icon/navigation-icon.tsx
+++ src/components/theme-icon/navigation-icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ThemeIcon from '.';
+import ThemeIcon from './theme-icon';
 import {
   ArrowLeft,
   ArrowRight,
