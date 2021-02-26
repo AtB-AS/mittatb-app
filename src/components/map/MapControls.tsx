32e29a71def4141febef60c43e079cce4f5e1430
Mikael Brevik <mikaelbre@gmail.com>
2021-02-26 16:22:25 +0100

    fix: reduces contrast for primary border color for dark mode

--- src/components/map/MapControls.tsx
+++ src/components/map/MapControls.tsx
@@ -14,48 +14,45 @@ const MapControls: React.FC<Props> = ({zoomIn, zoomOut}) => {
   const styles = useStyles();
   const {t} = useTranslation();
   return (
-    <View>
-      <View style={styles.zoomContainer}>
-        <TouchableOpacity
-          onPress={zoomIn}
-          accessibilityLabel={t(MapTexts.controls.zoomIn.a11yLabel)}
-          accessibilityRole="button"
-        >
-          <View style={styles.zoomInButton}>
-            <ThemeIcon svg={Add} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={zoomOut}
-          accessibilityLabel={t(MapTexts.controls.zoomOut.a11yLabel)}
-          accessibilityRole="button"
-        >
-          <View style={styles.zoomOutButton}>
-            <ThemeIcon svg={Remove} />
-          </View>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.zoomContainer}>
+      <TouchableOpacity
+        onPress={zoomIn}
+        accessibilityLabel={t(MapTexts.controls.zoomIn.a11yLabel)}
+        accessibilityRole="button"
+      >
+        <View style={styles.zoomInButton}>
+          <ThemeIcon svg={Add} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={zoomOut}
+        accessibilityLabel={t(MapTexts.controls.zoomOut.a11yLabel)}
+        accessibilityRole="button"
+      >
+        <View style={styles.zoomOutButton}>
+          <ThemeIcon svg={Remove} />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
   zoomContainer: {
-    backgroundColor: theme.background.level0,
+    backgroundColor: theme.background.level1,
     borderRadius: theme.border.radius.small,
+    alignContent: 'stretch',
     ...shadows,
   },
   zoomInButton: {
-    width: 36,
     height: 36,
     justifyContent: 'center',
     alignItems: 'center',
   },
   zoomOutButton: {
-    width: 36,
     height: 36,
     borderTopColor: theme.border.primary,
-    borderTopWidth: 0.5,
+    borderTopWidth: theme.border.width.slim,
     justifyContent: 'center',
     alignItems: 'center',
   },
