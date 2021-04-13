c999302359506787d5abcc87c45db5ce37841d2b
Espen MH <espen@monopoint.net>
2021-04-13 13:57:21 +0200

    update filllrate icon colors by rate

--- src/assets/svg/icons/transportation/Fillrate.tsx
+++ src/assets/svg/icons/transportation/Fillrate.tsx
@@ -1,18 +1,29 @@
-import Svg, {Path, SvgProps} from 'react-native-svg';
+import Svg, {Color, Path, SvgProps} from 'react-native-svg';
 import React from 'react';
+import {Theme, useTheme} from '@atb/theme';
 
-function SvgFillrate(props: SvgProps) {
+type SvgFillRateProps = {
+  fillColor: string;
+  size?: keyof Theme['icon']['size'];
+} & SvgProps;
+
+function SvgFillrate({fillColor, size = 'normal', ...props}: SvgFillRateProps) {
+  const {theme} = useTheme();
+  const colorString: string = fillColor;
+  const height = theme.icon.size[size];
+  const width = theme.icon.size[size];
+
   return (
-    <Svg width="8" height="10" viewBox="0 0 8 10" fill="#888" {...props}>
+    <Svg width={width} height={height} viewBox="0 -1 5 12" {...props}>
       <Path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M4 0.5C2.89543 0.5 2 1.39543 2 2.5C2 3.60457 2.89543 4.5 4 4.5C5.10457 4.5 6 3.60457 6 2.5C6 1.39543 5.10457 0.5 4 0.5Z"
-        fill="#007C92"
+        fill={colorString}
       />
       <Path
         d="M8 9.5V9C8 6.79086 6.20914 5 4 5C1.79086 5 0 6.79086 0 9V9.5H8Z"
-        fill="#007C92"
+        fill={colorString}
       />
     </Svg>
   );
--- src/screens/TripDetails/DepartureDetails/FillrateIndicator.tsx
+++ src/screens/TripDetails/DepartureDetails/FillrateIndicator.tsx
@@ -3,7 +3,7 @@ import {View, ViewProps} from 'react-native';
 import ThemeIcon from '@atb/components/theme-icon';
 import TripRow from '@atb/screens/TripDetails/components/TripRow';
 import {Fillrate, WalkingPerson} from '@atb/assets/svg/icons/transportation';
-import {StyleSheet} from '@atb/theme';
+import {StyleSheet, useTheme} from '@atb/theme';
 
 type FillrateProps = {
   rate: 'max' | 'med' | 'min' | 'unknown';
@@ -14,11 +14,34 @@ const FillrateIndicator: React.FC<FillrateProps> = ({
   ...props
 }) => {
   const styles = useStyle();
+  const {theme} = useTheme();
+
+  const cMax = theme.status.error.main.backgroundColor.toString();
+  const cMed = theme.status.info.main.backgroundColor.toString();
+  const cMin = theme.status.valid.main.backgroundColor.toString();
+  const cOff = '#bbb';
+
+  let iconColors: string[] = [cMin, cMed, cMax];
+  if (rate === 'max') {
+    iconColors = [cMax, cMax, cMax];
+  }
+  if (rate === 'med') {
+    iconColors = [cOff, cMed, cMed];
+  }
+  if (rate === 'min') {
+    iconColors = [cOff, cOff, cMin];
+  }
+
   return (
-    <View style={{flexDirection: 'row'}}>
-      <ThemeIcon style={styles.icon} svg={Fillrate} />
-      <ThemeIcon svg={Fillrate} />
-      <ThemeIcon svg={Fillrate} />
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+      }}
+    >
+      <Fillrate fillColor={iconColors[0]} style={styles.icon} />
+      <Fillrate fillColor={iconColors[1]} style={styles.icon} />
+      <Fillrate fillColor={iconColors[2]} style={styles.icon} />
     </View>
   );
 };
