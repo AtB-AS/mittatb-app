acf9168c18a704c830d0d6fedb49743dafd2bed1
Gøran Dalum <goran.dalum@kantega.no>
2021-04-23 10:26:43 +0200

    fix: Spacing between label and text in text-input (#1078)

--- src/components/sections/text-input.tsx
+++ src/components/sections/text-input.tsx
@@ -148,6 +148,7 @@ const useInputStyle = StyleSheet.createTheme((theme) => ({
   },
   label: {
     minWidth: 60 - theme.spacings.medium,
+    paddingRight: theme.spacings.xSmall,
   },
   inputClear: {
     position: 'absolute',
