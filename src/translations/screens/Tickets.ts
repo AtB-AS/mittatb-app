2f315dea0547f0d68dd7c81a80482b1fbac31e8b
Espen MH <espen@monopoint.net>
2021-05-26 11:14:57 +0200

    Remove redundant SR info

--- src/translations/screens/Tickets.ts
+++ src/translations/screens/Tickets.ts
@@ -15,15 +15,15 @@ const TicketsTexts = {
       single: {
         text: _('Ny enkeltbillett', 'New single ticket'),
         a11yHint: _(
-          'Aktivér for å kjøpe ny enkeltbillett',
-          'Activate to buy a new single ticket',
+          'Kjøp ny enkeltbillett',
+          'Buy a new single ticket',
         ),
       },
       period: {
         text: _('Ny periodebillett', 'New period ticket'),
         a11yHint: _(
-          'Aktivér for å kjøpe ny periodebillett',
-          'Activate to buy a new period ticket',
+          'Kjøp ny periodebillett',
+          'Buy a new period ticket',
         ),
       },
     },
