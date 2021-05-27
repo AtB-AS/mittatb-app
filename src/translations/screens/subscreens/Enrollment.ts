b4cede5f671f44368c748e4eb00d4724db851ead
Espen MH <espen@monopoint.net>
2021-05-27 12:29:23 +0200

    chore: removed beta for enrollment

--- src/translations/screens/subscreens/Enrollment.ts
+++ src/translations/screens/subscreens/Enrollment.ts
@@ -1,17 +1,17 @@
 import {translation as _} from '../../commons';
 const EnrollmentTexts = {
-  header: _('Beta-funksjonalitet', 'Beta features'),
+  header: _('Ny funksjonalitet', 'New features'),
   info: _(
-    'I forbindelse med utvikling av denne appen har vi enkelte lukket beta-grupper for å teste ut funksjonalitet. Hvis du har fått en kode for å melde deg inn i en slik gruppe, kan du gjøre det her.',
-    'While developing this app we are running closed beta-groups to test out certain new functionality. If you have been given a code to join such a group, you may do so here.',
+    'I forbindelse med utvikling av denne appen har vi enkelte lukkede grupper for å prøve ut funksjonalitet. Hvis du har fått en kode for å melde deg inn i en slik gruppe, kan du gjøre det her.',
+    'While developing this app we are running closed groups to try out some new features. If you have been given a code to join such a group, you may do so here.',
   ),
   betaButtonLabel: _(
-    'Jeg har kode til beta-funkjonalitet',
-    'I have a code for beta features',
+    'Jeg har kode til ny funkjonalitet',
+    'I have a code for new features',
   ),
   success: _(
-    'Du ble innrullert i beta-gruppen!',
-    'You were enrolled into the beta group!',
+    'Du ble innrullert i den lukkede gruppen!',
+    'You were enrolled in the closed  group!',
   ),
   label: _('Kode', 'Code'),
   button: _('Send inn kode', 'Send code'),
