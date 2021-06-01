9a41b0f9b60c47b5e3b242e2dda135f0ca4f39ba
Espen MH <espen@monopoint.net>
2021-06-01 13:40:05 +0200

    remove illustrations

--- src/assets/svg/illustrations/index.tsx
+++ src/assets/svg/illustrations/index.tsx
@@ -1,7 +1,5 @@
 export {default as CrashParachute} from './CrashParachute';
-export {default as Onboarding1} from './Onboarding1';
 export {default as Onboarding1_2} from './Onboarding1_2';
 export {default as Onboarding2} from './Onboarding2';
 export {default as Onboarding3} from './Onboarding3';
 export {default as ShinyTicketBanner} from './ShinyTicketBanner';
-export {default as TestPilotFigure} from './TestPilotFigure';
