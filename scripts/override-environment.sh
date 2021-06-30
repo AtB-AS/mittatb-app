b74528d1e30f337316b9e498376bcc8a43febc77
Morten Lundenes <morten.lundenes@bouvet.no>
2021-06-30 12:16:14 +0200

    changed workflows for updated env override script

--- scripts/override-environment.sh
+++ scripts/override-environment.sh
@@ -3,19 +3,22 @@ then
     echo "Argument error!"
     echo "First argument should be the environment name."
     echo "Available environment names:
+ - dev
+ - prodstaging
+ - staging
  - store"
-    echo "Second argument should be the app variant name."
+    echo "Second argument should be the app organisation name."
     echo "Available app variant names:
--AtB
--NFK"
+-atb
+-nfk"
 
     echo "Example:
 ./override-environment.sh store AtB"
     exit 1
 else
     APP_ENVIRONMENT=$1
-    APP_VARIANT=$2
-    ENV_FOLDER=env/$APP_VARIANT/$APP_ENVIRONMENT
+    APP_ORG=$2
+    ENV_FOLDER=env/$APP_ORG/$APP_ENVIRONMENT
     echo "Copying $APP_ENVIRONMENT .env file to root"
     cp $ENV_FOLDER/.env .
 
