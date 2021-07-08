edcfa28c60e3b081e4405ba4711bb0161d243725
Mikael Brevik <mikaelbre@gmail.com>
2021-07-08 09:44:14 +0200

    refactor: Setter funksjoner som memoed og fikser potensiell anonym innlogging bug (#1326)
    * fix: issue with logging in anonymously after logging out
    
    * refactor: removes unused auth util
    
    * refactor: changes signin phone to use useCallback

--- src/auth/utils.ts
+++
@@ -1,6 +0,0 @@
-import {FirebaseAuthTypes} from '@react-native-firebase/auth';
-
-export async function getAbtCustomerId(user: FirebaseAuthTypes.User) {
-  const idTokenResult = await user.getIdTokenResult();
-  return idTokenResult.claims['abt_id'];
-}
