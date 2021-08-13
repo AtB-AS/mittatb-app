5dd9266e3632b59bbe0a7dbb151d773f8287fc57
Espen MH <espen@monopoint.net>
2021-08-13 15:39:12 +0200

    fix: delayed render workaround for anim bug

--- src/screens/Profile/Appearance/index.tsx
+++ src/screens/Profile/Appearance/index.tsx
@@ -1,7 +1,7 @@
 import {ActionItem, Section} from '@atb/components/sections';
 import {StyleSheet, Theme, useTheme} from '@atb/theme';
 import {AppearanceSettingsTexts, useTranslation} from '@atb/translations';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
 import FullScreenHeader from '@atb/components/screen-header/full-header';
 import {View} from 'react-native';
@@ -16,6 +16,18 @@ export default function Appearance() {
   const style = useProfileHomeStyle();
   const {t} = useTranslation();
 
+  function useDelay(ms: number) {
+    const [gate, setGate] = useState(false);
+    useEffect(() => {
+      setTimeout(() => {
+        setGate(true);
+      }, ms);
+    });
+    return gate;
+  }
+
+  const delayRender = useDelay(100);
+
   return (
     <View style={style.container}>
       <FullScreenHeader
@@ -24,25 +36,27 @@ export default function Appearance() {
       />
 
       <ScrollView>
-        <Section withTopPadding withPadding>
-          <ActionItem
-            mode="toggle"
-            text={t(AppearanceSettingsTexts.actions.usePhoneSettings)}
-            checked={!overrideColorScheme}
-            onPress={(checked) => overrideOSThemePreference(!checked)}
-          />
-
-          {overrideColorScheme && (
+        {delayRender && (
+          <Section withTopPadding withPadding>
             <ActionItem
               mode="toggle"
-              text={t(AppearanceSettingsTexts.actions.darkMode)}
-              checked={storedColorScheme === 'dark'}
-              onPress={(checked) =>
-                updateThemePreference(checked ? 'dark' : 'light')
-              }
+              text={t(AppearanceSettingsTexts.actions.usePhoneSettings)}
+              checked={!overrideColorScheme}
+              onPress={(checked) => overrideOSThemePreference(!checked)}
             />
-          )}
-        </Section>
+
+            {overrideColorScheme && (
+              <ActionItem
+                mode="toggle"
+                text={t(AppearanceSettingsTexts.actions.darkMode)}
+                checked={storedColorScheme === 'dark'}
+                onPress={(checked) =>
+                  updateThemePreference(checked ? 'dark' : 'light')
+                }
+              />
+            )}
+          </Section>
+        )}
       </ScrollView>
     </View>
   );
