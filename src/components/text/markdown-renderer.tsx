67167d78747b079401299448a3fc987361ace080
Christian Brevik <christian.brevik@gmail.com>
2021-08-15 20:36:26 +0200

    fix: No longer need to nest in render-functions

--- src/components/text/markdown-renderer.tsx
+++ src/components/text/markdown-renderer.tsx
@@ -2,41 +2,44 @@ import React from 'react';
 import {Text} from 'react-native';
 import {lexer, Token} from 'marked';
 
-type MarkdownRenderer = () => React.ReactElement;
-
 export default function render(markdown: string): React.ReactElement[] {
   const tree = lexer(markdown);
-  return tree.map(getRenderer).map((re) => re());
+  return tree.map(renderToken);
 }
 
-function getRenderer(token: Token, index: number): MarkdownRenderer {
+function renderToken(token: Token, index: number): React.ReactElement {
   switch (token.type) {
     case 'text':
-      return () => <Text key={index}>{token.text}</Text>;
+      return <Text key={index}>{token.text}</Text>;
+
     case 'strong':
-      return () => (
+      return (
         <Text key={index} style={{fontWeight: 'bold'}}>
           {token.text}
         </Text>
       );
+
     case 'paragraph':
-      return () => (
+      return (
         <React.Fragment key={index}>
-          {token.tokens.map(getRenderer).map((re) => re())}
+          {token.tokens.map(renderToken)}
         </React.Fragment>
       );
+
     case 'space':
-      return () => <Text key={index}>{'\n'}</Text>;
+      return <Text key={index}>{'\n'}</Text>;
+
     case 'em':
-      return () => (
+      return (
         <Text key={index} style={{fontStyle: 'italic'}}>
           {token.text}
         </Text>
       );
+
     default:
       console.warn(
         `We haven't defined a renderer for markdown type: "${token.type}", rendering raw: "${token.raw}"`,
       );
-      return () => <Text key={index}>{token.raw}</Text>;
+      return <Text key={index}>{token.raw}</Text>;
   }
 }
