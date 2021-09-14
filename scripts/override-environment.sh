5599185ad2a68b7c59ed2633ac8827c97171f4c0
Christian Brevik <christian.brevik@gmail.com>
2021-09-14 14:23:49 +0200

    feat: First time setup commands (#1579)

--- scripts/override-environment.sh
+++ scripts/override-environment.sh
@@ -13,7 +13,7 @@ then
 -nfk"
 
     echo "Example:
-./override-environment.sh store AtB"
+./override-environment.sh store atb"
     exit 1
 else
     APP_ENVIRONMENT=$1
@@ -39,10 +39,4 @@ else
 
     echo "Copying boot splash image to assets/"
     cp $ORG_FOLDER/bootsplash_logo_original.png assets/
-
-    echo "Generating app icons and bootsplash screens"
-    export $(grep -v '^#' .env | gxargs -d '\n') > /dev/null 2>&1
-    yarn icons && yarn react-native generate-bootsplash assets/bootsplash_logo_original.png \
-        --assets-path=assets/ --background-color=$BOOTSPLASH_BACKGROUND_COLOR
-
 fi
