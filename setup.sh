935f3f79cae845f17b93f1ac34d0a5cfa470f315
Christian Brevik <christian.brevik@gmail.com>
2021-09-14 13:56:38 +0200

    feat: First time setup commands

---
+++ setup.sh
@@ -0,0 +1,21 @@
+if [ "$#" -ne 2 ]
+then
+    echo "Argument error!"
+    echo "First argument should be the environment name."
+    echo "Available environment names:
+ - dev
+ - prodstaging
+ - staging
+ - store"
+    echo "Second argument should be the app organisation name."
+    echo "Available app variant names:
+-atb
+-nfk"
+
+    echo "Example:
+./setup.sh dev atb"
+    exit 1
+else
+    sh ./scripts/override-environment.sh $1 $2
+    sh ./scripts/generate-native-assets.sh
+fi
