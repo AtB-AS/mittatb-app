d2d0e8a7d9c7bc3bbc81f2171fd56cd4996c6cf4
Espen MH <espen@monopoint.net>
2021-09-23 11:00:28 +0200

    white space

--- scripts/generate-native-assets.sh
+++ scripts/generate-native-assets.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 # Read a property safely from config file, instead of sourcing the file
-
 prop() {
   grep -e "^${1}=" ./.env | cut -d'=' -f2 | head -n 1;
 }
-
 
 echo "Generating app icons"
 yarn icons
