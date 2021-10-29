dd0dd431d11abfd3e4ff6aad1ec017352fbe3e30
Johannes Røsvik <j.rosvik@gmail.com>
2021-10-29 13:12:49 +0200

    Add PhoneInfoBox

--- src/login/AssignTravelToken.tsx
+++ src/login/AssignTravelToken.tsx
@@ -30,6 +30,19 @@ export default function AssignTravelToken({
   const {customerProfile} = useTicketState();
   const hasTravelCard = !!customerProfile?.travelcard;
 
+  const availableDevices = [
+    {
+      id: 0,
+      modelName: 'iPhone 13 Pro',
+      addedDate: '10. okt, kl. 09:10',
+    },
+    {
+      id: 1,
+      modelName: 'Samsung Galaxy S21',
+      addedDate: '10. okt, kl. 09:10',
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <FullScreenHeader
@@ -43,17 +56,19 @@ export default function AssignTravelToken({
         <View accessible={true} accessibilityRole="header" ref={focusRef}>
           <ThemeText
             type={'body__primary--jumbo--bold'}
-            style={styles.title}
+            style={[styles.alignCenter, styles.marginVertical]}
             color={themeColor}
           >
-            {hasTravelCard ? 'Ditt t:kort er satt som reisebevis' : 'No t:card'}
+            {hasTravelCard
+              ? 'Ditt t:kort er satt som reisebevis'
+              : 'Din mobil er satt som reisebevis'}
           </ThemeText>
         </View>
         <View accessible={true}>
           <ThemeText style={styles.description} color={themeColor}>
             {hasTravelCard
               ? 'Ta med deg t:kortet når du er ute på reise'
-              : 'No t:card'}
+              : 'Ta med deg mobilen når du er ute på reise'}
           </ThemeText>
         </View>
         {hasTravelCard && customerProfile?.travelcard?.id && (
@@ -65,7 +80,26 @@ export default function AssignTravelToken({
             ></ActiveTicketCard>
           </View>
         )}
-        <Button color={'primary_2'} onPress={() => {}} text={'OK'} />
+        {!hasTravelCard && (
+          <PhoneInfoBox
+            phoneName={availableDevices[0].modelName}
+            addedDate={availableDevices[0].addedDate}
+          ></PhoneInfoBox>
+        )}
+        <Button
+          color={'primary_2'}
+          style={styles.marginVertical}
+          onPress={() => {}}
+          text={'OK'}
+        />
+        {!hasTravelCard && availableDevices.length > 1 && (
+          <Button
+            mode="secondary"
+            style={styles.marginVertical}
+            onPress={() => {}}
+            text={'Velg en annen mobil'}
+          />
+        )}
         <ThemeText
           style={styles.description}
           color="primary_2"
@@ -79,6 +113,28 @@ export default function AssignTravelToken({
   );
 }
 
+type PhoneInfoBoxProps = {
+  phoneName: string;
+  addedDate: string;
+};
+
+export function PhoneInfoBox(props: PhoneInfoBoxProps): JSX.Element {
+  const styles = useThemeStyles();
+
+  return (
+    <View style={styles.phoneInfoBox}>
+      <View style={styles.phoneLine}></View>
+      <View style={styles.phoneInfoBoxInner}>
+        <ThemeText type="heading__title" style={styles.phoneInfoBoxTitle}>
+          {props.phoneName}
+        </ThemeText>
+        <ThemeText style={styles.alignCenter}>Lagt til:</ThemeText>
+        <ThemeText style={styles.alignCenter}>{props.addedDate}</ThemeText>
+      </View>
+    </View>
+  );
+}
+
 const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
   container: {
     backgroundColor: theme.colors[themeColor].backgroundColor,
@@ -87,9 +143,11 @@ const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
   mainView: {
     padding: theme.spacings.large,
   },
-  title: {
+  alignCenter: {
     textAlign: 'center',
-    marginVertical: theme.spacings.medium,
+  },
+  marginVertical: {
+    marginBottom: theme.spacings.medium,
   },
   description: {
     marginVertical: theme.spacings.large,
@@ -99,5 +157,33 @@ const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
     marginTop: theme.spacings.xLarge,
     marginBottom: theme.spacings.xLarge * 3,
     alignSelf: 'center',
+  },
+  phoneInfoBox: {
+    marginVertical: theme.spacings.xLarge,
+    alignSelf: 'center',
+    backgroundColor: theme.colors.background_3.backgroundColor,
+    padding: theme.spacings.xLarge,
+    borderRadius: theme.border.radius.circle,
+    minHeight: 300,
+    minWidth: 200,
+  },
+  phoneLine: {
+    width: theme.spacings.xLarge * 2,
+    borderRadius: theme.border.radius.regular,
+    height: theme.spacings.small,
+    backgroundColor: theme.colors.secondary_2.backgroundColor,
+    alignSelf: 'center',
+    marginTop: theme.spacings.small,
+    marginBottom: theme.spacings.small + theme.spacings.xLarge,
+  },
+  phoneInfoBoxInner: {
+    backgroundColor: theme.colors.background_1.backgroundColor,
+    borderRadius: theme.border.radius.regular,
+    padding: theme.spacings.medium,
+    alignSelf: 'center',
+  },
+  phoneInfoBoxTitle: {
+    marginBottom: theme.spacings.medium,
+    textAlign: 'center',
   },
 }));
