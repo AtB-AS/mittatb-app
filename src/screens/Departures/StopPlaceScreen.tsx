02389868a87584efd1699f31a7b045201591cc76
Johannes Røsvik <j.rosvik@gmail.com>
2021-11-03 15:43:17 +0100

    Add quay chips

--- src/screens/Departures/StopPlaceScreen.tsx
+++ src/screens/Departures/StopPlaceScreen.tsx
@@ -1,7 +1,7 @@
 import FullScreenHeader from '@atb/components/screen-header/full-header';
 import * as Sections from '@atb/components/sections';
 import MessageBox from '@atb/components/message-box';
-import {StyleSheet, Theme} from '@atb/theme';
+import {StyleSheet, Theme, useTheme} from '@atb/theme';
 import React from 'react';
 import {View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
@@ -9,6 +9,7 @@ import {StopPlaceDetails} from '@atb/sdk';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {NearbyStackParams} from '../Nearby';
 import {RouteProp} from '@react-navigation/native';
+import Button from '@atb/components/button';
 
 export type StopPlaceScreenParams = {
   stopPlaceDetails: StopPlaceDetails;
@@ -31,20 +32,42 @@ export default function StopPlaceScreen({
   },
 }: LoginOnboardingProps) {
   const style = useProfileHomeStyle();
-
-  console.log(stopPlaceDetails);
-
-  // const stopPlace = ;
+  const {theme} = useTheme();
 
   return (
     <View style={style.container}>
-      <FullScreenHeader title="Stop place" leftButton={{type: 'back'}} />
+      <FullScreenHeader
+        title={stopPlaceDetails.name}
+        leftButton={{type: 'back'}}
+      />
 
-      <ScrollView>
+      <ScrollView
+        style={style.quayChipContainer}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Button
+          onPress={() => {}}
+          text="Alle stopp"
+          color="secondary_3"
+          style={[style.quayChip, {marginLeft: theme.spacings.medium}]}
+        ></Button>
+        {stopPlaceDetails.quays?.map((quay) => (
+          <Button
+            onPress={() => {}}
+            text={
+              quay.publicCode ? quay.name + ' ' + quay.publicCode : quay.name
+            }
+            color="secondary_4"
+            style={style.quayChip}
+          ></Button>
+        ))}
+      </ScrollView>
+      <ScrollView style={style.contentContainer}>
         <Sections.Section withPadding withTopPadding>
-          {/* <Sections.HeaderItem
+          <Sections.HeaderItem
             text={stopPlaceDetails && stopPlaceDetails.name}
-          /> */}
+          />
 
           <Sections.GenericItem>
             <MessageBox message="This is a message" />
@@ -58,6 +81,16 @@ export default function StopPlaceScreen({
 const useProfileHomeStyle = StyleSheet.createThemeHook((theme: Theme) => ({
   container: {
     backgroundColor: theme.colors.background_1.backgroundColor,
-    flex: 1,
+  },
+  quayChipContainer: {
+    backgroundColor: theme.colors.secondary_1.backgroundColor,
+    flexShrink: 1,
+    paddingVertical: theme.spacings.medium,
+  },
+  quayChip: {
+    marginRight: theme.spacings.medium,
+  },
+  contentContainer: {
+    flexGrow: 1,
   },
 }));
