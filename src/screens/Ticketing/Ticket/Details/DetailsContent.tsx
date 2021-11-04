8ec9b9611b5268bb1c00d3c4e671e7b4d3292389
Gøran Dalum <goran.dalum@mittatb.no>
2021-11-04 10:13:12 +0100

    feat: Create mobile token context (#1733)

--- src/screens/Ticketing/Ticket/Details/DetailsContent.tsx
+++ src/screens/Ticketing/Ticket/Details/DetailsContent.tsx
@@ -1,19 +1,16 @@
 import * as Sections from '@atb/components/sections';
 import ThemeText from '@atb/components/text';
-import {StyleSheet} from '@atb/theme';
-import {FareContract, isPreactivatedTicket, useTicketState} from '@atb/tickets';
+import {FareContract, isPreactivatedTicket} from '@atb/tickets';
 import {TicketTexts, useTranslation} from '@atb/translations';
 import {formatToLongDateTime} from '@atb/utils/date';
 import {fromUnixTime} from 'date-fns';
-import qrcode from 'qrcode';
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import {View} from 'react-native';
-import {SvgXml} from 'react-native-svg';
 import TicketInfo from '../TicketInfo';
 import ValidityHeader from '../ValidityHeader';
 import ValidityLine from '../ValidityLine';
 import {getValidityStatus} from '@atb/screens/Ticketing/Ticket/utils';
-import useInterval from '@atb/utils/use-interval';
+import QrCodeSvg from '@atb/screens/Ticketing/Ticket/Details/QrCodeSvg';
 
 type Props = {
   fareContract: FareContract;
@@ -29,9 +26,6 @@ const DetailsContent: React.FC<Props> = ({
   hasActiveTravelCard = false,
 }) => {
   const {t, language} = useTranslation();
-  const styles = useStyles();
-  const qrCode = useQrCode(fc);
-  const qrCodeSvg = useQrCodeSvg(qrCode);
 
   const firstTravelRight = fc.travelRights[0];
   if (isPreactivatedTicket(firstTravelRight)) {
@@ -91,17 +85,10 @@ const DetailsContent: React.FC<Props> = ({
           onPress={onReceiptNavigate}
           accessibility={{accessibilityRole: 'button'}}
         />
-        {validityStatus === 'valid' && isInspectable && qrCodeSvg && (
-          <Sections.GenericItem>
-            <View
-              style={styles.qrCode}
-              accessible={true}
-              accessibilityLabel={t(TicketTexts.details.qrCodeA11yLabel)}
-            >
-              <SvgXml xml={qrCodeSvg} width="100%" height="100%" />
-            </View>
-          </Sections.GenericItem>
-        )}
+        <QrCodeSvg
+          validityStatus={validityStatus}
+          isInspectable={isInspectable}
+        />
       </Sections.Section>
     );
   } else {
@@ -122,78 +109,5 @@ function UnknownTicketDetails({fc}: {fc: FareContract}) {
     </Sections.Section>
   );
 }
-
-const useQrCodeSvg = (qrCode: string | undefined) => {
-  const [qrCodeSvg, setQrCodeSvg] = useState<string | undefined>();
-
-  useEffect(() => {
-    (async function () {
-      if (!qrCode) return;
-      const svg = await qrcode.toString(qrCode, {
-        type: 'svg',
-      });
-
-      setQrCodeSvg(svg);
-    })();
-  }, [qrCode]);
-  return qrCodeSvg;
-};
-
-const useQrCode = (fc: FareContract) => {
-  const {tokenStatus, generateQrCode, retryTokenClient} = useTicketState();
-  const [tokenQRCode, setTokenQRCode] = useState<string | undefined>(undefined);
-  const [retryCount, setRetryCount] = useState(0);
-
-  const updateQrCode = useCallback(async () => {
-    if (tokenStatus?.state === 'Valid') {
-      const qr = await generateQrCode();
-      setTokenQRCode(qr);
-    } else {
-      setTokenQRCode(undefined);
-    }
-  }, [tokenStatus]);
-
-  useInterval(
-    () => {
-      setRetryCount(retryCount + 1);
-      retryTokenClient(false); // todo: better retry logic
-    },
-    5000,
-    [retryTokenClient, tokenStatus?.error],
-    !tokenStatus?.error || retryCount >= 5,
-  );
-
-  useInterval(
-    () => {
-      setRetryCount(retryCount + 1);
-      retryTokenClient(true); // todo: better retry logic
-    },
-    30000,
-    [retryTokenClient, tokenStatus?.error],
-    !tokenStatus?.error || retryCount < 5,
-  );
-
-  useEffect(() => {
-    updateQrCode();
-  }, []);
-
-  // TODO: Update so this happens in TicketContext later. Just proof-of-concept
-  useInterval(
-    () => {
-      updateQrCode();
-    },
-    10000,
-    [],
-  );
-
-  return tokenQRCode ?? fc.qrCode;
-};
-
-const useStyles = StyleSheet.createThemeHook(() => ({
-  qrCode: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-}));
 
 export default DetailsContent;
