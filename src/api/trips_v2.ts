fcc48e5530173d3cf7df1a79c53ca99f8ef22e3a
Espen MH <espen@monopoint.net>
2021-12-07 10:33:39 +0100

    feat: experimental trips search (#1833)
    * localhost accesible from android emu on dev
    
    * localhost override for api
    
    * wip: bff_v2 apis Assistant_v2
    
    * wip
    
    * POST trips search
    
    * fetch more!
    
    * details query
    
    * debug security
    
    * bump trips query
    
    * merge
    
    * load more design and texts
    
    * toggle
    
    * refactor
    
    * refactor types
    
    * fix: remove dev URLs
    
    * Update src/api/types/trips.ts
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>
    
    * Update src/screens/Assistant_v2/Assistant.tsx
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>
    
    * Update src/screens/Assistant_v2/Assistant.tsx
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>
    
    * Update src/screens/Assistant_v2/Assistant.tsx
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>
    
    * tiny PR-fixes
    
    * linting
    
    Co-authored-by: Christian Brevik <christian.brevik@gmail.com>

---
+++ src/api/trips_v2.ts
@@ -0,0 +1,38 @@
+import {TripPattern} from '@atb/sdk';
+import client from './client';
+import {Location} from '@atb/favorites/types';
+import {AxiosRequestConfig} from 'axios';
+import {TripsQuery} from '@atb/api/types/trips';
+import {TripsQueryVariables} from '@atb/api/types/generated/TripsQuery';
+
+export type TripsSearchQuery = {
+  from: Location;
+  to: Location;
+  searchDate?: string;
+  arriveBy?: boolean;
+};
+
+export async function tripsSearch(
+  query: TripsQueryVariables,
+  opts?: AxiosRequestConfig,
+): Promise<TripsQuery> {
+  const url = 'bff/v2/trips';
+  const cleanQuery: TripsQueryVariables = {
+    to: {
+      name: query.to.name,
+      coordinates: query.to.coordinates,
+      place: query.to.place,
+    },
+    from: {
+      name: query.from.name,
+      coordinates: query.from.coordinates,
+      place: query.from.place,
+    },
+    when: query.when,
+  };
+
+  const response = await client.post<TripsQuery>(url, cleanQuery, {
+    ...opts,
+  });
+  return response.data;
+}
---
+++ src/screens/Assistant_v2/types.tsx
@@ -0,0 +1,5 @@
+export type SearchStateType =
+  | 'idle'
+  | 'searching'
+  | 'search-success'
+  | 'search-empty-result';
