46307188320856033c777796ecc80fc5142eea96
Morten Lundenes <morten.lundenes@bouvet.no>
2021-12-14 12:54:01 +0100

    reuse env files with fastlane

--- scripts/override-environment.sh
+++ scripts/override-environment.sh
@@ -23,6 +23,9 @@ else
     echo "Copying $APP_ENVIRONMENT .env file to root"
     cp $ENV_FOLDER/.env .
 
+    echo "Copying $APP_ENVIRONMENT .env file to fastlane"
+    cp $ENV_FOLDER/.env fastlane/.env.default
+
     echo "Copying $APP_ENVIRONMENT icons to icons folder"
     cp -a $ENV_FOLDER/icons/. icons/
 
@@ -30,7 +33,7 @@ else
     cp $ENV_FOLDER/.env ios/Configs/Config.xcconfig
     #replace urls containing :// with :/$()/ to be compatiable with xcconfig format
     sed -i '' -e "s,://,:/\$()/,g" ios/Configs/Config.xcconfig
-	
+
     echo "Copying $APP_ENVIRONMENT google-services.json to android folder"
     cp $ENV_FOLDER/google-services.json android/app
 
