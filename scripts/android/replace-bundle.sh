d909a8f5af86fcead92bd50393136b242eece8db
Christian Brevik <christian.brevik@gmail.com>
2021-12-15 20:54:56 +0100

    chore: Fix replace-bundle for Android

--- scripts/android/replace-bundle.sh
+++ scripts/android/replace-bundle.sh
@@ -49,5 +49,5 @@ else
     jarsigner -keystore $KEYSTORE_PATH -storepass "$KEYSTORE_PASS" -keypass "$KEY_PASS" -verbose -sigalg MD5withRSA -digestalg SHA1 -signedjar temp-$APK_FILE_NAME temp-$APK_FILE_NAME $KEY_ALIAS
 
     echo "The APK must be aligned to 4 byte boundaries to work on Android"
-    ~/Library/Android/sdk/build-tools/29.0.2/zipalign -f 4 temp-$APK_FILE_NAME $APK_FILE_NAME
+    ~/Library/Android/sdk/build-tools/29.0.2/zipalign -p -f 4 temp-$APK_FILE_NAME $APK_FILE_NAME
 fi
