dffa72a54bfbef9150dc53cb2f7ff12329decd38
Christian Brevik <christian.brevik@gmail.com>
2022-01-03 20:54:09 +0100

    chore: Bump to version 1.14 (#1933)

--- scripts/ios/upload-sourcemaps.sh
+++ scripts/ios/upload-sourcemaps.sh
@@ -14,7 +14,7 @@ else
 echo "Uploading iOS source maps"
     curl --http1.1 https://upload.bugsnag.com/react-native-source-map \
         -F apiKey=$BUGSNAG_API_KEY \
-        -F appVersion=1.13 \
+        -F appVersion=1.14 \
         -F appBundleVersion=$BUILD_ID \
         -F dev=false \
         -F platform=ios \
