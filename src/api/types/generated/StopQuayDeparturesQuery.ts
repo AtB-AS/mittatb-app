c5245264e9a54b6c3313adf455ed4700f3b376e6
Johannes Røsvik <j.rosvik@gmail.com>
2022-01-04 10:14:01 +0100

    Remove unused data from departure queries

--- src/api/types/generated/StopQuayDeparturesQuery.ts
+++ src/api/types/generated/StopQuayDeparturesQuery.ts
@@ -9,24 +9,15 @@ export type StopPlaceQuayDeparturesQuery = {
           id: string;
           estimatedCalls: Array<
             Types.Maybe<{
-              aimedDepartureTime?: Types.Maybe<any>;
               expectedDepartureTime?: Types.Maybe<any>;
               realtime?: Types.Maybe<boolean>;
-              realtimeState?: Types.Maybe<Types.RealtimeState>;
-              quay?: Types.Maybe<{
-                id: string;
-                stopPlace?: Types.Maybe<{id: string}>;
-              }>;
+              quay?: Types.Maybe<{id: string}>;
               destinationDisplay?: Types.Maybe<{
                 frontText?: Types.Maybe<string>;
               }>;
               serviceJourney?: Types.Maybe<{
                 id: string;
-                privateCode?: Types.Maybe<string>;
-                transportMode?: Types.Maybe<Types.TransportMode>;
-                transportSubmode?: Types.Maybe<Types.TransportSubmode>;
                 line: {
-                  name?: Types.Maybe<string>;
                   id: string;
                   description?: Types.Maybe<string>;
                   publicCode?: Types.Maybe<string>;
