d48764ac4792521e6fd6011005b68155c5e5334b
Christian Brevik <christian.brevik@gmail.com>
2022-01-12 21:46:53 +0100

    basic onboarding e2e

---
+++ e2e/onboarding.e2e.js
@@ -0,0 +1,27 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp({
+      permissions: {
+        location: 'always',
+      },
+    });
+  });
+
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.text('Welcome to the AtB app'))).toBeVisible();
+  });
+
+  it('should show help us make app better after next', async () => {
+    await element(by.text('Next')).tap();
+    await expect(element(by.text('Help us make the app better'))).toBeVisible();
+  });
+
+  it('should assistant view after onboarding', async () => {
+    await element(by.text('Start using the app')).tap();
+    await expect(element(by.text('Travel assistant'))).toBeVisible();
+  });
+});
