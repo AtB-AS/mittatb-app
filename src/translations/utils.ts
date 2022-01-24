bf8ac4fea1f9d5e79ded2ff471d903309c623cbc
Morten Lundenes <morten.lundenes@bouvet.no>
2022-01-24 13:57:36 +0100

    removed test code

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -9,7 +9,6 @@ export default function orgSpecificTranslations<T>(
   translationTexts: T,
   overrides: Partial<{[org in AppOrgs]: Overrides<T>}>,
 ) {
-  const test = {...translationTexts, ...overrides.nfk};
   if (APP_ORG === AppOrgs.nfk) {
     return merge(translationTexts, overrides.nfk);
   } else {
