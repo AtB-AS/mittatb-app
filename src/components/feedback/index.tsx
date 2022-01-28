413f1c3b5879aeba2676275d3f4437c788a11bb5
Tore Stensaker Tefre <tore@tefre.it>
2022-01-28 12:36:51 +0100

    add questios UI and sample questions

--- src/components/feedback/index.tsx
+++ src/components/feedback/index.tsx
@@ -1,7 +1,9 @@
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import {StyleSheet, useTheme} from '@atb/theme';
+import {Section, ActionItem} from '@atb/components/sections';
 import ThemeText from '@atb/components/text';
-import React from 'react';
+import Button from '../button';
 
 type Props = {
   questions: Array<Question>;
@@ -12,25 +14,69 @@ export enum QuestionTypes {
   TextAlternatives = 'TEXTALTERNATIVES',
 }
 
+export type Alternative = {
+  alternativeText: string;
+  checked: boolean;
+};
+
 export type Question = {
   questionType: QuestionTypes;
   questionText: string;
-  alternatives?: Array<string>;
+  alternatives: Array<Alternative>;
+};
+
+const submitFeedback = () => {
+  console.log('feedback submitted');
 };
 
 export const Feedback = ({questions}: Props) => {
-  // const styles = useThemeStyles();
+  const styles = useThemeStyles();
+  const {theme} = useTheme();
+
+  const [receivedFeedback, setReceivedFeedback] = useState({
+    addedQuestions: [
+      {
+        questionText: '',
+        answers: [''],
+      },
+    ],
+  });
+
+  const handleAnswerPress = (clickedAnswer: string) => {
+    console.log(clickedAnswer);
+  };
+
+  console.log(questions[0].alternatives);
 
   return (
-    <View>
-      {questions.map((question: any) => (
-        <ThemeText>{question.questionText}</ThemeText>
+    <View style={styles.container}>
+      {questions.map((question: Question) => (
+        <>
+          <ThemeText>{question.questionText}</ThemeText>
+
+          <Section withTopPadding withBottomPadding>
+            {question.alternatives.map((alternative) => (
+              <ActionItem
+                text={alternative.alternativeText}
+                onPress={() => handleAnswerPress(alternative.alternativeText)}
+                mode="check"
+                type="compact"
+              />
+            ))}
+          </Section>
+        </>
       ))}
+
+      <Button
+        text="Send tilbakemelding"
+        onPress={submitFeedback}
+        mode="primary"
+        color="secondary_1"
+      />
     </View>
   );
 };
 
-/*
 const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
   container: {
     paddingHorizontal: theme.spacings.medium,
@@ -38,6 +84,5 @@ const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
   },
   infoBoxText: theme.typography.body__primary,
 }));
-*/
 
 export default Feedback;
