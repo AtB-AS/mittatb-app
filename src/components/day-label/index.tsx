edb1ddfd0555850863bf1ad311fe4cef51cf68e2
EspenMH <espen@monopoint.net>
2022-01-31 16:02:37 +0100

    mer space over dayLabel

--- src/components/day-label/index.tsx
+++ src/components/day-label/index.tsx
@@ -52,7 +52,7 @@ export default function DayLabel({
 const useDayTextStyle = StyleSheet.createThemeHook((theme) => ({
   title: {
     paddingHorizontal: theme.spacings.medium,
-    marginTop: theme.spacings.medium,
+    marginTop: theme.spacings.xLarge,
     color: theme.text.colors.secondary,
   },
 }));
