aa5ca13b92c925b9e675bb85bcc717e076af06f1
Christian Brevik <christian.brevik@gmail.com>
2022-02-01 08:53:53 +0100

    print gps coords

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -3,6 +3,7 @@ import {spawn} from 'child-process-promise';
 export default async function setLocation(latitude: number, longitude: number) {
   // Run this command in shell.
   // Every argument needs to be a separate string in an an array.
+  console.log(`Setting GPS coordinates to "[${latitude}, ${longitude}]"`);
   const command = 'applesimutils';
   const args = ['--setLocation', `"[${latitude}, ${longitude}]"`];
   const options = {
