fb4e9518011be60500eec7e73177406e945ddbc6
Christian Brevik <christian.brevik@gmail.com>
2022-02-10 16:05:04 +0100

    fix: Move invitation code to beta-section

--- src/screens/Profile/Home/index.tsx
+++ src/screens/Profile/Home/index.tsx
@@ -159,10 +159,6 @@ export default function ProfileHome({navigation}: ProfileScreenProps) {
               onPress={() => navigation.navigate('Language')}
             />
           )}
-          <Sections.LinkItem
-            text={t(ProfileTexts.sections.settings.linkItems.enrollment.label)}
-            onPress={() => navigation.navigate('Enrollment')}
-          />
         </Sections.Section>
         <Sections.Section withPadding>
           <Sections.GenericItem>
@@ -190,6 +186,10 @@ export default function ProfileHome({navigation}: ProfileScreenProps) {
               });
               setPreference({newDepartures});
             }}
+          />
+          <Sections.LinkItem
+            text={t(ProfileTexts.sections.settings.linkItems.enrollment.label)}
+            onPress={() => navigation.navigate('Enrollment')}
           />
         </Sections.Section>
 
