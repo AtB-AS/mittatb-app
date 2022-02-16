cce0681b5ab75cea74439ddadf67cf8b873818ec
Tore Stensaker Tefre <tore@tefre.it>
2022-02-17 00:06:01 +0100

    update smiley icons

--- src/components/feedback/GoodOrBadButton.tsx
+++ src/components/feedback/GoodOrBadButton.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {GenericClickableItem} from '../sections';
 import {StyleSheet, useTheme} from '@atb/theme';
 import {View, TouchableOpacity} from 'react-native';
 import ThemeText from '../text';
@@ -40,79 +39,39 @@ export const GoodOrBadButton = ({
         </ThemeText>
 
         {opinion === Opinions.Good && (
-          <Svg
-            width={24}
-            height={24}
-            fill="none"
-            viewBox="0 0 24 24"
-            {...props}
-          >
+          <Svg width={24} height={24} fill="none" {...props}>
             <Path
-              d="M12.75 22C18.2728 22 22.75 17.5228 22.75 12C22.75 6.47715 18.2728 2 12.75 2C7.22715 2 2.75 6.47715 2.75 12C2.75 17.5228 7.22715 22 12.75 22Z"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              d="M12.75 22c5.523 0 10-4.477 10-10s-4.477-10-10-10-10 4.477-10 10 4.477 10 10 10Z"
+              stroke="white"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <Path
-              d="M8.75 14C8.75 14 10.25 16 12.75 16C15.25 16 16.75 14 16.75 14"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <Path
-              d="M9.75 9H9.76"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <Path
-              d="M15.75 9H15.76"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              d="M8.75 14s1.5 2 4 2 4-2 4-2M9.75 9h.01M15.75 9h.01"
+              stroke="white"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </Svg>
         )}
 
         {opinion === Opinions.Bad && (
-          <Svg
-            width={24}
-            height={24}
-            fill="none"
-            viewBox="0 0 24 24"
-            {...props}
-          >
+          <Svg width={24} height={24} fill="none" {...props}>
             <Path
-              d="M12.25 22C17.7728 22 22.25 17.5228 22.25 12C22.25 6.47715 17.7728 2 12.25 2C6.72715 2 2.25 6.47715 2.25 12C2.25 17.5228 6.72715 22 12.25 22Z"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              d="M12.25 22c5.523 0 10-4.477 10-10s-4.477-10-10-10-10 4.477-10 10 4.477 10 10 10Z"
+              stroke="white"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <Path
-              d="M16.25 16C16.25 16 14.75 14 12.25 14C9.75 14 8.25 16 8.25 16"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <Path
-              d="M9.25 9H9.26"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <Path
-              d="M15.25 9H15.26"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              d="M16.25 16s-1.5-2-4-2-4 2-4 2M9.25 9h.01M15.25 9h.01"
+              stroke="white"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </Svg>
         )}
