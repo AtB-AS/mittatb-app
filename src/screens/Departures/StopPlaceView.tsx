1bb5c1f30494134619b5e0bf25a7b84260ff0321
Tore Stensaker Tefre <toretefre@users.noreply.github.com>
2022-02-22 11:56:55 +0100

    add missing parathesis

--- src/screens/Departures/StopPlaceView.tsx
+++ src/screens/Departures/StopPlaceView.tsx
@@ -74,6 +74,7 @@ export default function StopPlaceView({
                 <Feedback mode="departures" quayListData={quayListData} />
               )}
             </>
+          )}
         />
       )}
     </>
