2d39c849c871981c38c7d0d3d88e72bb91001322
Tore Stensaker Tefre <toretefre@users.noreply.github.com>
2022-02-26 20:51:13 +0100

    typos in translations

--- src/translations/components/InfoPrompt.ts
+++ src/translations/components/InfoPrompt.ts
@@ -10,19 +10,19 @@ export const InfoPrompt = {
       bulletPoints: [
         _(
           'Reiseforslagene beregnes noe annerledes enn tidligere.',
-          'Trip proposals are now calculated slightly different from before',
+          'Trip proposals are now calculated slightly different from before.',
         ),
         _(
-          'Du kan også velge å hente ut flere reiseforslag ved å trykke på "Last inn flere" reiseforslag',
-          'You may also get more proposals by pressing "Load more results"',
+          'Du kan også velge å hente ut flere reiseforslag ved å trykke på "Last inn flere reiseforslag".',
+          'You may also get more proposals by pressing "Load more results".',
         ),
       ],
       text: _(
-        'Du kan bytte mellom gammelt og nytt reisesøk fra Min Profil-fanen',
-        'You may switch between the old and new travel assistant using the Profile tab',
+        'Du kan bytte mellom gammelt og nytt reisesøk fra Min Profil-fanen.',
+        'You may switch between the old and new travel assistant using the Profile tab.',
       ),
     },
-    button: _('Den er grei!', 'Alright'),
+    button: _('Den er grei!', 'Great!'),
   },
 };
 
