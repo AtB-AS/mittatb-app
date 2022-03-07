fba80520dd4847268900add6daa9c2c70458b478
Morten Lundenes <8726756+mortlun@users.noreply.github.com>
2022-03-07 12:08:21 +0100

    refactor: different UI structure for "info and prices" screens for orgs (#2174)
    * extracted info into own component to render json of different screens
    
    * added table and json for rest of screens
    
    * renamed header to heading and fixed type
    
    * moved json files to reference data folder
    
    * refactor table component
    
    * added key
    
    * cosmetic adjustments in text
    
    * refactor variable names
    
    * moved information json files to assets
    
    * refactor

--- src/screens/Profile/Information/PaymentInformation.tsx
+++ src/screens/Profile/Information/PaymentInformation.tsx
@@ -1,43 +1,15 @@
-import {Linking, TouchableOpacity, View} from 'react-native';
-import FullScreenHeader from '@atb/components/screen-header/full-header';
 import {InformationTexts, useTranslation} from '@atb/translations';
-import {ScrollView} from 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, Theme} from '@atb/theme';
-import ThemeText from '@atb/components/text';
-
+import paymentInfo from '../../../assets/information-data/paymentInformation.json';
+import Information, {InformationElement} from './Information';
 export default function PaymentInformation() {
-  const styles = useStyles();
-  const {t} = useTranslation();
-  return (
-    <View style={styles.container}>
-      <FullScreenHeader
-        title={t(InformationTexts.payment.title)}
-        leftButton={{type: 'back'}}
-      />
+  const {t, language} = useTranslation();
+  const data = paymentInfo[language];
 
-      <ScrollView contentContainerStyle={styles.content}>
-        <ThemeText style={styles.paragraphHeading} type="heading__paragraph">
-          {t(InformationTexts.payment.texts.part1Heading)}
-        </ThemeText>
-        <ThemeText>{t(InformationTexts.payment.texts.part1Text)}</ThemeText>
-        <ThemeText style={styles.paragraphHeading} type="heading__paragraph">
-          {t(InformationTexts.payment.texts.part2Heading)}
-        </ThemeText>
-      </ScrollView>
-    </View>
+  return (
+    <Information
+      informations={data as InformationElement[]}
+      title={t(InformationTexts.payment.title)}
+    ></Information>
   );
 }
-
-const useStyles = StyleSheet.createThemeHook((theme: Theme) => ({
-  container: {
-    backgroundColor: theme.colors.background_1.backgroundColor,
-    flex: 1,
-  },
-  content: {
-    padding: theme.spacings.medium,
-  },
-  paragraphHeading: {
-    marginVertical: theme.spacings.medium,
-  },
-}));
--- src/screens/Profile/Information/TicketInspectionInformation.tsx
+++ src/screens/Profile/Information/TicketInspectionInformation.tsx
@@ -1,83 +1,17 @@
-import {Linking, TouchableOpacity, View} from 'react-native';
-import FullScreenHeader from '@atb/components/screen-header/full-header';
 import {InformationTexts, useTranslation} from '@atb/translations';
-import {ScrollView} from 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, Theme} from '@atb/theme';
-import ThemeText from '@atb/components/text';
+import ticketInspectionInfo from '../../../assets/information-data/ticketInspectionInformation.json';
 
+import Information, {InformationElement} from './Information';
 export default function TicketInspectionInformation() {
-  const styles = useStyles();
-  const {t} = useTranslation();
-  return (
-    <View style={styles.container}>
-      <FullScreenHeader
-        title={t(InformationTexts.inspection.title)}
-        leftButton={{type: 'back'}}
-      />
+  const {t, language} = useTranslation();
 
-      <ScrollView contentContainerStyle={styles.content}>
-        <ThemeText>{t(InformationTexts.inspection.texts.part1Text1)}</ThemeText>
-        <ThemeText style={styles.bullet}>
-          {t(InformationTexts.inspection.texts.part1Bullet1)}
-        </ThemeText>
-        <ThemeText style={styles.bullet}>
-          {t(InformationTexts.inspection.texts.part1Bullet2)}
-        </ThemeText>
-        <ThemeText style={styles.bullet}>
-          {t(InformationTexts.inspection.texts.part1Bullet3)}
-        </ThemeText>
-        <ThemeText style={styles.paragraphSpace}>
-          {t(InformationTexts.inspection.texts.part1Text2)}
-        </ThemeText>
-        <ThemeText style={styles.paragraphSpace}>
-          {t(InformationTexts.inspection.texts.part1Text3)}
-        </ThemeText>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() =>
-            Linking.openURL(t(InformationTexts.inspection.texts.part1Link1.url))
-          }
-          accessibilityRole="button"
-        >
-          <ThemeText type="body__primary--underline">
-            {t(InformationTexts.inspection.texts.part1Link1.text)}
-          </ThemeText>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() =>
-            Linking.openURL(t(InformationTexts.inspection.texts.part1Link2.url))
-          }
-          accessibilityRole="button"
-        >
-          <ThemeText type="body__primary--underline">
-            {t(InformationTexts.inspection.texts.part1Link2.text)}
-          </ThemeText>
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
+  const data = ticketInspectionInfo[language];
+
+  return (
+    <Information
+      informations={data as InformationElement[]}
+      title={t(InformationTexts.inspection.title)}
+    ></Information>
   );
 }
-
-const useStyles = StyleSheet.createThemeHook((theme: Theme) => ({
-  container: {
-    backgroundColor: theme.colors.background_1.backgroundColor,
-    flex: 1,
-  },
-  content: {
-    padding: theme.spacings.medium,
-  },
-  paragraphHeading: {
-    marginVertical: theme.spacings.medium,
-  },
-  bullet: {
-    marginTop: theme.spacings.medium,
-  },
-  paragraphSpace: {
-    marginTop: theme.spacings.medium,
-  },
-  link: {
-    marginTop: theme.spacings.medium,
-  },
-}));
