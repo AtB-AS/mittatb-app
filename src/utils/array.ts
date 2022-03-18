0be370541743f01eb103b1f6b36897e81723d796
Johannes Røsvik <j.rosvik@gmail.com>
2022-03-18 12:53:37 +0100

    add interpose function

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -4,3 +4,13 @@ export function flatMap<T, U>(
 ): U[] {
   return Array.prototype.concat(...array.map(callbackfn));
 }
+
+/*
+Takes an array and an element and returns a new array with the given element
+inserted in between every element of the original array.
+
+E.g. interpose([1,2,3], '+') -> [1, '+', 2, '+', 3]
+*/
+export function interpose<T, U>(array: Array<T>, delim: U) {
+  return flatMap(array, (el) => [delim, el]).slice(1);
+}
