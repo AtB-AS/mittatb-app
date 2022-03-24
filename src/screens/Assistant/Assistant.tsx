9808de4625eee37c25a12c08037776b7c6627606
Espen MH <espen@monopoint.net>
2022-03-24 12:34:02 +0100

    back button resets trip search (#2392)

--- src/screens/Assistant/Assistant.tsx
+++ src/screens/Assistant/Assistant.tsx
@@ -47,7 +47,12 @@ import {
 } from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
-import {ActivityIndicator, TouchableOpacity, View} from 'react-native';
+import {
+  ActivityIndicator,
+  TouchableOpacity,
+  View,
+  BackHandler,
+} from 'react-native';
 import {AssistantParams} from '.';
 import Loading from '../Loading';
 import FadeBetween from './FadeBetween';
@@ -410,6 +415,21 @@ const Assistant: React.FC<Props> = ({
         break;
     }
   }, [searchState]);
+
+  // Reset view on back press instead of exiting app
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (screenHasFocus && from && to) {
+          resetView();
+          return true; // prevent default action
+        }
+        return false;
+      },
+    );
+    return () => backHandler.remove();
+  });
 
   return (
     <DisappearingHeader
