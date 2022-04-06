58d64b949ea268e797360de1d0702c7699cdf9fb
Tore Stensaker Tefre <toretefre@users.noreply.github.com>
2022-04-06 14:49:25 +0200

    add accessability hints to mitigate #2419

--- src/translations/screens/Feedback.ts
+++ src/translations/screens/Feedback.ts
@@ -1,6 +1,18 @@
 import {translation as _} from '../commons';
 
 const FeedbackTexts = {
+  alternatives: {
+    a11yHints: {
+      checked: _(
+        'Alternativet er valgt. Aktivér for å fjerne markeringen på dette alternativet.',
+        'This alternative is already selected. Activate to uncheck this alternative.',
+      ),
+      unchecked: _(
+        'Alternativet er ikke valgt. Aktivér for å velge dette alternativet.',
+        'This alternative is not selected. Activate to check this alternative.',
+      ),
+    },
+  },
   goodOrBadTexts: {
     good: _('Bra', 'Good'),
     bad: _('Dårlig', 'Bad'),
