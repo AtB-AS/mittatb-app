f8a5ad1a4c2c8276947ef3bb71e2934393ab7e04
EspenMH <espen@monopoint.net>
2022-04-27 15:32:57 +0200

    refactor Themed Ticket Illustration

--- src/components/ticket-illustration/index.tsx
+++ src/components/ticket-illustration/index.tsx
@@ -2,17 +2,19 @@ import {useTheme} from '@atb/theme';
 import React from 'react';
 import * as Light from '@atb/assets/svg/color/illustrations/ticket-type/light';
 import * as Dark from '@atb/assets/svg/color/illustrations/ticket-type/dark';
+import {SvgProps} from 'react-native-svg';
 
 export type TicketIllustration = keyof typeof Light;
+type TicketIllustrationsProps = {name: TicketIllustration} & SvgProps;
 
 const ThemedTicketIllustration = ({
   name,
-}: {
-  name: TicketIllustration;
-}): JSX.Element => {
+  ...props
+}: TicketIllustrationsProps): JSX.Element => {
   const {themeName} = useTheme();
-  const themeIcons = themeName === 'light' ? Light : Dark;
-  return themeIcons[name]({});
+  const themeIllustrations = themeName === 'light' ? Light : Dark;
+  const Illustration = themeIllustrations[name];
+  return <Illustration {...props} />;
 };
 
 export default ThemedTicketIllustration;
