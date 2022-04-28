b31f0f24bde2e95b2563fb74f2bfffcde52e0676
Morten Lundenes <8726756+mortlun@users.noreply.github.com>
2022-04-28 16:15:01 +0200

    fix: removed headset icon and changed text for both external link buttons in contact sheet (#2549)
    * removed headset icon and changed text for both external link buttons in contactsheet
    
    * Typos
    
    * revert wrong merge conflict decision
    
    Co-authored-by: Tore Stensaker Tefre <tore@tefre.it>

--- src/chat/ContactSheet.tsx
+++ src/chat/ContactSheet.tsx
@@ -12,7 +12,6 @@ import {AccessibilityProps, Linking, View} from 'react-native';
 import FullScreenFooter from '@atb/components/screen-footer/full-footer';
 import {ScreenHeaderWithoutNavigation} from '@atb/components/screen-header';
 import {BottomSheetContainer} from '@atb/components/bottom-sheet';
-import {Support} from '@atb/assets/svg/mono-icons/actions';
 import {ChatUnread} from '@atb/assets/svg/color/icons/actions';
 import useChatUnreadCount from './use-chat-unread-count';
 import Intercom from 'react-native-intercom';
@@ -53,9 +52,6 @@ const ContactSheet = forwardRef<View, Props>(({close}, focusRef) => {
           body={t(ContactSheetTexts.customer_service.body)}
           buttonText={t(ContactSheetTexts.customer_service.button)}
           focusRef={focusRef}
-          icon={() => (
-            <ThemeIcon colorType="background_accent_3" svg={Support} />
-          )}
           accessibilityHint={t(ContactSheetTexts.customer_service.a11yHint)}
           onPress={() => {
             Linking.openURL(customer_service_url);
@@ -65,10 +61,12 @@ const ContactSheet = forwardRef<View, Props>(({close}, focusRef) => {
 
         {showWebsiteFeedback ? (
           <ContactItem
-            title={t(ContactSheetTexts.customer_feedback.title)}
-            body={t(ContactSheetTexts.customer_feedback.body)}
-            buttonText={t(ContactSheetTexts.customer_feedback.button)}
-            accessibilityHint={t(ContactSheetTexts.customer_feedback.a11yHint)}
+            title={t(ContactSheetTexts.customer_feedback_website.title)}
+            body={t(ContactSheetTexts.customer_feedback_website.body)}
+            buttonText={t(ContactSheetTexts.customer_feedback_website.button)}
+            accessibilityHint={t(
+              ContactSheetTexts.customer_feedback_website.a11yHint,
+            )}
             onPress={() => {
               Linking.openURL(customer_feedback_url);
               close();
