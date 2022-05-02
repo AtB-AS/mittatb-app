09fc6c7ab9423ee538243b045eecd2cce4425107
Tore Stensaker Tefre <tore@tefre.it>
2022-05-02 13:30:56 +0200

    replace organise with organize

--- src/translations/screens/TicketSplash.ts
+++ src/translations/screens/TicketSplash.ts
@@ -12,7 +12,7 @@ const TicketSplashTexts = {
     title: _('Billettkjøp i app kommer snart!', 'App ticketing on its way!'),
     paragraph1: _(
       'Her kan du snart kjøpe og administrere billetter til reisen din! Da vil du ha alt du trenger i en og samme app.',
-      'Soon you can purchase and organise tickets for your trips – right here in the app!',
+      'Soon you can purchase and organize tickets for your trips – right here in the app!',
     ),
     betaButtonLabel: _(
       'Jeg har kode til beta for billettkjøp',
