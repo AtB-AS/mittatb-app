9f4e9ae740342ef3c5e4ae30cf6ccb2615e48cf1
Gøran Dalum <goran.dalum@kantega.no>
2022-07-05 10:38:58 +0200

    Update Entur registry credentials handling

---
+++ scripts/add-entur-private-registry.sh
@@ -0,0 +1,20 @@
+echo "Adding Entur private registry credentials"
+
+NPMRC_FILE="$HOME/.npmrc"
+echo "Adding Entur registry properties to $NPMRC_FILE"
+
+echo "@entur:registry=https://entur2.jfrog.io/entur2/api/npm/partner-npm-local/" >> $NPMRC_FILE
+echo "@entur-private:registry=https://entur2.jfrog.io/entur2/api/npm/partner-npm-local/" >> $NPMRC_FILE
+echo "//entur2.jfrog.io/entur2/api/npm/partner-npm-local/:_authToken=$ENTUR_REGISTRY_TOKEN" >> $NPMRC_FILE
+
+GRADLE_DIRECTORY="$HOME/.gradle"
+if [[ ! -e $GRADLE_DIRECTORY ]]; then
+    mkdir $GRADLE_DIRECTORY
+fi
+GRADLE_PROPERTIES="$GRADLE_DIRECTORY/gradle.properties"
+echo "Adding Entur registry properties to $GRADLE_PROPERTIES"
+
+echo "entur_artifactory_partner_resolve_snapshot_url=https://entur2.jfrog.io/entur2/partner-snapshot" >> $GRADLE_PROPERTIES
+echo "entur_artifactory_partner_resolve_release_url=https://entur2.jfrog.io/entur2/partner-release" >> $GRADLE_PROPERTIES
+echo "entur_artifactory_user=$ENTUR_REGISTRY_USER" >> $GRADLE_PROPERTIES
+echo "entur_artifactory_password=$ENTUR_REGISTRY_TOKEN" >> $GRADLE_PROPERTIES
