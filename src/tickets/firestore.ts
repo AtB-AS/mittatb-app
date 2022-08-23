d64cefd33914d8e99396262faeed331057456b2e
Mikael Brevik <mikaelbre@gmail.com>
2022-08-23 09:33:52 +0200

    Revert "chore: adds logging on "old" reservations (#2868)" (#2875)
    This reverts commit 7979014df9b797f59c5d63c4cb89eb4a22323318.

--- src/tickets/firestore.ts
+++ src/tickets/firestore.ts
@@ -1,11 +1,9 @@
-import Bugsnag, {OnErrorCallback} from '@bugsnag/react-native';
+import Bugsnag from '@bugsnag/react-native';
 import firestore, {
   FirebaseFirestoreTypes,
 } from '@react-native-firebase/firestore';
-import {addHours, isBefore, subMinutes} from 'date-fns';
+import {addHours} from 'date-fns';
 import {CustomerProfile, FareContract, Reservation} from './types';
-
-const OLD_ITEM_THRESHOLD_IN_MINUTES = 1;
 
 type SnapshotListener<T> = {
   onSnapshot: (snapshot: T) => void;
@@ -51,15 +49,6 @@ export default function setupFirestoreListener(
     .where('created', '>', addHours(Date.now(), -1))
     .onSnapshot(
       (snapshot) => {
-        warningOnSlowItems(
-          snapshot.docChanges() as FirebaseFirestoreTypes.DocumentChange<Reservation>[],
-          function (event) {
-            event.addMetadata('reservation', {
-              abtCustomerId,
-              reservation: true,
-            });
-          },
-        );
         const reservations = (
           snapshot as FirebaseFirestoreTypes.QuerySnapshot<Reservation>
         ).docs.map<Reservation>((d) => d.data());
@@ -104,39 +93,4 @@ export default function setupFirestoreListener(
     reservationsSub();
     customerProfileSub();
   };
-}
-
-function warningOnSlowItems(
-  docsChange: FirebaseFirestoreTypes.DocumentChange<{
-    created: FirebaseFirestoreTypes.Timestamp;
-    orderId: string;
-  }>[],
-  onError: OnErrorCallback,
-) {
-  try {
-    const itemsOlderThanAMinute = docsChange
-      .map((d) => d.doc.data())
-      .filter((item) =>
-        isBefore(
-          item.created.toDate(),
-          subMinutes(new Date(), OLD_ITEM_THRESHOLD_IN_MINUTES),
-        ),
-      );
-
-    if (!itemsOlderThanAMinute.length) return;
-    Bugsnag.notify(
-      new Error('Reservations added after some time'),
-      function (event, cb) {
-        event.severity = 'info';
-        event.addMetadata(
-          'itemIds',
-          itemsOlderThanAMinute.map((res) => res.orderId),
-        );
-
-        return onError(event, cb);
-      },
-    );
-  } catch (e) {
-    // Do nothing.
-  }
 }
