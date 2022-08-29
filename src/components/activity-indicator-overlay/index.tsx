a7d860e6bbddd6492a18f2a991c4fc17f2679641
Nisha1306 <nshrautela@gmail.com>
2022-08-29 08:40:02 +0200

    fix: add spinner during logout (#2894)

---
+++ src/components/activity-indicator-overlay/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {ActivityIndicator, View} from 'react-native';
+import {StyleSheet} from '@atb/theme';
+
+const ActivityIndicatorOverlay = () => {
+  const style = useStyles();
+  return (
+    <View style={style.spinner}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+const useStyles = StyleSheet.createThemeHook(() => ({
+  spinner: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    opacity: 0.4,
+  },
+}));
+
+export default ActivityIndicatorOverlay;
