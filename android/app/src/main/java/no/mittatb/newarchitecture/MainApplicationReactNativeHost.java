89dd6f4cb7c751088be35f1f24d021fe34aed173
Mikael Brevik <mikaelbre@gmail.com>
2022-09-01 11:44:55 +0200

    chore: fix android gradle

--- android/app/src/main/java/no/mittatb/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/no/mittatb/newarchitecture/MainApplicationReactNativeHost.java
@@ -19,7 +19,7 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.mittatb.BuildConfig;
+import no.mittatb.BuildConfig;
 import no.mittatb.newarchitecture.components.MainComponentsRegistry;
 import no.mittatb.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
