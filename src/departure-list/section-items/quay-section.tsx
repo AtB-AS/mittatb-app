2a8a988b509753855f743c9de79eb8e39834c034
Tore Stensaker Tefre <tore@tefre.it>
2022-09-06 11:07:31 +0200

    add border between widget lines

--- src/departure-list/section-items/quay-section.tsx
+++ src/departure-list/section-items/quay-section.tsx
@@ -4,7 +4,7 @@ import {
   StopPlaceInfo,
 } from '@atb/api/departures/types';
 import {Section} from '@atb/components/sections';
-import {useTheme} from '@atb/theme';
+import {StyleSheet, useTheme} from '@atb/theme';
 import {NearbyTexts, useTranslation} from '@atb/translations';
 import haversineDistance from 'haversine-distance';
 import sortBy from 'lodash.sortby';
@@ -41,6 +41,7 @@ const QuaySection = React.memo(function QuaySection({
   const [limit, setLimit] = useState(LIMIT_SIZE);
   const {t} = useTranslation();
   const {theme} = useTheme();
+  const styles = useStyles();
 
   useEffect(() => {
     setLimit(LIMIT_SIZE);
@@ -59,7 +60,7 @@ const QuaySection = React.memo(function QuaySection({
 
   return (
     <Fragment>
-      <Section testID={testID}>
+      <Section testID={testID} style={styles.quaySection}>
         <QuayHeaderItem
           quay={quayGroup.quay}
           distance={getDistanceInfo(quayGroup, currentLocation)}
@@ -110,3 +111,10 @@ function getDistanceInfo(group: QuayGroup, currentLocation?: Location): number {
     ? 0
     : haversineDistance(currentLocation.coordinates, pos);
 }
+
+const useStyles = StyleSheet.createThemeHook((theme) => ({
+  quaySection: {
+    backgroundColor: theme.static.background.background_1.background,
+    borderRadius: theme.border.radius.regular,
+  },
+}));
