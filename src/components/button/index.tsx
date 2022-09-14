23b60a4e49bc77908338ad05cf87cc1b8dcc0001
Gøran Dalum <goran.dalum@kantega.no>
2022-09-14 13:57:18 +0200

    Small refactor

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -60,119 +60,124 @@ export type ButtonProps = {
 
 const DISABLED_OPACITY = 0.2;
 
-const Button = React.forwardRef<any, ButtonProps>((props, ref) => {
-  const {
-    onPress,
-    interactiveColor = 'interactive_0',
-    mode = 'primary',
-    type = 'block',
-    icon: Icon,
-    iconPosition = 'left',
-    text,
-    disabled,
-    active,
-    style,
-    viewContainerStyle,
-    textContainerStyle,
-    textStyle,
-    ...otherProps
-  } = props;
-  const modeData = DefaultModeStyles[mode];
-  const themeColor = interactiveColor;
-  const css = useButtonStyle();
-  const {theme} = useTheme();
-  const fadeAnim = useRef(
-    new Animated.Value(disabled ? DISABLED_OPACITY : 1),
-  ).current;
-
-  React.useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: disabled ? DISABLED_OPACITY : 1,
-      duration: 200,
-      easing: Easing.inOut(Easing.ease),
-      useNativeDriver: true,
-    }).start();
-  }, [disabled, fadeAnim]);
-
-  const isInline = type === 'compact' || type === 'inline';
-
-  const spacing =
-    type === 'compact' ? theme.spacings.small : theme.spacings.medium;
-  const leftIconSpacing = Icon && iconPosition === 'left' ? spacing : undefined;
-  const rightIconSpacing =
-    Icon && iconPosition === 'right' ? spacing : undefined;
-
-  const {background: backgroundColor, text: textColor} = themeColor
-    ? theme.interactive[themeColor][active ? 'active' : 'default']
-    : {
-        background: 'transparent',
-        text: theme.text.colors.primary,
-      };
-
-  const styleContainer: ViewStyle[] = [
-    css.button,
+const Button = React.forwardRef<any, ButtonProps>(
+  (
     {
-      backgroundColor: modeData.withBackground ? backgroundColor : undefined,
-      borderColor: modeData.visibleBorder ? textColor : 'transparent',
-      padding: spacing,
-      alignSelf: isInline ? 'flex-start' : undefined,
+      onPress,
+      interactiveColor = 'interactive_0',
+      mode = 'primary',
+      type = 'block',
+      icon: Icon,
+      iconPosition = 'left',
+      text,
+      disabled,
+      active,
+      style,
+      viewContainerStyle,
+      textContainerStyle,
+      textStyle,
+      ...props
     },
-  ];
-  const styleText: TextStyle = {color: textColor};
-  const textContainer: TextStyle = {
-    flex: isInline ? undefined : 1,
-    alignItems: 'center',
-    marginHorizontal: Icon && !isInline ? theme.spacings.xLarge : 0,
-  };
-  const iconContainer: ViewStyle = isInline
-    ? {
-        position: 'relative',
-        left: undefined,
-        right: undefined,
-      }
-    : {
-        position: 'absolute',
-        left: leftIconSpacing,
-        right: rightIconSpacing,
-      };
+    ref,
+  ) => {
+    const modeData = DefaultModeStyles[mode];
+    const themeColor = interactiveColor;
+    const css = useButtonStyle();
+    const {theme} = useTheme();
+    const fadeAnim = useRef(
+      new Animated.Value(disabled ? DISABLED_OPACITY : 1),
+    ).current;
 
-  return (
-    <Animated.View style={[{opacity: fadeAnim}, viewContainerStyle]}>
-      <TouchableOpacity
-        style={[styleContainer, style]}
-        onPress={disabled ? undefined : onPress}
-        disabled={disabled}
-        accessibilityRole="button"
-        accessibilityState={{disabled: !!disabled}}
-        ref={ref}
-        {...otherProps}
-      >
-        {Icon && iconPosition === 'left' && (
-          <View style={iconContainer}>
-            <ThemeIcon svg={Icon} fill={textColor} />
-          </View>
-        )}
-        {text && (
-          <View style={[textContainer, textContainerStyle]}>
-            <ThemeText
-              type={
-                mode === 'tertiary' ? 'body__primary' : 'body__primary--bold'
-              }
-              style={[styleText, textStyle]}
-            >
-              {text}
-            </ThemeText>
-          </View>
-        )}
-        {Icon && iconPosition === 'right' && (
-          <View style={iconContainer}>
-            <ThemeIcon svg={Icon} fill={textColor} />
-          </View>
-        )}
-      </TouchableOpacity>
-    </Animated.View>
-  );
-});
+    React.useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: disabled ? DISABLED_OPACITY : 1,
+        duration: 200,
+        easing: Easing.inOut(Easing.ease),
+        useNativeDriver: true,
+      }).start();
+    }, [disabled, fadeAnim]);
+
+    const isInline = type === 'compact' || type === 'inline';
+
+    const spacing =
+      type === 'compact' ? theme.spacings.small : theme.spacings.medium;
+    const leftIconSpacing =
+      Icon && iconPosition === 'left' ? spacing : undefined;
+    const rightIconSpacing =
+      Icon && iconPosition === 'right' ? spacing : undefined;
+
+    const {background: backgroundColor, text: textColor} = themeColor
+      ? theme.interactive[themeColor][active ? 'active' : 'default']
+      : {
+          background: 'transparent',
+          text: theme.text.colors.primary,
+        };
+
+    const styleContainer: ViewStyle[] = [
+      css.button,
+      {
+        backgroundColor: modeData.withBackground ? backgroundColor : undefined,
+        borderColor: modeData.visibleBorder ? textColor : 'transparent',
+        padding: spacing,
+        alignSelf: isInline ? 'flex-start' : undefined,
+      },
+    ];
+    const styleText: TextStyle = {color: textColor};
+    const textContainer: TextStyle = {
+      flex: isInline ? undefined : 1,
+      alignItems: 'center',
+      marginHorizontal: Icon && !isInline ? theme.spacings.xLarge : 0,
+    };
+    const iconContainer: ViewStyle = isInline
+      ? {
+          position: 'relative',
+          left: undefined,
+          right: undefined,
+        }
+      : {
+          position: 'absolute',
+          left: leftIconSpacing,
+          right: rightIconSpacing,
+        };
+
+    return (
+      <Animated.View style={[{opacity: fadeAnim}, viewContainerStyle]}>
+        <TouchableOpacity
+          style={[styleContainer, style]}
+          onPress={disabled ? undefined : onPress}
+          disabled={disabled}
+          accessibilityRole="button"
+          accessibilityState={{disabled: !!disabled}}
+          ref={ref}
+          {...props}
+        >
+          {Icon && iconPosition === 'left' && (
+            <View style={iconContainer}>
+              <ThemeIcon svg={Icon} fill={textColor} />
+            </View>
+          )}
+          {text && (
+            <View style={[textContainer, textContainerStyle]}>
+              <ThemeText
+                type={
+                  mode === 'tertiary' ? 'body__primary' : 'body__primary--bold'
+                }
+                style={[styleText, textStyle]}
+              >
+                {text}
+              </ThemeText>
+            </View>
+          )}
+          {Icon && iconPosition === 'right' && (
+            <View style={iconContainer}>
+              <ThemeIcon svg={Icon} fill={textColor} />
+            </View>
+          )}
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  },
+);
 export default Button;
 
 const useButtonStyle = StyleSheet.createThemeHook((theme: Theme) => ({
