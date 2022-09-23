48019ff5f8a96944db222b20fdaded0286586f97
Mikael Brevik <mikaelbre@gmail.com>
2022-09-23 10:15:36 +0200

    ci: fix replace bundle android and cache case (#2970)
    * ci: fix replace bundle android
    
    * ci: fix override environment
    
    * ci: fix zipalign/apksigner for unix

--- scripts/android/override-config-files.sh
+++ scripts/android/override-config-files.sh
@@ -5,22 +5,12 @@ cat .netrc >> ~/.netrc
 chmod 600 ~/.netrc
 
 echo "Installing pre-build dependencies"
-brew install findutils xmlstarlet # for git-crypt
-# findutils for gxargs which is used to load environment variables from .env file
+brew install xmlstarlet # for git-crypt
 # xmlstarlet to edit androidmanifest
 
 echo "Loading all env variables from .env file"
-export $(grep -v '^#' .env | gxargs -d '\n') > /dev/null 2>&1
+export $(grep -v '^#' .env | xargs -d '\n') > /dev/null 2>&1
 
-echo "Currently set ios bundle id: $IOS_BUNDLE_IDENTIFIER"
-
-echo "Adding Bugsnag API key and release stage to Info.plist"
-/usr/libexec/PlistBuddy -c "Add :BugsnagAPIKey string $BUGSNAG_API_KEY" ./ios/atb/Info.plist
-/usr/libexec/PlistBuddy -c "Add :BugsnagReleaseStage string $BUGSNAG_RELEASE_STAGE" ./ios/atb/Info.plist
-
-echo "Adding reversed client id as custom URL scheme to enable reCAPTCHA"
-REVERSED_CLIENT_ID=`/usr/libexec/PlistBuddy -c "Print :REVERSED_CLIENT_ID" ./ios/atb/GoogleService-Info.plist`
-/usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:1:CFBundleURLSchemes:0 $REVERSED_CLIENT_ID" ./ios/atb/Info.plist
 
 echo "Adding Bugsnag API key and release stage to AndroidManifest.xml"
 xmlstarlet edit --inplace --omit-decl \
@@ -34,9 +24,6 @@ xmlstarlet edit --inplace --omit-decl \
   -r //manifest/application/bugsnagreleasestage -v meta-data \
    android/app/src/main/AndroidManifest.xml
 
-echo "Set Intercom API key and App ID in Intercom.plist"
-/usr/libexec/PlistBuddy -c "Set :IntercomApiKey $INTERCOM_IOS_API_KEY" ./ios/atb/Intercom.plist
-/usr/libexec/PlistBuddy -c "Set :IntercomAppId $INTERCOM_APP_ID" ./ios/atb/Intercom.plist
 
 echo "Set Intercom API key and App ID in Intercom.xml"
 xmlstarlet edit --inplace --omit-decl \
