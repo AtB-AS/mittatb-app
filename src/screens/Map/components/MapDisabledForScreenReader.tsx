1c95aae72a955775f427cf5e021c2fd92c5dd842
Johannes Røsvik <j.rosvik@gmail.com>
2022-10-12 12:12:52 +0200

    add a11y focus and label

--- src/screens/Map/components/MapDisabledForScreenReader.tsx
+++ src/screens/Map/components/MapDisabledForScreenReader.tsx
@@ -1,21 +1,35 @@
+import {screenReaderPause} from '@atb/components/accessible-text';
 import ThemeText from '@atb/components/text';
 import {StyleSheet} from '@atb/theme';
 import {ThemedMapImage} from '@atb/theme/ThemedAssets';
 import {MapTexts, useTranslation} from '@atb/translations';
+import useFocusOnLoad from '@atb/utils/use-focus-on-load';
+import {View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 export function MapDisabledForScreenReader() {
   const styles = useStyles();
   const {t} = useTranslation();
+  const focusRef = useFocusOnLoad();
   return (
     <SafeAreaView style={styles.container}>
       <ThemedMapImage />
-      <ThemeText type="body__primary--bold" style={styles.header}>
-        {t(MapTexts.disabledForScreenReader.title)}
-      </ThemeText>
-      <ThemeText style={styles.description}>
-        {t(MapTexts.disabledForScreenReader.description)}
-      </ThemeText>
+      <View
+        ref={focusRef}
+        accessible={true}
+        accessibilityLabel={
+          t(MapTexts.disabledForScreenReader.title) +
+          screenReaderPause +
+          t(MapTexts.disabledForScreenReader.description)
+        }
+      >
+        <ThemeText type="body__primary--bold" style={styles.header}>
+          {t(MapTexts.disabledForScreenReader.title)}
+        </ThemeText>
+        <ThemeText style={styles.description}>
+          {t(MapTexts.disabledForScreenReader.description)}
+        </ThemeText>
+      </View>
     </SafeAreaView>
   );
 }
