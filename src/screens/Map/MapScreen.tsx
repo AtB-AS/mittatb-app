58c6fbf58c27aad27109b833936a467ef8608f4d
Johannes Røsvik <j.rosvik@gmail.com>
2022-10-18 13:33:41 +0200

    feat: set status bar content color on map screen (#3008)

--- src/screens/Map/MapScreen.tsx
+++ src/screens/Map/MapScreen.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import {Map} from '@atb/components/map';
 import useIsScreenReaderEnabled from '@atb/utils/use-is-screen-reader-enabled';
 import {MapDisabledForScreenReader} from './components/MapDisabledForScreenReader';
+import StatusBarOnFocus from '@atb/components/status-bar-on-focus';
 
 export function MapScreen() {
   const isScreenReaderEnabled = useIsScreenReaderEnabled();
   if (isScreenReaderEnabled) return <MapDisabledForScreenReader />;
 
-  return <Map selectionMode={'ExploreStops'} />;
+  return (
+    <>
+      <StatusBarOnFocus barStyle="dark-content" />
+      <Map selectionMode="ExploreStops" />
+    </>
+  );
 }
