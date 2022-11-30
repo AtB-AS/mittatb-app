9ed7d2e2fe8ac71e395004c03afd33f7ca28b284
Jorge Osorio <jorelosorio@gmail.com>
2022-11-30 17:51:15 +0100

    Update converters.ts

--- src/configuration/converters.ts
+++ src/configuration/converters.ts
@@ -9,12 +9,16 @@ import {
   FareProductType,
 } from '@atb/screens/Ticketing/FareContracts/utils';
 import {LanguageAndTextType} from '@atb/translations/types';
+import Bugsnag from '@bugsnag/react-native';
 import {isArray} from 'lodash';
 
 export function mapToFareProductTypeConfigs(
   config: any,
 ): FareProductTypeConfig[] | undefined {
-  if (!isArray(config)) return;
+  if (!isArray(config)) {
+    Bugsnag.notify(`fare product configurations should be of type "array"`);
+    return;
+  }
 
   return config
     .map((val) => mapToFareProductTypeConfig(val))
@@ -31,29 +35,55 @@ function mapToFareProductTypeConfig(
     'description',
     'configuration',
   ];
-  if (!fields.every((f) => f in config)) return;
+  if (!fields.every((f) => f in config)) {
+    Bugsnag.notify(
+      `fare product is missing one or more of the following fields: ${fields}`,
+    );
+    return;
+  }
 
-  if (!isArray(config.name) || !isArray(config.description)) return;
+  const fcTypes = ['single', 'period', 'hour24', 'night'];
+  const fcType = mapToStringAlternatives<FareProductType>(config.type, fcTypes);
+  if (!fcType) {
+    notifyWrongConfigurationType(config.type, 'type', fcTypes);
+    return;
+  }
+
+  if (!isArray(config.name) || !isArray(config.description)) {
+    Bugsnag.notify(
+      `fare product type: "${fcType}", fields "name" or "description" should be defined as an array.`,
+    );
+    return;
+  }
 
   let name = mapLanguageAndTextType(config.name);
   let description = mapLanguageAndTextType(config.description);
 
-  if (!name || !description) return;
-
-  const fcType = mapToStringAlternatives<FareProductType>(config.type, [
-    'single',
-    'period',
-    'hour24',
-    'night',
-  ]);
-  if (!fcType) return;
-
-  const configuration = mapToFareProductConfigSettings(config.configuration);
-  if (!configuration) return;
-
-  if (!isArray(config.transportModes)) return;
-  if (!config.transportModes.every((value: any) => typeof value === 'string'))
+  if (!name || !description) {
+    Bugsnag.notify(
+      `fare product type: "${fcType}", "name" or "description" should conform: "LanguageAndTextType"`,
+    );
     return;
+  }
+
+  if (!isArray(config.transportModes)) {
+    Bugsnag.notify(
+      `fare product type: "${fcType}", "transportModes" should be of type "array"`,
+    );
+    return;
+  }
+  if (!config.transportModes.every((value: any) => typeof value === 'string')) {
+    Bugsnag.notify(
+      `fare product type: "${fcType}", one or more of the "transportModes" values is not of type "string"`,
+    );
+    return;
+  }
+
+  const configuration = mapToFareProductConfigSettings(
+    fcType,
+    config.configuration,
+  );
+  if (!configuration) return;
 
   return {
     type: fcType,
@@ -65,39 +95,77 @@ function mapToFareProductTypeConfig(
 }
 
 function mapToFareProductConfigSettings(
+  fareProductType: FareProductType,
   settings: any,
 ): FareProductTypeConfigSettings | undefined {
+  const zoneSelectionModeTypes = ['single', 'multiple', 'none'];
   const zoneSelectionMode = mapToStringAlternatives<ZoneSelectionMode>(
     settings.zoneSelectionMode,
-    ['single', 'multiple', 'none'],
+    zoneSelectionModeTypes,
   );
+  if (!zoneSelectionMode) {
+    notifyWrongConfigurationType(
+      fareProductType,
+      'zoneSelectionMode',
+      zoneSelectionModeTypes,
+    );
+    return;
+  }
+
+  const travellerSelectionModeTypes = ['multiple', 'single', 'none'];
   const travellerSelectionMode =
     mapToStringAlternatives<TravellerSelectionMode>(
       settings.travellerSelectionMode,
-      ['multiple', 'single', 'none'],
+      travellerSelectionModeTypes,
     );
+  if (!travellerSelectionMode) {
+    notifyWrongConfigurationType(
+      fareProductType,
+      'travellerSelectionMode',
+      travellerSelectionModeTypes,
+    );
+    return;
+  }
+
+  const timeSelectionModeTypes = ['datetime', 'none'];
   const timeSelectionMode = mapToStringAlternatives<TimeSelectionMode>(
     settings.timeSelectionMode,
-    ['datetime', 'none'],
+    timeSelectionModeTypes,
   );
+  if (!timeSelectionMode) {
+    notifyWrongConfigurationType(
+      fareProductType,
+      'timeSelectionMode',
+      timeSelectionModeTypes,
+    );
+    return;
+  }
+
+  const productSelectionModeTypes = ['duration', 'product', 'none'];
   const productSelectionMode = mapToStringAlternatives<ProductSelectionMode>(
     settings.productSelectionMode,
-    ['duration', 'product', 'none'],
+    productSelectionModeTypes,
   );
+  if (!productSelectionMode) {
+    notifyWrongConfigurationType(
+      fareProductType,
+      'productSelectionMode',
+      productSelectionModeTypes,
+    );
+    return;
+  }
+
+  const offerEndpointTypes = ['zones', 'authority'];
   const offerEndpoint = mapToStringAlternatives<OfferEndpoint>(
     settings.offerEndpoint,
-    ['zones', 'authority'],
+    offerEndpointTypes,
   );
-
-  if (
-    !(
-      zoneSelectionMode &&
-      travellerSelectionMode &&
-      timeSelectionMode &&
-      productSelectionMode &&
-      offerEndpoint
-    )
-  ) {
+  if (!offerEndpoint) {
+    notifyWrongConfigurationType(
+      fareProductType,
+      'offerEndpoint',
+      offerEndpointTypes,
+    );
     return;
   }
 
@@ -108,6 +176,16 @@ function mapToFareProductConfigSettings(
     productSelectionMode,
     offerEndpoint,
   } as FareProductTypeConfigSettings;
+}
+
+function notifyWrongConfigurationType(
+  fareProductType: string,
+  field: string,
+  types: string[],
+) {
+  Bugsnag.notify(
+    `fare product of type: "${fareProductType}", the "${field}" field, does not belong to any of the following types: "${types}"`,
+  );
 }
 
 function mapToStringAlternatives<T>(value: any, alternatives: string[]) {
