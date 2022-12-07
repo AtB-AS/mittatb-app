67fe7d142f37d9da244a0efc933405ca49f0a188
Johannes Røsvik <j.rosvik@gmail.com>
2022-12-07 12:57:49 +0100

    update map title padding

--- src/screens/Departures/StopPlaceView.tsx
+++ src/screens/Departures/StopPlaceView.tsx
@@ -170,7 +170,7 @@ export default function StopPlaceView(props: StopPlaceViewProps) {
               <ThemeText
                 type="body__secondary"
                 color="secondary"
-                style={[styles.title, styles.paddingHorizontal]}
+                style={styles.title}
               >
                 {t(DeparturesTexts.header.title)}
               </ThemeText>
@@ -288,11 +288,9 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
   loadingIndicator: {
     padding: theme.spacings.medium,
   },
-  paddingHorizontal: {
-    paddingHorizontal: theme.spacings.medium,
-  },
   title: {
-    paddingBottom: theme.spacings.small,
+    marginTop: theme.spacings.medium,
+    paddingHorizontal: theme.spacings.medium,
   },
   distanceLabel: {
     flexDirection: 'row',
