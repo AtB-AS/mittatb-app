9b8a2714dbad0fc07ec4d2c20ff644946fa03305
Gøran Dalum <goran.dalum@kantega.no>
2022-12-20 16:05:29 +0100

    refactor: Don't show situation icons when searching for favorites

--- src/screens/Departures/components/EstimatedCallItem.tsx
+++ src/screens/Departures/components/EstimatedCallItem.tsx
@@ -132,11 +132,15 @@ export default function EstimatedCallItem({
               publicCode={line.publicCode}
               transportMode={line.transportMode}
               transportSubmode={line.transportSubmode}
-              icon={getSvgForMostCriticalSituationOrNotice(
-                departure.situations,
-                notices,
-                departure.cancellation,
-              )}
+              icon={
+                mode !== 'Favourite'
+                  ? getSvgForMostCriticalSituationOrNotice(
+                      departure.situations,
+                      notices,
+                      departure.cancellation,
+                    )
+                  : undefined
+              }
               testID={testID}
             />
           )}
