5267f7389853d757a7f2d02020ce46a1b8c701fd
ingvisim <85479566+ingvisim@users.noreply.github.com>
2022-12-22 10:31:04 +0100

    feat: Add realtime icon to trip leg times (#3160)
    * feat: Add realtime icon to trip leg times
    
    * update dictionary
    
    * refactor
    
    * Update src/screens/TripDetails/components/Time.tsx
    
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>
    
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- src/screens/TripDetails/components/TripSection.tsx
+++ src/screens/TripDetails/components/TripSection.tsx
@@ -121,7 +121,7 @@ const TripSection: React.FC<TripSectionProps> = ({
               language,
               t,
             )}
-            rowLabel={<Time {...startTimes} />}
+            rowLabel={<Time timeValues={startTimes} />}
             onPress={() => handleQuayPress(leg.fromPlace.quay)}
             testID="fromPlace"
           >
@@ -208,7 +208,7 @@ const TripSection: React.FC<TripSectionProps> = ({
               language,
               t,
             )}
-            rowLabel={<Time {...endTimes} />}
+            rowLabel={<Time timeValues={endTimes} />}
             onPress={() => handleQuayPress(leg.toPlace.quay)}
             testID="toPlace"
           >
@@ -359,7 +359,7 @@ export function getPlaceName(place: Place): string {
   return place.quay ? getQuayName(place.quay) ?? fallback : fallback;
 }
 export function mapLegToTimeValues(leg: Leg) {
-  const legIsMissingRealTime = !leg.realtime && leg.mode !== 'foot';
+  const legIsMissingRealTime = !leg.realtime;
   return {
     startTimes: {
       expectedTime: leg.expectedStartTime,
