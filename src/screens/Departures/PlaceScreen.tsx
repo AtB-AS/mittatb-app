424b4540f1d4f0ec4c8efa31544cf91be1338281
ingvisim <85479566+ingvisim@users.noreply.github.com>
2023-01-06 15:28:11 +0100

    Update src/screens/Departures/PlaceScreen.tsx
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- src/screens/Departures/PlaceScreen.tsx
+++ src/screens/Departures/PlaceScreen.tsx
@@ -177,6 +177,5 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
   quayData: {flex: 1},
   messageBox: {
     margin: theme.spacings.medium,
-    padding: theme.spacings.medium,
   },
 }));
