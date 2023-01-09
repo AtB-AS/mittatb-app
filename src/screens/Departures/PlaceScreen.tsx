3805f4894382aa51e26cf5258f040cc2dac6cdc4
ingvisim <ingvild.simonsen@atb.no>
2023-01-09 10:35:15 +0100

    fix: replace code

--- src/screens/Departures/PlaceScreen.tsx
+++ src/screens/Departures/PlaceScreen.tsx
@@ -50,6 +50,10 @@ export default function PlaceScreen({
     place.quays === undefined ? [place.id] : undefined,
   );
 
+  if (state.data && place.quays === undefined) {
+    place = state.data.stopPlaces[0];
+  }
+
   const navigateToDetails = (
     serviceJourneyId: string,
     serviceDate: string,
