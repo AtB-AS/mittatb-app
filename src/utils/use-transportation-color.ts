2d3f26fca6a0f99b01ca6c122f2b6cae8423eab1
Kristian Selnæs <kristian@selnaes.net>
2023-01-25 15:34:23 +0100

    Cleanup unused code

--- src/utils/use-transportation-color.ts
+++ src/utils/use-transportation-color.ts
@@ -23,12 +23,8 @@ export const useThemeColorForTransportMode = (
         return 'transport_city';
       }
       return 'transport_region';
-    case 'car':
-      return 'transport_other'; // TODO: Change when car is added to design system
     case 'rail':
       return 'transport_train';
-    case 'scooter':
-      return 'transport_other'; // TODO: Change when car is added to design system
     case 'tram':
       return 'transport_city';
     case 'water':
