59e446aa980839efe543a7000aff8fc5228f5ae6
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-01-27 13:17:18 +0100

    fix: no force unwrap

--- ios/departureWidget/Views/ChipView.swift
+++ ios/departureWidget/Views/ChipView.swift
@@ -4,14 +4,16 @@ struct ChipView: View {
     let departure: DepartureLinkLabel
 
     var body: some View {
-        Link(destination: URL(string: departure.link)!) {
-            Text(departure.label)
-                .lineLimit(1)
-                .fixedSize()
-                .padding(8)
-                .background(Color("TimeTileBackgroundColor"))
-                .cornerRadius(8)
-                .font(DefaultFonts.bold)
+        if let url = URL(string: departure.link) {
+            Link(destination: url) {
+                Text(departure.label)
+                    .lineLimit(1)
+                    .fixedSize()
+                    .padding(8)
+                    .background(Color("TimeTileBackgroundColor"))
+                    .cornerRadius(8)
+                    .font(DefaultFonts.bold)
+            }
         }
     }
 }
