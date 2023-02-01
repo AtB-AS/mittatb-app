39f5ea682edccf4cde86cb0b58d661f11f48283e
Tore Stensaker Tefre <tore@tefre.it>
2023-02-01 15:49:22 +0100

    remove local dev reversed ids before adding GH value

--- scripts/ios/override-config-files.sh
+++ scripts/ios/override-config-files.sh
@@ -18,6 +18,9 @@ echo "Adding Bugsnag API key and release stage to Info.plist"
 /usr/libexec/PlistBuddy -c "Add :BugsnagAPIKey string $BUGSNAG_API_KEY" ./ios/atb/Info.plist
 /usr/libexec/PlistBuddy -c "Add :BugsnagReleaseStage string $BUGSNAG_RELEASE_STAGE" ./ios/atb/Info.plist
 
+echo "Removing local dev reCAPTCHA related reversed client ids"
+/usr/libexec/PlistBuddy -c "Delete :CFBundleURLTypes:1" ./ios/atb/Info.plist
+
 echo "Adding reversed client id as custom URL scheme to enable reCAPTCHA"
 REVERSED_CLIENT_ID=`/usr/libexec/PlistBuddy -c "Print :REVERSED_CLIENT_ID" ./ios/atb/GoogleService-Info.plist`
 /usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:1:CFBundleURLSchemes:0 $REVERSED_CLIENT_ID" ./ios/atb/Info.plist
