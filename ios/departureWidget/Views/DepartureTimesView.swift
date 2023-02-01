2cb933ef2530158bc7e6342af9f050c63a60452d
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-02-01 15:04:44 +0100

    fix: Added "..." to the fade and found a better solution to how the fade is created.  (#3254)
    Updated fade and removed small widget size

--- ios/departureWidget/Views/DepartureTimesView.swift
+++ ios/departureWidget/Views/DepartureTimesView.swift
@@ -1,14 +1,42 @@
 import Foundation
 import SwiftUI
 
+private enum K {
+    static let padding: CGFloat = 16.0
+    static let lineInformationColor = Color("LineInformationTextColor")
+    static let backgroundColor = Color("WidgetBackgroundColor")
+    static let widgetGradient = LinearGradient(gradient: Gradient(stops: [
+        .init(color: backgroundColor, location: 0.95),
+        .init(color: .clear, location: 1),
+    ]), startPoint: .leading, endPoint: .trailing)
+    static let iconWidth: CGFloat = 20
+}
+
 struct DepartureTimesView: View {
     var departures: [DepartureLinkLabel]
+    let parentSize: CGSize
+
+    var width: CGFloat {
+        parentSize.width - K.padding * 2
+    }
+
+    var departuresWidth: CGFloat {
+        width - K.iconWidth - K.padding
+    }
 
     var body: some View {
         HStack {
-            ForEach(departures, id: \.self) {
-                ChipView(departure: $0)
-            }
-        }.clipShape(Rectangle())
+            HStack {
+                ForEach(departures, id: \.self) {
+                    ChipView(departure: $0)
+                }
+            }.frame(maxWidth: departuresWidth, alignment: .leading).clipped()
+                .mask(K.widgetGradient)
+
+            Image(systemName: "ellipsis")
+                .frame(width: K.iconWidth)
+                .padding(8)
+                .foregroundColor(K.lineInformationColor)
+        }.frame(maxWidth: width, alignment: .leading)
     }
 }
