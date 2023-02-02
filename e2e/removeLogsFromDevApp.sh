2c291f4958231dae895bc06fea60994f62185956
Tor K Moseng <tor@moseng.no>
2023-02-02 11:27:16 +0100

    different approach 2

--- e2e/removeLogsFromDevApp.sh
+++ e2e/removeLogsFromDevApp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # copy the index.js wich removes the logs
-echo "Copy inex.js from e2e/index_noLogs"
-cp e2e/index_noLogs/index.js index.js
+echo "Copy index.js from e2e/index_noLogs"
+# cp e2e/index_noLogs/index.js index.js
+echo "import {LogBox} from 'react-native';" >> index.js
+echo "LogBox.ignoreAllLogs(true);" >> index.js
