a5e7a75d8b0d30a2558ef3caaf261304c32ff3d4
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-02-10 22:27:54 +0100

    fix: use new colors for zone selection (#3292)

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Purchase/TariffZones/index.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Purchase/TariffZones/index.tsx
@@ -457,10 +457,13 @@ const TariffZones: React.FC<TariffZonesProps> = ({
                     // Mapbox Expression syntax
                     'case',
                     ['==', selectedZones.from.id, ['id']],
-                    hexToRgba(theme.static.status.valid.background, 0.6),
+                    hexToRgba(theme.static.zone_selection.from.background, 0.6),
                     ['==', selectedZones.to.id, ['id']],
                     !isApplicableOnSingleZoneOnly
-                      ? hexToRgba(theme.static.status.info.background, 0.6)
+                      ? hexToRgba(
+                          theme.static.zone_selection.to.background,
+                          0.6,
+                        )
                       : 'transparent',
                     'transparent',
                   ],
