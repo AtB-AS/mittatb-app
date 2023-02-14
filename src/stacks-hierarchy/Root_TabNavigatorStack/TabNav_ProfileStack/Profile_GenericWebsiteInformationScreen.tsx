523b092b93d904ed8d6f3a985505dc876090fb35
Tore Stensaker Tefre <tore@tefre.it>
2023-02-14 14:44:57 +0100

    nitpick: rearrange imports

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_GenericWebsiteInformationScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_GenericWebsiteInformationScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {useTranslation} from '@atb/translations';
-import {ThemeText} from '@atb/components/text';
-import GenericWebsiteInformationScreen from '@atb/translations/screens/subscreens/GenericWebsiteInformationScreen';
 import {View} from 'react-native';
+import {ThemeText} from '@atb/components/text';
 import {FullScreenHeader} from '@atb/components/screen-header';
 import {Section} from '@atb/components/sections';
+import {useTranslation} from '@atb/translations';
+import GenericWebsiteInformationScreen from '@atb/translations/screens/subscreens/GenericWebsiteInformationScreen';
 
 export const Profile_GenericWebsiteInformationScreen = () => {
   const {t} = useTranslation();
