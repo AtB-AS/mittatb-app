735cd8565d83c1598365a895324fc792428ad027
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-02-17 15:36:29 +0100

    fix: widget options and fix in location battery drain (#3309)
    * better code for fade and added ellipsis icon
    
    * removed the small widget
    
    * Update DepartureTimesView.swift
    
    * refactor
    
    * Started on adding configuration to widget
    
    * Working widget options:)
    
    * Added new name to the widget
    
    Making it more clear what the widget is about
    
    * Added sections to intent picker
    
    * Changed string
    
    * Sorting sections alphabetically
    
    * Update Localizable.strings
    
    * Added so that all variations of line is shown
    
    * Merge remote-tracking branch 'origin/master' into adriawh/widget-options
    
    # Conflicts:
    #	ios/departureWidget/DepartureWidget.swift
    
    * Revert "Merge remote-tracking branch 'origin/master' into adriawh/widget-options"
    
    This reverts commit ba79825ce80e9badac6fae4aad4f44d80aa0b7ec.
    
    * Update project.pbxproj
    
    * Less location updates and hopefully less battery drain
    
    * Made "..." clickable
    
    * Update Provider.swift
    
    * add team id and id to the intent
    
    * prepare fastlane for sign the app intent
    
    * swift lint
    
    * Update project.pbxproj
    
    * Remove intent if widget is disabled
    
    * enable widget
    
    * nfk intent env var
    
    * Add groups to the intent and remove unused group from the widget
    
    * Update LocationChangeManager.swift
    
    * Update LocationChangeManager.swift
    
    * Update Fastfile
    
    ---------
    
    Co-authored-by: Jorge Osorio <jorelosorio@gmail.com>

--- ios/departureWidget/Provider.swift
+++ ios/departureWidget/Provider.swift
@@ -3,31 +3,38 @@ import Foundation
 import SwiftUI
 import WidgetKit
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
     private enum K {
-        static var previewEntry: Entry { Entry(date: Date.now, favouriteDeparture: FavouriteDeparture.dummy, stopPlaceGroup: StopPlaceGroup.dummy, state: .preview) }
-        static let noFavouritesTimeline = Timeline<Entry>(entries: [Entry(date: Date.now, favouriteDeparture: nil, stopPlaceGroup: nil, state: .noFavouriteDepartures)], policy: .never)
+        static var previewEntry: DepartureWidgetEntry { Entry(date: Date.now, favouriteDeparture: FavouriteDeparture.dummy, stopPlaceGroup: StopPlaceGroup.dummy, departures: nil, state: .preview) }
+        static let noFavouritesTimeline = Timeline<Entry>(entries: [Entry(date: Date.now, favouriteDeparture: nil, stopPlaceGroup: nil, departures: nil, state: .noFavouriteDepartures)], policy: .never)
     }
 
     private let apiService = APIService()
     private let locationManager = LocationChangeManager()
 
     /// The placeholder is shown in transitions and while loading the snapshot when adding the widget to the home screen
-    func placeholder(in _: Context) -> Entry {
+    func placeholder(in _: Context) -> DepartureWidgetEntry {
         K.previewEntry
     }
 
     /// The snapshot shows is used to preview the widget when adding it to the home screen
-    func getSnapshot(in _: Context, completion: @escaping (Entry) -> Void) {
+    func getSnapshot(for _: UseLocationIntent, in _: Context, completion: @escaping (DepartureWidgetEntry) -> Void) {
         completion(K.previewEntry)
     }
 
-    func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+    func getTimeline(for intent: UseLocationIntent, in _: Context, completion: @escaping (Timeline<DepartureWidgetEntry>) -> Void) {
         // No favorite departures
         guard let favoriteDepartures = Manifest.data?.favouriteDepartures,
-              let firstFavoriteDeparture = favoriteDepartures.first
-        else {
+              let firstFavoriteDeparture = favoriteDepartures.first,
+              let intentDeparture = intent.FavoriteDeparture else {
             return completion(K.noFavouritesTimeline)
+        }
+
+        if intentDeparture.identifier != "showClosest" {
+            guard let chosenFavoriteDeparture = favoriteDepartures.first(where: { $0.id == intentDeparture.identifier }) else {
+                return completion(K.noFavouritesTimeline)
+            }
+            fetchFavouriteDepartureTimes(favouriteDeparture: chosenFavoriteDeparture, completion: completion)
         }
 
         apiService.fetchQuayCoordinates(favouriteDepartures: favoriteDepartures) { (result: Result<QuaysCoordinatesResponse, Error>) in
@@ -51,20 +58,36 @@ struct Provider: TimelineProvider {
 
     /// Fetch departures for a given quay
     private func fetchFavouriteDepartureTimes(favouriteDeparture departure: FavouriteDeparture, completion: @escaping (Timeline<Entry>) -> Void) {
+        let noDeparturesTimeline = Timeline<Entry>(entries: [Entry(date: Date.now, favouriteDeparture: departure, stopPlaceGroup: nil, departures: nil, state: .noDepartureQuays)], policy: .after(Date.now.addingTimeInterval(5 * 60)))
         // Fetch departure data for the closest favorite
         apiService.fetchFavouriteDepartureTimes(favouriteDeparture: departure) { (result: Result<StopPlaceGroup, Error>) in
             switch result {
             case let .success(stopPlaceGroup):
 
-                guard let firstQuayGroup = stopPlaceGroup.quays.first(where: { $0.quay.id == departure.quayId })?.group.first else {
-                    return completion(Timeline<Entry>(entries: [Entry(date: Date.now, favouriteDeparture: departure, stopPlaceGroup: nil, state: .noDepartureQuays)], policy: .after(Date.now.addingTimeInterval(5 * 60))))
+                guard let quayGroup = stopPlaceGroup.quays.first(where: { $0.quay.id == departure.quayId }) else {
+                    return completion(noDeparturesTimeline)
                 }
+                var departures: [DepartureTime] = []
+
+                if departure.lineName == nil {
+                    quayGroup.group.forEach { line in
+                        departures.append(contentsOf: line.departures)
+                    }
+                } else {
+                    guard let lineDepartures = quayGroup.group.first(where: { $0.lineInfo.lineName == departure.lineName })?.departures else {
+                        return completion(noDeparturesTimeline)
+                    }
+                    departures = lineDepartures
+                }
+
+                departures = departures.sorted(by: { $0.aimedTime.compare($1.aimedTime) == .orderedAscending })
 
                 // Rerenders widget when a departure has passed, by giving IOS more information about future
                 // dates we hopefully get better timed rerenders
-                var entries = firstQuayGroup.departures.map { departureTime in Entry(date: departureTime.aimedTime, favouriteDeparture: departure, stopPlaceGroup: stopPlaceGroup, state: .complete) }
+                // Rerenders widget 1 minute after departure have passed
+                var entries = departures.map { departureTime in Entry(date: departureTime.aimedTime.addingTimeInterval(60), favouriteDeparture: departure, stopPlaceGroup: stopPlaceGroup, departures: departures, state: .complete) }
 
-                entries.insert(Entry(date: Date.now, favouriteDeparture: departure, stopPlaceGroup: stopPlaceGroup, state: .complete), at: 0)
+                entries.insert(Entry(date: Date.now, favouriteDeparture: departure, stopPlaceGroup: stopPlaceGroup, departures: departures, state: .complete), at: 0)
 
                 // Remove the last entries so the view model has enough quays to show.
                 if entries.count > 10 {
@@ -79,7 +102,7 @@ struct Provider: TimelineProvider {
                 return completion(Timeline(entries: entries, policy: .after(nextMidnight)))
 
             case .failure:
-                return completion(Timeline(entries: [Entry(date: Date.now, favouriteDeparture: departure, stopPlaceGroup: nil, state: .noDepartureQuays)], policy: .after(Date.now.addingTimeInterval(5 * 60))))
+                return completion(noDeparturesTimeline)
             }
         }
     }
