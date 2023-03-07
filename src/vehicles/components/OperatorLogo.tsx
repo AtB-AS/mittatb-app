857f80713cbc471ca0decf979052ab8f46c9d0b1
Kristian Selnæs <kristian@selnaes.net>
2023-03-07 15:45:41 +0100

    Remote config to disable vehicle operator logos

--- src/vehicles/components/OperatorLogo.tsx
+++ src/vehicles/components/OperatorLogo.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import {Image, View} from 'react-native';
 import {ThemeText} from '@atb/components/text';
+import {useRemoteConfig} from '@atb/RemoteConfigContext';
 
 type OperatorLogoProps = {
   operatorName: string;
   logoUrl: string | undefined;
 };
-export const OperatorLogo = ({operatorName, logoUrl}: OperatorLogoProps) => (
-  <View style={{flex: 1, alignItems: 'center'}}>
-    {logoUrl ? (
-      <Image
-        source={{uri: logoUrl}}
-        style={{height: 50, width: 120}}
-        resizeMode="contain"
-      />
-    ) : (
-      <ThemeText type={'body__primary--big--bold'}>{operatorName}</ThemeText>
-    )}
-  </View>
-);
+export const OperatorLogo = ({operatorName, logoUrl}: OperatorLogoProps) => {
+  const {enable_vehicle_operator_logo} = useRemoteConfig();
+  return (
+    <View style={{flex: 1, alignItems: 'center'}}>
+      {logoUrl && enable_vehicle_operator_logo ? (
+        <Image
+          source={{uri: logoUrl}}
+          style={{height: 50, width: 120}}
+          resizeMode="contain"
+        />
+      ) : (
+        <ThemeText type={'body__primary--big--bold'}>{operatorName}</ThemeText>
+      )}
+    </View>
+  );
+};
