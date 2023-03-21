0786c8ca7dbabcfde5ffceeac9fd75d448193c0a
Kristian Selnæs <kristian@selnaes.net>
2023-03-21 08:38:06 +0100

    Handle operator logos being either svg or raster

--- src/vehicles/components/OperatorLogo.tsx
+++ src/vehicles/components/OperatorLogo.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Image, View} from 'react-native';
 import {ThemeText} from '@atb/components/text';
 import {useRemoteConfig} from '@atb/RemoteConfigContext';
+import {SvgUri} from 'react-native-svg';
+
+const LOGO_MAX_HEIGHT = 50;
+const LOGO_MAX_WIDTH = 120;
 
 type OperatorLogoProps = {
   operatorName: string;
@@ -9,14 +13,24 @@ type OperatorLogoProps = {
 };
 export const OperatorLogo = ({operatorName, logoUrl}: OperatorLogoProps) => {
   const {enable_vehicle_operator_logo} = useRemoteConfig();
+  const isSvg = (url: string) => url.endsWith('.svg');
+
   return (
     <View style={{flex: 1, alignItems: 'center'}}>
       {logoUrl && enable_vehicle_operator_logo ? (
-        <Image
-          source={{uri: logoUrl}}
-          style={{height: 50, width: 120}}
-          resizeMode="contain"
-        />
+        isSvg(logoUrl) ? (
+          <SvgUri
+            height={LOGO_MAX_HEIGHT}
+            width={LOGO_MAX_WIDTH}
+            uri={logoUrl}
+          />
+        ) : (
+          <Image
+            source={{uri: logoUrl}}
+            style={{height: LOGO_MAX_HEIGHT, width: LOGO_MAX_WIDTH}}
+            resizeMode="contain"
+          />
+        )
       ) : (
         <ThemeText type={'body__primary--big--bold'}>{operatorName}</ThemeText>
       )}
