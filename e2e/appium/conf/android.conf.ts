eb61f7957f99d31ced494e7a5c62391d80e39c6e
Tor K Moseng <tor@moseng.no>
2023-03-23 15:54:39 +0100

    clean up

--- e2e/appium/conf/android.conf.ts
+++ e2e/appium/conf/android.conf.ts
@@ -3,7 +3,6 @@ import { config as sharedConfig } from './wdio.conf';
 export const config: WebdriverIO.Config = {
   ...sharedConfig,
   ...{
-    //runnerEnv: { ['APP_PATH']: process.env.APP_PATH || "/dummyPath/" },
     port: 4723,
     //path: '/wd/hub',
     services: [
@@ -35,7 +34,6 @@ export const config: WebdriverIO.Config = {
       //platformVersion: AndroidInfo.platFormVersion(),
       //app: path.resolve(`./apps/${AndroidInfo.appName()}`)
       //"appium:waitForIdleTimeout": 10000,
-      //app: '/Users/tormos/Documents/atb/mittatb-app/e2e/appium/android/app-staging.apk',
 
       'appium:isHeadless': true,
       //'appium:gpsEnabled': false,
@@ -44,15 +42,11 @@ export const config: WebdriverIO.Config = {
       'appium:automationName': 'UiAutomator2',
       //'appium:deviceName': 'emulator-5554',//
       'appium:deviceName': 'Android',
-      //'appium:appPackage': 'no.mittatb.staging',
-      //'appium:appWaitPackage': 'no.mittatb.staging',
       'appium:appPackage': 'no.mittatb.staging',
       'appium:appWaitPackage': 'no.mittatb.staging',
       'appium:appActivity': 'no.mittatb.MainActivity',
       'appium:appWaitActivity': 'no.mittatb.MainActivity',
-      //'appium:app': '../../android/app/build/outputs/apk/debug/app-debug.apk',
-      //'appium:app': '/Users/tormos/Documents/atb/mittatb-app/e2e/appium/app/app-staging.apk',
-      'appium:app': `${process.env.APP_PATH || "EMPTY_ENV_APP_PATH"}/android/app/build/outputs/apk/staging/app-staging.apk`,
+      'appium:app': `${process.env.APP_PATH || "EMPTY_ENV_APP_PATH"}/e2e/appium/apk/app-staging.apk`,
       //'appium:appWaitForLaunch': true,
       //'appium:skipDeviceInitialization': true
     }],
