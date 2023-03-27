6f761addad935907a0046b6db7fa4d8306372cd8
andersheftoycarlsen <anders.heftoy@gmail.com>
2023-03-27 13:51:36 +0200

    Added: Context for ticket assistant questionnaire

--- src/stacks-hierarchy/Root_TicketAssistantStack/Root_TicketAssistantStack.tsx
+++ src/stacks-hierarchy/Root_TicketAssistantStack/Root_TicketAssistantStack.tsx
@@ -21,7 +21,7 @@ import {TicketAssistant_ZonePickerScreen} from './TicketAssistant_ZonePickerScre
 import {TicketAssistant_SummaryScreen} from './TicketAssistant_SummaryScreen';
 
 import {FullScreenHeader} from '@atb/components/screen-header';
-import {RootStackScreenProps} from '@atb/stacks-hierarchy';
+import {TicketAssistantProvider} from './TicketAssistantContext';
 
 const Tab = createMaterialTopTabNavigator<TicketAssistantStackParams>();
 const themeColor: StaticColorByType<'background'> = 'background_accent_0';
@@ -31,7 +31,7 @@ export const Root_TicketAssistantStack = ({navigation}: WelcomeScreenProps) => {
   const [activeTab, setActiveTab] = useState(0);
   const [previousTab, setPreviousTab] = useState<any>();
   return (
-    <>
+    <TicketAssistantProvider>
       {activeTab !== 0 ? (
         <FullScreenHeader
           leftButton={{
@@ -82,7 +82,7 @@ export const Root_TicketAssistantStack = ({navigation}: WelcomeScreenProps) => {
           />
         </Tab.Navigator>
       </SafeAreaView>
-    </>
+    </TicketAssistantProvider>
   );
 };
 
