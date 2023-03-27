d95f5e24fdd73f9d832482113595129a765e8b47
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-03-27 13:35:14 +0200

    Finished category picker

--- src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_CategoryPickerScreen.tsx
+++ src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_CategoryPickerScreen.tsx
@@ -1,85 +1,94 @@
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {StyleSheet} from '@atb/theme';
 import * as Sections from '@atb/components/sections';
 import {TicketAssistantTexts, useTranslation} from '@atb/translations';
 import {ThemeText} from '@atb/components/text';
 import React from 'react';
 import {Button} from '@atb/components/button';
+import {themeColor} from '@atb/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_WelcomeScreen';
+import {DashboardBackground} from '@atb/assets/svg/color/images';
 
 export const TicketAssistant_CategoryPickerScreen = () => {
   const styles = useThemeStyles();
   const {t} = useTranslation();
 
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <View>
-        <Text>TODO</Text>
-        <View style={styles.innerContainer}>
-          <Sections.Section style={styles.tipsContainer}>
-            {/*eslint-disable-next-line rulesdir/translations-warning*/}
-            {TicketAssistantTexts.categoryPicker.categories.map(
-              ({title, description}, index) => (
-                <Sections.ExpandableSectionItem
-                  key={index}
-                  textType={'body__primary--bold'}
-                  text={t(title)}
-                  showIconText={false}
-                  expandContent={
-                    <View>
-                      <ThemeText
-                        type={'body__tertiary'}
-                        style={styles.expandedContent}
-                        isMarkdown={true}
-                      >
-                        {t(description)}
-                      </ThemeText>
-                      <Button
-                        style={styles.goToAssistantButton}
-                        onPress={() => {}}
-                        text={t(
-                          TicketAssistantTexts.categoryPicker.chooseButton,
-                        )}
-                      />
-                    </View>
-                  }
-                />
-              ),
-            )}
-          </Sections.Section>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.backdrop}>
+        <DashboardBackground width={'100%'} height={'100%'} />
       </View>
-    </ScrollView>
+      <ScrollView>
+        <ThemeText
+          type={'body__primary--jumbo--bold'}
+          style={styles.header}
+          color={themeColor}
+          accessibilityLabel={t(TicketAssistantTexts.welcome.titleA11yLabel)}
+        >
+          {t(TicketAssistantTexts.categoryPicker.title)}
+        </ThemeText>
+
+        <Sections.Section style={styles.categoriesContainer}>
+          {/*eslint-disable-next-line rulesdir/translations-warning*/}
+          {TicketAssistantTexts.categoryPicker.categories.map(
+            ({title, description}, index) => (
+              <Sections.ExpandableSectionItem
+                key={index}
+                textType={'body__primary--bold'}
+                text={t(title)}
+                showIconText={false}
+                expandContent={
+                  <View>
+                    <ThemeText
+                      type={'body__tertiary'}
+                      style={styles.expandedContent}
+                      isMarkdown={true}
+                    >
+                      {t(description)}
+                    </ThemeText>
+                    <Button
+                      style={styles.chooseButton}
+                      onPress={() => {}}
+                      text={t(TicketAssistantTexts.categoryPicker.chooseButton)}
+                    />
+                  </View>
+                }
+              />
+            ),
+          )}
+        </Sections.Section>
+      </ScrollView>
+    </View>
   );
 };
 const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
+  backdrop: {
+    position: 'absolute',
+    height: 250,
+    left: 0,
+    right: 0,
+    bottom: 250,
+    padding: 0,
+    margin: 0,
+  },
+  header: {
+    textAlign: 'center',
+  },
   container: {
-    alignContent: 'center',
+    height: '100%',
     flex: 1,
+    //place content at top
     backgroundColor: theme.static.background.background_accent_0.background,
   },
-  goToAssistantButton: {
+  chooseButton: {
     marginTop: theme.spacings.medium,
   },
   expandedContent: {
     color: theme.text.colors.secondary,
   },
-  tipsContainer: {
+  categoriesContainer: {
+    marginTop: theme.spacings.xLarge,
     borderRadius: theme.border.radius.regular,
     backgroundColor: theme.static.background.background_1.background,
-  },
-  contentContainer: {
-    marginTop: theme.spacings.small,
-    backgroundColor: theme.static.background.background_accent_0.background,
-  },
-  title: {
-    width: '100%',
-    textAlign: 'center',
-    marginVertical: theme.spacings.xLarge,
-  },
-  innerContainer: {
-    paddingHorizontal: theme.spacings.xLarge,
+    marginHorizontal: theme.spacings.xLarge,
   },
 }));
