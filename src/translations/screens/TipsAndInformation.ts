2fcd057548eddf4c86d47f3e90d747c1e2daf554
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-03-27 10:58:20 +0200

    Update TipsAndInformation.ts

--- src/translations/screens/TipsAndInformation.ts
+++ src/translations/screens/TipsAndInformation.ts
@@ -12,21 +12,21 @@ const TipsAndInformationTexts = {
       title: _('‍🚲 Reise med sykkel', '‍🚲 Traveling with bike'),
       tip: _(
         'For å reise med sykkel på bussen må du kjøpe en barnebillett. På hurtigbåt er sykkel gratis.',
-        'To bring your bike on the bus, you must buy a Child ticket. Bikes are free to bring on speedboats',
+        'To travel with a bike on the bus, you must buy a child ticket. Bikes are free on the express boat.',
       ),
     },
     {
       title: _('👩‍👧‍👦 Reise med barn', '👩‍👧‍👦 Traveling with kids'),
       tip: _(
         'Barn over 6 år må ha barnebillett, men i helger kan barn reise gratis med en voksen. En voksen kan ha med seg opp til 4 barn gratis.',
-        'Children older thatn 6 years must have a Child ticket. In the weekends a parent can bring up to 4 kids for free.',
+        'Children over 6 years old must have a child ticket, but on weekends, children can travel for free with an adult. One adult can bring up to 4 children for free.',
       ),
     },
     {
       title: _('🌖 Nattbuss', '🌖 Night bus'),
       tip: _(
-        'For å reise på nattbuss må du enten ha nattbussbillett eller en periodebillett for student. Vanlig enkeltbillett er ikke gyldig på nattbussen',
-        'To travel on the night bus, either a night ticket or a periodic ticket for students are required. Regular single ticket is not valid.  ',
+        'For å reise på nattbuss må du enten ha nattbussbillett eller en periodebillett for student. Vanlig enkeltbillett er ikke gyldig på nattbussen.',
+        'To travel on the night bus, you must have either a night bus ticket or a student period ticket. A regular single ticket is not valid on the night bus.',
       ),
     },
     {
@@ -36,7 +36,7 @@ const TipsAndInformationTexts = {
       ),
       tip: _(
         'Enkeltbiletter er vanligvis gyldige i 1 time og 30 minutter. I helger, på helligdager og etter kl 18 på hverdager er den gyldig i 3 timer.',
-        'Single tickets are usually valid for 1 hour and 30 minutes. However, in the weekends and after 6 pm on weekdays they are valid for 3 hours',
+        'Single tickets are usually valid for 1 hour and 30 minutes. On weekends, holidays, and after 6pm on weekdays, they are valid for 3 hours.',
       ),
     },
   ],
@@ -46,11 +46,11 @@ const TipsAndInformationTexts = {
   ticketAssistantTip: {
     title: _(
       '💰 Hvilken billett lønner seg?',
-      '💰 How long is a single ticket valid?',
+      '💰 Which ticket is the best value?',
     ),
     tip: _(
       'For å finne ut hvilken billett som lønner seg kan du ta veilederen!',
-      'To find your best ticket, you can use the ticket assistant.',
+      'To find out which ticket is the best value, you can use the assistant!',
     ),
   },
 };
