2f376a676b04339e6de51f3d2d8f5d1839d9ae37
Tor K Moseng <tor@moseng.no>
2023-03-29 13:40:21 +0200

    e2e departure tests

---
+++ e2e/appium/test/pageobjects/departure.overview.page.ts
@@ -0,0 +1,103 @@
+import Page from './page';
+import AppHelper from '../utils/app.helper';
+import ElementHelper from '../utils/element.helper';
+
+class DepartureOverviewPage extends Page {
+  /**
+   * The 'all stops' button
+   */
+  async showAllQuays() {
+    const buttonId = `//*[@resource-id="allStopsSelectionButton"]`;
+    await $(buttonId).click();
+    await ElementHelper.waitForElement('id', 'departureItem0');
+  }
+
+  /**
+   * The single quay button
+   * @param quayIndex: index for the quay button
+   */
+  async showQuay(quayIndex: number = 0) {
+    const buttonId = `//*[@resource-id="quaySelectionButton"]`;
+    await $$(buttonId)[quayIndex].click();
+    await AppHelper.pause(500);
+    await ElementHelper.waitForElement('id', 'quaySection');
+    await ElementHelper.waitForElement('id', 'departureItem0');
+  }
+
+  /**
+   * Get the given departure for the given quay
+   * @param quayIndex: index for the quay, default: first
+   * @param departureIndex: index for the departure, default: first
+   */
+  async getDeparture(quayIndex: number = 0, departureIndex: number = 0) {
+    const quayId = `//*[@resource-id="quaySection${quayIndex}"]`;
+    const depId = `//*[@resource-id="departureItem${departureIndex}"]`;
+    return $(quayId).$(depId);
+  }
+
+  /**
+   * Hide or expand (depending on the state) departures section for a given quay
+   * @param quayIndex: index for the quay, default: first
+   */
+  async hideExpandDeps(quayIndex: number = 0) {
+    const hideId = `//*[@resource-id="quaySection${quayIndex}HideAction"]`;
+    await $(hideId).click();
+    await AppHelper.pause(1000);
+  }
+
+  /**
+   * Return the public code
+   * @param quayIndex: index of the quay
+   * @param depIndex: index of the departure
+   */
+  async getLinePublicCode(quayIndex: number = 0, depIndex: number = 0) {
+    const quayId = `//*[@resource-id="quaySection${quayIndex}"]`;
+    const publicCodeId = `//*[@resource-id="departureItem${depIndex}PublicCode"]`;
+    return $(quayId).$(publicCodeId).getText();
+  }
+
+  /**
+   * Return the line name
+   * @param quayIndex: index of the quay
+   * @param depIndex: index of the departure
+   */
+  async getLineName(quayIndex: number = 0, depIndex: number = 0) {
+    const quayId = `//*[@resource-id="quaySection${quayIndex}"]`;
+    const lineNameId = `//*[@resource-id="departureItem${depIndex}Name"]`;
+    return $(quayId).$(lineNameId).getText();
+  }
+
+  /**
+   * Get number of departures
+   * @param quayIndex: index of quay id overview or empty if quay-view
+   */
+  async getNumberOfDepartures(quayIndex: string = '') {
+    const quayId = `//*[@resource-id="quaySection${quayIndex}"]`;
+    let count = 0;
+    // check departure items with a max to avoid endless loop
+    // considers only visible element
+    while (count < 20) {
+      const depId = `//*[@resource-id="departureItem${count}"]`;
+      if (await $(quayId).$(depId).isExisting()) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Tap the 'show more departures' button for a quay
+   * @param: quayIndex: index for the quay
+   */
+  async showMoreDepartures(quayIndex: number = 0) {
+    const moreId = `//*[@resource-id="quaySection${quayIndex}More"]`;
+    await $(moreId).click();
+    await AppHelper.pause(500);
+    await ElementHelper.waitForElement('id', 'quaySection');
+    await ElementHelper.waitForElement('id', 'departureItem0');
+  }
+}
+
+export default new DepartureOverviewPage();
---
+++ e2e/appium/test/specs/departure.e2e.ts
@@ -0,0 +1,178 @@
+import OnboardingPage from '../pageobjects/onboarding.page';
+import AppHelper from '../utils/app.helper';
+import ElementHelper from '../utils/element.helper';
+import SearchPage from '../pageobjects/search.page';
+import NavigationHelper from '../utils/navigation.helper';
+import DepartureSearchPage from '../pageobjects/departure.search.page';
+import DepartureOverviewPage from '../pageobjects/departure.overview.page';
+import DepartureDetailsPage from '../pageobjects/departure.details.page';
+
+describe('Departure', () => {
+  before(async () => {
+    await AppHelper.launchApp();
+    await OnboardingPage.skipOnboarding();
+    await NavigationHelper.tapMenu('departures');
+    await DepartureSearchPage.confirmOnboarding();
+  });
+  beforeEach(async () => {
+    await NavigationHelper.tapMenu('departures');
+    await NavigationHelper.tapMenu('departures');
+  });
+
+  /**
+   * Search for a place and choose a stop place
+   */
+  it('should search for a stop place nearby', async () => {
+    const placeSearch = 'Emilies ELD';
+    const stopPlace = 'Prinsens gate';
+
+    try {
+      await ElementHelper.waitForElement('id', 'searchFromButton');
+      await DepartureSearchPage.searchFrom.click();
+      await SearchPage.setSearchLocation(placeSearch);
+
+      // Nearby stop places
+      await DepartureSearchPage.chooseStopPlace(stopPlace);
+
+      // Stop place
+      await ElementHelper.expectText(stopPlace);
+      const linePublicCode = await DepartureOverviewPage.getLinePublicCode();
+      const lineName = await DepartureOverviewPage.getLineName();
+      //Note: quaySelectionButton doesn't give the button text
+
+      // Hide / expand quay
+      expect(await DepartureOverviewPage.getDeparture()).toExist();
+      await DepartureOverviewPage.hideExpandDeps();
+      await AppHelper.screenshot('hide');
+      expect(await DepartureOverviewPage.getDeparture()).not.toExist();
+      await DepartureOverviewPage.hideExpandDeps();
+      expect(await DepartureOverviewPage.getDeparture()).toExist();
+
+      // Departure details
+      const departure = await DepartureOverviewPage.getDeparture();
+      await departure.click();
+      await ElementHelper.waitForElement('id', 'departureDetailsContentView');
+      await ElementHelper.expectText(`${linePublicCode} ${lineName}`);
+
+      // Back
+      await NavigationHelper.back();
+      await ElementHelper.expectText(stopPlace);
+    } catch (errMsg) {
+      await AppHelper.screenshot(
+        'error_departure_should_search_for_a_stop_place',
+      );
+      throw errMsg;
+    }
+  });
+
+  /**
+   * Search for a stop place directly and show its details
+   */
+  it('should search for and choose a stop place directly', async () => {
+    const stopPlace = 'Kongens gate';
+
+    try {
+      await ElementHelper.waitForElement('id', 'searchFromButton');
+      await DepartureSearchPage.searchFrom.click();
+      await SearchPage.setSearchLocation(stopPlace);
+
+      // Stop place
+      await ElementHelper.waitForElement('id', 'departuresContentView');
+      await ElementHelper.expectText(stopPlace);
+      expect(await DepartureOverviewPage.getDeparture()).toExist();
+    } catch (errMsg) {
+      await AppHelper.screenshot(
+        'error_departure_should_search_for_and_choose_a_stop_place_directly',
+      );
+      throw errMsg;
+    }
+  });
+
+  /**
+   * Check the show more departures and quay tabs
+   */
+  it('should show more departures for a single quay', async () => {
+    const stopPlace = 'Prinsens gate';
+
+    try {
+      await ElementHelper.waitForElement('id', 'searchFromButton');
+      await DepartureSearchPage.searchFrom.click();
+      await SearchPage.setSearchLocation(stopPlace);
+
+      // Stop place
+      await ElementHelper.waitForElement('id', 'departuresContentView');
+      await ElementHelper.expectText(stopPlace);
+      const noDepartures = await DepartureOverviewPage.getNumberOfDepartures(
+        '0',
+      );
+
+      // Show more departures (section link)
+      await DepartureOverviewPage.showMoreDepartures();
+      let noDeparturesQuay =
+        await DepartureOverviewPage.getNumberOfDepartures();
+      expect(noDepartures).toBeLessThan(noDeparturesQuay);
+
+      // Show stop place
+      await DepartureOverviewPage.showAllQuays();
+      expect(await DepartureOverviewPage.getNumberOfDepartures('0')).toEqual(
+        noDepartures,
+      );
+
+      // Show more departures (quay button)
+      await DepartureOverviewPage.showQuay();
+      expect(noDepartures).toBeLessThan(
+        await DepartureOverviewPage.getNumberOfDepartures(),
+      );
+    } catch (errMsg) {
+      await AppHelper.screenshot(
+        'error_departure_should_show_more_departures_for_a_single_quay',
+      );
+      throw errMsg;
+    }
+  });
+
+  /**
+   * Check intermediate stops on departure details
+   */
+  it('should show intermediate stops', async () => {
+    //NB! If same stop place is chosen after tapping 'departuresTab', you don't go directly
+    const stopPlace = 'Kongens gate';
+
+    try {
+      await ElementHelper.waitForElement('id', 'searchFromButton');
+      await DepartureSearchPage.searchFrom.click();
+      await SearchPage.setSearchLocation(stopPlace);
+
+      // Stop place
+      await ElementHelper.waitForElement('id', 'departuresContentView');
+      await ElementHelper.expectText(stopPlace);
+
+      // Departure details
+      const linePublicCode = await DepartureOverviewPage.getLinePublicCode();
+      const lineName = await DepartureOverviewPage.getLineName();
+      const departure = await DepartureOverviewPage.getDeparture();
+      await departure.click();
+      await ElementHelper.waitForElement('id', 'departureDetailsContentView');
+      await ElementHelper.expectText(`${linePublicCode} ${lineName}`);
+
+      // Show intermediate stops
+      const noPassed = await DepartureDetailsPage.passedLegs.length;
+      // Tap 'intermediate stops': Missing id
+      // expect(await DepartureDetailsPage.passedLegs.length).toBeGreaterThan(noPassed)
+      const quayName = await DepartureDetailsPage.getQuayName('passed', 0);
+
+      // Show another quay
+      await DepartureDetailsPage.tapQuay('passed', 0);
+      await ElementHelper.expectText(quayName);
+
+      await NavigationHelper.back();
+      await ElementHelper.waitForElement('id', 'departureDetailsContentView');
+      await ElementHelper.expectText(`${linePublicCode} ${lineName}`);
+    } catch (errMsg) {
+      await AppHelper.screenshot(
+        'error_departure_should_show_intermediate_stops',
+      );
+      throw errMsg;
+    }
+  });
+});
