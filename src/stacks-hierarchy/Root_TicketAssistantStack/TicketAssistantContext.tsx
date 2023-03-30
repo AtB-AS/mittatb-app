361378d2e373174877f1b6781a122ac490498192
andersheftoycarlsen <anders.heftoy@gmail.com>
2023-03-30 14:29:05 +0200

    Added: Connection to the backend and updated context to match the response and body required

--- src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistantContext.tsx
+++ src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistantContext.tsx
@@ -3,12 +3,27 @@ import {createContext, FunctionComponent, useState} from 'react';
 export interface TicketAssistantContextValue {
   data: TicketAssistantData;
   updateData: (newData: TicketAssistantData) => void;
+  response?: Response;
+  setResponse?: (response: any) => void;
 }
 
 export interface TicketAssistantData {
   frequency: number;
-  category: string;
   duration: number;
+  traveller: {id: string; user_type: string};
+  zones: string[];
+}
+
+export interface TicketAssistantResponse {
+  duration: number;
+  quantity: number;
+  price: number;
+  traveller: {id: string; user_type: string};
+}
+
+export interface Response {
+  totalCost: number;
+  tickets: TicketAssistantResponse[];
   zones: string[];
 }
 
@@ -18,16 +33,19 @@ const TicketAssistantContext =
 export const TicketAssistantProvider: FunctionComponent = ({children}) => {
   const [data, setData] = useState<TicketAssistantData>({
     frequency: 0,
-    category: '',
+    traveller: {id: 'ADULT', user_type: 'ADULT'},
     duration: 0,
-    zones: [],
+    zones: ['ATB:TariffZone:1', 'ATB:TariffZone:2'],
   });
+  const [response, setResponse] = useState<Response>();
 
   const updateData = (newData: TicketAssistantData) => {
     setData((prevState) => ({...prevState, ...newData}));
   };
   return (
-    <TicketAssistantContext.Provider value={{data, updateData}}>
+    <TicketAssistantContext.Provider
+      value={{data, updateData, response, setResponse}}
+    >
       {children}
     </TicketAssistantContext.Provider>
   );
