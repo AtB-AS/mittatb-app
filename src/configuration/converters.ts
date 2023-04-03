acdd06e0abfb96b96ff036779bbbc191746cc33a
Mikael Brevik <mikaelbre@gmail.com>
2023-04-03 10:40:16 +0200

    refactor: changes to use shared fare product type config package

--- src/configuration/converters.ts
+++ src/configuration/converters.ts
@@ -1,25 +1,17 @@
-import {
-  FareProductTypeConfig,
-  FareProductTypeConfigSettings,
-  ProductSelectionMode,
-  TimeSelectionMode,
-  TravellerSelectionMode,
-  ZoneSelectionMode,
-} from './types';
-import {LanguageAndTextType} from '@atb/translations/types';
-import Bugsnag from '@bugsnag/react-native';
-import {isArray} from 'lodash';
-import {TransportModeType} from '@atb/configuration/types';
-import type {
-  TransportIconModeType,
-  TransportModeFilterOptionType,
-} from '@atb/stacks-hierarchy/Root_TabNavigatorStack/TabNav_DashboardStack/types';
+import {FareProductTypeConfig} from '@atb-as/config-specs';
 import {
   TransportMode,
   TransportModes,
   TransportSubmode,
 } from '@atb/api/types/generated/journey_planner_v3_types';
+import type {
+  TransportIconModeType,
+  TransportModeFilterOptionType,
+} from '@atb/stacks-hierarchy/Root_TabNavigatorStack/TabNav_DashboardStack/types';
+import {LanguageAndTextType} from '@atb/translations/types';
 import {enumFromString} from '@atb/utils/enum-from-string';
+import Bugsnag from '@bugsnag/react-native';
+import {isArray} from 'lodash';
 
 export function mapToFareProductTypeConfigs(
   config: any,
@@ -37,180 +29,17 @@ export function mapToFareProductTypeConfigs(
 function mapToFareProductTypeConfig(
   config: any,
 ): FareProductTypeConfig | undefined {
-  const fields = [
-    'type',
-    'name',
-    'transportModes',
-    'description',
-    'configuration',
-  ];
-  if (!fields.every((f) => f in config)) {
-    Bugsnag.notify(
-      `fare product is missing one or more of the following fields: ${fields}`,
-    );
+  const typeConfigPotential = FareProductTypeConfig.safeParse(config);
+
+  if (!typeConfigPotential.success) {
+    Bugsnag.notify('fare product mapping issue', function (event) {
+      event.addMetadata('decode_errors', {
+        issues: typeConfigPotential.error.issues,
+      });
+    });
     return;
   }
-
-  const fcType = mapToString(config.type);
-  if (!fcType) {
-    return;
-  }
-
-  const illustration = mapToString(config.illustration);
-
-  if (!isArray(config.name) || !isArray(config.description)) {
-    Bugsnag.notify(
-      `fare product of type: "${fcType}", fields "name" or "description" should be defined as an "array".`,
-    );
-    return;
-  }
-
-  let name = mapLanguageAndTextType(config.name);
-  let description = mapLanguageAndTextType(config.description);
-
-  if (!name || !description) {
-    Bugsnag.notify(
-      `fare product of type: "${fcType}", "name" or "description" should conform: "LanguageAndTextType"`,
-    );
-    return;
-  }
-
-  if (!isArray(config.transportModes)) {
-    Bugsnag.notify(
-      `fare product of type: "${fcType}", "transportModes" should be of type "array"`,
-    );
-    return;
-  }
-
-  const transportModes = mapTransportModeTypes(config.transportModes);
-
-  if (!transportModes) {
-    Bugsnag.notify(
-      `fare product of type: "${fcType}", "transportModes" should conform: "TransportModeType"`,
-    );
-    return;
-  }
-
-  const configuration = mapToFareProductConfigSettings(
-    fcType,
-    config.configuration,
-  );
-  if (!configuration) return;
-
-  return {
-    type: fcType,
-    illustration,
-    name,
-    description,
-    transportModes,
-    configuration,
-  };
-}
-
-function mapToFareProductConfigSettings(
-  fareProductType: string,
-  settings: any,
-): FareProductTypeConfigSettings | undefined {
-  const zoneSelectionModeTypes = [
-    'single',
-    'single-zone',
-    'single-stop',
-    'multiple',
-    'multiple-zone',
-    'multiple-stop',
-    'none',
-  ];
-  const zoneSelectionMode = mapToStringAlternatives<ZoneSelectionMode>(
-    settings.zoneSelectionMode,
-    zoneSelectionModeTypes,
-  );
-  if (!zoneSelectionMode) {
-    notifyWrongConfigurationType(
-      fareProductType,
-      'zoneSelectionMode',
-      zoneSelectionModeTypes,
-    );
-    return;
-  }
-
-  const travellerSelectionModeTypes = ['multiple', 'single', 'none'];
-  const travellerSelectionMode =
-    mapToStringAlternatives<TravellerSelectionMode>(
-      settings.travellerSelectionMode,
-      travellerSelectionModeTypes,
-    );
-  if (!travellerSelectionMode) {
-    notifyWrongConfigurationType(
-      fareProductType,
-      'travellerSelectionMode',
-      travellerSelectionModeTypes,
-    );
-    return;
-  }
-
-  const timeSelectionModeTypes = ['datetime', 'none'];
-  const timeSelectionMode = mapToStringAlternatives<TimeSelectionMode>(
-    settings.timeSelectionMode,
-    timeSelectionModeTypes,
-  );
-  if (!timeSelectionMode) {
-    notifyWrongConfigurationType(
-      fareProductType,
-      'timeSelectionMode',
-      timeSelectionModeTypes,
-    );
-    return;
-  }
-
-  const productSelectionModeTypes = [
-    'duration',
-    'product',
-    'productAlias',
-    'none',
-  ];
-  const productSelectionMode = mapToStringAlternatives<ProductSelectionMode>(
-    settings.productSelectionMode,
-    productSelectionModeTypes,
-  );
-  if (!productSelectionMode) {
-    notifyWrongConfigurationType(
-      fareProductType,
-      'productSelectionMode',
-      productSelectionModeTypes,
-    );
-    return;
-  }
-
-  const productSelectionTitle = mapLanguageAndTextType(
-    settings.productSelectionTitle,
-  );
-
-  const requiresLogin = settings.requiresLogin;
-  if (requiresLogin === undefined) {
-    Bugsnag.notify(
-      `fare product of type: "${fareProductType}" is missing 'requiresLogin' in configuration`,
-    );
-    return;
-  }
-
-  return {
-    zoneSelectionMode,
-    travellerSelectionMode,
-    timeSelectionMode,
-    productSelectionMode,
-    productSelectionTitle,
-    requiresLogin,
-  };
-}
-
-function notifyWrongConfigurationType(
-  fareProductType: string,
-  field: string,
-  types: string[],
-) {
-  Bugsnag.notify(
-    `fare product of type: "${fareProductType}", the "${field}" field, does not belong to any of the following types: "${types}"`,
-  );
+  return typeConfigPotential.data;
 }
 
 export const mapToTransportModeFilterOptions = (
@@ -309,33 +138,10 @@ const mapToTransportModes = (raw: any): TransportModes | undefined => {
   };
 };
 
-const mapToString = (value: any) =>
-  typeof value == 'string' ? value : undefined;
-
-function mapToStringAlternatives<T>(value: any, alternatives: string[]) {
-  if (typeof value !== 'string') return;
-  if (!alternatives.includes(value)) return;
-  return value as T;
-}
-
 export function mapLanguageAndTextType(text?: any) {
   if (!text) return;
   if (!text.every((item: any) => ['lang', 'value'].every((f) => f in item)))
     return;
 
   return text as LanguageAndTextType[];
-}
-
-function mapTransportModeTypes(transportModes: any[]) {
-  if (
-    !transportModes.every(
-      (item: any) =>
-        typeof item === 'object' &&
-        'mode' in item &&
-        typeof item.mode === 'string',
-    )
-  )
-    return;
-
-  return transportModes as TransportModeType[];
 }
