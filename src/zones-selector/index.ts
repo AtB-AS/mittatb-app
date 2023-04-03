226b0cebf9616c2817366ad9f70d5f7bfcbacc58
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-04-03 19:57:17 +0200

    Update index.ts

--- src/zones-selector/index.ts
+++ src/zones-selector/index.ts
@@ -1,2 +1,6 @@
 export {ZonesSelectorButtons} from './ZonesSelectorButtons';
 export {ZonesSelectorMap} from './ZonesSelectorMap';
+export type {
+  ZonesSelectorButtonsProps,
+  ZonesSelectorMapsProps,
+} from './navigation-types';
