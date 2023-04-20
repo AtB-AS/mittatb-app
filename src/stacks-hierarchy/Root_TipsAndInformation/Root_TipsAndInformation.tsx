8bcb9ca0fcaf78e8c59fdfc5ad194faa114add93
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-04-20 21:02:46 +0200

    Refactoring and added firebase

--- src/stacks-hierarchy/Root_TipsAndInformation/Root_TipsAndInformation.tsx
+++ src/stacks-hierarchy/Root_TipsAndInformation/Root_TipsAndInformation.tsx
@@ -1,23 +1,50 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {FullScreenHeader} from '@atb/components/screen-header';
-import {TipsAndInformationTexts, useTranslation} from '@atb/translations';
+import {
+  getTextForLanguage,
+  TipsAndInformationTexts,
+  useTranslation,
+} from '@atb/translations';
 import {StyleSheet, Theme} from '@atb/theme';
 import {ThemeText} from '@atb/components/text';
 import {themeColor} from '@atb/stacks-hierarchy/Root_OnboardingStack/Onboarding_WelcomeScreen';
 import * as Sections from '@atb/components/sections';
-import {Button} from '@atb/components/button';
 import {RootStackScreenProps} from '@atb/stacks-hierarchy';
+import firestore from '@react-native-firebase/firestore';
+import {
+  TipRaw,
+  TipType,
+} from '@atb/stacks-hierarchy/Root_TipsAndInformation/types';
+import {mapToTips} from '@atb/stacks-hierarchy/Root_TipsAndInformation/converters';
+import {Button} from '@atb/components/button';
 
 type Props = RootStackScreenProps<'Root_TipsAndInformation'>;
 
 export const Root_TipsAndInformation = ({navigation}: Props) => {
   const styles = useScreenStyle();
-  const {t} = useTranslation();
+  const {t, language} = useTranslation();
   const [currentlyOpen, setCurrentlyOpen] = useState<number>(0);
+  const [assistantTipIndex, setAssistantTipsIndex] = useState<number>(0);
 
-  const assistantTipIndex = TipsAndInformationTexts.tips.length;
+  const [tips, setTips] = useState<TipType[]>([]);
 
+  useEffect(
+    () =>
+      firestore()
+        .collection<TipRaw>('TipsAndInformation')
+        .onSnapshot(
+          async (snapshot) => {
+            const newTips = mapToTips(snapshot.docs);
+            setTips(newTips);
+            setAssistantTipsIndex(newTips.length);
+          },
+          (err) => {
+            console.warn(err);
+          },
+        ),
+    [],
+  );
   return (
     <View style={styles.container}>
       <FullScreenHeader leftButton={{type: 'close'}} />
@@ -33,28 +60,34 @@ export const Root_TipsAndInformation = ({navigation}: Props) => {
 
       <View style={styles.innerContainer}>
         <Sections.Section style={styles.tipsContainer}>
-          {/* eslint-disable-next-line rulesdir/translations-warning */}
-          {TipsAndInformationTexts.tips.map(({title, tip}, index) => (
-            <Sections.ExpandableSectionItem
-              key={index}
-              textType="body__primary--bold"
-              text={t(title)}
-              showIconText={false}
-              expanded={currentlyOpen === index}
-              onPress={() => {
-                setCurrentlyOpen(index);
-              }}
-              expandContent={
-                <ThemeText
-                  type="body__tertiary"
-                  style={styles.expandedContent}
-                  isMarkdown={true}
-                >
-                  {t(tip)}
-                </ThemeText>
-              }
-            />
-          ))}
+          {tips.map((tip, index) => {
+            const title = getTextForLanguage(tip.title, language);
+            const description = getTextForLanguage(tip.description, language);
+
+            if (!title || !description) return null;
+
+            return (
+              <Sections.ExpandableSectionItem
+                key={index}
+                textType="body__primary--bold"
+                text={title}
+                showIconText={false}
+                expanded={currentlyOpen === index}
+                onPress={() => {
+                  setCurrentlyOpen(index);
+                }}
+                expandContent={
+                  <ThemeText
+                    type="body__tertiary"
+                    style={styles.expandedContent}
+                    isMarkdown={true}
+                  >
+                    {description}
+                  </ThemeText>
+                }
+              />
+            );
+          })}
           <Sections.ExpandableSectionItem
             textType="body__primary--bold"
             text={t(TipsAndInformationTexts.ticketAssistantTip.title)}
