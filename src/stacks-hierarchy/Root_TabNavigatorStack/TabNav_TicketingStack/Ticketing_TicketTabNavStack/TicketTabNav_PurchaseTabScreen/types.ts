dedfca2533e2fefc37318b322d5d78a27892705c
Gøran Dalum <goran.dalum@mittatb.no>
2023-04-21 13:26:28 +0200

    fix: Duplicate key on recent fare contracts (#3492)
    The react component key for recent fare contracts only used fare
    product and travellers, and not zones. Which ment that we got a
    duplicate key error if a user had two recent fare contracts
    with same product and travellers, but different zones.
    
    When doing this the logic for the key ended up being almost a
    duplicate of checking for unique fare contracts. So now we use the
    underlying fields for the recent fare contract to create an id
    which can be used both as react key and for filtering out
    duplicates.

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/types.ts
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/types.ts
@@ -2,6 +2,11 @@ import {PreassignedFareProduct, TariffZone} from '@atb/reference-data/types';
 import {UserProfileWithCount} from '@atb/stacks-hierarchy/Root_PurchaseOverviewScreen/components/Travellers/use-user-count-state';
 
 export type RecentFareContract = {
+  /**
+   * An id created from the underlying fields. Can be used to check for uniques
+   * and as react key.
+   */
+  id: string;
   preassignedFareProduct: PreassignedFareProduct;
   fromTariffZone?: TariffZone;
   toTariffZone?: TariffZone;
