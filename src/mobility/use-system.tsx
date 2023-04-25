ef4c8144e49f6d08a457015a9c0948d0cb7d5081
Kristian Selnæs <kristian@selnaes.net>
2023-04-25 08:06:16 +0200

    Simplify hook

--- src/mobility/use-system.tsx
+++ src/mobility/use-system.tsx
@@ -2,37 +2,32 @@ import {
   SystemFragment,
   TranslatedStringFragment,
 } from '@atb/api/types/generated/fragments/mobility-shared';
-import {useEffect, useState} from 'react';
 import {getTextForLanguage, useTranslation} from '@atb/translations';
 import {MobilityTexts} from '@atb/translations/screens/subscreens/MobilityTexts';
-import {Appearance, Platform} from 'react-native';
+import {Platform} from 'react-native';
+import {useTheme} from '@atb/theme';
 
 export const useSystem = <T extends {system: SystemFragment}>(
   entity: T | undefined,
   operator: TranslatedStringFragment | undefined = entity?.system.name,
 ) => {
   const {t, language} = useTranslation();
-  const [appStoreUri, setAppStoreUri] = useState<string>();
-  const [brandLogoUrl, setBrandLogoUrl] = useState<string>();
-  const [operatorName, setOperatorName] = useState('');
+  const {themeName} = useTheme();
 
-  useEffect(() => {
-    setAppStoreUri(
-      Platform.OS === 'ios'
-        ? entity?.system.rentalApps?.ios?.storeUri
-        : entity?.system.rentalApps?.android?.storeUri,
-    );
-    setBrandLogoUrl(
-      Appearance.getColorScheme() === 'dark'
-        ? entity?.system.brandAssets?.brandImageUrlDark
-        : entity?.system.brandAssets?.brandImageUrl,
-    );
-    setOperatorName(
-      getTextForLanguage(operator?.translation, language) ??
-        t(MobilityTexts.unknownOperator) ??
-        '',
-    );
-  }, [entity]);
+  const appStoreUri = () =>
+    Platform.OS === 'ios'
+      ? entity?.system.rentalApps?.ios?.storeUri
+      : entity?.system.rentalApps?.android?.storeUri;
+
+  const brandLogoUrl =
+    themeName === 'dark'
+      ? entity?.system.brandAssets?.brandImageUrlDark
+      : entity?.system.brandAssets?.brandImageUrl;
+
+  const operatorName =
+    getTextForLanguage(operator?.translation, language) ??
+    t(MobilityTexts.unknownOperator) ??
+    '';
 
   return {
     appStoreUri,
