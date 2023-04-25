93d98878b4d223f73dc3e619699572a80ded8648
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-04-25 15:25:40 +0200

    Update fetch-data.ts

--- src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_SummaryScreen/fetch-data.ts
+++ src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_SummaryScreen/fetch-data.ts
@@ -1,19 +1,17 @@
-// TicketAssistant_SummaryScreenHooks.ts
 import {useEffect} from 'react';
+import {useFirestoreConfiguration} from '@atb/configuration';
 import {useTicketAssistantState} from '@atb/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistantContext';
 import {getRecommendedTicket} from '@atb/stacks-hierarchy/Root_TicketAssistantStack/api';
-import {useFirestoreConfiguration} from '@atb/configuration';
 import {handleRecommendedTicketResponse} from '@atb/stacks-hierarchy/Root_TicketAssistantStack/handle-recommended-ticket-response';
 
-export const useTicketAssistantDataFetch = (navigation: any) => {
+export const useTicketAssistantDataFetch = (nav: any) => {
   const {
     tariffZones,
     userProfiles,
     preassignedFareProducts,
     fareProductTypeConfigs,
   } = useFirestoreConfiguration();
-
-  let {
+  const {
     response,
     setResponse,
     data,
@@ -23,55 +21,44 @@ export const useTicketAssistantDataFetch = (navigation: any) => {
     setCrashed,
   } = useTicketAssistantState();
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = async () => {
+    try {
       setCrashed(false);
-      await getRecommendedTicket(data)
-        .then((r) => {
-          setHasDataChanged(false);
-          if (r.tickets.length === 0) {
-            setCrashed(true);
-            return;
-          }
-          setResponse(r);
-          try {
-            if (r.tickets !== undefined) {
-              setPurchaseDetails(
-                handleRecommendedTicketResponse(
-                  r,
-                  tariffZones,
-                  userProfiles,
-                  preassignedFareProducts,
-                  fareProductTypeConfigs,
-                ),
-              );
-            }
-          } catch (e) {
-            setCrashed(true);
-          }
-        })
-        .catch(() => {
-          setCrashed(true);
-        });
-    };
+      const r = await getRecommendedTicket(data);
+      setHasDataChanged(false);
 
-    const unsubscribe = navigation.addListener('focus', () => {
-      if (hasDataChanged) {
-        fetchData();
+      if (!r.tickets.length) {
+        setCrashed(true);
+      } else {
+        setResponse(r);
+        setPurchaseDetails(
+          handleRecommendedTicketResponse(
+            r,
+            tariffZones,
+            userProfiles,
+            preassignedFareProducts,
+            fareProductTypeConfigs,
+          ),
+        );
       }
-    });
+    } catch (e) {
+      setCrashed(true);
+    }
+  };
 
+  useEffect(() => {
+    const unsub = nav.addListener('focus', async () => {
+      if (hasDataChanged) await fetchData();
+    });
     return () => {
-      unsubscribe();
+      unsub();
     };
   }, [
     data,
     hasDataChanged,
     response,
     setResponse,
-    data,
     setPurchaseDetails,
-    hasDataChanged,
     setHasDataChanged,
   ]);
 };
