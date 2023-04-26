796b8347afa3dcc6c5a423b3745bae761ada1f93
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-04-26 08:52:55 +0200

    Update utils.ts

--- src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_DurationScreen/utils.ts
+++ src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_DurationScreen/utils.ts
@@ -2,12 +2,13 @@
 import {addDays} from 'date-fns';
 import {dateToDateString} from '@atb/components/sections/items/date-input/utils';
 
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
 export function dateDiffInDays(a: Date, b: Date) {
   // Discard the time and time-zone information.
   const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
   const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
-  let _MS_PER_DAY = 1000 * 60 * 60 * 24;
   return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
 
