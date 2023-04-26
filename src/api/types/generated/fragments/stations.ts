35d7ec39a11a645c5a141bb1f69f674438a4af72
Kristian Selnæs <kristian@selnaes.net>
2023-04-26 10:46:40 +0200

    Use scooter query with id param to fetch single scooter

--- src/api/types/generated/fragments/stations.ts
+++ src/api/types/generated/fragments/stations.ts
@@ -1,3 +1,4 @@
+import * as Types from '../mobility-types_v2';
 import {
   PricingPlanFragment,
   RentalUrisFragment,
@@ -5,15 +6,46 @@ import {
   TranslatedStringFragment,
 } from '@atb/api/types/generated/fragments/mobility-shared';
 
-export type StationFragment = {
+export type VehicleTypeAvailabilityBasicFragment = {
+  count: number;
+  vehicleType: {formFactor: Types.FormFactor};
+};
+
+export type StationBasicFragment = {
   id: string;
   lat: number;
   lon: number;
-  capacity?: number;
-  numBikesAvailable: number;
+  vehicleTypesAvailable?: Array<VehicleTypeAvailabilityBasicFragment>;
+};
+
+export type BikeStationFragment = {
   numDocksAvailable?: number;
   name: TranslatedStringFragment;
   pricingPlans: Array<PricingPlanFragment>;
   system: SystemFragment;
   rentalUris?: RentalUrisFragment;
+} & StationBasicFragment;
+
+export type CarVehicleTypeFragment = {
+  formFactor: Types.FormFactor;
+  propulsionType: Types.PropulsionType;
+  maxRangeMeters?: number;
+  riderCapacity?: number;
+  make?: string;
+  model?: string;
+  vehicleAccessories?: Array<Types.VehicleAccessory>;
+  name?: TranslatedStringFragment;
 };
+
+export type CarAvailabilityFragment = {
+  count: number;
+  vehicleType: CarVehicleTypeFragment;
+};
+
+export type CarStationFragment = {
+  name: TranslatedStringFragment;
+  pricingPlans: Array<PricingPlanFragment>;
+  system: SystemFragment;
+  rentalUris?: RentalUrisFragment;
+  vehicleTypesAvailable?: Array<CarAvailabilityFragment>;
+} & StationBasicFragment;
