8e54e514a5091208b3d9fe8352630c29933a659e
Kristian Selnæs <kristian@selnaes.net>
2023-04-26 13:35:50 +0200

    Remove stations update from this pr

--- src/api/types/generated/fragments/stations.ts
+++ src/api/types/generated/fragments/stations.ts
@@ -1,4 +1,3 @@
-import * as Types from '../mobility-types_v2';
 import {
   PricingPlanFragment,
   RentalUrisFragment,
@@ -6,46 +5,15 @@ import {
   TranslatedStringFragment,
 } from '@atb/api/types/generated/fragments/mobility-shared';
 
-export type VehicleTypeAvailabilityBasicFragment = {
-  count: number;
-  vehicleType: {formFactor: Types.FormFactor};
-};
-
-export type StationBasicFragment = {
+export type StationFragment = {
   id: string;
   lat: number;
   lon: number;
-  vehicleTypesAvailable?: Array<VehicleTypeAvailabilityBasicFragment>;
-};
-
-export type BikeStationFragment = {
+  capacity?: number;
+  numBikesAvailable: number;
   numDocksAvailable?: number;
   name: TranslatedStringFragment;
   pricingPlans: Array<PricingPlanFragment>;
   system: SystemFragment;
   rentalUris?: RentalUrisFragment;
-} & StationBasicFragment;
-
-export type CarVehicleTypeFragment = {
-  formFactor: Types.FormFactor;
-  propulsionType: Types.PropulsionType;
-  maxRangeMeters?: number;
-  riderCapacity?: number;
-  make?: string;
-  model?: string;
-  vehicleAccessories?: Array<Types.VehicleAccessory>;
-  name?: TranslatedStringFragment;
 };
-
-export type CarAvailabilityFragment = {
-  count: number;
-  vehicleType: CarVehicleTypeFragment;
-};
-
-export type CarStationFragment = {
-  name: TranslatedStringFragment;
-  pricingPlans: Array<PricingPlanFragment>;
-  system: SystemFragment;
-  rentalUris?: RentalUrisFragment;
-  vehicleTypesAvailable?: Array<CarAvailabilityFragment>;
-} & StationBasicFragment;
