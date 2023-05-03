46b2e846335b0d12a1e0508e5a80a3467225d12f
hanne-at-atb <126664682+hanne-at-atb@users.noreply.github.com>
2023-05-03 16:15:21 +0200

    feat: move map content, remove realtime from trip timestamps (#3515)

--- src/travel-details-screens/components/Trip.tsx
+++ src/travel-details-screens/components/Trip.tsx
@@ -1,20 +1,25 @@
 import {Leg, TripPattern} from '@atb/api/types/trips';
 import {Feedback} from '@atb/components/feedback';
-import {StyleSheet} from '@atb/theme';
+import {StyleSheet, useTheme} from '@atb/theme';
 import {secondsBetween} from '@atb/utils/date';
 import {AxiosError} from 'axios';
 import React from 'react';
 import {View} from 'react-native';
 import {TripMessages} from './DetailsMessages';
 import {TripSection, getPlaceName, InterchangeDetails} from './TripSection';
-import {Summary} from './TripSummary';
+import {TripSummary} from './TripSummary';
 import {WaitDetails} from './WaitSection';
 import {ServiceJourneyDeparture} from '@atb/travel-details-screens/types';
 import {StopPlaceFragment} from '@atb/api/types/generated/fragments/stop-places';
 import {isSignificantFootLegWalkOrWaitTime} from '@atb/travel-details-screens/utils';
-import {TravelDetailsMapScreenParams} from '@atb/travel-details-map-screen';
+import {
+  CompactTravelDetailsMap,
+  TravelDetailsMapScreenParams,
+} from '@atb/travel-details-map-screen';
 import {useGetServiceJourneyVehicles} from '@atb/stacks-hierarchy/Root_TabNavigatorStack/TabNav_DashboardStack/Dashboard_TripSearchScreen/use-get-service-journey-vehicles';
 import {useRealtimeMapEnabled} from '@atb/components/map';
+import {AnyMode} from '@atb/components/icon-box';
+import {Divider} from '@atb/components/divider';
 
 export type TripProps = {
   tripPattern: TripPattern;
@@ -34,6 +39,8 @@ export const Trip: React.FC<TripProps> = ({
   onPressQuay,
 }) => {
   const styles = useStyle();
+  const {theme} = useTheme();
+
   const legs = tripPattern.legs.filter((leg, i) =>
     isSignificantFootLegWalkOrWaitTime(leg, tripPattern.legs[i + 1]),
   );
@@ -50,6 +57,14 @@ export const Trip: React.FC<TripProps> = ({
         .filter(filterLegs)
     : undefined;
   const {vehiclePositions} = useGetServiceJourneyVehicles(ids);
+
+  const tripPatternLegs = tripPattern?.legs.map((leg) => {
+    let mode: AnyMode = !!leg.bookingArrangements ? 'flex' : leg.mode;
+    return {
+      ...leg,
+      mode,
+    };
+  });
 
   return (
     <View style={styles.container}>
@@ -94,7 +109,22 @@ export const Trip: React.FC<TripProps> = ({
             );
           })}
       </View>
-      <Summary {...tripPattern} />
+      <Divider style={{marginVertical: theme.spacings.medium}} />
+      {tripPatternLegs && (
+        <CompactTravelDetailsMap
+          mapLegs={tripPatternLegs}
+          fromPlace={tripPatternLegs[0]?.fromPlace}
+          toPlace={tripPatternLegs[tripPatternLegs.length - 1].toPlace}
+          onExpand={() => {
+            onPressDetailsMap({
+              legs: tripPatternLegs,
+              fromPlace: tripPatternLegs[0]?.fromPlace,
+              toPlace: tripPatternLegs[tripPatternLegs.length - 1].toPlace,
+            });
+          }}
+        />
+      )}
+      <TripSummary {...tripPattern} />
       <Feedback metadata={tripPattern} viewContext="assistant" />
     </View>
   );
