135f4a1c698882e8fea2eca5111e57fc45c5c4d2
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-05-05 09:43:47 +0200

    Added explaining const name

--- src/global-messages/is-within-time-range.ts
+++ src/global-messages/is-within-time-range.ts
@@ -1,11 +1,13 @@
 import {GlobalMessageType} from '@atb/global-messages/types';
 
+const MAX_DATE_VALUE = 8640000000000000;
+
 export const isWithinTimeRange = (
   globalMessage: GlobalMessageType,
   now: number,
 ) => {
   const startDate = globalMessage.startDate ?? 0;
-  const endDate = globalMessage.endDate ?? 8640000000000000;
+  const endDate = globalMessage.endDate ?? MAX_DATE_VALUE;
 
   return startDate <= now && endDate >= now;
 };
