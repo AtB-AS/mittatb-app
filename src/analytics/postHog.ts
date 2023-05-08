2385ef11f287132cb1ff859b9f7883fee8fe5249
Kristian Selnæs <kristian@selnaes.net>
2023-05-08 15:43:07 +0200

    Add POSTHOG_HOST env variable

--- src/analytics/postHog.ts
+++ src/analytics/postHog.ts
@@ -1,9 +1,9 @@
 import PostHog from 'posthog-react-native';
-import {POSTHOG_API_KEY} from '@env';
+import {POSTHOG_API_KEY, POSTHOG_HOST} from '@env';
 
 export const initPostHog = () =>
   POSTHOG_API_KEY
     ? PostHog.initAsync(POSTHOG_API_KEY, {
-        host: 'https://eu.posthog.com',
+        host: POSTHOG_HOST,
       })
     : Promise.resolve(undefined);
--- types/react-native-dotenv.d.ts
+++ types/react-native-dotenv.d.ts
@@ -16,4 +16,5 @@ declare module '@env' {
   export const SAFETY_NET_API_KEY: string;
   export const ENABLE_TICKETING: string | undefined;
   export const POSTHOG_API_KEY: string | undefined;
+  export const POSTHOG_HOST: string | undefined;
 }
