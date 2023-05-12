81d615dce6f876f6df276e8aa1243b402d5e1e31
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-05-12 10:18:43 +0200

    Fix calculations

--- src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_SummaryScreen/TicketSummary/utils.ts
+++ src/stacks-hierarchy/Root_TicketAssistantStack/TicketAssistant_SummaryScreen/TicketSummary/utils.ts
@@ -1,38 +1,11 @@
 import {TicketResponseData} from '@atb/stacks-hierarchy/Root_TicketAssistantStack/types';
-import {daysInWeek} from 'date-fns';
-
-export function calculateSavings(
-  ticketPrice: number,
-  alternativePrice: number,
-): number {
-  return alternativePrice - ticketPrice;
-}
-
-export function calculateSingleTickets(
-  duration: number,
-  frequency: number,
-): number {
-  return Math.ceil((duration / daysInWeek) * frequency);
-}
 
 export function getLongestDurationTicket(
   tickets: TicketResponseData[],
 ): TicketResponseData {
-  let longestDuration = 0;
-  let longestDurationIndex = 0;
-  tickets.forEach((ticket, index) => {
-    if (ticket.duration > longestDuration) {
-      longestDuration = ticket.duration;
-      longestDurationIndex = index;
-    }
+  return tickets.reduce((longestTicket, currentTicket) => {
+    return currentTicket.duration > longestTicket.duration
+      ? currentTicket
+      : longestTicket;
   });
-  return tickets[longestDurationIndex];
-}
-
-export function perTripSavings(
-  savings: number,
-  duration: number,
-  frequency: number,
-): number {
-  return savings / ((duration / daysInWeek) * frequency);
 }
