3133d9267da0f73267ff659213024dfa4ce3305f
Kristian Selnæs <kristian@selnaes.net>
2023-05-19 10:58:46 +0200

    Refactor to improve code readability

--- src/components/map/components/mobility/Stations.tsx
+++ src/components/map/components/mobility/Stations.tsx
@@ -16,30 +16,47 @@ export type StationsWithCount = FeatureCollection<
 >;
 
 export const Stations = ({stations}: Props) => {
-  const getFeaturesOfType = (
-    stations: FeatureCollection<GeoJSON.Point, StationBasicFragment>,
-    formFactor: FormFactor,
-  ) => ({
-    stations: {
-      ...stations,
-      features: stations.features.filter((station) =>
-        station.properties.vehicleTypesAvailable?.some(
-          (type) => type.vehicleType.formFactor === formFactor,
-        ),
-      ),
-    },
-    formFactor,
-  });
+  const bikeStations = stationsWithCount(stations, FormFactor.Bicycle);
+  const carStations = stationsWithCount(stations, FormFactor.Car);
 
-  const stationsWithCount = ({
-    stations,
-    formFactor,
-  }: {
-    stations: FeatureCollection<GeoJSON.Point, StationBasicFragment>;
-    formFactor: FormFactor;
-  }): StationsWithCount => ({
-    ...stations,
-    features: stations.features.map((feature) => ({
+  return (
+    <>
+      <BikeStations stations={bikeStations} />
+      <CarStations stations={carStations} />
+    </>
+  );
+};
+
+/**
+ * Filters the collection of stations to return only those with the given form factor
+ * @param stations
+ * @param formFactor
+ */
+const getFeaturesOfType = (
+  stations: FeatureCollection<GeoJSON.Point, StationBasicFragment>,
+  formFactor: FormFactor,
+): FeatureCollection<GeoJSON.Point, StationBasicFragment> => ({
+  ...stations,
+  features: stations.features.filter((station) =>
+    station.properties.vehicleTypesAvailable?.some(
+      (type) => type.vehicleType.formFactor === formFactor,
+    ),
+  ),
+});
+
+/**
+ * Adds a 'count' property with the number of available vehicles for each station.
+ * @param allStations
+ * @param formFactor
+ */
+const stationsWithCount = (
+  allStations: FeatureCollection<GeoJSON.Point, StationBasicFragment>,
+  formFactor: FormFactor,
+): StationsWithCount => {
+  const stationWithFormFactor = getFeaturesOfType(allStations, formFactor);
+  return {
+    ...stationWithFormFactor,
+    features: stationWithFormFactor.features.map((feature) => ({
       ...feature,
       properties: {
         ...feature.properties,
@@ -49,19 +66,5 @@ export const Stations = ({stations}: Props) => {
         ),
       },
     })),
-  });
-
-  const bikeStations = stationsWithCount(
-    getFeaturesOfType(stations, FormFactor.Bicycle),
-  );
-  const carStations = stationsWithCount(
-    getFeaturesOfType(stations, FormFactor.Car),
-  );
-
-  return (
-    <>
-      <BikeStations stations={bikeStations} />
-      <CarStations stations={carStations} />
-    </>
-  );
+  };
 };
