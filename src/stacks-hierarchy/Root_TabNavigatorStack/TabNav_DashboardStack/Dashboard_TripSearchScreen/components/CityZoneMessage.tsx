08ce160c57f274fca1157238a3ad0c7172d70a90
Jorge Osorio <jorelosorio@gmail.com>
2023-05-22 11:08:37 +0200

    Add proper accessibility to the link button

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_DashboardStack/Dashboard_TripSearchScreen/components/CityZoneMessage.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_DashboardStack/Dashboard_TripSearchScreen/components/CityZoneMessage.tsx
@@ -105,6 +105,8 @@ const CityZoneBox = ({
                 text={actionButton.text}
                 onPress={actionButton.onPress}
                 style={styles.action}
+                accessibilityRole="link"
+                accessibilityHint={t(CityBoxMessageTexts.a11yLabel)}
                 rightIcon={{svg: ExternalLink}}
               />
             ))}
--- src/translations/components/CityBoxMessage.ts
+++ src/translations/components/CityBoxMessage.ts
@@ -5,6 +5,10 @@ const CityBoxMessageTexts = {
     'Bestillingstransport fra adresse til adresse er tilgjengelig.',
     'On-demand transport from address to address is available.',
   ),
+  a11yLabel: _(
+    'Aktivér for å åpne ekstern side',
+    'Activate to open external content',
+  ),
 };
 
 export default CityBoxMessageTexts;
