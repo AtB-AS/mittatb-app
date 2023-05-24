be9b85796306a10d21fbe7f79b6057f50f0a278e
Gøran Dalum <goran.dalum@gmail.com>
2023-05-24 13:30:14 +0200

    chore: Brew update in android build

--- scripts/android/replace-bundle.sh
+++ scripts/android/replace-bundle.sh
@@ -30,6 +30,7 @@ else
     echo "Compile JS to Hermes Bytecode"
     ./node_modules/react-native/sdks/hermesc/linux64-bin/hermesc -emit-binary -source-map=bundle/temp.bundle.map -output-source-map -out bundle/index.android.bundle bundle/temp.bundle
 
+    brew update
     brew install apktool yq
 
     echo "Decompile Android APK"
