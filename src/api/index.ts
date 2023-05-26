025f9aa79e41cc3a565906b469aace547a128f77
Johannes Røsvik <j.rosvik@gmail.com>
2023-05-26 14:35:57 +0200

    feat: handle live position error states (#3562)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export {CancelToken, isCancel, client} from './client';
 export {autocomplete, reverse} from './geocoder';
 export {search} from './trips';
+export type {SubscriptionState} from './use-subscription';
