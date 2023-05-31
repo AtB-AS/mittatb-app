04ad36d0dc4cf3bf2c17a14e40e457faf8cff606
Ingvild Simonsen <85479566+ingvisim@users.noreply.github.com>
2023-05-31 14:09:16 +0200

    fix: Do not scale map bus icon (#3607)
    * fix: Do not scale map bus icon
    
    * refactor

--- src/travel-details-map-screen/TravelDetailsMapScreenComponent.tsx
+++ src/travel-details-map-screen/TravelDetailsMapScreenComponent.tsx
@@ -289,6 +289,7 @@ const LiveVehicleIcon = ({
       <ThemeIcon
         svg={svg}
         fill={theme.interactive.interactive_destructive.default.background}
+        allowFontScaling={false}
       />
     );
   if (isLoading || isStale)
@@ -298,7 +299,7 @@ const LiveVehicleIcon = ({
       />
     );
 
-  return <ThemeIcon svg={svg} fill={fillColor} />;
+  return <ThemeIcon svg={svg} fill={fillColor} allowFontScaling={false} />;
 };
 
 const useStyles = StyleSheet.createThemeHook(() => ({
