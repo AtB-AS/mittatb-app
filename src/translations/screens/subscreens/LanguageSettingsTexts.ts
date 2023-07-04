ebbea2f42aacee0def7d14d674bf5f7df4de9059
Tore Stensaker Tefre <tore@tefre.it>
2023-07-04 15:30:29 +0200

    nynorsk attempt

--- src/translations/screens/subscreens/LanguageSettingsTexts.ts
+++ src/translations/screens/subscreens/LanguageSettingsTexts.ts
@@ -1,15 +1,17 @@
 import {translation as _} from '../../commons';
 const LanguageSettingsTexts = {
   header: {
-    title: _('Språk', 'Language'),
+    title: _('Språk', 'Language', '§'),
   },
   usePhoneSettings: _(
     'Bruk telefoninnstillingene mine',
     'Use my phone settings',
+    '§',
   ),
   options: {
     nb: _('Norsk bokmål', 'Norsk bokmål'),
     en: _('English', 'English'),
+    nn: _('Nynorsk', 'Nynorsk'),
   },
 };
 export default LanguageSettingsTexts;
