816c0c2a13828cc129fe238323ad694dc4301f0a
Johannes Røsvik <j.rosvik@gmail.com>
2023-07-07 10:27:52 +0200

    add focus handling to serviceJourneyVehicles

--- src/travel-details-screens/use-get-service-journey-vehicles.ts
+++ src/travel-details-screens/use-get-service-journey-vehicles.ts
@@ -1,10 +1,12 @@
 import {getServiceJourneyVehicles} from '@atb/api/vehicles';
-import {useCallback} from 'react';
+import {useCallback, useEffect} from 'react';
 import {usePollableResource} from '@atb/utils/use-pollable-resource';
 import {AxiosError} from 'axios';
 import {GetServiceJourneyVehicles} from '@atb/api/types/vehicles';
+import {useIsFocusedAndActive} from '@atb/utils/use-is-focused-and-active';
 
 export const useGetServiceJourneyVehicles = (serviceJourneyIds?: string[]) => {
+  const isFocused = useIsFocusedAndActive();
   const fetchVehicles = useCallback(
     (signal?: AbortSignal) =>
       getServiceJourneyVehicles(serviceJourneyIds, {
@@ -13,13 +15,21 @@ export const useGetServiceJourneyVehicles = (serviceJourneyIds?: string[]) => {
     [JSON.stringify(serviceJourneyIds)],
   );
 
-  const [updatedVehicles, , , error] = usePollableResource<
+  const [updatedVehicles, reload, , error] = usePollableResource<
     GetServiceJourneyVehicles | undefined,
     AxiosError
   >(fetchVehicles, {
     initialValue: undefined,
     pollingTimeInSeconds: 20,
+    disabled: !isFocused,
   });
+
+  useEffect(() => {
+    if (!isFocused) return;
+    const abortController = new AbortController();
+    reload('WITH_LOADING', abortController);
+    return () => abortController.abort();
+  }, [isFocused]);
 
   return {
     vehiclePositions: updatedVehicles ?? undefined,
