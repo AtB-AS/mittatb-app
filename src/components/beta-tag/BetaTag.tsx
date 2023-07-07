59b006a06fde3ee67f9e43d34739af8c80d7791d
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2023-07-07 12:51:18 +0200

    Use InfoChip in BetaTag

--- src/components/beta-tag/BetaTag.tsx
+++ src/components/beta-tag/BetaTag.tsx
@@ -1,7 +1,7 @@
-import {StyleProp, View, ViewStyle} from 'react-native';
-import {ThemeText} from '@atb/components/text';
+import {StyleProp, ViewStyle} from 'react-native';
 import {StyleSheet} from '@atb/theme';
 import React from 'react';
+import {InfoChip} from '../info-chip';
 
 type Props = {
   style?: StyleProp<ViewStyle>;
@@ -9,11 +9,11 @@ type Props = {
 export const BetaTag = ({style}: Props) => {
   const styles = useStyles();
   return (
-    <View style={[styles.betaLabel, style]}>
-      <ThemeText type={'body__tertiary'} color="background_accent_2">
-        Beta
-      </ThemeText>
-    </View>
+    <InfoChip
+      text="Beta"
+      interactiveColor="interactive_0"
+      style={[styles.betaLabel, style]}
+    />
   );
 };
 
@@ -21,7 +21,6 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
   betaLabel: {
     backgroundColor: theme.static.status.info.background,
     paddingHorizontal: theme.spacings.small,
-    paddingVertical: theme.spacings.xSmall,
     borderRadius: theme.border.radius.circle,
   },
 }));
