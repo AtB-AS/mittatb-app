765ba21834a82cbb2c05e23168f4bae4f15f30d4
Tore Stensaker Tefre <tore@tefre.it>
2023-07-14 13:10:15 +0200

    hide nynorsk for AtB and NFK

--- src/translations/screens/subscreens/LanguageSettingsTexts.ts
+++ src/translations/screens/subscreens/LanguageSettingsTexts.ts
@@ -9,9 +9,9 @@ const LanguageSettingsTexts = {
     'Bruk telefoninnstillingane mine',
   ),
   options: {
-    nb: _('Norsk - bokmål', 'Norsk bokmål', 'Norsk - bokmål'),
+    nb: _('Norsk - bokmål', 'Norsk - bokmål', 'Norsk - bokmål'),
     en: _('English', 'English', 'English'),
-    nn: _('Norsk - nynorsk', 'Nynorsk', 'Norsk - nynorsk'),
+    nn: _('Norsk - nynorsk', 'Norsk - nynorsk', 'Norsk - nynorsk'),
   },
 };
 export default LanguageSettingsTexts;
