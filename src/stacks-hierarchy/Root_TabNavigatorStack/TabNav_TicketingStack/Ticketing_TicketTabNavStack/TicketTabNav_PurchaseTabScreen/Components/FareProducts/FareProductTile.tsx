52942ebc629f113974055f7fea601d6843d3a94c
Jorge Osorio <jorelosorio@gmail.com>
2023-07-26 11:05:45 +0200

    Update FareProductTile.tsx

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/Components/FareProducts/FareProductTile.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/Components/FareProducts/FareProductTile.tsx
@@ -43,30 +43,3 @@ export const FareProductTile = ({
     />
   );
 };
-
-const getFareProductTravelModesText = (
-  modes: TransportModePair[],
-  t: TranslateFunction,
-  modesDisplayLimit = 2,
-): string => {
-  const modesCount = modes.length;
-
-  if (!modes) return '';
-  if (modesCount > modesDisplayLimit) {
-    return t(FareContractTexts.transportModes.multipleTravelModes);
-  }
-
-  const travelModes = modes
-    .map((tm) => t(FareContractTexts.transportMode(tm.mode, tm.subMode)))
-    .filter((value, index, array) => array.indexOf(value) === index); // remove duplicates
-
-  if (travelModes.length < 2) {
-    return travelModes[0] || '';
-  } else {
-    return (
-      travelModes.splice(0, travelModes.length - 1).join(', ') +
-      ` ${t(FareContractTexts.transportModes.concatListWord)} ` + // add " and " between the last two
-      travelModes
-    );
-  }
-};
