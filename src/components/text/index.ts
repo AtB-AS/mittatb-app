6364aa17273b96f777609d1bf2ed6a6bb2cfe883
Kristian Selnæs <kristian@selnaes.net>
2023-08-23 15:55:09 +0200

    fix: Remove unnecessary export

--- src/components/text/index.ts
+++ src/components/text/index.ts
@@ -1,4 +1,3 @@
 export {ThemeText} from './ThemeText';
 export {AccessibleText} from './AccessibleText';
 export {screenReaderPause, MAX_FONT_SCALE} from './utils';
-export type {ColorType} from './ThemeText';
