a30387ccac0cd83ee85efbfdc63d6b31e83da943
Kristian Selnæs <kristian@selnaes.net>
2023-08-25 15:15:49 +0200

    refactor: Don't use useEffect

--- src/harbors/use-harbors.tsx
+++ src/harbors/use-harbors.tsx
@@ -1,42 +1,24 @@
-import {useHarborsQuery} from '@atb/queries';
-import {useHarborConnectionOverrides} from '@atb/harbors/use-harbor-connection-overrides';
-import {useEffect, useState} from 'react';
-import {StopPlaceFragment} from '@atb/api/types/generated/fragments/stop-places';
 import {HarborConnectionOverrideType} from '@atb-as/config-specs';
+import {StopPlaceFragment} from '@atb/api/types/generated/fragments/stop-places';
+import {useHarborConnectionOverrides} from '@atb/harbors/use-harbor-connection-overrides';
+import {useHarborsQuery} from '@atb/queries';
 import {isDefined} from '@atb/utils/presence';
 import _ from 'lodash';
+import {useMemo} from 'react';
 
 export const useHarbors = (fromHarborId?: string) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState<unknown>();
-  const [data, setData] = useState<StopPlaceFragment[]>([]);
-
   const harbors = useHarborsQuery();
   const connections = useHarborsQuery(fromHarborId);
   const overrides = useHarborConnectionOverrides(fromHarborId);
 
-  useEffect(() => {
-    setIsLoading(harbors.isLoading || connections.isLoading);
-  }, [harbors.isLoading, connections.isLoading]);
+  const isLoading = harbors.isLoading || connections.isLoading;
+  const isError = harbors.isError || connections.isError;
+  const error = fromHarborId ? connections.error : harbors.error;
+  const isSuccess = fromHarborId ? connections.isSuccess : harbors.isSuccess;
 
-  useEffect(() => {
-    setIsError(harbors.isError || connections.isError);
-  }, [harbors.isError, connections.isError]);
-
-  useEffect(() => {
-    setError(fromHarborId ? connections.error : harbors.error);
-  }, [fromHarborId, harbors.error, connections.error]);
-
-  useEffect(() => {
-    setIsSuccess(fromHarborId ? connections.isSuccess : harbors.isSuccess);
-  }, [fromHarborId, harbors.isSuccess, connections.isSuccess]);
-
-  useEffect(() => {
+  const data = useMemo(() => {
     if (!fromHarborId) {
-      setData(harbors.data ?? []);
-      return;
+      return harbors.data ?? [];
     }
 
     //Map overrides (if there are any) into complete StopPlaceFragments
@@ -45,12 +27,7 @@ export const useHarbors = (fromHarborId?: string) => {
       harbors.data,
     );
     // Add the extra stop places (override harbors) to the list of existing connections.
-    const allConnections = _.unionBy(
-      connections.data ?? [],
-      overrideHarbors,
-      'id',
-    );
-    setData(allConnections);
+    return _.unionBy(connections.data ?? [], overrideHarbors, 'id');
   }, [fromHarborId, harbors.data, connections.data, overrides]);
 
   const refetch = fromHarborId ? harbors.refetch : connections.refetch;
