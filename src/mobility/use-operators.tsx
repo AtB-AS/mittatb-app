05f5738ed0054f626d2a262302060d908b68e3de
Kristian Selnæs <kristian@selnaes.net>
2023-08-25 11:04:11 +0200

    refactor: Wrap in useCallback

--- src/mobility/use-operators.tsx
+++ src/mobility/use-operators.tsx
@@ -1,30 +1,37 @@
 import {useFirestoreConfiguration} from '@atb/configuration';
 import {FormFactor} from '@atb/api/types/generated/mobility-types_v2';
+import {useCallback} from 'react';
 
 export const useOperators = () => {
   const {mobilityOperators} = useFirestoreConfiguration();
 
-  const byFormFactor = (types: undefined | FormFactor | FormFactor[]) => {
-    if (!types) {
-      return mobilityOperators ?? [];
-    }
-    const formFactors = (Array.isArray(types) ? types : [types]).map((t) =>
-      t.valueOf(),
-    );
-    return (
-      mobilityOperators
-        ?.filter((o) => o.showInApp)
-        .filter((o) =>
-          o.formFactors.some(
-            (f) => types?.length === 0 || formFactors.includes(f),
-          ),
-        )
-        .sort((a, b) => a.name.localeCompare(b.name)) ?? []
-    );
-  };
+  const byFormFactor = useCallback(
+    (types: undefined | FormFactor | FormFactor[]) => {
+      if (!types) {
+        return mobilityOperators ?? [];
+      }
+      const formFactors = (Array.isArray(types) ? types : [types]).map((t) =>
+        t.valueOf(),
+      );
+      return (
+        mobilityOperators
+          ?.filter((o) => o.showInApp)
+          .filter((o) =>
+            o.formFactors.some(
+              (f) => types?.length === 0 || formFactors.includes(f),
+            ),
+          )
+          .sort((a, b) => a.name.localeCompare(b.name)) ?? []
+      );
+    },
+    [mobilityOperators],
+  );
 
-  const byId = (id: string | undefined) =>
-    mobilityOperators?.find((operator) => operator.id === id);
+  const byId = useCallback(
+    (id: string | undefined) =>
+      mobilityOperators?.find((operator) => operator.id === id),
+    [mobilityOperators],
+  );
 
   return {
     byFormFactor,
