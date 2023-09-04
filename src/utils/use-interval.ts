4456bf74ff7cf76d31ec4a4892e47c62a69808fb
Mikael Brevik <mikaelbre@gmail.com>
2023-09-04 09:52:18 +0200

    chore: adds comments specifying behaviour for useInterval

--- src/utils/use-interval.ts
+++ src/utils/use-interval.ts
@@ -1,6 +1,13 @@
 import {useRef, useEffect} from 'react';
 
 type IntervalCallback = () => Promise<void> | void;
+/***
+ * Periodically execute a callback every N milliseconds.
+ * Does not guarantee precision in interval if execution time of the callback is higher than delay.
+ * Should not be used for animations or timers where precision is needed.
+ *
+ * If callback returns a promise execution for each interval awaits fulfilled promise.
+ */
 export function useInterval(
   callback: IntervalCallback,
   delay: number,
@@ -23,6 +30,10 @@ export function useInterval(
     let id: NodeJS.Timeout | number = 0;
 
     function tick() {
+      // Uses recursive setTimout loop instead of setInterval to prevent filling the message queue with
+      // executions when execution of callback takes longer than the interval specified.
+      // This means that we won't guarantee any specific execution time, but this is in liue with the intention of this function in any case.
+      // See example https://developer.mozilla.org/en-US/docs/Web/API/setInterval#ensure_that_execution_duration_is_shorter_than_interval_frequency
       return setTimeout(async function innerCallback() {
         await savedCallback.current();
 
