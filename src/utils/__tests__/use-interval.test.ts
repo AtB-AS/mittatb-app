03a8f8d35e28a312eb89aab7b8098dcbdf5675ce
Mikael Brevik <mikaelbre@gmail.com>
2023-09-04 12:05:32 +0200

    chore: fixes test promises

--- src/utils/__tests__/use-interval.test.ts
+++ src/utils/__tests__/use-interval.test.ts
@@ -18,35 +18,79 @@ describe('useInterval', () => {
     jest.useRealTimers();
   });
 
-  it('should invoke with specified delay', () => {
+  it('should invoke with specified delay', async () => {
     const delay = 300;
-    const {result} = renderHook(() => useInterval(callback, delay));
+    renderHook(() => useInterval(callback, delay));
     expect(callback).toHaveBeenCalledTimes(0);
 
-    jest.advanceTimersByTime(delay);
-    expect(result.current).toBeUndefined();
+    await advanceByTimer(delay);
+
     expect(callback).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), delay);
   });
 
-  it('should invoke with specified delay and immediate if set', () => {
+  it('should invoke with specified delay and immediate if set', async () => {
     const delay = 300;
-    const {result} = renderHook(
-      // () => useInterval(callback, 300),
-      () => useInterval(callback, delay, [], false, true),
-    );
+    renderHook(() => useInterval(callback, delay, [], false, true));
+    expect(callback).toHaveBeenCalledTimes(1);
 
-    jest.advanceTimersByTime(delay);
+    // This is needed to make timers move passed by promises.
+    await resolvePromiseEnforceStep();
 
-    expect(result.current).toBeUndefined();
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), delay);
+
+    await runPendingTimers();
     expect(callback).toHaveBeenCalledTimes(2);
   });
 
-  it('should not have less than 100ms delay', () => {
-    const {result} = renderHook(() => useInterval(callback, 0));
-    jest.advanceTimersByTime(100);
+  it('should not have less than 100ms delay', async () => {
+    const minimumDelay = 100;
+    renderHook(() => useInterval(callback, 0));
 
-    expect(result.current).toBeUndefined();
     expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 100);
+    expect(setTimeout).toHaveBeenLastCalledWith(
+      expect.any(Function),
+      minimumDelay,
+    );
+    await nextTimerTick();
+
+    expect(callback).toBeCalledTimes(1);
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(setTimeout).toHaveBeenLastCalledWith(
+      expect.any(Function),
+      minimumDelay,
+    );
+
+    await runPendingTimers();
+    expect(callback).toBeCalledTimes(2);
   });
 });
+
+async function advanceByTimer(time: number) {
+  jest.advanceTimersByTime(time);
+  // This is a workaround for fake timers in jest to support async code
+  // as useInterval supports promises.
+  // See https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
+  await resolvePromiseEnforceStep();
+}
+
+async function nextTimerTick() {
+  jest.advanceTimersToNextTimer();
+  // This is a workaround for fake timers in jest to support async code
+  // as useInterval supports promises.
+  // See https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
+  await resolvePromiseEnforceStep();
+}
+
+async function runPendingTimers() {
+  jest.runOnlyPendingTimers();
+  // This is a workaround for fake timers in jest to support async code
+  // as useInterval supports promises.
+  // See https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
+  await resolvePromiseEnforceStep();
+}
+
+async function resolvePromiseEnforceStep() {
+  await Promise.resolve();
+}
