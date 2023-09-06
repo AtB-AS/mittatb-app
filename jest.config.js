e2c3718c4a3adabdcfe8d6c5691c3524a23b2964
Mikael Brevik <mikaelbre@gmail.com>
2023-09-06 11:04:11 +0200

    chore: fix jest test preset to work with react native

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'react-native',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@atb/(.*)$': '<rootDir>/src/$1',
