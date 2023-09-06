6949ecea6d313afffe021fe7a5fe303cb42c051f
Mikael Brevik <mikaelbre@gmail.com>
2023-09-06 11:27:39 +0200

    chore: specifically set time zone for testing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   preset: 'react-native',
   testEnvironment: 'node',
+  testRegex: '\\.test\\.tsx?$',
   moduleNameMapper: {
     '^@atb/(.*)$': '<rootDir>/src/$1',
   },
+  globalSetup: './__tests__/test-setup.js',
 };
