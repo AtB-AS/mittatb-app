0616f0c0bcbed6f6221a6a28ec7c0b75abdca13d
Tor K Moseng <tor@moseng.no>
2023-09-06 10:19:20 +0200

    tests for measuring performance with flashlight

---
+++ e2e/test/pageobjects/ticket.page.ts
@@ -0,0 +1,17 @@
+import Page from './page';
+import AppHelper from '../utils/app.helper';
+
+class TicketPage extends Page {
+
+  /**
+   * Choose a ticket a.k.a fare product
+   * @param fareProduct: fare product to choose, e.g. 'single'
+   */
+  async chooseFareProduct(fareProduct: string) {
+    const reqId = `//*[@resource-id="${fareProduct}FareProduct"]`;
+    await $(reqId).click();
+    await AppHelper.pause();
+  }
+}
+
+export default new TicketPage();
