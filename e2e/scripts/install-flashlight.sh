659b533bf4224714933eda3e11dd085b522d3fe8
Tor K Moseng <tor@moseng.no>
2023-09-06 14:49:38 +0200

    test 3 flashlight install

--- e2e/scripts/install-flashlight.sh
+++ e2e/scripts/install-flashlight.sh
@@ -4,8 +4,8 @@ echo "Install Flashlight"
 curl https://get.flashlight.dev | bash
 
 echo "Add Flashlight"
-#source /Users/runner/.profile
-export PATH="/Users/runner/.flashlight/bin:$PATH"
+source /Users/runner/.profile
+#export PATH="/Users/runner/.flashlight/bin:$PATH"
 
 echo "Test Flashlight"
 flashlight -v
--- e2e/scripts/restart-adb.sh
+++ e2e/scripts/restart-adb.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+echo "Install Flashlight"
+curl https://get.flashlight.dev | bash
+
+echo "Add Flashlight"
+source /Users/runner/.profile
+#export PATH="/Users/runner/.flashlight/bin:$PATH"
+
+echo "Test Flashlight"
+flashlight -v
+
+echo "Restart adb"
 adb kill-server
 sleep 20
 adb start-server
