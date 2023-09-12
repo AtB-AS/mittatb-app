2dc393638cb77ba3950df698b741d9e0cfd2f654
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-09-12 09:38:32 +0200

    Update Slider.tsx

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -24,57 +24,61 @@ type Props = {
   trackMarkComponent?: (texts: number) => React.ReactNode;
 };
 export function Slider({
-                         value,
-                         maximumValue,
-                         minimumValue,
-                         minimumTrackTintColor = "interactive_0",
-                         maximumTrackTintColor = "interactive_0",
-                         step,
-                         trackClickable = true,
-                         thumbTintColor = "interactive_0",
-                         onValueChange,
-                         onSlidingComplete,
-                         thumbStyle,
-                         trackStyle,
-                         thumbTouchSize,
-                         containerStyle,
-                         trackMarkComponent,
-                         trackMarks,
-                       }: Props) {
+  value,
+  maximumValue,
+  minimumValue,
+  minimumTrackTintColor = 'interactive_0',
+  maximumTrackTintColor = 'interactive_0',
+  step,
+  trackClickable = true,
+  thumbTintColor = 'interactive_0',
+  onValueChange,
+  onSlidingComplete,
+  thumbStyle,
+  trackStyle,
+  thumbTouchSize,
+  containerStyle,
+  trackMarkComponent,
+  trackMarks,
+}: Props) {
   const {theme} = useTheme();
   const minColor = theme.interactive[minimumTrackTintColor].default.background;
   const maxColor = theme.interactive[maximumTrackTintColor].active.background;
   const thumbColor = theme.interactive[thumbTintColor].default.background;
 
-  const touchSize = 50;
+  const touchSize = 40;
   const thumbSize = 20;
 
   return (
-      <RNSlider
-          thumbStyle={thumbStyle ? thumbStyle : {width:thumbSize, height: thumbSize}}
-          thumbTouchSize={thumbTouchSize ? thumbTouchSize : {width: touchSize, height: touchSize}}
-          trackStyle={trackStyle}
-          onSlidingComplete={(number) =>
-              onSlidingComplete && onSlidingComplete(number[0])
-          }
-          onValueChange={(number) => onValueChange(number[0])}
-          containerStyle={containerStyle}
-          value={value}
-          minimumValue={minimumValue}
-          maximumValue={maximumValue}
-          minimumTrackTintColor={minColor}
-          maximumTrackTintColor={maxColor}
-          step={step}
-          trackClickable={trackClickable}
-          thumbTintColor={thumbColor}
-          trackMarks={trackMarks}
-          renderTrackMarkComponent={
-            trackMarks && trackMarkComponent
-                ? (index) => {
-                  return trackMarkComponent(index);
-                }
-                : undefined
-          }
-      />
+    <RNSlider
+      thumbStyle={
+        thumbStyle ? thumbStyle : {width: thumbSize, height: thumbSize}
+      }
+      thumbTouchSize={
+        thumbTouchSize ? thumbTouchSize : {width: touchSize, height: touchSize}
+      }
+      trackStyle={trackStyle}
+      onSlidingComplete={(number) =>
+        onSlidingComplete && onSlidingComplete(number[0])
+      }
+      onValueChange={(number) => onValueChange(number[0])}
+      containerStyle={containerStyle}
+      value={value}
+      minimumValue={minimumValue}
+      maximumValue={maximumValue}
+      minimumTrackTintColor={minColor}
+      maximumTrackTintColor={maxColor}
+      step={step}
+      trackClickable={trackClickable}
+      thumbTintColor={thumbColor}
+      trackMarks={trackMarks}
+      renderTrackMarkComponent={
+        trackMarks && trackMarkComponent
+          ? (index) => {
+              return trackMarkComponent(index);
+            }
+          : undefined
+      }
+    />
   );
 }
