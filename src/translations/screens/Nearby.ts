e826098429852126187b8111cbeac35b3e1aaf1f
Kristian Selnæs <kristian@selnaes.net>
2023-09-19 09:32:11 +0200

    Move texts in FavoriteDialogSheet

--- src/translations/screens/Nearby.ts
+++ src/translations/screens/Nearby.ts
@@ -40,33 +40,5 @@ const NearbyTexts = {
         `Lastar avgangar nær ${locationName}`,
       ),
   },
-
-  favoriteDialogSheet: {
-    title: _(
-      'Velg favorittavgang',
-      'Select favourite departure',
-      'Vel favorittavgang',
-    ),
-    description: (lineNumber: string, lineName: string) =>
-      _(
-        `Vil du favorittmarkere kun '${lineNumber} ${lineName}' eller alle variasjoner av linje ${lineNumber}?`,
-        `Do you want to favourite mark only '${lineNumber} ${lineName}' or all variations of line ${lineNumber}?`,
-        `Vil du kun merke '${lineNumber} ${lineName}' som favoritt eller alle variasjonane av linje ${lineNumber}?`,
-      ),
-    buttons: {
-      specific: (lineNumber: string, lineName: string) =>
-        _(
-          `Kun '${lineNumber} ${lineName}'`,
-          `Only '${lineNumber} ${lineName}'`,
-          `Berre '${lineNumber} ${lineName}'`,
-        ),
-      all: (lineNumber: string) =>
-        _(
-          `Alle variasjoner av linje ${lineNumber}`,
-          `All variations of line ${lineNumber}`,
-          `Alle variasjonar av linje ${lineNumber}`,
-        ),
-    },
-  },
 };
 export default NearbyTexts;
