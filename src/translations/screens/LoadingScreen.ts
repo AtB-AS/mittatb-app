d176e00ba34c80dce43bc430e9c70cb2883ce420
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-09-19 11:30:06 +0200

    fix: Change loading screen texts for FRAM

--- src/translations/screens/LoadingScreen.ts
+++ src/translations/screens/LoadingScreen.ts
@@ -1,4 +1,5 @@
 import {translation as _} from '../commons';
+import {orgSpecificTranslations} from '@atb/translations/orgSpecificTranslations';
 
 const LoadingScreenTexts = {
   error: {
@@ -37,4 +38,49 @@ const LoadingScreenTexts = {
     },
   },
 };
-export default LoadingScreenTexts;
+export default orgSpecificTranslations(LoadingScreenTexts, {
+  fram: {
+    error: {
+      heading: _(
+        'Noe gikk galt...',
+        'Something went wrong...',
+        'Noko gjekk gale...',
+      ),
+      description: _(
+        'Appen har problemer med å starte opp. Om det fortsetter, kan du forsøke å:\n' +
+          '\n  • Sjekke om du har nett' +
+          '\n  • Lukke og åpne appen igjen' +
+          '\n  • Slette og installere appen igjen' +
+          '\n  • Kontakte FRAM Kundesenter',
+        'The app is having trouble starting up. If it continues, you can try to:\n' +
+          '\n  • Check if you have internet' +
+          '\n  • Close and reopen the app' +
+          '\n  • Delete and reinstall the app' +
+          '\n  • Contact FRAM Customer Service',
+        'Appen har problem med å starta opp. Om det held fram, kan du prøve å:\n' +
+          '\n  • Sjekka om du har nett' +
+          '\n  • Lukke og opne appen igjen' +
+          '\n  • Sletta og installere appen igjen' +
+          '\n  • Kontakte FRAM Kundesenter',
+      ),
+      installId: (installId: string) =>
+        _(
+          `Install-id: ${installId}`,
+          `Install-id: ${installId}`,
+          `Install-id: ${installId}`,
+        ),
+      contactButton: {
+        text: _(
+          'Kontakt FRAM Kundesenter',
+          'Contact FRAM Customer Centre',
+          'Kontakt FRAM Kundesenter',
+        ),
+        a11yLabel: _(
+          'Kontakt FRAM Kundesenter, åpner side i nettleser',
+          'Contact FRAM Customer Centre, opens page in browser',
+          'Kontakt FRAM Kundesenter, opner side i nettlesar',
+        ),
+      },
+    },
+  },
+});
