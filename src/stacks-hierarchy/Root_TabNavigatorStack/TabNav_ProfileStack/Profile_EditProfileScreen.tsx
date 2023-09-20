2c809151b364ceb3c748ca41278af6c9d2d1677c
Hanne <hanne.overrein@atb.no>
2023-09-20 16:23:25 +0200

    feat: Added content to edit profile screen

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_EditProfileScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_EditProfileScreen.tsx
@@ -1,50 +1,126 @@
 import {ProfileScreenProps} from '@atb/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/navigation-types';
 import {Text, View} from 'react-native';
-import {LinkSectionItem} from '@atb/components/sections';
+import {
+  PhoneInputSectionItem,
+  Section,
+  TextInputSectionItem,
+} from '@atb/components/sections';
 import React from 'react';
 import {useTranslation} from '@atb/translations';
 import {EditProfileTexts} from '@atb/translations/screens/subscreens/EditProfileScreen';
 import {FullScreenView} from '@atb/components/screen-view';
+import {ThemeText} from '@atb/components/text';
+import {useTheme} from '@atb/theme';
+import {useAuthState} from '@atb/auth';
+import {Button} from '@atb/components/button';
+import Delete from '@atb/assets/svg/mono-icons/actions/Delete';
+import parsePhoneNumber from 'libphonenumber-js';
 
 type EditProfileScreenProps = ProfileScreenProps<'Profile_EditProfileScreen'>;
 export const Profile_EditProfileScreen = ({
   navigation,
 }: EditProfileScreenProps) => {
   const {t} = useTranslation();
+  const {theme} = useTheme();
+  const {authenticationType, user, customerNumber} = useAuthState();
+  const phoneNumber = parsePhoneNumber(
+    user?.phoneNumber ?? '',
+  )?.formatInternational();
 
   return (
-    <View>
-      {/*<FullScreenHeader*/}
-      {/*  title={t(EditProfileTexts.header.title)}*/}
-      {/*  leftButton={{type: 'back'}}*/}
-      {/*/>*/}
-      <FullScreenView
-        headerProps={{
-          title: 't(FavoriteDeparturesTexts.favoriteItemAdd.label)',
-          leftButton: {type: 'back'},
-        }}
-        // parallaxContent={() => (
-        //   <>
-        //     <View style={{height: 500, backgroundColor: 'red'}}>
-        //       <Text>{'Hello'}</Text>
-        //     </View>
-        //     <LinkSectionItem
-        //       text={t(EditProfileTexts.deleteProfile)}
-        //       onPress={() => navigation.navigate('Profile_DeleteProfileScreen')}
-        //     />
-        //   </>
-        // )}
-      >
-        <>
-          <View style={{height: 500, backgroundColor: 'red'}}>
-            <Text>{'Hello'}</Text>
+    <FullScreenView
+      headerProps={{
+        title: t(EditProfileTexts.header.title),
+        leftButton: {type: 'back', withIcon: true},
+      }}
+      parallaxContent={() => (
+        <View style={{marginHorizontal: theme.spacings.medium}}>
+          <ThemeText
+            type="heading--medium"
+            color="background_accent_0"
+            style={{flexShrink: 1}}
+          >
+            {t(EditProfileTexts.header.title)}
+          </ThemeText>
+        </View>
+      )}
+    >
+      <>
+        <Text accessibilityRole="header">
+          {t(EditProfileTexts.personalia.header)}
+        </Text>
+
+        <Section withPadding withTopPadding>
+          <TextInputSectionItem
+            label={t(EditProfileTexts.personalia.firstName.label)}
+            placeholder={t(EditProfileTexts.personalia.firstName.placeholder)}
+            keyboardType="phone-pad"
+            showClear
+            inlineLabel={false}
+          />
+        </Section>
+        <Section withPadding>
+          <TextInputSectionItem
+            label={t(EditProfileTexts.personalia.surname.label)}
+            placeholder={t(EditProfileTexts.personalia.surname.placeholder)}
+            keyboardType="phone-pad"
+            showClear
+            inlineLabel={false}
+          />
+        </Section>
+        <Section withPadding>
+          <TextInputSectionItem
+            label={'E-mail address'}
+            placeholder={'Add e-mail address'}
+            keyboardType="email-address"
+            showClear
+            inlineLabel={false}
+          />
+        </Section>
+        {authenticationType !== 'phone' ? (
+          <Section withPadding>
+            <PhoneInputSectionItem
+              label={t(EditProfileTexts.personalia.phone.header)}
+              prefix={'47'}
+              onChangePrefix={() => {}}
+              showClear
+            />
+          </Section>
+        ) : (
+          <View>
+            <ThemeText>{t(EditProfileTexts.personalia.phone.header)}</ThemeText>
+            <ThemeText>
+              {t(EditProfileTexts.personalia.phone.loggedIn(phoneNumber))}
+            </ThemeText>
           </View>
-          <LinkSectionItem
+        )}
+        <Section withPadding withTopPadding>
+          <Button
+            mode="primary"
+            text={t(EditProfileTexts.save)}
+            onPress={() => {}}
+          />
+        </Section>
+        <View>
+          <ThemeText>{'About profile'}</ThemeText>
+          <ThemeText>{'Log in provider'}</ThemeText>
+          <ThemeText>{t(EditProfileTexts.otp(phoneNumber))}</ThemeText>
+        </View>
+        <View>
+          <ThemeText>{t(EditProfileTexts.customerNumber)}</ThemeText>
+          <ThemeText>{customerNumber}</ThemeText>
+        </View>
+
+        <Section withPadding withTopPadding>
+          <Button
+            mode="primary"
+            interactiveColor={'interactive_destructive'}
+            leftIcon={{svg: Delete}}
             text={t(EditProfileTexts.deleteProfile)}
             onPress={() => navigation.navigate('Profile_DeleteProfileScreen')}
           />
-        </>
-      </FullScreenView>
-    </View>
+        </Section>
+      </>
+    </FullScreenView>
   );
 };
--- src/translations/screens/subscreens/EditProfileScreen.ts
+++ src/translations/screens/subscreens/EditProfileScreen.ts
@@ -2,7 +2,36 @@ import {translation as _} from '@atb/translations';
 
 export const EditProfileTexts = {
   header: {
-    title: _('Rediger Min profil', 'Edit My profile', 'Rediger Min profil'),
+    title: _('Rediger Min profil', 'Edit My profile', ''),
   },
+  personalia: {
+    header: _('Personalia', 'Personalia', ''),
+    firstName: {
+      label: _('Fornavn', 'First name', ''),
+      placeholder: _('Legg til fornavn', 'Add first name', ''),
+    },
+    surname: {
+      label: _('Etternavn', 'Surname', ''),
+      placeholder: _('Legg til etternavn', 'Add surname', ''),
+    },
+    phone: {
+      header: _('Telefon', 'Phone', ''),
+      loggedIn: (phoneNumber: string | undefined) =>
+        _(
+          `${phoneNumber} kan ikke endres fordi telefonnummeret er brukt til innlogging`,
+          `${phoneNumber} cannot be changed because the phone number is used for logging in.`,
+          '',
+        ),
+    },
+  },
+  customerNumber: _('Kundenummer', 'Customer number', 'Kundenummer'),
+  otp: (phoneNumber: string | undefined) =>
+    _(
+      `Engangspassord på SMS til ${phoneNumber}`,
+      `One-time password on SMS til ${phoneNumber}`,
+      '',
+    ),
+
+  save: _('Lagre', 'Save', 'Lagre'),
   deleteProfile: _('Slett Min profil', 'Delete My profile', 'Slett Min profil'),
 };
