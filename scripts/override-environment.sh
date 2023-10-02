da910647b59c18e738310f4ab5636d5a376c086f
Jorge Osorio <jorelosorio@gmail.com>
2023-10-02 10:20:53 +0200

    Revert changes and make fixes on gradle file config

--- scripts/override-environment.sh
+++ scripts/override-environment.sh
@@ -27,29 +27,6 @@ else
     echo "Copying $APP_ENVIRONMENT .env file to root"
     cp $ENV_FOLDER/.env .
 
-    echo "Copying AndroidManifest.xm file to android folder"
-    cp $ORG_FOLDER/AndroidManifest.xml android/app/src/main/AndroidManifest.xml
-
-    # Get KETTLE_API_KEY value
-    KETTLE_API_KEY="$(envprop 'KETTLE_API_KEY')"
-    if [[ -n "${KETTLE_API_KEY}" ]]; then
-        # NOTE: `brew` is a dependency, refer to README.md for more details
-        # Check if xmlstarlet is available, if not and brew is available, install xmlstarlet
-        command -v brew >/dev/null 2>&1 && echo "Installing xmlstarlet dependency" && brew install xmlstarlet
-
-        command -v xmlstarlet >/dev/null 2>&1 && \
-        echo "Adding [Kettle] API Key to AndroidManifest.xml" && \
-        xmlstarlet edit --inplace --omit-decl \
-            -s //manifest/application -t elem -n "kettlekey" \
-            -i //manifest/application/kettlekey -t attr -n "android:name" -v "com.kogenta.kettle.sdk.API_KEY" \
-            -i //manifest/application/kettlekey -t attr -n "android:value" -v "$KETTLE_API_KEY" \
-            -r //manifest/application/kettlekey -v meta-data \
-            android/app/src/main/AndroidManifest.xml || \
-        echo "Cannot add [Kettle] API Key to AndroidManifest.xml."
-    else
-        echo "Skipping [Kettle] API Key to AndroidManifest.xml"
-    fi
-
     echo "Copying $APP_ENVIRONMENT .env file to fastlane"
     cp $ENV_FOLDER/.env fastlane/.env.default
 
