817dda145f79c231be0f63eafdc6c93586aee51c
Jorge Osorio <jorelosorio@gmail.com>
2023-10-19 10:37:25 +0200

    Update run-android.sh

--- scripts/android/run-android.sh
+++ scripts/android/run-android.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-source ./scripts/utils.sh
+# Get the directory of the currently executing script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Source utils.sh from the same directory as this script
+source "${DIR}/utils.sh"
 
 react-native run-android --appId "$(envprop 'ANDROID_APPLICATION_ID')"
