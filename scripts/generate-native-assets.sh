b3234ba76a6c30d28bb43c64c7c0839433b4c011
Jorge Osorio <jorelosorio@gmail.com>
2023-10-19 10:37:29 +0200

    Update generate-native-assets.sh

--- scripts/generate-native-assets.sh
+++ scripts/generate-native-assets.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-source ./scripts/utils.sh
+# Get the directory of the currently executing script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Source utils.sh from the same directory as this script
+source "${DIR}/utils.sh"
 
 echo "Generating app icons"
 yarn icons
  
 echo "Generating native bootsplash"
-yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')" --flavor=$( [ "$(envprop 'APP_ORG')" = "atb" ] && echo 'beacons' || echo 'app' )
+yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')"
