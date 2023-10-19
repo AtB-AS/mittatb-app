ee427b3ad79c83465963def16f2049e1987ec628
Jorge Osorio <jorelosorio@gmail.com>
2023-10-19 10:37:27 +0200

    Update export-workflow-parameters.sh

--- scripts/export-workflow-parameters.sh
+++ scripts/export-workflow-parameters.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source ./scripts/utils.sh
+# Get the directory of the currently executing script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Source utils.sh from the same directory as this script
+source "${DIR}/utils.sh"
 
 # Export env vars from .env file
 echo "APP_VERSION=$(envprop APP_VERSION)" >> $GITHUB_ENV
