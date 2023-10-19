d6ac911d555fa3b6f0f6b25669f33a441d150707
Jorge Osorio <jorelosorio@gmail.com>
2023-10-19 11:05:17 +0200

    Update override-environment.sh

--- scripts/override-environment.sh
+++ scripts/override-environment.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-source ./scripts/utils.sh
+# Get the directory of the currently executing script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Source utils.sh from the same directory as this script
+source "${DIR}/utils.sh"
 
 if [ "$#" -ne 2 ]
 then
