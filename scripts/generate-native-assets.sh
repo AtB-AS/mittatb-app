54815026d2c45ac2b4148c5ca84123d70e7248ad
Jorge Osorio <43450882+jorelosorio@users.noreply.github.com>
2023-10-24 14:27:18 +0200

    revert: Kettle SDK (#3970)

--- scripts/generate-native-assets.sh
+++ scripts/generate-native-assets.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-envprop() { grep "^${1}=" ./.env | cut -d'=' -f2 | head -n 1; }
+# Read a property safely from config file, instead of sourcing the file
+prop() {
+  grep -e "^${1}=" ./.env | cut -d'=' -f2 | head -n 1;
+}
 
 echo "Generating app icons"
 yarn icons
  
 echo "Generating native bootsplash"
-yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')"
+yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(prop 'BOOTSPLASH_BACKGROUND_COLOR')"
