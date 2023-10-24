7faab4b42349e50226a7f8f15c798774277bdf99
Jorge Osorio <jorelosorio@gmail.com>
2023-10-24 15:15:55 +0200

    Revert "revert: Kettle SDK (#3970)"
    This reverts commit 54815026d2c45ac2b4148c5ca84123d70e7248ad.

--- scripts/generate-native-assets.sh
+++ scripts/generate-native-assets.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Read a property safely from config file, instead of sourcing the file
-prop() {
-  grep -e "^${1}=" ./.env | cut -d'=' -f2 | head -n 1;
-}
+envprop() { grep "^${1}=" ./.env | cut -d'=' -f2 | head -n 1; }
 
 echo "Generating app icons"
 yarn icons
  
 echo "Generating native bootsplash"
-yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(prop 'BOOTSPLASH_BACKGROUND_COLOR')"
+yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-path=assets/ --background-color="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')"
