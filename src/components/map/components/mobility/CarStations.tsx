e101b8fd08fd08cc85901948bc9a7acc992d19de
Johannes Røsvik <j.rosvik@gmail.com>
2023-10-25 15:15:36 +0200

    handle cluster click

--- src/components/map/components/mobility/CarStations.tsx
+++ src/components/map/components/mobility/CarStations.tsx
@@ -1,16 +1,23 @@
-import MapboxGL from '@rnmapbox/maps';
-import React from 'react';
-import {StationsWithCount} from './Stations';
-import {useTransportationColor} from '@atb/utils/use-transportation-color';
 import {Mode} from '@atb/api/types/generated/journey_planner_v3_types';
+import {useTransportationColor} from '@atb/utils/use-transportation-color';
+import MapboxGL, {ShapeSource} from '@rnmapbox/maps';
+import {OnPressEvent} from '@rnmapbox/maps/lib/typescript/types/OnPressEvent';
+import React, {RefObject, useRef} from 'react';
+import {StationsWithCount} from './Stations';
 
 type Props = {
   stations: StationsWithCount;
+  onClusterClick: (
+    e: OnPressEvent,
+    clustersSource: RefObject<ShapeSource>,
+  ) => void;
 };
 
-export const CarStations = ({stations}: Props) => {
+export const CarStations = ({stations, onClusterClick}: Props) => {
   const stationBackgroundColor = useTransportationColor(Mode.Car);
   const stationTextColor = useTransportationColor(Mode.Car, undefined, 'text');
+  const clustersSource = useRef<MapboxGL.ShapeSource>(null);
+
   return (
     <>
       <MapboxGL.ShapeSource
@@ -38,12 +45,14 @@ export const CarStations = ({stations}: Props) => {
       </MapboxGL.ShapeSource>
       <MapboxGL.ShapeSource
         id="carStationCluster"
+        ref={clustersSource}
         shape={stations}
         tolerance={0}
         cluster
         clusterProperties={{
           sum: ['+', ['get', 'count']],
         }}
+        onPress={(e) => onClusterClick(e, clustersSource)}
       >
         <MapboxGL.SymbolLayer
           id="carStationClusterPin"
--- src/components/map/components/mobility/Stations.tsx
+++ src/components/map/components/mobility/Stations.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import {FeatureCollection, GeoJSON} from 'geojson';
 import {StationBasicFragment} from '@atb/api/types/generated/fragments/stations';
 import {FormFactor} from '@atb/api/types/generated/mobility-types_v2';
-import {BikeStations} from './BikeStations';
+import {
+  StationFeatures,
+  flyToLocation,
+  isClusterFeature,
+} from '@atb/components/map';
 import {getAvailableVehicles} from '@atb/mobility/utils';
+import {Camera, ShapeSource} from '@rnmapbox/maps';
+import {OnPressEvent} from '@rnmapbox/maps/lib/typescript/types/OnPressEvent';
+import {Feature, FeatureCollection, Point} from 'geojson';
+import React, {RefObject} from 'react';
+import {Cluster} from '../../types';
+import {mapPositionToCoordinates} from '../../utils';
+import {BikeStations} from './BikeStations';
 import {CarStations} from './CarStations';
-import {StationFeatures} from '@atb/components/map';
 
 type Props = {
   stations: StationFeatures;
+  mapCameraRef: RefObject<Camera>;
+  onClusterClick: (feature: Feature<Point, Cluster>) => void;
 };
 
 export type StationsWithCount = FeatureCollection<
@@ -16,14 +26,33 @@ export type StationsWithCount = FeatureCollection<
   StationBasicFragment & {count: number}
 >;
 
-export const Stations = ({stations}: Props) => {
+export const Stations = ({stations, onClusterClick, mapCameraRef}: Props) => {
   const bikeStations = stationsWithCount(stations.bicycles, FormFactor.Bicycle);
   const carStations = stationsWithCount(stations.cars, FormFactor.Car);
+
+  const handleClusterClick = async (
+    e: OnPressEvent,
+    clustersSource: RefObject<ShapeSource>,
+  ) => {
+    const [feature, ,] = e.features;
+    if (isClusterFeature(feature)) {
+      const clusterExpansionZoom =
+        (await clustersSource.current?.getClusterExpansionZoom(feature)) ?? 0;
+      const zoomLevel = Math.max(clusterExpansionZoom, 17.5);
+      flyToLocation({
+        coordinates: mapPositionToCoordinates(feature.geometry.coordinates),
+        mapCameraRef,
+        zoomLevel,
+        animationDuration: 400,
+      });
+      onClusterClick(feature);
+    }
+  };
 
   return (
     <>
       <BikeStations stations={bikeStations} />
-      <CarStations stations={carStations} />
+      <CarStations stations={carStations} onClusterClick={handleClusterClick} />
     </>
   );
 };
