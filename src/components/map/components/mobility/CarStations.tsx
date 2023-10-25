dcdd350b420ca32a9b3cc52a90130ab9c89bff93
Johannes Røsvik <j.rosvik@gmail.com>
2023-10-25 13:22:23 +0200

    feat: add car station clusters

--- src/components/map/components/mobility/CarStations.tsx
+++ src/components/map/components/mobility/CarStations.tsx
@@ -12,35 +12,68 @@ export const CarStations = ({stations}: Props) => {
   const stationBackgroundColor = useTransportationColor(Mode.Car);
   const stationTextColor = useTransportationColor(Mode.Car, undefined, 'text');
   return (
-    <MapboxGL.ShapeSource id="carStations" shape={stations} tolerance={0}>
-      <MapboxGL.SymbolLayer
-        id="carStationPin"
-        minZoomLevel={12}
-        style={{
-          textField: ['get', 'count'],
-          textAnchor: 'center',
-          textOffset: [0.75, 0],
-          textColor: stationBackgroundColor,
-          textSize: 12,
-          textAllowOverlap: true,
-          iconImage: 'CarChip',
-          iconAllowOverlap: true,
-          iconSize: 0.85,
-        }}
-      />
-      <MapboxGL.CircleLayer
-        id="carStationMini"
-        maxZoomLevel={12}
-        minZoomLevel={11}
-        style={{
-          circleColor: stationBackgroundColor,
-          circleStrokeColor: stationTextColor,
-          circleOpacity: 0.7,
-          circleStrokeOpacity: 0.7,
-          circleRadius: 4,
-          circleStrokeWidth: 1,
-        }}
-      />
-    </MapboxGL.ShapeSource>
+    <>
+      <MapboxGL.ShapeSource
+        id="carStation"
+        shape={stations}
+        tolerance={0}
+        cluster
+      >
+        <MapboxGL.SymbolLayer
+          id="carStationPin"
+          filter={['!', ['has', 'point_count']]}
+          minZoomLevel={12}
+          style={{
+            textField: ['get', 'count'],
+            textAnchor: 'center',
+            textOffset: [0.75, 0],
+            textColor: stationBackgroundColor,
+            textSize: 12,
+            textAllowOverlap: false,
+            iconImage: 'CarChip',
+            iconAllowOverlap: true,
+            iconSize: 0.85,
+          }}
+        />
+      </MapboxGL.ShapeSource>
+      <MapboxGL.ShapeSource
+        id="carStationCluster"
+        shape={stations}
+        tolerance={0}
+        cluster
+      >
+        <MapboxGL.SymbolLayer
+          id="carStationClusterPin"
+          minZoomLevel={12}
+          filter={['has', 'point_count']}
+          style={{
+            textField: ['concat', ['get', 'point_count'], '+'],
+            textAnchor: 'center',
+            textOffset: [0.75, 0],
+            textColor: stationBackgroundColor,
+            textSize: 12,
+            textAllowOverlap: false,
+            iconImage: 'CarChip',
+            iconAllowOverlap: true,
+            iconSize: 0.85,
+          }}
+        />
+      </MapboxGL.ShapeSource>
+      <MapboxGL.ShapeSource id="carStationMini" shape={stations} tolerance={0}>
+        <MapboxGL.CircleLayer
+          id="carStationMiniPin"
+          maxZoomLevel={12}
+          minZoomLevel={11}
+          style={{
+            circleColor: stationBackgroundColor,
+            circleStrokeColor: stationTextColor,
+            circleOpacity: 0.7,
+            circleStrokeOpacity: 0.7,
+            circleRadius: 4,
+            circleStrokeWidth: 1,
+          }}
+        />
+      </MapboxGL.ShapeSource>
+    </>
   );
 };
