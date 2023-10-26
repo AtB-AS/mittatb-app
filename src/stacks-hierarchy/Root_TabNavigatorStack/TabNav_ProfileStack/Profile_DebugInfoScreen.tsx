58411a733f46480f288ea45358a2f8b65d689bb1
Jorge Osorio <jorelosorio@gmail.com>
2023-10-26 15:20:08 +0200

    Update Profile_DebugInfoScreen.tsx

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_DebugInfoScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_DebugInfoScreen.tsx
@@ -606,7 +606,7 @@ export const Profile_DebugInfoScreen = () => {
         {isBeaconsEnabled && !!KETTLE_API_KEY && (
           <Section withPadding withTopPadding>
             <ExpandableSectionItem
-              text="Kettle SDK"
+              text="Beacons"
               showIconText={true}
               expandContent={
                 <View>
