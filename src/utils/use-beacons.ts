980286c06b6fbdfcd3a7b3448bc81c902b0fd87d
Jorge Osorio <jorelosorio@gmail.com>
2023-10-26 22:00:18 +0200

    Update use-beacons.ts

--- src/utils/use-beacons.ts
+++ src/utils/use-beacons.ts
@@ -1,17 +1,22 @@
+import {useIsBeaconsEnabled} from '@atb/beacons/use-is-beacons-enabled';
+import {KETTLE_API_KEY} from '@env';
 import {useCallback, useEffect} from 'react';
 import {NativeModules} from 'react-native';
 
 export const useBeacons = () => {
-  const startKettleSDK = useCallback(async () => {
+  const [isBeaconsEnabled] = useIsBeaconsEnabled();
+  const isBeaconsSupported = isBeaconsEnabled && !!KETTLE_API_KEY;
+
+  const initializeBeacons = useCallback(async () => {
     // This will set kettle if KETTLE_API_KEY is set on the native side
     await NativeModules.KettleSDKExtension.initializeKettleSDK();
-  }, []);
+  }, [NativeModules]);
 
   useEffect(() => {
-    startKettleSDK();
-  }, [startKettleSDK]);
+    if (isBeaconsSupported) {
+      initializeBeacons();
+    }
+  }, [initializeBeacons, isBeaconsSupported]);
 
-  return {
-    startKettleSDK,
-  };
+  return {};
 };
