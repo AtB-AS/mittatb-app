efdfff9db27a32aea3380d2ce34c646e4bace232
Kristian Selnæs <kristian@selnaes.net>
2023-11-01 15:28:59 +0100

    useMutation instead of useQuery

--- src/notifications/use-register-for-push-notifications.tsx
+++ src/notifications/use-register-for-push-notifications.tsx
@@ -1,12 +1,7 @@
-import {useQuery} from '@tanstack/react-query';
+import {useMutation} from '@tanstack/react-query';
 import {registerForPushNotifications} from '@atb/notifications/api';
 
-const ONE_DAY = 1000 * 60 * 60 * 24;
-export const useRegisterForPushNotifications = (token?: string) =>
-  useQuery({
-    queryKey: ['notification/register', {token}],
-    queryFn: () => registerForPushNotifications(token!),
-    enabled: Boolean(token),
-    staleTime: ONE_DAY,
-    cacheTime: ONE_DAY,
+export const useRegisterForPushNotifications = () =>
+  useMutation({
+    mutationFn: registerForPushNotifications,
   });
