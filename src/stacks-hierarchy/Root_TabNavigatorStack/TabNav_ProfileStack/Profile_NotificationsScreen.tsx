5c8287788e21eb228be92b0f62ef0201cb930944
Kristian Selnæs <kristian@selnaes.net>
2023-11-01 14:50:02 +0100

    Register device token in backend

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_NotificationsScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_NotificationsScreen.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import {FullScreenView} from '@atb/components/screen-view';
 import {Section, ToggleSectionItem} from '@atb/components/sections';
-import {View} from 'react-native';
+import {Linking, View} from 'react-native';
 import {ThemeText} from '@atb/components/text';
 import {StaticColorByType} from '@atb/theme/colors';
 import {StyleSheet} from '@atb/theme';
 import {ProfileTexts, useTranslation} from '@atb/translations';
+import {usePushNotifications} from '@atb/notifications';
+import {MessageBox} from '@atb/components/message-box';
 
 const themeColor: StaticColorByType<'background'> = 'background_accent_0';
 
 export const Profile_NotificationsScreen = () => {
   const style = useStyles();
   const {t} = useTranslation();
+  const {
+    isPermissionAccepted: isPushPermissionAccepted,
+    isLoading,
+    isError,
+    register,
+  } = usePushNotifications();
 
   const handlePushNotificationToggle = async (checked: boolean) => {
-    console.log('Turn push', checked ? 'on' : 'off');
+    if (checked) {
+      register();
+    }
+    // TODO: Update /config if !checked
   };
 
   return (
@@ -47,9 +58,46 @@ export const Profile_NotificationsScreen = () => {
               ProfileTexts.sections.settings.linkSectionItems.notifications
                 .pushToggle.subText,
             )}
+            value={isPushPermissionAccepted}
+            disabled={isLoading}
             onValueChange={handlePushNotificationToggle}
           />
         </Section>
+        {!isLoading && !isError && isPushPermissionAccepted === false && (
+          <MessageBox
+            style={style.messageBox}
+            type="info"
+            title={t(
+              ProfileTexts.sections.settings.linkSectionItems.notifications
+                .permissionRequired.title,
+            )}
+            message={t(
+              ProfileTexts.sections.settings.linkSectionItems.notifications
+                .permissionRequired.message,
+            )}
+            onPressConfig={{
+              text: t(
+                ProfileTexts.sections.settings.linkSectionItems.notifications
+                  .permissionRequired.action,
+              ),
+              action: () => Linking.openSettings(),
+            }}
+          />
+        )}
+        {!isLoading && isError && (
+          <MessageBox
+            style={style.messageBox}
+            type="error"
+            title={t(
+              ProfileTexts.sections.settings.linkSectionItems.notifications
+                .permissionError.title,
+            )}
+            message={t(
+              ProfileTexts.sections.settings.linkSectionItems.notifications
+                .permissionError.message,
+            )}
+          />
+        )}
       </View>
     </FullScreenView>
   );
@@ -61,5 +109,8 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
   },
   content: {
     marginTop: theme.spacings.medium,
+  },
+  messageBox: {
+    margin: theme.spacings.medium,
   },
 }));
