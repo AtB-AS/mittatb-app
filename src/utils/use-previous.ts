624d5e040ba5ff54cb1b6f823339bf795f31c11d
Gøran Dalum <goran.dalum@gmail.com>
2023-11-02 10:33:42 +0100

    refactor: Make use-pollable-resource exhaustive deps fix
    - Adding prevState to dependency array of reload funcion is safe as
      it is a reference.
    - Adding setisLoading to dependency array of reload function is
      safe as it is referentially stable.
    - Adding reload to dependency array of the reload-on-focus
      useEffect is safe because the original
      load-on-reload-function-change useEffect now sets firstLoadIsDone
      to false.
    - AbortController changed to ref as we don't want to add it in the
      dependency list for the useInterval hook.
    
    And as a final note we should probably look into using useReactQuery
    for this logic, as it supports most of what usePollableResource is
    trying to achieve.

--- src/utils/use-previous.ts
+++ src/utils/use-previous.ts
@@ -1,5 +1,9 @@
 import {useEffect, useRef} from 'react';
 
+/**
+ * Returns a current reference to the previous value. The returned value is safe
+ * to use in dependency arrays as refs won't trigger reruns.
+ */
 export const usePrevious = <T>(value: T): T | undefined => {
   const ref = useRef<T>();
   useEffect(() => {
