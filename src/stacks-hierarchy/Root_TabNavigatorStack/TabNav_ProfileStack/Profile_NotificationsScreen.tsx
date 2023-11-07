d4c0e056b38eebdcea2820e775ff6afbb8ce0280
Kristian Selnæs <kristian@selnaes.net>
2023-11-07 13:32:04 +0100

    Update src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_NotificationsScreen.tsx
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_NotificationsScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_NotificationsScreen.tsx
@@ -105,7 +105,7 @@ export const Profile_NotificationsScreen = () => {
                 }}
               />
             )}
-          {!isLoading && isError && (
+          {isError && (
             <MessageBox
               style={style.messageBox}
               type="error"
