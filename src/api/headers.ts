6b3848b27a6292239615f902e2e7d7646f524091
Kristian Selnæs <kristian@selnaes.net>
2023-11-10 15:20:36 +0100

    Move headers

--- src/api/headers.ts
+++ src/api/headers.ts
@@ -2,4 +2,6 @@ export const InstallIdHeaderName = 'Atb-Install-Id';
 export const RequestIdHeaderName = 'Atb-Request-Id';
 export const FirebaseAuthIdHeaderName = 'Atb-Firebase-Auth-Id';
 export const AppVersionHeaderName = 'Atb-App-Version';
-export const PlatformVersionHeaderName = 'Atb-Platform-Version';
+export const PlatformHeaderName = 'Atb-App-Platform';
+export const PlatformVersionHeaderName = 'Atb-App-Platform-Version';
+export const AppIdentifierHeaderName = 'Atb-App-Identifier';
