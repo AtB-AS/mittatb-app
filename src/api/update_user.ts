eb28062cbfb2c44aca346b1f5f20ae6627cc3adb
Hanne <hanne.overrein@atb.no>
2023-11-14 13:28:20 +0100

    feat: add enpoints

--- src/api/update_user.ts
+++ src/api/update_user.ts
@@ -1,18 +1,38 @@
 import {client} from '@atb/api/client';
+import {AxiosRequestConfig} from 'axios';
 
-type ProfileParams = {
-  email?: string;
-  firstName?: string;
-  id?: string;
-  surname?: string;
-  phone?: string;
-  tcard?: string;
-};
+export type CustomerProfileUpdate = Partial<{
+  firstName: string;
+  surname: string;
+  email: string;
+  phone: string;
+}>;
 
-export const updateProfile = async (profileParams: ProfileParams) => {
-  const url = '';
-  const response = await client.post(url, profileParams, {
+export const getProfile = async () => {
+  const url = 'api/profile/get'; // ??
+  const response = await client.get(url, {
     authWithIdToken: true,
   });
+  return response.data;
+};
+
+export const updateProfile = async (profileParams: CustomerProfileUpdate) => {
+  const url = 'api/profile/update';
+  const response = await client.patch(
+    url,
+    {profileParams},
+    {
+      authWithIdToken: true,
+    },
+  );
+  return response.data;
+};
+
+export const emailAvailable = async (
+  email: string,
+  opts?: AxiosRequestConfig,
+): Promise<boolean> => {
+  const url = `/webshop/v1/available-email?email=${encodeURIComponent(email)}`; // https://github.com/AtB-AS/webshop2/blob/3fdbec83f9cb7368b519c3efd98531dc23dc777b/src/server-api-service/unauth-client.ts
+  const response = await client.get<boolean>(url, opts);
   return response.data;
 };
