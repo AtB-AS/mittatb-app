cd07e03e326d778c8df2275c91edae8dcda118fd
Tor M <tor.kjetil.moseng@kantega.no>
2023-11-21 14:33:43 +0100

    Update review comment
    Co-authored-by: marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>

--- src/translations/screens/subscreens/DeleteProfile.ts
+++ src/translations/screens/subscreens/DeleteProfile.ts
@@ -8,7 +8,7 @@ const DeleteProfileTexts = {
   deleteInfo: _(
     'Når du sletter Min Profil vil alle dine lagrede data slettes. Du mister også tilgang på billett- og kjøpshistorikk, kvitteringer og dine innstillinger.',
     'When you delete My Profile, all your stored data will be deleted. You will also lose access to recent tickets, receipts and settings',
-    'Dersom du sletter Min profil vil alle dine lagra data bli sletta. Du mistar òg tilgang til billett- og kjøpshistorikk, kvitteringar og innstillingane dine.',
+    'Dersom du slettar Min Profil vil alle dine lagra data bli sletta. Du mistar òg tilgang til billett- og kjøpshistorikk, kvitteringar og innstillingane dine.',
   ),
   buttonA11ytext: (customerNumber: string | undefined) =>
     _(
