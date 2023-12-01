8fc5a2c8622e6820c92e2cd20927efcdc62ca2be
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2023-12-01 14:07:02 +0100

    Update AnonymousPurchaseConsequencesScreenComponent.tsx

--- src/anonymous-purchase-consequences-screen/AnonymousPurchaseConsequencesScreenComponent.tsx
+++ src/anonymous-purchase-consequences-screen/AnonymousPurchaseConsequencesScreenComponent.tsx
@@ -4,26 +4,25 @@ import {Receipt} from '@atb/assets/svg/mono-icons/ticketing';
 import {FullScreenHeader} from '@atb/components/screen-header';
 import {ThemeText} from '@atb/components/text';
 import {ThemeIcon} from '@atb/components/theme-icon';
-import {Actions} from '@atb/anonymous-purchase-consequences-screen/components/Actions';
-import {Consequence} from '@atb/anonymous-purchase-consequences-screen/components/Consequence';
+
+import {Consequence} from './components/Consequence';
 import {StyleSheet, useTheme} from '@atb/theme';
 import {getStaticColor, StaticColorByType} from '@atb/theme/colors';
 import {AnonymousPurchasesTexts, useTranslation} from '@atb/translations';
 import {useFocusOnLoad} from '@atb/utils/use-focus-on-load';
 import React from 'react';
 import {ScrollView, View} from 'react-native';
+import {Button} from '@atb/components/button';
+import {ArrowRight} from '@atb/assets/svg/mono-icons/navigation';
+import {FullScreenFooter} from '@atb/components/screen-footer';
 
 const themeColor: StaticColorByType<'background'> = 'background_accent_0';
 
 type Props = {
-  onPressLogin: () => void;
   onPressContinueWithoutLogin: () => void;
-  showHeader: boolean;
 };
 
-export const AnonymousPurchaseConsequencesScreenComponent = ({
-  showHeader,
-  onPressLogin,
+export const AnonymousPurchaseConsequencesScreen = ({
   onPressContinueWithoutLogin,
 }: Props) => {
   const styles = useStyle();
@@ -34,26 +33,20 @@ export const AnonymousPurchaseConsequencesScreenComponent = ({
   const fillColor = getStaticColor(themeName, themeColor).text;
 
   return (
-    <>
-      {showHeader && (
-        <FullScreenHeader
-          leftButton={{type: 'cancel'}}
-          setFocusOnLoad={false}
-        />
-      )}
+    <View style={styles.container}>
+      <FullScreenHeader leftButton={{type: 'back'}} setFocusOnLoad={false} />
       <ScrollView
-        style={styles.container}
         contentContainerStyle={styles.contentContainer}
         ref={focusRef}
       >
-        <ThemeText
-          type="heading--jumbo"
-          color={themeColor}
-          style={styles.header}
-        >
-          {t(AnonymousPurchasesTexts.consequences.title)}
-        </ThemeText>
-        <View>
+        <View style={styles.mainContent}>
+          <ThemeText
+            type="heading--big"
+            color={themeColor}
+            style={styles.header}
+          >
+            {t(AnonymousPurchasesTexts.consequences.title)}
+          </ThemeText>
           <Consequence
             value={t(AnonymousPurchasesTexts.consequences.messages[0])}
             icon={<ThemeIcon svg={Phone} fill={fillColor} size="large" />}
@@ -67,33 +60,45 @@ export const AnonymousPurchaseConsequencesScreenComponent = ({
             icon={<ThemeIcon svg={Support} fill={fillColor} size="large" />}
           />
         </View>
-        <View style={styles.buttons}>
-          <Actions
-            primaryAction={onPressLogin}
-            secondaryAction={onPressContinueWithoutLogin}
-          />
-        </View>
       </ScrollView>
-    </>
+      <FullScreenFooter>
+        <Button
+          interactiveColor="interactive_0"
+          mode="primary"
+          onPress={onPressContinueWithoutLogin}
+          style={styles.acceptConsequencesButton}
+          text={t(AnonymousPurchasesTexts.consequences.button.accept.label)}
+          accessibilityHint={t(
+            AnonymousPurchasesTexts.consequences.button.accept.a11yHint,
+          )}
+          rightIcon={{svg: ArrowRight}}
+          testID="acceptConsequencesButton"
+        />
+      </FullScreenFooter>
+    </View>
   );
 };
 
 const useStyle = StyleSheet.createThemeHook((theme) => ({
   container: {
     backgroundColor: theme.static.background.background_accent_0.background,
-    paddingHorizontal: theme.spacings.xLarge,
-    paddingTop: theme.spacings.xLarge,
     flex: 1,
   },
   contentContainer: {
-    justifyContent: 'space-between',
     flexGrow: 1,
+    marginHorizontal: theme.spacings.xLarge,
+  },
+  mainContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
   header: {
-    marginTop: theme.spacings.xLarge,
+    margin: theme.spacings.xLarge,
     textAlign: 'center',
+    fontWeight: '700',
   },
-  buttons: {
-    marginVertical: theme.spacings.xLarge,
+  acceptConsequencesButton: {
+    marginHorizontal: theme.spacings.medium,
+    marginTop: theme.spacings.medium,
   },
 }));
