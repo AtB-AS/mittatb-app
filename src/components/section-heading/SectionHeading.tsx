56744e2c2f9c8527509f9ebe4d86f811eda0e2df
Johannes Røsvik <j.rosvik@gmail.com>
2023-12-05 15:27:03 +0100

    feat: use SectionHeading on Profile RootScreen

--- src/components/section-heading/SectionHeading.tsx
+++ src/components/section-heading/SectionHeading.tsx
@@ -7,17 +7,23 @@ import {ContrastColor, StaticColor, TextColor} from '@atb/theme/colors';
 type SectionHeaderProps = {
   text: string;
   color?: TextColor | StaticColor | ContrastColor;
+  accessibilityLabel?: string;
 };
 
 export function SectionHeading({
   text,
   color = 'secondary',
+  accessibilityLabel,
 }: SectionHeaderProps): JSX.Element {
   const styles = useStyles();
 
   return (
     <View style={styles.container}>
-      <ThemeText type="body__secondary" color={color}>
+      <ThemeText
+        type="body__secondary"
+        color={color}
+        accessibilityLabel={accessibilityLabel}
+      >
         {text}
       </ThemeText>
     </View>
