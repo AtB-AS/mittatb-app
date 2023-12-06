946fdc2e0bbf04fe8bb5daff7ee52ba84b5742e4
Kristian Selnæs <kristian@selnaes.net>
2023-12-06 09:24:47 +0100

    fix: Update mobility images according to theme (#4102)
    * fix: Update mobility images according to theme
    
    * Remove unused hook
    
    * Update BicycleSheet

--- src/mobility/components/BicycleSheet.tsx
+++ src/mobility/components/BicycleSheet.tsx
@@ -24,8 +24,8 @@ import {OperatorBenefit} from '@atb/mobility/components/OperatorBenefit';
 import {FormFactor} from '@atb/api/types/generated/mobility-types_v2';
 import {MobilityStats} from '@atb/mobility/components//MobilityStats';
 import {MobilityStat} from '@atb/mobility/components//MobilityStat';
-import {CityBike} from '@atb/assets/svg/color/images/mobility';
 import {BrandingImage} from '@atb/mobility/components/BrandingImage';
+import {ThemedCityBike} from '@atb/theme/ThemedAssets';
 
 type Props = {
   vehicleId: VehicleId;
@@ -119,7 +119,7 @@ export const BicycleSheet = ({vehicleId: id, close}: Props) => {
                     />
                     <BrandingImage
                       logoUrl={brandLogoUrl}
-                      fallback={<CityBike />}
+                      fallback={<ThemedCityBike />}
                     />
                   </View>
                 </GenericSectionItem>
