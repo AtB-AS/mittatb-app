e793f0c8723af90af25c58773e990f5519babfb2
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-12-18 14:55:18 +0100

    Added to and updated storybook

---
+++ src/storybook/stories/MessageInfoText.stories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View} from 'react-native';
+import {getStaticColor} from '@atb/theme/colors';
+import {MessageInfoText} from '@atb/components/message-info-text';
+
+const MessageInfoTextMeta = {
+  title: 'MessageInfoText',
+  component: MessageInfoText,
+  argTypes: {
+    theme: {
+      control: 'select',
+      options: ['light', 'dark'],
+    },
+  },
+  args: {
+    theme: 'light',
+    message: 'The message body.',
+    iconSide: 'left',
+    isMarkdown: false,
+  },
+  decorators: [
+    (Story, {args}) => (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 12,
+          flex: 1,
+          backgroundColor:
+            getStaticColor(args.theme, args.textColor)?.background ||
+            getStaticColor(args.theme, 'background_0').background,
+          gap: 12,
+        }}
+      >
+        <Story args={{...args, type: 'info'}} />
+        <Story args={{...args, type: 'valid'}} />
+        <Story args={{...args, type: 'warning'}} />
+        <Story args={{...args, type: 'error'}} />
+      </View>
+    ),
+  ],
+};
+
+export default MessageInfoTextMeta;
+
+export const LeftIcon = {};
+export const RightIcon = {args: {iconSide: 'right'}};
+export const WithMarkdown = {
+  args: {
+    isMarkdown: true,
+    message: 'Message **with** markdown _enabled_.',
+  },
+};
