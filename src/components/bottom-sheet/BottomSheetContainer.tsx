ef61a653f4658464be68e2aaa5cda6139a962c8a
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-12-20 12:52:01 +0100

    Add to all bottomsheets

--- src/components/bottom-sheet/BottomSheetContainer.tsx
+++ src/components/bottom-sheet/BottomSheetContainer.tsx
@@ -1,11 +1,13 @@
 import React, {ReactNode} from 'react';
 import {useWindowDimensions, View} from 'react-native';
+import {BottomSheetHeader} from '@atb/components/bottom-sheet';
 
 export type BottomSheetContainerProps = {
   children: ReactNode;
   maxHeightValue?: number;
   fullHeight?: boolean;
   testID?: string;
+  bottomSheetTitle?: string;
 };
 
 export function BottomSheetContainer({
@@ -13,12 +15,14 @@ export function BottomSheetContainer({
   maxHeightValue = 0.8,
   fullHeight,
   testID,
+  bottomSheetTitle,
 }: BottomSheetContainerProps) {
   const {height: windowHeight} = useWindowDimensions();
   const maxHeight = windowHeight * maxHeightValue;
   const height = fullHeight ? maxHeight : 'auto';
   return (
     <View style={{maxHeight, height}} testID={testID}>
+      <BottomSheetHeader title={bottomSheetTitle} />
       {children}
     </View>
   );
