a754e78a2db030463d677e477aca4cf0fd6f90c8
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2023-12-21 14:12:08 +0100

    Update use-offer-defaults.ts

--- src/stacks-hierarchy/Root_PurchaseOverviewScreen/use-offer-defaults.ts
+++ src/stacks-hierarchy/Root_PurchaseOverviewScreen/use-offer-defaults.ts
@@ -34,7 +34,7 @@ export function useOfferDefaults(
     .filter((product) => isProductSellableInApp(product, customerProfile))
     .filter((product) => product.type === productType);
   const defaultPreassignedFareProduct =
-    preassignedFareProduct ?? selectableProducts[1]; //Default 30 day ticket
+    preassignedFareProduct ?? selectableProducts[1]; //Default 30day ticket
 
   // Get default TariffZones
   const defaultTariffZone = useDefaultTariffZone(tariffZones);
