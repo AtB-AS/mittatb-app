31452dc04a083b67ea268253250c60ebd3441310
Kristian Selnæs <kristian@selnaes.net>
2023-12-22 14:54:10 +0100

    PageHeading component

---
+++ src/components/heading/PageHeading.tsx
@@ -0,0 +1,38 @@
+import {StyleSheet} from '@atb/theme';
+import React, {forwardRef} from 'react';
+import {View} from 'react-native';
+import {ThemeText} from '../text';
+import {ContrastColor, StaticColor, TextColor} from '@atb/theme/colors';
+
+type PageHeadingProps = {
+  text: string;
+  color?: TextColor | StaticColor | ContrastColor;
+  accessibilityLabel?: string;
+};
+
+export const PageHeading = forwardRef<any, PageHeadingProps>(
+  (
+    {text, color = 'background_accent_0', accessibilityLabel}: PageHeadingProps,
+    ref,
+  ) => {
+    const styles = useStyles();
+
+    return (
+      <View style={styles.container} ref={ref}>
+        <ThemeText
+          type="heading--medium"
+          color={color}
+          accessibilityLabel={accessibilityLabel}
+        >
+          {text}
+        </ThemeText>
+      </View>
+    );
+  },
+);
+
+const useStyles = StyleSheet.createThemeHook((theme) => ({
+  container: {
+    marginHorizontal: theme.spacings.medium,
+  },
+}));
