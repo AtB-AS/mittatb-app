7c420569b8677def775bb844e33c98050cba6359
Kristian Selnæs <kristian@selnaes.net>
2023-12-22 15:20:09 +0100

    Use PageHeading on TicketHistoryScreen

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_TicketHistoryScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_TicketHistoryScreen.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
 import {useTranslation, TicketingTexts} from '@atb/translations';
 import {StyleSheet} from '@atb/theme';
 import {filterExpiredFareContracts, useTicketingState} from '@atb/ticketing';
 import {FareContractAndReservationsList} from '@atb/fare-contracts';
-import {FullScreenHeader} from '@atb/components/screen-header';
 import TicketHistoryTexts from '@atb/translations/screens/subscreens/TicketHistory';
 import {useTimeContextState} from '@atb/time';
+import {FullScreenView} from '@atb/components/screen-view';
+import {PageHeading} from '@atb/components/heading';
 
 export const Profile_TicketHistoryScreen: React.FC = () => {
   const {
@@ -22,14 +22,17 @@ export const Profile_TicketHistoryScreen: React.FC = () => {
     serverNow,
   );
 
-  const styles = useStyles();
   const {t} = useTranslation();
   return (
-    <View style={styles.container}>
-      <FullScreenHeader
-        title={t(TicketHistoryTexts.header)}
-        leftButton={{type: 'back'}}
-      />
+    <FullScreenView
+      headerProps={{
+        title: t(TicketHistoryTexts.header),
+        leftButton: {type: 'back', withIcon: true},
+      }}
+      parallaxContent={() => (
+        <PageHeading text={t(TicketHistoryTexts.header)} />
+      )}
+    >
       <FareContractAndReservationsList
         fareContracts={expiredFareContracts}
         reservations={rejectedReservations}
@@ -45,13 +48,8 @@ export const Profile_TicketHistoryScreen: React.FC = () => {
             .emptyTicketHistoryDetails,
         )}
       />
-    </View>
+    </FullScreenView>
   );
 };
 
-const useStyles = StyleSheet.createThemeHook((theme) => ({
-  container: {
-    flex: 1,
-    backgroundColor: theme.static.background.background_1.background,
-  },
-}));
+const useStyles = StyleSheet.createThemeHook((theme) => ({}));
