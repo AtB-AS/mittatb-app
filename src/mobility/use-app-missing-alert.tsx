c4e448384ab3a3ccbc308ff3d31341d36c513296
Kristian Selnæs <kristian@selnaes.net>
2023-12-29 11:45:47 +0100

    fix: Remove exhaustive deps errors in mobility (#4165)
    * fix: Remove exhaustive deps errors in mobility
    
    * Remove useCallback
    
    * Revert useVehicles fixes

--- src/mobility/use-app-missing-alert.tsx
+++ src/mobility/use-app-missing-alert.tsx
@@ -1,58 +1,62 @@
 import {Alert, AlertButton, Linking} from 'react-native';
 import {MobilityTexts} from '@atb/translations/screens/subscreens/MobilityTexts';
 import {useTranslation} from '@atb/translations';
+import {useCallback} from 'react';
 
 export const useAppMissingAlert = () => {
   const {t} = useTranslation();
 
-  const appStoreOpenError = (operatorName: string) => {
-    const appStore = t(MobilityTexts.appStore());
-    Alert.alert(
-      '',
-      t(MobilityTexts.appStoreOpenError.text(appStore, operatorName)),
-      [
-        {
-          text: t(MobilityTexts.appStoreOpenError.button),
-          style: 'cancel',
-        },
-      ],
-    );
-  };
+  const appStoreOpenError = useCallback(
+    (operatorName: string) => {
+      const appStore = t(MobilityTexts.appStore());
+      Alert.alert(
+        '',
+        t(MobilityTexts.appStoreOpenError.text(appStore, operatorName)),
+        [
+          {
+            text: t(MobilityTexts.appStoreOpenError.button),
+            style: 'cancel',
+          },
+        ],
+      );
+    },
+    [t],
+  );
 
   type AppMissingAlertArgs = {
     operatorName: string;
     appStoreUri: string | undefined;
   };
-  const showAppMissingAlert = ({
-    appStoreUri,
-    operatorName,
-  }: AppMissingAlertArgs) => {
-    const buttons: AlertButton[] = [
-      {
-        text: t(MobilityTexts.appMissingAlert.buttons.cancel),
-        style: 'cancel',
-      },
-    ];
-    if (appStoreUri) {
-      buttons.push({
-        text: t(
-          MobilityTexts.appMissingAlert.buttons.openAppStore(
-            t(MobilityTexts.appStore()),
+  const showAppMissingAlert = useCallback(
+    ({appStoreUri, operatorName}: AppMissingAlertArgs) => {
+      const buttons: AlertButton[] = [
+        {
+          text: t(MobilityTexts.appMissingAlert.buttons.cancel),
+          style: 'cancel',
+        },
+      ];
+      if (appStoreUri) {
+        buttons.push({
+          text: t(
+            MobilityTexts.appMissingAlert.buttons.openAppStore(
+              t(MobilityTexts.appStore()),
+            ),
           ),
-        ),
-        style: 'default',
-        onPress: () =>
-          Linking.openURL(appStoreUri).catch(() =>
-            appStoreOpenError(operatorName),
-          ),
-      });
-    }
-    Alert.alert(
-      t(MobilityTexts.appMissingAlert.title(operatorName)),
-      t(MobilityTexts.appMissingAlert.text(operatorName)),
-      buttons,
-    );
-  };
+          style: 'default',
+          onPress: () =>
+            Linking.openURL(appStoreUri).catch(() =>
+              appStoreOpenError(operatorName),
+            ),
+        });
+      }
+      Alert.alert(
+        t(MobilityTexts.appMissingAlert.title(operatorName)),
+        t(MobilityTexts.appMissingAlert.text(operatorName)),
+        buttons,
+      );
+    },
+    [appStoreOpenError, t],
+  );
 
   return {showAppMissingAlert};
 };
