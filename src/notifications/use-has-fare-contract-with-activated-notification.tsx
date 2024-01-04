25a254b95229c6b24e62c6a9b95175ac1f91ee2e
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2024-01-04 12:53:05 +0100

    fix: onboarding appearing twice  (#4199)

---
+++ src/notifications/use-has-fare-contract-with-activated-notification.tsx
@@ -0,0 +1,32 @@
+import {
+  findReferenceDataById,
+  useFirestoreConfiguration,
+} from '@atb/configuration';
+
+import {useValidRightNowFareContract} from '@atb/ticketing/use-valid-right-now-fare-contracts';
+import {useNotifications} from '@atb/notifications';
+
+export function useHasFareContractWithActivatedNotification(): boolean {
+  const {config: notificationsConfig} = useNotifications();
+  const {preassignedFareProducts} = useFirestoreConfiguration();
+  const validFareContracts = useValidRightNowFareContract();
+
+  if (!notificationsConfig) return false;
+
+  return validFareContracts.some((validFareContract) => {
+    const fareProductRef = validFareContract.travelRights[0]?.fareProductRef;
+
+    if (!fareProductRef) {
+      return false;
+    }
+
+    const preassignedFareProduct = findReferenceDataById(
+      preassignedFareProducts,
+      fareProductRef,
+    );
+
+    return notificationsConfig?.groups.some(
+      (group) => group.id === preassignedFareProduct?.type && group.enabled,
+    );
+  });
+}
