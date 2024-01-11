4fa6a91f561614bfb100089a525f4d10a5eeeb22
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2024-01-11 14:52:08 +0100

    Revert: "fix: remove redundant close function for bottom sheet in map (#4221)" (#4222)
    This reverts commit 2db7475319ce8a90aa47f411089e1503ce043648.

--- src/components/map/hooks/use-update-bottom-sheet-when-selected-entity-changes.tsx
+++ src/components/map/hooks/use-update-bottom-sheet-when-selected-entity-changes.tsx
@@ -168,6 +168,8 @@ export const useUpdateBottomSheetWhenSelectedEntityChanges = (
             />
           );
         }, false);
+      } else {
+        closeBottomSheet();
       }
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
