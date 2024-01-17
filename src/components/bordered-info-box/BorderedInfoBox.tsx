b2584e4c3d60bcecb55f1612652f83a7913858f5
Gøran Dalum <goran.dalum@mittatb.no>
2024-01-17 14:43:23 +0100

    feat: Add BorderedInfoBox component (#4241)
    Adds a new component to the src and storybook. This is the bordered
    info box introduced in the on behalf of product initiative. Pretty
    basic component, where the main cavaet is that the border is based
    on the text color with 0.1 opacity. This was since we didn't have
    an appropriate color to use in the design-system, and we didn't
    want to introduced a new one just for this use case.

---
+++ src/components/bordered-info-box/BorderedInfoBox.tsx
@@ -0,0 +1,60 @@
+import {getStaticColor, StaticColor} from '@atb/theme/colors';
+import {ThemeText} from '@atb/components/text';
+import {StyleProp, View, ViewStyle} from 'react-native';
+import React, {ReactNode} from 'react';
+import {StyleSheet} from '@atb/theme';
+import {addOpacity} from '@atb/utils/add-opacity';
+
+type Props =
+  | {
+      backgroundColor: StaticColor;
+      type: 'large' | 'small';
+      style?: StyleProp<ViewStyle>;
+    } & ({text: string} | {children: ReactNode});
+
+/**
+ * A bordered info box, where the border color is based on the text color of the
+ * background where it is applied. The component can be used either by sending
+ * in text or children, but if using children remember to ensure correct
+ * typography and text color on the child component.
+ */
+export const BorderedInfoBox = ({
+  type,
+  backgroundColor,
+  style,
+  ...props
+}: Props) => {
+  const styles = useStyles(type, backgroundColor);
+  return (
+    <View style={[styles.container, style]}>
+      {'text' in props ? (
+        <ThemeText type="body__tertiary" color={backgroundColor}>
+          {props.text}
+        </ThemeText>
+      ) : (
+        props.children
+      )}
+    </View>
+  );
+};
+
+const useStyles = (
+  type: Props['type'],
+  backgroundColor: Props['backgroundColor'],
+) =>
+  StyleSheet.createThemeHook((theme, themeName) => ({
+    container: {
+      borderColor: addOpacity(
+        getStaticColor(themeName, backgroundColor).text,
+        0.1,
+      ),
+      borderWidth: theme.border.width.slim,
+      borderRadius: theme.border.radius.regular,
+      paddingHorizontal:
+        type === 'large' ? theme.spacings.medium : theme.spacings.small,
+      paddingVertical:
+        type === 'large' ? theme.spacings.medium : theme.spacings.xSmall,
+      width: type === 'large' ? '100%' : undefined,
+      alignSelf: 'flex-start',
+    },
+  }))();
