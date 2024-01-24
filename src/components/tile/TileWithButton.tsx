88eafe18620ddf04c531de32ffb6cc77644d506d
Johannes Røsvik <j.rosvik@gmail.com>
2024-01-24 10:45:38 +0100

    feat: add TileWithButton component (#4261)

---
+++ src/components/tile/TileWithButton.tsx
@@ -0,0 +1,115 @@
+import {PressableOpacity} from '@atb/components/pressable-opacity';
+import {ThemeText} from '@atb/components/text';
+import {ThemeIcon} from '@atb/components/theme-icon';
+import {StyleSheet, useTheme} from '@atb/theme';
+import {InteractiveColor, getInteractiveColor} from '@atb/theme/colors';
+import React from 'react';
+import {StyleProp, View, ViewStyle} from 'react-native';
+import {SvgProps} from 'react-native-svg';
+
+export type TileWithButtonProps = {
+  mode: 'compact' | 'spacious';
+  interactiveColor: InteractiveColor;
+  onPress: () => void;
+  accessibilityLabel: string;
+  accessibilityHint?: string;
+  style?: StyleProp<ViewStyle>;
+  testID?: string;
+  buttonText: string;
+  buttonSvg: (props: SvgProps) => JSX.Element;
+  children?: React.ReactNode;
+};
+
+export function TileWithButton({
+  mode,
+  interactiveColor,
+  onPress,
+  accessibilityLabel,
+  accessibilityHint,
+  style,
+  testID,
+  buttonText,
+  buttonSvg,
+  children,
+}: TileWithButtonProps): JSX.Element {
+  const styles = useStyles(interactiveColor);
+  const {themeName} = useTheme();
+  const color = getInteractiveColor(themeName, interactiveColor);
+
+  return (
+    <PressableOpacity
+      style={[styles.container, style]}
+      accessible={true}
+      onPress={onPress}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityHint={accessibilityHint}
+      testID={testID}
+    >
+      <View
+        style={
+          mode === 'spacious'
+            ? styles.spaciousContentContainer
+            : styles.compactContentContainer
+        }
+        importantForAccessibility="no-hide-descendants"
+      >
+        {children}
+      </View>
+      <View
+        style={[
+          styles.button,
+          mode === 'spacious' ? styles.spaciousButton : styles.compactButton,
+        ]}
+        testID={testID + 'Button'}
+      >
+        <ThemeText
+          style={styles.buttonText}
+          type={mode === 'spacious' ? 'body__primary' : 'body__tertiary'}
+        >
+          {buttonText}
+        </ThemeText>
+        <ThemeIcon
+          size={mode === 'spacious' ? 'normal' : 'small'}
+          svg={buttonSvg}
+          fill={color.outline.text}
+        />
+      </View>
+    </PressableOpacity>
+  );
+}
+
+const useStyles = (interactiveColor: InteractiveColor) =>
+  StyleSheet.createThemeHook((theme) => ({
+    container: {
+      backgroundColor: theme.interactive[interactiveColor].default.background,
+      borderRadius: theme.border.radius.regular,
+      overflow: 'hidden',
+      borderWidth: theme.border.width.slim,
+      borderColor: theme.static.background.background_2.background,
+    },
+    spaciousContentContainer: {
+      padding: theme.spacings.xLarge,
+      flexGrow: 1,
+    },
+    compactContentContainer: {
+      padding: theme.spacings.medium,
+      flexGrow: 1,
+    },
+    button: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: theme.interactive[interactiveColor].outline.background,
+    },
+    spaciousButton: {
+      paddingHorizontal: theme.spacings.xLarge,
+      paddingVertical: theme.spacings.medium,
+    },
+    compactButton: {
+      paddingHorizontal: theme.spacings.medium,
+      paddingVertical: theme.spacings.small,
+    },
+    buttonText: {
+      color: theme.interactive[interactiveColor].outline.text,
+    },
+  }))();
--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/Components/RecentFareContracts/RecentFareContractComponent.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_PurchaseTabScreen/Components/RecentFareContracts/RecentFareContractComponent.tsx
@@ -10,18 +10,17 @@ import {
   getReferenceDataName,
   useFirestoreConfiguration,
 } from '@atb/configuration';
-import {ThemeIcon} from '@atb/components/theme-icon';
 import {ArrowRight} from '@atb/assets/svg/mono-icons/navigation';
 import {InteractiveColor} from '@atb/theme/colors';
 import {
   getTransportModeText,
   TransportModes,
 } from '@atb/components/transportation-modes';
-import {PressableOpacity} from '@atb/components/pressable-opacity';
 import {FareContractHarborStopPlaces} from '@atb/fare-contracts';
 import {useHarborsQuery} from '@atb/queries';
 import {TravelRightDirection} from '@atb/ticketing';
 import {BorderedInfoBox} from '@atb/components/bordered-info-box';
+import {TileWithButton} from '@atb/components/tile';
 
 type RecentFareContractProps = {
   recentFareContract: RecentFareContract;
@@ -136,155 +135,119 @@ export const RecentFareContractComponent = ({
   const showTwoWayIcon = direction === TravelRightDirection.Both;
 
   return (
-    <PressableOpacity
-      style={styles.container}
-      accessible={true}
-      onPress={() => onSelect(recentFareContract, fareProductTypeConfig)}
-      accessibilityLabel={currentAccessibilityLabel}
+    <TileWithButton
+      buttonSvg={ArrowRight}
       accessibilityHint={t(RecentFareContractsTexts.repeatPurchase.a11yHint)}
-      testID={testID}
+      accessibilityLabel={currentAccessibilityLabel}
+      buttonText={t(RecentFareContractsTexts.repeatPurchase.label)}
+      interactiveColor="interactive_2"
+      mode="spacious"
+      onPress={() => onSelect(recentFareContract, fareProductTypeConfig)}
+      style={{minWidth: width * 0.6}}
     >
-      <View
-        style={[styles.upperPart, {minWidth: width * 0.6}]}
-        importantForAccessibility="no-hide-descendants"
-      >
-        <View style={styles.travelModeWrapper}>
-          <TransportModes
-            iconSize="small"
-            modes={fareProductTypeConfig.transportModes}
-            style={{flex: 2}}
+      <View style={styles.travelModeWrapper}>
+        <TransportModes
+          iconSize="small"
+          modes={fareProductTypeConfig.transportModes}
+          style={{flex: 2}}
+        />
+      </View>
+
+      <View style={styles.productName} testID={testID + 'Title'}>
+        <ThemeText
+          type="body__secondary--bold"
+          color={interactiveColor.default}
+        >
+          {getReferenceDataName(preassignedFareProduct, language)}
+        </ThemeText>
+      </View>
+
+      {direction !== undefined && pointToPointValidity && (
+        <View style={styles.harbors}>
+          <FareContractHarborStopPlaces
+            showTwoWayIcon={showTwoWayIcon}
+            fromStopPlaceId={pointToPointValidity?.fromPlace}
+            toStopPlaceId={pointToPointValidity?.toPlace}
           />
         </View>
+      )}
 
-        <View style={styles.productName} testID={testID + 'Title'}>
-          <ThemeText
-            type="body__secondary--bold"
-            color={interactiveColor.default}
-          >
-            {getReferenceDataName(preassignedFareProduct, language)}
+      <View style={styles.horizontalFlex}>
+        <View style={styles.detailContainer}>
+          <ThemeText type="label__uppercase" color="secondary">
+            {t(RecentFareContractsTexts.titles.travellers)}
           </ThemeText>
-        </View>
-
-        {direction !== undefined && pointToPointValidity && (
-          <View style={styles.harbors}>
-            <FareContractHarborStopPlaces
-              showTwoWayIcon={showTwoWayIcon}
-              fromStopPlaceId={pointToPointValidity?.fromPlace}
-              toStopPlaceId={pointToPointValidity?.toPlace}
-            />
-          </View>
-        )}
-
-        <View style={styles.horizontalFlex}>
-          <View>
-            <View>
-              <ThemeText type="label__uppercase" color="secondary">
-                {t(RecentFareContractsTexts.titles.travellers)}
+          {userProfilesWithCount.length <= 2 &&
+            userProfilesWithCount.map((u) => (
+              <BorderedInfoBox
+                backgroundColor={interactiveColorName}
+                type="small"
+                key={u.id}
+                text={`${u.count} ${getReferenceDataName(u, language)}`}
+                style={styles.infoChip_travellers}
+                testID={`${testID}Travellers${userProfilesWithCount.indexOf(
+                  u,
+                )}`}
+              />
+            ))}
+          {userProfilesWithCount.length > 2 && (
+            <>
+              {userProfilesWithCount.slice(0, 1).map((u) => (
+                <BorderedInfoBox
+                  key={u.id}
+                  type="small"
+                  backgroundColor={interactiveColorName}
+                  text={`${u.count} ${getReferenceDataName(u, language)}`}
+                  testID={`${testID}Travellers${userProfilesWithCount.indexOf(
+                    u,
+                  )}`}
+                />
+              ))}
+              <ThemeText
+                type="body__tertiary"
+                testID={`${testID}TravellersOthers`}
+                color={interactiveColor.default}
+                style={styles.additionalCategories}
+              >
+                + {userProfilesWithCount.slice(1).length}{' '}
+                {t(RecentFareContractsTexts.titles.moreTravelers)}
               </ThemeText>
-              <View style={styles.travellersTileWrapper}>
-                {userProfilesWithCount.length <= 2 &&
-                  userProfilesWithCount.map((u) => (
-                    <BorderedInfoBox
-                      backgroundColor={interactiveColorName}
-                      type="small"
-                      key={u.id}
-                      text={`${u.count} ${getReferenceDataName(u, language)}`}
-                      style={[styles.infoChip, styles.infoChip_travellers]}
-                      testID={`${testID}Travellers${userProfilesWithCount.indexOf(
-                        u,
-                      )}`}
-                    />
-                  ))}
-                {userProfilesWithCount.length > 2 && (
-                  <>
-                    {userProfilesWithCount.slice(0, 1).map((u) => (
-                      <BorderedInfoBox
-                        key={u.id}
-                        type="small"
-                        backgroundColor={interactiveColorName}
-                        style={styles.infoChip}
-                        text={`${u.count} ${getReferenceDataName(u, language)}`}
-                        testID={`${testID}Travellers${userProfilesWithCount.indexOf(
-                          u,
-                        )}`}
-                      />
-                    ))}
-                    <View style={styles.additionalCategories}>
-                      <ThemeText
-                        type="body__tertiary"
-                        testID={`${testID}TravellersOthers`}
-                        color={interactiveColor.default}
-                      >
-                        + {userProfilesWithCount.slice(1).length}{' '}
-                        {t(RecentFareContractsTexts.titles.moreTravelers)}
-                      </ThemeText>
-                    </View>
-                  </>
-                )}
-              </View>
-            </View>
-          </View>
-          {fareProductTypeConfig.configuration.zoneSelectionMode !== 'none' &&
-            fromZoneName &&
-            toZoneName && (
-              <View>
-                <ThemeText type="label__uppercase" color="secondary">
-                  {t(RecentFareContractsTexts.titles.zone)}
-                </ThemeText>
-                {fromZoneName === toZoneName ? (
-                  <BorderedInfoBox
-                    backgroundColor={interactiveColorName}
-                    type="small"
-                    style={styles.infoChip}
-                    text={`${fromZoneName}`}
-                    testID={`${testID}Zone`}
-                  />
-                ) : (
-                  <BorderedInfoBox
-                    backgroundColor={interactiveColorName}
-                    type="small"
-                    style={styles.infoChip}
-                    text={`${fromZoneName} - ${toZoneName}`}
-                    testID={`${testID}Zones`}
-                  />
-                )}
-              </View>
-            )}
+            </>
+          )}
         </View>
+        {fareProductTypeConfig.configuration.zoneSelectionMode !== 'none' &&
+          fromZoneName &&
+          toZoneName && (
+            <View style={styles.detailContainer}>
+              <ThemeText type="label__uppercase" color="secondary">
+                {t(RecentFareContractsTexts.titles.zone)}
+              </ThemeText>
+              {fromZoneName === toZoneName ? (
+                <BorderedInfoBox
+                  backgroundColor={interactiveColorName}
+                  type="small"
+                  text={`${fromZoneName}`}
+                  testID={`${testID}Zone`}
+                />
+              ) : (
+                <BorderedInfoBox
+                  backgroundColor={interactiveColorName}
+                  type="small"
+                  text={`${fromZoneName} - ${toZoneName}`}
+                  testID={`${testID}Zones`}
+                />
+              )}
+            </View>
+          )}
       </View>
-      <View
-        style={[
-          styles.buyButton,
-          {backgroundColor: interactiveColor.outline.background},
-        ]}
-        testID={testID + 'BuyButton'}
-      >
-        <ThemeText color={interactiveColor.outline}>
-          {t(RecentFareContractsTexts.repeatPurchase.label)}
-        </ThemeText>
-        <ThemeIcon svg={ArrowRight} fill={interactiveColor.outline.text} />
-      </View>
-    </PressableOpacity>
+    </TileWithButton>
   );
 };
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
-  container: {
-    marginHorizontal: theme.spacings.small,
-    backgroundColor: theme.interactive[interactiveColorName].default.background,
-    borderRadius: theme.border.radius.regular,
-    overflow: 'hidden',
-  },
-  upperPart: {
-    padding: theme.spacings.xLarge,
-    flexGrow: 1,
-  },
   travelModeWrapper: {
     flexShrink: 1,
     marginBottom: theme.spacings.medium,
-  },
-  transportationIcon: {
-    marginRight: theme.spacings.xSmall,
   },
   productName: {
     marginBottom: theme.spacings.medium,
@@ -298,21 +261,14 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  infoChip: {
-    marginVertical: theme.spacings.xSmall,
+  detailContainer: {
+    rowGap: theme.spacings.xSmall,
   },
   infoChip_travellers: {
     marginRight: theme.spacings.xSmall,
   },
   additionalCategories: {
     marginHorizontal: theme.spacings.small,
-    marginVertical: theme.spacings.xSmall,
-  },
-  buyButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: theme.spacings.xLarge,
-    paddingVertical: theme.spacings.medium,
   },
   harbors: {
     marginBottom: theme.spacings.medium,
