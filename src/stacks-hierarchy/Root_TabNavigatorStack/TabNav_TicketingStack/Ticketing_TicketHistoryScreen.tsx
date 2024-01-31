3731280444d36a70b5c8bc887851de9da3691f27
Ridwan Aditama <reidzeibel@gmail.com>
2024-01-31 09:59:37 +0100

    refactor: fix wrong import

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketHistoryScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketHistoryScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {TicketTabNavScreenProps} from './Ticketing_TicketTabNavStack/navigation-types';
 import {TicketHistoryScreenComponent} from '@atb/ticket-history';
+import {TicketingScreenProps} from './navigation-types';
 
-type Props = TicketTabNavScreenProps<'Ticketing_TicketHistoryScreen'>;
+type Props = TicketingScreenProps<'Ticketing_TicketHistoryScreen'>;
 
 export const Ticketing_TicketHistoryScreen = ({route}: Props) => {
   return <TicketHistoryScreenComponent mode={route.params.mode} />;
