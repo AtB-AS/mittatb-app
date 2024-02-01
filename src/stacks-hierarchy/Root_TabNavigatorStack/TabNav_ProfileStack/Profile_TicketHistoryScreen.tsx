eb06c8d1345e991a8ccc5456be68f9a13dea91c2
Ridwan Aditama <reidzeibel@gmail.com>
2024-02-01 11:34:00 +0100

    feat: add ticket selector to profile - ticket history screen (#4292)

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_TicketHistoryScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_TicketHistoryScreen.tsx
@@ -1,57 +1,9 @@
 import React from 'react';
-import {useTranslation, TicketingTexts} from '@atb/translations';
-import {filterExpiredFareContracts, useTicketingState} from '@atb/ticketing';
-import {FareContractAndReservationsList} from '@atb/fare-contracts';
-import TicketHistoryTexts from '@atb/translations/screens/subscreens/TicketHistory';
-import {useTimeContextState} from '@atb/time';
-import {FullScreenView} from '@atb/components/screen-view';
-import {ScreenHeading} from '@atb/components/heading';
-import {RefreshControl} from 'react-native-gesture-handler';
+import {ProfileScreenProps} from './navigation-types';
+import {TicketHistoryScreenComponent} from '@atb/ticket-history';
 
-export const Profile_TicketHistoryScreen: React.FC = () => {
-  const {
-    fareContracts,
-    isRefreshingFareContracts,
-    rejectedReservations,
-    resubscribeFirestoreListeners,
-  } = useTicketingState();
+type Props = ProfileScreenProps<'Profile_TicketHistoryScreen'>;
 
-  const {serverNow} = useTimeContextState();
-  const expiredFareContracts = filterExpiredFareContracts(
-    fareContracts,
-    serverNow,
-  );
-
-  const {t} = useTranslation();
-  return (
-    <FullScreenView
-      headerProps={{
-        title: t(TicketHistoryTexts.header),
-        leftButton: {type: 'back', withIcon: true},
-      }}
-      parallaxContent={(focusRef) => (
-        <ScreenHeading ref={focusRef} text={t(TicketHistoryTexts.header)} />
-      )}
-      refreshControl={
-        <RefreshControl
-          refreshing={isRefreshingFareContracts}
-          onRefresh={resubscribeFirestoreListeners}
-        />
-      }
-    >
-      <FareContractAndReservationsList
-        fareContracts={expiredFareContracts}
-        reservations={rejectedReservations}
-        now={serverNow}
-        emptyStateTitleText={t(
-          TicketingTexts.activeFareProductsAndReservationsTab
-            .emptyTicketHistoryTitle,
-        )}
-        emptyStateDetailsText={t(
-          TicketingTexts.activeFareProductsAndReservationsTab
-            .emptyTicketHistoryDetails,
-        )}
-      />
-    </FullScreenView>
-  );
+export const Profile_TicketHistoryScreen = ({route}: Props) => {
+  return <TicketHistoryScreenComponent mode={route.params.mode} />;
 };
