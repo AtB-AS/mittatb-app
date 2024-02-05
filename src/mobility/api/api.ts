b8930a5a2d0d66d02159746db3028bec64d456c8
Johannes Røsvik <j.rosvik@gmail.com>
2024-02-05 16:31:59 +0100

    rename getFareProductBenefits types

--- src/mobility/api/api.ts
+++ src/mobility/api/api.ts
@@ -49,19 +49,23 @@ export const getValueCode = (
     });
 };
 
-const FareProductBenefit = z.object({
+const FareProductBenefitReferences = z.object({
   operator: z.string(),
   benefits: OperatorBenefitId.array(),
 });
 
-export type FareProductBenefitType = z.infer<typeof FareProductBenefit>;
+type FareProductBenefitReferenceType = z.infer<
+  typeof FareProductBenefitReferences
+>;
 
 export const getFareProductBenefits = (
   productId: PreassignedFareProductId,
-): Promise<FareProductBenefitType[]> => {
+): Promise<FareProductBenefitReferenceType[]> => {
   return client
     .get(`/mobility/v1/benefits/${productId}`, {
       authWithIdToken: true,
     })
-    .then((response) => FareProductBenefit.array().parse(response.data ?? []));
+    .then((response) =>
+      FareProductBenefitReferences.array().parse(response.data ?? []),
+    );
 };
