9ec30784d88358c37b4098ba51fe70a2f2eb9f1d
Ridwan Aditama <reidzeibel@gmail.com>
2024-02-16 10:47:39 +0100

    feat: popover for tickets sent to others button on my ticket screen.

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_ActiveFareProductsTabScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_TicketingStack/Ticketing_TicketTabNavStack/TicketTabNav_ActiveFareProductsTabScreen.tsx
@@ -4,8 +4,13 @@ import {
   filterExpiredFareContracts,
   useTicketingState,
 } from '@atb/ticketing';
-import React from 'react';
-import {View} from 'react-native';
+import React, {useCallback, useRef} from 'react';
+import {
+  Dimensions,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  View,
+} from 'react-native';
 import {FareContractAndReservationsList} from '@atb/fare-contracts';
 import {useTranslation, TicketingTexts} from '@atb/translations';
 import {useAnalytics} from '@atb/analytics';
@@ -14,6 +19,9 @@ import {LinkSectionItem, Section} from '@atb/components/sections';
 import {TicketHistoryModeTexts} from '@atb/translations/screens/Ticketing';
 import {TicketTabNavScreenProps} from './navigation-types';
 import {RefreshControl, ScrollView} from 'react-native-gesture-handler';
+import {useFocusEffect} from '@react-navigation/native';
+import {usePopOver} from '@atb/popover';
+import {useOneTimePopover} from '@atb/popover/use-one-time-popover';
 
 type Props =
   TicketTabNavScreenProps<'TicketTabNav_ActiveFareProductsTabScreen'>;
@@ -38,15 +46,80 @@ export const TicketTabNav_ActiveFareProductsTabScreen = ({
 
   const styles = useStyles();
   const {t} = useTranslation();
+  const {addPopOver} = usePopOver();
 
   const hasExpiredFareContracts =
     filterExpiredFareContracts(fareContracts, serverNow).length > 0;
 
   const hasSentFareContracts = sentFareContracts.length > 0;
 
+  const sentFareContractRef = useRef(null);
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const {isPopOverSeen, setPopOverSeen} = useOneTimePopover();
+
+  // if user has sent fare contracts AND the popOver is not seen yet,
+  // the popOver should be shown
+  const shouldShowPopOver =
+    hasSentFareContracts && !isPopOverSeen('on-behalf-of-first-time-purchase');
+
+  const showPopOver = useCallback(() => {
+    addPopOver({
+      oneTimeKey: 'on-behalf-of-first-time-purchase',
+      target: sentFareContractRef,
+    });
+    setPopOverSeen('on-behalf-of-first-time-purchase');
+  }, [addPopOver, setPopOverSeen]);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (shouldShowPopOver) {
+        // Check if the button for tickets sent to others is visible on screen or not
+        if (sentFareContractRef.current) {
+          const currentView = sentFareContractRef.current as View;
+          currentView.measure((x, y, width, height, pageX, pageY) => {
+            const rectTop = pageX;
+            const rectBottom = pageY + height;
+            const rectWidth = pageX + width;
+            const window = Dimensions.get('window');
+            const isVisible =
+              rectBottom != 0 &&
+              rectTop >= 0 &&
+              rectBottom <= window.height &&
+              rectWidth > 0 &&
+              rectWidth <= window.width;
+
+            // if layout is visible : show pop over
+            // else : scroll to bottom, and then show pop over (see handleScrollEvent)
+            if (isVisible) {
+              showPopOver();
+            } else {
+              scrollViewRef.current?.scrollToEnd({animated: true});
+            }
+          });
+        }
+      }
+    }, [shouldShowPopOver, showPopOver]),
+  );
+
+  // show popover when it reaches the end of scrollview
+  const handleScrollEvent = (
+    event: NativeSyntheticEvent<NativeScrollEvent>,
+  ) => {
+    if (shouldShowPopOver) {
+      const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
+      if (layoutMeasurement.height + contentOffset.y >= contentSize.height) {
+        showPopOver();
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView
+        ref={scrollViewRef}
+        onScroll={handleScrollEvent}
+        scrollEventThrottle={0} // this will make the onScroll event sent only once
         contentContainerStyle={styles.content}
         refreshControl={
           <RefreshControl
@@ -92,18 +165,20 @@ export const TicketTabNav_ActiveFareProductsTabScreen = ({
           )}
 
           {hasSentFareContracts && (
-            <LinkSectionItem
-              text={t(TicketHistoryModeTexts.sent.title)}
-              accessibility={{
-                accessibilityHint: t(TicketHistoryModeTexts.sent.titleA11y),
-              }}
-              testID="sentToOthersButton"
-              onPress={() =>
-                navigation.navigate('Ticketing_TicketHistoryScreen', {
-                  mode: 'sent',
-                })
-              }
-            />
+            <View ref={sentFareContractRef} collapsable={false}>
+              <LinkSectionItem
+                text={t(TicketHistoryModeTexts.sent.title)}
+                accessibility={{
+                  accessibilityHint: t(TicketHistoryModeTexts.sent.titleA11y),
+                }}
+                testID="sentToOthersButton"
+                onPress={() =>
+                  navigation.navigate('Ticketing_TicketHistoryScreen', {
+                    mode: 'sent',
+                  })
+                }
+              />
+            </View>
           )}
         </Section>
       </ScrollView>
