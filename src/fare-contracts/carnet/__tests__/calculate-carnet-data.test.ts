08b1bb9394f7ca3915b501119e0538d34ad16eb1
Johannes Røsvik <j.rosvik@gmail.com>
2024-02-21 10:20:02 +0100

    test: add unit tests for carnet data "padding"

--- src/fare-contracts/carnet/__tests__/calculate-carnet-data.test.ts
+++ src/fare-contracts/carnet/__tests__/calculate-carnet-data.test.ts
@@ -74,6 +74,32 @@ describe('Carnet footer: 30 tickets, active', () => {
   });
 });
 
+describe('Carnet footer: Padded when not divisible by 10', () => {
+  it(`13 tickets, 1 used, not active.`, async () => {
+    const carnetData = calculateCarnetData(false, 13, 1);
+    expect(carnetData.accessesRemaining).toEqual(12);
+    expect(carnetData.multiCarnetArray.length).toEqual(1);
+    expect(carnetData.unusedArray.length).toEqual(2);
+    expect(carnetData.usedArray.length).toEqual(8);
+  });
+
+  it(`13 tickets, 3 used, not active.`, async () => {
+    const carnetData = calculateCarnetData(false, 13, 3);
+    expect(carnetData.accessesRemaining).toEqual(10);
+    expect(carnetData.multiCarnetArray.length).toEqual(0);
+    expect(carnetData.unusedArray.length).toEqual(10);
+    expect(carnetData.usedArray.length).toEqual(0);
+  });
+
+  it(`13 tickets, 3 used, active.`, async () => {
+    const carnetData = calculateCarnetData(true, 13, 3);
+    expect(carnetData.accessesRemaining).toEqual(10);
+    expect(carnetData.multiCarnetArray.length).toEqual(1);
+    expect(carnetData.unusedArray.length).toEqual(0);
+    expect(carnetData.usedArray.length).toEqual(9);
+  });
+});
+
 // These states are invalid, but we shouldn't crash if it happends. Make sure we
 // handle it gracefully.
 describe('Carnet footer: Invalid state', () => {
