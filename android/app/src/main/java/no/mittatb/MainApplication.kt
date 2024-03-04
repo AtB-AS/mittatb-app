3b538461e4a57c2d561e2d7f35ff812f60d0fb7d
Jorge Osorio <jorelosorio@gmail.com>
2024-03-04 15:48:07 +0100

    Change code to kotlin

---
+++ android/app/src/main/java/no/mittatb/MainApplication.kt
@@ -0,0 +1,78 @@
+package no.mittatb
+
+import android.content.pm.PackageManager
+import android.text.TextUtils
+import androidx.multidex.MultiDexApplication
+import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.soloader.SoLoader
+import io.intercom.android.sdk.Intercom
+
+class MainApplication : MultiDexApplication(), ReactApplication {
+    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            val packages: MutableList<ReactPackage> = PackageList(this).packages
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            packages.add(KettleSDKPackage())
+            return packages
+        }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        tryInitializeBugsnag()
+        tryInitializeIntercom()
+        SoLoader.init(this,  /* native exopackage */false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+
+    private fun tryInitializeBugsnag() {
+        try {
+            val ai = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+            val bundle = ai.metaData
+            val bugsnagKey = bundle.getString("com.bugsnag.android.API_KEY")
+            if (!TextUtils.isEmpty(bugsnagKey)) {
+                val config = Configuration(bugsnagKey!!)
+                val bugsnagReleaseStage = bundle.getString("com.bugsnag.android.RELEASE_STAGE")
+                if (!TextUtils.isEmpty(bugsnagReleaseStage)) {
+                    config.releaseStage = bugsnagReleaseStage
+                }
+                Bugsnag.start(this, config)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun tryInitializeIntercom() {
+        try {
+            val intercomApiKey = getString(R.string.IntercomApiKey)
+            val intercomAppId = getString(R.string.IntercomAppId)
+            if (!TextUtils.isEmpty(intercomApiKey) && !TextUtils.isEmpty(intercomAppId)) {
+                Intercom.initialize(this, intercomApiKey, intercomAppId)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
