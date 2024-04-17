adf6d3bc3db1c069f6b41bbccaa7fd2918afda9e
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2024-04-17 12:19:23 +0200

    build: support Troms staging build job (#4441)

--- types/app-orgs.ts
+++ types/app-orgs.ts
@@ -2,10 +2,12 @@ export enum AppOrgs {
   nfk = 'nfk',
   atb = 'atb',
   fram = 'fram',
+  troms = 'troms',
 }
 
 export enum TariffZoneAuthorities {
   atb = 'ATB',
   nfk = 'NOR',
   fram = 'MOR',
+  troms = 'TRO',
 }
