6ea896d02fb4ce59599c2916aeda9fd62b8e0347
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2024-04-25 10:13:51 +0200

    build: change register-app-version script to use new host + audience (#4454)

--- scripts/register-local-app-version.sh
+++ scripts/register-local-app-version.sh
@@ -10,8 +10,7 @@ export $(grep -v '^#' .env | xargs -0 -n1 echo) > /dev/null 2>&1
 credentials=$(gcloud secrets versions access --project atb-staging-c420 --secret=entur-client-credentials-publish latest)
 
 export APP_ENVIRONMENT=staging
-export ENTUR_CLIENT_ID=$(echo $credentials | jq '.clientId' -r)
-export ENTUR_CLIENT_SECRET=$(echo $credentials | jq '.clientSecret' -r)
+export ENTUR_PUBLISH_CLIENT=$(echo $credentials | base64)
 export BUILD_ID=1
 export DISABLE_LISTED_ON_PLAY_STORE=true
 
