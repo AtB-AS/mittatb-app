23d1df691df34d0fa6f8eaa3fa194509f6a57868
Jorge Osorio <jorelosorio@gmail.com>
2024-04-25 14:48:13 +0200

    Update ReactNativeFlipperInitializer.java

--- android/app/src/store/java/no/mittatb/ReactNativeFlipperInitializer.java
+++ android/app/src/store/java/no/mittatb/ReactNativeFlipperInitializer.java
@@ -11,7 +11,7 @@ import com.facebook.react.ReactInstanceManager;
  * Class responsible of loading Flipper inside your React Native application. This is the release
  * flavor of it so it's empty as we don't want to load Flipper.
  */
-public class ReactNativeFlipper {
+public class ReactNativeFlipperInitializer {
   public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     // Do nothing as we don't want to initialize Flipper on Release.
   }
