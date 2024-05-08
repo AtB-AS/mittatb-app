1aa0f2b6f8020422c6a0144b77e0e340a3efc5a6
Ridwan Aditama <reidzeibel@gmail.com>
2024-05-08 10:42:40 +0200

    refactor: move register chat to RootStack

--- src/onboarding/OnboardingContext.tsx
+++ src/onboarding/OnboardingContext.tsx
@@ -134,8 +134,6 @@ export const OnboardingContextProvider: React.FC = ({children}) => {
         ),
       );
 
-      registerChatUser();
-
       dispatch({
         type: 'LOAD_ONBOARDING_SETTINGS',
         loadedOnboardingSections,
