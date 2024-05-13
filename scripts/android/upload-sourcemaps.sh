69926144a8904dd018a3c6d1f60db845fe7cda20
Jorge Osorio <jorelosorio@gmail.com>
2024-05-13 15:26:40 +0200

    Update upload-sourcemaps.sh

--- scripts/android/upload-sourcemaps.sh
+++ scripts/android/upload-sourcemaps.sh
@@ -4,21 +4,23 @@ if [[
     -z "${BUGSNAG_API_KEY}"
     || -z "${BUILD_ID}"
     || -z "${APP_VERSION}"
+    || -z "${ANDROID_APPLICATION_ID}"
    ]]; then
     echo "Argument error!"
     echo "Expected three env variables:
   - BUGSNAG_API_KEY
   - APP_VERSION
-  - BUILD_ID"
+  - BUILD_ID
+  - ANDROID_APPLICATION_ID"
 
     exit 1
 else
 echo "Generating and uploading Android source maps"
-    npx bugsnag-source-maps upload-react-native \
-        --api-key=$BUGSNAG_API_KEY \
-        --app-version=$APP_VERSION \
-        --app-version-code=$BUILD_ID \
-        --platform=android \
-        --proguard=bundle/mapping.txt \
-        --overwrite
+    curl --http1.1 https://upload.bugsnag.com/ \
+    -F proguard=bundle/mapping.txt \
+    -F apiKey=$BUGSNAG_API_KEY \
+    -F versionCode=$BUILD_ID \
+    -F appId=$ANDROID_APPLICATION_ID \
+    -F versionName=$APP_VERSION \
+    -F overwrite
 fi
