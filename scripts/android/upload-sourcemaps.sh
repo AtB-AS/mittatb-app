438373c4e56aa8292b071f07790bea34cf84204f
Jorge Osorio <jorelosorio@gmail.com>
2024-05-15 19:17:18 +0200

    Update upload-sourcemaps.sh

--- scripts/android/upload-sourcemaps.sh
+++ scripts/android/upload-sourcemaps.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [[
-    -z "${BUGSNAG_API_KEY}"
-    || -z "${BUILD_ID}"
+    -z "${BUILD_ID}"
     || -z "${APP_VERSION}"
     || -z "${APP_FLAVOR}"
    ]]; then
     echo "Argument error!"
     echo "Expected env variables:
-  - BUGSNAG_API_KEY
   - APP_VERSION
   - BUILD_ID
   - APP_FLAVOR"
@@ -20,9 +18,4 @@ else
       --version-name=$APP_VERSION \
       --version-code=$BUILD_ID \
       --variant=$(echo $APP_FLAVOR${APP_ENVIRONMENT^})
-
-    npx bugsnag-cli upload android-proguard ./bundle/mapping.txt \
-      --api-key=$BUGSNAG_API_KEY \
-      --version-name=$APP_VERSION \
-      --version-code=$BUILD_ID
 fi
