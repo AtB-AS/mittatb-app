2d731c795f1d0cb7e3fd4879da59aeb23bee5109
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-22 13:37:34 +0200

    fix: only call mobility/code for single and free unlock (#4490)
    * Only call mobility/code for single and free unlock
    
    * Simplify benefitRequiresValueCodeToUnlock calculation
    
    * Simplification fix
    
    * Refactor OperatorActionButton
    
    * Add tests for useIsEligibleForBenefit
    
    * Get benefitIdsRequiringValueCode from firestore

--- src/mobility/use-is-eligible-for-benefit.tsx
+++ src/mobility/use-is-eligible-for-benefit.tsx
@@ -1,23 +1,32 @@
 import {useUserBenefitsQuery} from '@atb/mobility/queries/use-user-benefits-query';
 import {OperatorBenefitType} from '@atb-as/config-specs/lib/mobility-operators';
+import {isDefined} from '@atb/utils/presence';
+import {useFirestoreConfiguration} from '@atb/configuration';
 
 export const useIsEligibleForBenefit = (
-  operatorBenefit: OperatorBenefitType | undefined,
+  operatorBenefit?: OperatorBenefitType,
 ) => {
+  const {benefitIdsRequiringValueCode} = useFirestoreConfiguration();
   const {
     data: userBenefits,
     isInitialLoading: isLoading,
     isError,
   } = useUserBenefitsQuery(!!operatorBenefit);
 
+  const userBenefitIds =
+    userBenefits?.flatMap((b) => b.benefitIds).filter(isDefined) || [];
+
   const isUserEligibleForBenefit =
-    userBenefits && operatorBenefit?.id
-      ? userBenefits.flatMap((b) => b.benefitIds).includes(operatorBenefit.id)
-      : false;
+    operatorBenefit && userBenefitIds.includes(operatorBenefit.id);
+
+  const benefitRequiresValueCodeToUnlock =
+    operatorBenefit &&
+    benefitIdsRequiringValueCode?.includes(operatorBenefit.id);
 
   return {
     isLoading,
     isError,
-    isUserEligibleForBenefit,
+    isUserEligibleForBenefit: !!isUserEligibleForBenefit,
+    benefitRequiresValueCodeToUnlock: !!benefitRequiresValueCodeToUnlock,
   };
 };
