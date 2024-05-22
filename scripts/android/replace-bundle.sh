e27c3d2f798df0df587a92b71271af0f8986ccf1
Jorge Osorio <jorelosorio@gmail.com>
2024-05-22 11:14:59 +0200

    chore: Update Android bundle generation and replacement script

--- scripts/android/replace-bundle.sh
+++ scripts/android/replace-bundle.sh
@@ -24,8 +24,8 @@ if [[
 else
     mkdir -p bundle
 
-    BUNDLE_PATH = "android/app/build/generated/assets/createBundle$(echo $APP_FLAVOR${APP_ENVIRONMENT^})JsAndAssets/index.android.bundle"
-    SOURCEMAP_PATH = "android/app/build/generated/sourcemaps/react/$(echo $APP_FLAVOR${APP_ENVIRONMENT^})/index.android.bundle.map"
+    BUNDLE_PATH="android/app/build/generated/assets/createBundle$(echo $APP_FLAVOR${APP_ENVIRONMENT^})JsAndAssets/index.android.bundle"
+    SOURCEMAP_PATH="android/app/build/generated/sourcemaps/react/$(echo $APP_FLAVOR${APP_ENVIRONMENT^})/index.android.bundle.map"
 
     echo "Re-generate bundle"
     npx react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output $BUNDLE_PATH --sourcemap-output $SOURCEMAP_PATH
