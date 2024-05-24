abf1a9b8911907c304d2d9941facea80e2ce0cdb
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-24 09:44:48 +0200

    Remove console statement

--- src/components/snackbar/use-snackbar-vertical-position-animation.ts
+++ src/components/snackbar/use-snackbar-vertical-position-animation.ts
@@ -28,7 +28,7 @@ export const useSnackbarVerticalPositionAnimation = (
   const visibleY = 0; // no offset
   // make sure to move it far enough out to also hide the shadow
   const viewHeightIncludingShadow = height + (shadows?.shadowRadius || 8);
-  console.log('isScreenReaderEnabled', isScreenReaderEnabled);
+
   const hiddenY = isScreenReaderEnabled
     ? visibleY // jump directly to visible position when screen reader enabled
     : position === 'top'
