9d876701d68d50e2f4b586a81ca9114d3782a773
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-24 12:51:05 +0200

    Fix useStableProp typing

--- src/utils/use-stable-prop.ts
+++ src/utils/use-stable-prop.ts
@@ -5,11 +5,12 @@ import {useEffect, useState} from 'react';
  * Custom hook to memoize a prop using deep comparison. The memoized value
  * will only be updated if the new prop is not deeply equal to the current prop.
  *
- * @param {any} prop - The prop to be memoized.
- * @returns {any} - The memoized prop.
+ * @template T
+ * @param {T} prop - The prop to be memoized.
+ * @returns {T} - The memoized prop.
  */
-export const useStableProp = (prop: any) => {
-  const [stableProp, setStableProp] = useState(prop);
+export const useStableProp = <T>(prop: T): T => {
+  const [stableProp, setStableProp] = useState<T>(prop);
 
   useEffect(() => {
     if (!isEqual(stableProp, prop)) {
