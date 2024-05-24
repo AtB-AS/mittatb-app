75f8e990729c16eb199cf1dfc2e22f1f67e4e82b
Adrian Santana Berg <adrian_berg96@hotmail.com>
2024-05-24 12:33:38 +0200

    fix: add missing translation when no token for troms (#4537)
    * fix: add missing translation when no token for troms
    
    * fix: travelcard → travel card

--- src/translations/components/TravelTokenBox.ts
+++ src/translations/components/TravelTokenBox.ts
@@ -37,7 +37,7 @@ const TravelTokenBoxTexts = {
 
 export default orgSpecificTranslations(TravelTokenBoxTexts, {
   nfk: {
-    tcardName: _('reisekort', 'travelcard', 'reisekort'),
+    tcardName: _('reisekort', 'travel card', 'reisekort'),
     errorMessages: {
       tokensNotLoadedTitle: _(
         'Klarer ikke hente informasjon om reisekort / mobil.',
@@ -57,7 +57,27 @@ export default orgSpecificTranslations(TravelTokenBoxTexts, {
     },
   },
   fram: {
-    tcardName: _('reisekort', 'travelcard', 'reisekort'),
+    tcardName: _('reisekort', 'travel card', 'reisekort'),
+    errorMessages: {
+      tokensNotLoadedTitle: _(
+        'Klarer ikke hente informasjon om reisekort / mobil.',
+        'Unable to retrieve information about your travel card / phone.',
+        'Klarer ikkje hente informasjon om reisekort / mobil.',
+      ),
+      tokensNotLoaded: _(
+        'Billetter må brukes på enten et reisekort eller en mobil, men akkurat nå klarer vi ikke finne ut hvor den er i bruk. Sjekk at du har tilgang på nett der du er.',
+        `Tickets must be used on either a travel card or phone, but right now we are unable to find where the ticket is in use. Check that you have internet access.`,
+        'Billettar må brukast på enten eit reisekort eller ein mobil, men nett no klarar vi ikkje finne ut kvar den er i bruk. Sjekk at du har tilgang på nett der du er.',
+      ),
+      noInspectableToken: _(
+        'Billetter må brukes på enten et reisekort eller en mobil, og det ser ikke ut som du har valgt en av dem.\n\nGå til **Min bruker, Bytt mellom reisekort / mobil** for å velge.',
+        `Tickets must be used on either a travel card or a phone, and it looks like you haven't chosen one. Go to **My user, switch between travel card / phone** to select`,
+        'Billettar må brukast på enten eit reisekort eller ein mobil, og det ser ikkje ut som du har valgt ein av dei.\n\nGå til **Min brukar, Bytt mellom reisekort / mobil** for å velje.',
+      ),
+    },
+  },
+  troms: {
+    tcardName: _('reisekort', 'travel card', 'reisekort'),
     errorMessages: {
       tokensNotLoadedTitle: _(
         'Klarer ikke hente informasjon om reisekort / mobil.',
