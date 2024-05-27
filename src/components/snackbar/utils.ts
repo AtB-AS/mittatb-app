04caec6dde608a169218753fa6f623879b546d9d
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-27 13:22:17 +0200

    Simplify getSnackbarHasTextContent

--- src/components/snackbar/utils.ts
+++ src/components/snackbar/utils.ts
@@ -1,6 +1,4 @@
-import {isDefined} from '@atb/utils/presence';
 import {SnackbarTextContent} from '@atb/components/snackbar';
 
 export const getSnackbarHasTextContent = (textContent?: SnackbarTextContent) =>
-  isDefined(textContent) &&
-  (isDefined(textContent?.title) || isDefined(textContent?.description));
+  textContent?.title || textContent?.description || false;
