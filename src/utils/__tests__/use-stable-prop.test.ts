87418dbbadff16c7f9152f9aec485b749a4306a7
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-28 16:42:46 +0200

    Simplify useStableProp tests

--- src/utils/__tests__/use-stable-prop.test.ts
+++ src/utils/__tests__/use-stable-prop.test.ts
@@ -1,4 +1,3 @@
-import {useEffect} from 'react';
 import {renderHook, act} from '@testing-library/react-hooks';
 import {useStableProp} from '../use-stable-prop';
 
@@ -6,50 +5,18 @@ type TestProp = {a?: number; b?: number} | undefined;
 
 describe('useStableProp', () => {
   it('should trigger useEffect only when prop is not deeply equal', () => {
-    let prop: TestProp = {a: 1};
-    const effectSpy = jest.fn();
+    const prop1 = {a: 1};
+    const prop2 = {a: 1};
 
-    const useTestHook = (value: TestProp) => {
-      const stableValue = useStableProp(value);
-      useEffect(() => {
-        effectSpy();
-      }, [stableValue]);
-      return stableValue;
-    };
-
-    const {rerender} = renderHook(({prop}) => useTestHook(prop), {
-      initialProps: {prop},
+    const {result, rerender} = renderHook((prop) => useStableProp(prop), {
+      initialProps: prop1,
     });
 
-    expect(effectSpy).toHaveBeenCalledTimes(1); // Initial render
+    expect(result.current).toBe(prop1);
 
-    act(() => {
-      prop = {a: 1}; // new object with the same content
-      rerender({prop});
-    });
-
-    expect(effectSpy).toHaveBeenCalledTimes(1); // No change, no effect call
-
-    act(() => {
-      prop = {a: 2};
-      rerender({prop});
-    });
-
-    expect(effectSpy).toHaveBeenCalledTimes(2); // Changed, effect called
-
-    act(() => {
-      prop = {a: 2}; // new object with the same content
-      rerender({prop});
-    });
-
-    expect(effectSpy).toHaveBeenCalledTimes(2); // No change, no effect call
-
-    act(() => {
-      prop = {b: 3};
-      rerender({prop});
-    });
-
-    expect(effectSpy).toHaveBeenCalledTimes(3); // Changed, effect called
+    rerender(prop2);
+    expect(result.current).toBe(prop1);
+    expect(result.current).not.toBe(prop2);
   });
 
   it('should return the initial prop value on initial render', () => {
@@ -59,60 +26,48 @@ describe('useStableProp', () => {
   });
 
   it('should update the stable value when the new prop is not deeply equal to the current prop', () => {
-    let prop: TestProp = {a: 1};
-    const {result, rerender} = renderHook(() => useStableProp(prop));
+    const {result, rerender} = renderHook(
+      (prop: TestProp) => useStableProp(prop),
+      {
+        initialProps: {a: 1},
+      },
+    );
 
-    act(() => {
-      prop = {a: 2};
-      rerender(prop);
-    });
-
+    rerender({a: 2});
     expect(result.current).toEqual({a: 2});
 
-    act(() => {
-      prop = {b: 3};
-      rerender(prop);
-    });
-
+    rerender({b: 3});
     expect(result.current).toEqual({b: 3});
   });
 
   it('should not update the stable value when the new prop is deeply equal to the current prop', () => {
-    let prop: TestProp = {a: 1};
-    const {result, rerender} = renderHook(() => useStableProp(prop));
+    const initialProp: TestProp = {a: 1};
+    const {result, rerender} = renderHook(
+      (prop: TestProp) => useStableProp(prop),
+      {
+        initialProps: initialProp,
+      },
+    );
 
-    act(() => {
-      prop = {a: 1}; // new object with the same content
-      rerender(prop);
-    });
+    rerender({a: 1}); // new object with the same content
+    expect(result.current).toEqual(initialProp);
 
-    expect(result.current).toEqual({a: 1});
-
-    act(() => {
-      prop = {a: 2}; // change content
-      rerender(prop);
-    });
-
+    rerender({a: 2}); // change content
     expect(result.current).toEqual({a: 2});
   });
 
-  it('should handle undefined initial prop value', () => {
-    const initialProp = undefined;
-    const {result} = renderHook(() => useStableProp(initialProp));
-    expect(result.current).toBeUndefined();
-  });
-
   it('should update the stable value from undefined to a defined value', () => {
-    let prop: TestProp = undefined;
-    const {result, rerender} = renderHook(() => useStableProp(prop));
+    const initialProp = undefined;
+    const {result, rerender} = renderHook(
+      (prop: TestProp) => useStableProp(prop),
+      {
+        initialProps: initialProp,
+      },
+    );
 
     expect(result.current).toBeUndefined();
 
-    act(() => {
-      prop = {a: 1};
-      rerender(prop);
-    });
-
+    rerender({a: 1});
     expect(result.current).toEqual({a: 1});
   });
 
