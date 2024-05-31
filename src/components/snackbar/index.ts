f81872977258562238e685ce235d1fd4912c32bb
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-05-31 10:00:42 +0200

    feat: Add snackbar to storybook (#4549)
    * Add Snackbar to Storybook
    
    * Update storybook.requires.js
    
    * Snackbar padding fix
    
    * Dark mode fix

--- src/components/snackbar/index.ts
+++ src/components/snackbar/index.ts
@@ -1,5 +1,9 @@
 export {Snackbar} from './Snackbar';
-export type {SnackbarPosition, SnackbarTextContent} from './Snackbar';
+export type {
+  SnackbarPosition,
+  SnackbarTextContent,
+  SnackbarProps,
+} from './Snackbar';
 export {useSnackbarVerticalPositionAnimation} from './use-snackbar-vertical-position-animation';
 export {useSnackbarIsVisible} from './use-snackbar-is-visible';
 export {useSnackbarScreenReaderFocus} from './use-snackbar-screen-reader-focus';
