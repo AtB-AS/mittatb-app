257a616b1e52792063cdd81970217e479c569304
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-06-11 15:25:18 +0200

    Remove enabled condition in useGeofencingZonesQuery

--- src/mobility/queries/use-geofencing-zones.tsx
+++ src/mobility/queries/use-geofencing-zones.tsx
@@ -1,17 +1,10 @@
 import {useQuery} from '@tanstack/react-query';
 import {getGeofencingZones} from '@atb/api/mobility';
-import {useGeofencingZonesEnabled} from '../use-geofencing-zones-enabled';
 
 const TWELVE_HOURS_MS = 1000 * 60 * 60 * 12;
 
 export const useGeofencingZonesQuery = (systemId?: string) => {
-  const [geofencingZonesEnabled, geofencingZonesEnabledDebugOverrideReady] =
-    useGeofencingZonesEnabled();
   return useQuery({
-    enabled:
-      systemId !== undefined &&
-      geofencingZonesEnabled &&
-      geofencingZonesEnabledDebugOverrideReady,
     queryKey: ['getGeofencingZones', systemId],
     queryFn: ({signal}) =>
       getGeofencingZones(systemId !== undefined ? [systemId] : [], {signal}),
