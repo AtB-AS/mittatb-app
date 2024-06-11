0e7c79790ea983422a4f6095c7b2e10f46eca81e
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-06-11 16:34:28 +0200

    Create GeofencingZones

--- src/mobility/queries/use-vehicle-query.tsx
+++ src/mobility/queries/use-vehicle-query.tsx
@@ -2,10 +2,10 @@ import {useQuery} from '@tanstack/react-query';
 import {getVehicle} from '@atb/api/mobility';
 
 const ONE_MINUTE = 1000 * 60;
-export const useVehicleQuery = (id: string) =>
+export const useVehicleQuery = (id?: string) =>
   useQuery({
     queryKey: ['getVehicle', id],
-    queryFn: ({signal}) => getVehicle(id, {signal}),
+    queryFn: ({signal}) => (id ? getVehicle(id, {signal}) : undefined),
     staleTime: ONE_MINUTE,
     cacheTime: ONE_MINUTE,
   });
