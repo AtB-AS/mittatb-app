785e1451c19dbfedfa28a6ae49d0aaead50dd005
Johannes Røsvik <j.rosvik@gmail.com>
2024-06-12 16:11:03 +0200

    feat: use direction config to show boat validity texts (#4578)

--- src/translations/screens/subscreens/PurchaseConfirmation.ts
+++ src/translations/screens/subscreens/PurchaseConfirmation.ts
@@ -30,19 +30,21 @@ const PurchaseConfirmationTexts = {
           `Gyldig frå sone ${zoneNameFrom} til sone ${zoneNameTo}`,
         ),
     },
+    direction: {
+      'one-way': (from: string, to: string) =>
+        _(
+          `Gyldig fra ${from} til ${to}`,
+          `Valid from ${from} to ${to}`,
+          `Gyldig frå ${from} til ${to}`,
+        ),
+      'two-way': (from: string, to: string) =>
+        _(
+          `Gyldig i begge retninger mellom ${from} og ${to}`,
+          `Valid in both directions from ${from} to ${to}`,
+          `Gyldig i begge retningar mellom ${from} og ${to}`,
+        ),
+    },
     harbor: {
-      single: (harborNameFrom: string, harborNameTo: string) =>
-        _(
-          `Gyldig fra ${harborNameFrom} til ${harborNameTo}`,
-          `Valid from ${harborNameFrom} to ${harborNameTo}`,
-          `Gyldig frå ${harborNameFrom} til ${harborNameTo}`,
-        ),
-      period: (harborNameFrom: string, harborNameTo: string) =>
-        _(
-          `Gyldig i begge retninger mellom ${harborNameFrom} og ${harborNameTo}`,
-          `Valid in both directions from ${harborNameFrom} to ${harborNameTo}`,
-          `Gyldig i begge retningar mellom ${harborNameFrom} og ${harborNameTo}`,
-        ),
       messageInHarborZones: _(
         'Gjelder for buss/trikk i sonene du reiser til og fra',
         'Applies for bus/tram in departure and destination zones',
