0280d7f3816addc0d2eca2879f9a269b55520336
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-06-17 09:35:31 +0200

    Reduce test size

--- src/components/map/__tests__/decode-polyline-encoded-multi-polygons.test.ts
+++ src/components/map/__tests__/decode-polyline-encoded-multi-polygons.test.ts
@@ -1,21 +1,10 @@
 import {decodePolylineEncodedMultiPolygons} from '../geofencing-zone-utils';
 
-import voiTrondheimEncoded from './voiTrondheimEncoded.json';
-import tierTrondheimEncoded from './tierTrondheimEncoded.json';
-import voiTrondheimDecoded from './voiTrondheimDecoded.json';
-import tierTrondheimDecoded from './tierTrondheimDecoded.json';
+import tierTrondheimEncoded from './tierTrondheimEncodedMinified.json';
+import tierTrondheimDecoded from './tierTrondheimDecodedMinified.json';
 import {PreProcessedGeofencingZones} from '../types';
 
 describe('decodePolylineEncodedMultiPolygons', () => {
-  it('returns correct geofencingZones with decoded MultiPolygons for voitrondheim', () => {
-    expect(
-      decodePolylineEncodedMultiPolygons(
-        voiTrondheimEncoded.data
-          .geofencingZones as any as PreProcessedGeofencingZones[],
-      ),
-    ).toMatchObject(voiTrondheimDecoded.data.geofencingZones);
-  });
-
   it('returns correct geofencingZones with decoded MultiPolygons for tiertrondheim', () => {
     expect(
       decodePolylineEncodedMultiPolygons(
