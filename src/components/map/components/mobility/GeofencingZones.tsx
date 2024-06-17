a1241a16c4ff83ddf540a268fcc81943ee3a846a
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-06-17 14:53:49 +0200

    Extract GeofencingZone to separate component

--- src/components/map/components/mobility/GeofencingZones.tsx
+++ src/components/map/components/mobility/GeofencingZones.tsx
@@ -2,7 +2,10 @@ import MapboxGL from '@rnmapbox/maps';
 
 import {FeatureCollection, GeoJsonProperties, Point, Feature} from 'geojson';
 
-import {usePreProcessedGeofencingZones} from '@atb/components/map';
+import {
+  PreProcessedGeofencingZones,
+  usePreProcessedGeofencingZones,
+} from '@atb/components/map';
 import {useVehicleQuery} from '@atb/mobility/queries/use-vehicle-query';
 import {isBicycle, isScooter} from '@atb/mobility';
 import {hitboxCoveringIconOnly} from '@atb/components/map';
@@ -34,50 +37,59 @@ export const GeofencingZones = ({selectedFeature}: GeofencingZonesProps) => {
     return <></>;
   }
 
-  const mappedGeofencingZones = preProcessedGeofencingZones.map(
-    (geofencingZone) => {
-      const getGeofencingZoneCustomProps = ['get', 'geofencingZoneCustomProps'];
-      const bgColor = [
-        'get',
-        'background',
-        ['get', 'color', getGeofencingZoneCustomProps],
-      ];
-      const fillOpacity = ['get', 'fillOpacity', getGeofencingZoneCustomProps];
-      const lineOpacity = [
-        'get',
-        'background',
-        ['get', 'strokeOpacity', getGeofencingZoneCustomProps],
-      ];
-
-      return (
-        <MapboxGL.ShapeSource
+  return (
+    <>
+      {preProcessedGeofencingZones.map((geofencingZone) => (
+        <GeofencingZone
+          geofencingZone={geofencingZone}
           key={geofencingZone?.renderKey}
-          id={'geofencingZonesShapeSource_' + geofencingZone?.renderKey}
-          shape={geofencingZone.geojson as FeatureCollection} // todo: fix GeofencingZonesType in mobility-types_v2
-          hitbox={hitboxCoveringIconOnly} // to not be able to hit multiple zones with one click
-        >
-          <MapboxGL.FillLayer
-            id="parkingFill"
-            style={{
-              fillAntialias: true,
-              fillColor: bgColor,
-              fillOpacity,
-            }}
-            aboveLayerID="water-point-label"
-          />
-          <MapboxGL.LineLayer
-            id="tariffZonesLine"
-            style={{
-              lineWidth: 3,
-              lineColor: bgColor,
-              lineOpacity,
-            }}
-            aboveLayerID="water-point-label"
-          />
-        </MapboxGL.ShapeSource>
-      );
-    },
+        />
+      ))}
+    </>
   );
+};
 
-  return <>{mappedGeofencingZones}</>;
+type GeofencingZoneProps = {
+  geofencingZone: PreProcessedGeofencingZones;
+};
+const GeofencingZone = ({geofencingZone}: GeofencingZoneProps) => {
+  const getGeofencingZoneCustomProps = ['get', 'geofencingZoneCustomProps'];
+  const bgColor = [
+    'get',
+    'background',
+    ['get', 'color', getGeofencingZoneCustomProps],
+  ];
+  const fillOpacity = ['get', 'fillOpacity', getGeofencingZoneCustomProps];
+  const lineOpacity = [
+    'get',
+    'background',
+
+    ['get', 'strokeOpacity', getGeofencingZoneCustomProps],
+  ];
+  return (
+    <MapboxGL.ShapeSource
+      id={'geofencingZonesShapeSource_' + geofencingZone?.renderKey}
+      shape={geofencingZone.geojson as FeatureCollection} // FeatureCollection from mobility-types_v2 and FeatureCollection from geojson used by MapboxGL don't match perfectly
+      hitbox={hitboxCoveringIconOnly} // to not be able to hit multiple zones with one click
+    >
+      <MapboxGL.FillLayer
+        id="parkingFill"
+        style={{
+          fillAntialias: true,
+          fillColor: bgColor,
+          fillOpacity,
+        }}
+        aboveLayerID="water-point-label"
+      />
+      <MapboxGL.LineLayer
+        id="tariffZonesLine"
+        style={{
+          lineWidth: 3,
+          lineColor: bgColor,
+          lineOpacity,
+        }}
+        aboveLayerID="water-point-label"
+      />
+    </MapboxGL.ShapeSource>
+  );
 };
