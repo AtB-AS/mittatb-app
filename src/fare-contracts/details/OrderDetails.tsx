33de8a3be387022ba8bc8bae8cd81a91405262a6
Ridwan Aditama <reidzeibel@gmail.com>
2024-06-26 14:49:44 +0200

    feat: hide payment method and price on refunded tickets (#4609)
    * feat: hide payment method and price on refunded tickets
    
    * remove stray apple
    
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>
    
    ---------
    
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- src/fare-contracts/details/OrderDetails.tsx
+++ src/fare-contracts/details/OrderDetails.tsx
@@ -1,4 +1,8 @@
-import {FareContract, isNormalTravelRight} from '@atb/ticketing';
+import {
+  FareContract,
+  FareContractState,
+  isNormalTravelRight,
+} from '@atb/ticketing';
 import {FareContractTexts, useTranslation} from '@atb/translations';
 import {View} from 'react-native';
 import {ThemeText} from '@atb/components/text';
@@ -16,10 +20,9 @@ export const OrderDetails = ({fareContract}: {fareContract: FareContract}) => {
     FareContractTexts.details.orderId(fareContract.orderId),
   );
   const firstTravelRight = fareContract.travelRights[0];
-  const priceString =
-    fareContract.totalAmount
-      ? formatDecimalNumber(parseFloat(fareContract.totalAmount), language, 2)
-      : undefined;
+  const priceString = fareContract.totalAmount
+    ? formatDecimalNumber(parseFloat(fareContract.totalAmount), language, 2)
+    : undefined;
 
   return (
     <View accessible={true}>
@@ -59,21 +62,23 @@ export const OrderDetails = ({fareContract}: {fareContract: FareContract}) => {
           </ThemeText>
         </>
       )}
-      <ThemeText
-        type="body__secondary"
-        color="secondary"
-        style={style.marginTop}
-      >
-        {t(FareContractTexts.details.paymentMethod)}
-        {_.capitalize(fareContract?.paymentType?.join(', '))}
-      </ThemeText>
-      {priceString && (
+      {fareContract.state !== FareContractState.Refunded && priceString && (
         <ThemeText
           type="body__secondary"
           color="secondary"
           style={style.marginTop}
         >
           {t(FareContractTexts.details.totalPrice(priceString))}
+        </ThemeText>
+      )}
+      {fareContract.state !== FareContractState.Refunded && (
+        <ThemeText
+          type="body__secondary"
+          color="secondary"
+          style={style.marginTop}
+        >
+          {t(FareContractTexts.details.paymentMethod)}
+          {_.capitalize(fareContract?.paymentType?.join(', '))}
         </ThemeText>
       )}
       <ThemeText style={style.marginTop}>{orderIdText}</ThemeText>
