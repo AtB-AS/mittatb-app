e8ab0c885b5802d3983c841b609b5971c6b1d71c
Jorge Osorio <jorelosorio@gmail.com>
2024-08-12 16:07:33 +0200

    fix: bootsplash

--- scripts/generate-native-assets.sh
+++ scripts/generate-native-assets.sh
@@ -6,4 +6,4 @@ echo "Generating app icons"
 yarn icons
  
 echo "Generating native bootsplash"
-yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-output=assets/bootsplash --background="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')"
+yarn react-native generate-bootsplash assets/bootsplash_logo_original.png --assets-output=assets/bootsplash --background="$(envprop 'BOOTSPLASH_BACKGROUND_COLOR')" --platforms=android,ios --flavor=main
