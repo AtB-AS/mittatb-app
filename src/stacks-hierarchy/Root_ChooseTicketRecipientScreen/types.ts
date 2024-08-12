bafc46f001d53ff9e1a0a82e0f37db0087b273a5
Gøran Dalum <goran.dalum@mittatb.no>
2024-08-12 14:58:28 +0200

    fix: Some fixes for ticket recipient feature (#4658)

--- src/stacks-hierarchy/Root_ChooseTicketRecipientScreen/types.ts
+++ src/stacks-hierarchy/Root_ChooseTicketRecipientScreen/types.ts
@@ -12,7 +12,6 @@ export type OnBehalfOfErrorCode =
 export type RecipientSelectionState = {
   settingPhone: boolean;
   settingName: boolean;
-  recipients?: ExistingRecipientType[];
   recipient?: ExistingRecipientType;
   phone?: string;
   prefix: string;
