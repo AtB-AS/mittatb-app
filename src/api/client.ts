38562e2cad9d6410d70b23301dea25f39d515035
Jorge Osorio <jorelosorio@gmail.com>
2024-09-17 09:53:36 +0200

    Revert "Use retry and onError to check for 401"
    This reverts commit de528e42a56cae03dcb0720d2c9705cf13b2ab37.

--- src/api/client.ts
+++ src/api/client.ts
@@ -43,7 +43,7 @@ declare module 'axios' {
   }
 }
 
-export function shouldRetry(error: AxiosError): boolean {
+function shouldRetry(error: AxiosError): boolean {
   const isRateLimited = error.response?.status === 429;
   if (isRateLimited) return false;
 
--- src/queries/ReactQueryProvider.tsx
+++ src/queries/ReactQueryProvider.tsx
@@ -1,26 +1,7 @@
-import {shouldRetry} from '@atb/api/client';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {AxiosError} from 'axios';
 import {ReactNode} from 'react';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: (_, error) => {
-        if (error instanceof AxiosError) {
-          shouldRetry(error);
-        }
-        return false;
-      },
-      onError: (error: any) => {
-        if (error?.config) {
-          error.config.forceRefreshIdToken =
-            error.config.authWithIdToken && error.response?.status === 401;
-        }
-      },
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 export const ReactQueryProvider = ({children}: {children: ReactNode}) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
