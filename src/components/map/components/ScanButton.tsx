6ecad9313797d925bf603eebda77044b4a9b28e4
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-09-27 15:23:10 +0200

    Fix touch events bug in ScanButton by removing container

--- src/components/map/components/ScanButton.tsx
+++ src/components/map/components/ScanButton.tsx
@@ -4,7 +4,6 @@ import {StyleSheet} from '@atb/theme';
 import {Qr} from '@atb/assets/svg/mono-icons/ticketing';
 import {useAnalytics} from '@atb/analytics';
 import {MapTexts, useTranslation} from '@atb/translations';
-import {View} from 'react-native';
 import {useControlPositionsStyle} from '../hooks/use-control-styles';
 
 type ScanButtonProps = {
@@ -14,43 +13,32 @@ export const ScanButton = ({onPress}: ScanButtonProps) => {
   const styles = useStyles();
   const analytics = useAnalytics();
   const {t} = useTranslation();
-
   const {controlsContainer} = useControlPositionsStyle();
 
   return (
-    <View
-      style={{
-        ...styles.scanButtonContainer,
-        bottom: controlsContainer.bottom,
+    <Button
+      style={{...styles.scanButton, bottom: controlsContainer.bottom}}
+      type="medium"
+      compact={true}
+      interactiveColor="interactive_2"
+      accessibilityRole="button"
+      onPress={() => {
+        analytics.logEvent('Map', 'Scan');
+        onPress();
       }}
-    >
-      <Button
-        style={styles.scanButton}
-        type="medium"
-        compact={true}
-        interactiveColor="interactive_2"
-        accessibilityRole="button"
-        onPress={() => {
-          analytics.logEvent('Map', 'Scan');
-          onPress();
-        }}
-        text={t(MapTexts.qr.scan)}
-        rightIcon={{svg: Qr}}
-        hasShadow={true}
-      />
-    </View>
+      text={t(MapTexts.qr.scan)}
+      rightIcon={{svg: Qr}}
+      hasShadow={true}
+    />
   );
 };
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
-  scanButtonContainer: {
-    position: 'absolute',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
   scanButton: {
+    position: 'absolute',
+    display: 'flex',
+    alignSelf: 'center',
+    pointerEvents: 'auto',
     marginBottom: theme.spacings.small,
   },
 }));
