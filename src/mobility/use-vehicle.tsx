6d5c37f13b512fc7557b4f9e675fad2aa4d9f1d0
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2024-10-08 10:27:20 +0200

    Remove unnecessary useMemo

--- src/mobility/use-vehicle.tsx
+++ src/mobility/use-vehicle.tsx
@@ -1,11 +1,9 @@
 import {useSystem} from '@atb/mobility/use-system';
 import {getRentalAppUri} from '@atb/mobility/utils';
 import {useVehicleQuery} from '@atb/mobility/queries/use-vehicle-query';
-import {useMemo} from 'react';
 
 export const useVehicle = (id: string) => {
-  const {data: vehicleData, isLoading, isError} = useVehicleQuery(id);
-  const vehicle = useMemo(() => vehicleData, [vehicleData]);
+  const {data: vehicle, isLoading, isError} = useVehicleQuery(id);
   const {appStoreUri, brandLogoUrl, operatorId, operatorName} = useSystem(
     vehicle,
     vehicle?.system.operator.name,
