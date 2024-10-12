a08e5f6d856da2598128f074768d036fbc644d09
Ridwan Aditama <reidzeibel@gmail.com>
2024-10-12 11:08:06 +0200

    feat: translations for travel aid page

--- src/translations/screens/subscreens/TravelAidSettingsTexts.ts
+++ src/translations/screens/subscreens/TravelAidSettingsTexts.ts
@@ -4,6 +4,38 @@ const TravelAidSettingsTexts = {
   header: {
     title: _('Tilgjengelighet', 'Accessibility', 'Tilgjengelegheit'),
   },
+
+  toggle: {
+    title: _('Reisehjelp', 'Journey Aid', 'Reisehjelp'),
+    subText: _(
+      'Reisehjelp er en forenklet modus spesielt tilrettelagt for deg som trenger ekstra støtte når du reiser.\n\n* Stoppe en buss eller trikk med appen, fra holdeplassen der du skal på.\n* Raskere tilgang på sanntids-oppdateringer om ankomst og neste holdeplass.\n* Snarvei til gyldige billetter.\n* Invitasjonskode for tilgang til funksjonen får du gjennom Blindeforbundet eller AtBs kundeservice.',
+      'Journey Aid is a simplified mode designed specifically for those who need extra support when traveling.\n\n* Use the app to signal a stop for a bus or tram from your boarding stop.\n* Get faster access to real-time updates on arrivals and upcoming stops, as well as a shortcut to your tickets.* Shortcut to valid tickets.* You can request an invitation code for this feature through the The Norwegian Association of the Blind and Partially Sighted or AtB’s customer service.',
+      'Reisehjelp er en forenklet modus spesielt tilrettelagt for deg som trenger ekstra støtte når du reiser.\n\n* Stoppe en buss eller trikk med appen, fra holdeplassen der du skal på.\n* Raskere tilgang på sanntids-oppdateringer om ankomst og neste holdeplass.\n* Snarvei til gyldige billetter.\n* Invitasjonskode for tilgang til funksjonen får du gjennom Blindeforbundet eller AtBs kundeservice.',
+    ),
+  },
+
+  button: {
+    importantInfo: {
+      title: _('Viktig å vite', 'Important information', 'Viktig å vite'),
+      a11yHint: _(
+        'Aktivér for å vise informasjonsside',
+        'Activate to show information page',
+        'Aktivér for å vise informasjonsside',
+      ),
+    },
+    contact: {
+      title: _(
+        'Ring kundeservice',
+        'Call Customer Service',
+        'Ring kundeservice',
+      ),
+      a11yHint: _(
+        'Aktivér for å ringe kundeservice',
+        'Activate to call customer service',
+        'Trykk for å ringe kundeservice',
+      ),
+    },
+  },
 };
 
 export default TravelAidSettingsTexts;
