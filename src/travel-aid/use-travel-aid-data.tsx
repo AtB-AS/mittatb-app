d63e61a47d3de2691fde737aafe030f25516cc15
Johannes Røsvik <j.rosvik@gmail.com>
2024-10-17 11:11:44 +0200

    update journey aid screen

--- src/travel-aid/use-travel-aid-data.tsx
+++ src/travel-aid/use-travel-aid-data.tsx
@@ -1,12 +1,9 @@
 import {useQuery} from '@tanstack/react-query';
-import {ONE_HOUR_MS} from '@atb/utils/durations.ts';
 import {getServiceJourneyWithEstimatedCalls} from '@atb/api/serviceJourney';
-import {formatISO} from 'date-fns';
 
 export const useTravelAidDataQuery = (id: string, date: Date) =>
   useQuery({
-    queryKey: ['travelAidData', id, formatISO(date, {representation: 'date'})],
+    queryKey: ['travelAidData', id],
     queryFn: () => getServiceJourneyWithEstimatedCalls(id, date),
-    staleTime: ONE_HOUR_MS,
-    cacheTime: ONE_HOUR_MS,
+    refetchInterval: 10000,
   });
