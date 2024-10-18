7c751854bac0421e25b94044a056681fb1da2b24
Jorge Osorio <43450882+jorelosorio@users.noreply.github.com>
2024-10-18 08:20:04 +0200

    Update src/translations/screens/subscreens/TravelAidSettingsTexts.ts
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- src/translations/screens/subscreens/TravelAidSettingsTexts.ts
+++ src/translations/screens/subscreens/TravelAidSettingsTexts.ts
@@ -13,9 +13,9 @@ const TravelAidSettingsTexts = {
   toggle: {
     title: _('Reisehjelp', 'Journey Aid', 'Reisehjelp'),
     subText: _(
-      'Reisehjelp er en forenklet modus spesielt tilrettelagt for deg som trenger ekstra støtte når du reiser.\n\n* Stoppe en buss eller trikk med appen, fra holdeplassen der du skal på.\n* Raskere tilgang på sanntids-oppdateringer om ankomst og neste holdeplass.\n* Snarvei til gyldige billetter.\n* Invitasjonskode for tilgang til funksjonen får du gjennom Blindeforbundet eller AtBs kundeservice.',
-      'Journey Aid is a simplified mode designed specifically for those who need extra support when traveling.\n\n* Use the app to signal a stop for a bus or tram from your boarding stop.\n* Get faster access to real-time updates on arrivals and upcoming stops, as well as a shortcut to your tickets.* Shortcut to valid tickets.* You can request an invitation code for this feature through the The Norwegian Association of the Blind and Partially Sighted or AtB’s customer service.',
-      'Reisehjelp er en forenklet modus spesielt tilrettelagt for deg som trenger ekstra støtte når du reiser.\n\n* Stoppe en buss eller trikk med appen, fra holdeplassen der du skal på.\n* Raskere tilgang på sanntids-oppdateringer om ankomst og neste holdeplass.\n* Snarvei til gyldige billetter.\n* Invitasjonskode for tilgang til funksjonen får du gjennom Blindeforbundet eller AtBs kundeservice.',
+      'Reisehjelp er en forenklet modus spesielt tilrettelagt for deg som trenger ekstra støtte når du reiser.\n\n* Stopp en buss eller trikk med appen, fra holdeplassen der du skal på.\n* Raskere tilgang på sanntids-oppdateringer om ankomst og neste holdeplass.\n* Snarvei til gyldige billetter.',
+      'Journey Aid is a simplified mode designed specifically for those who need extra support when traveling.\n\n* Use the app to signal a stop for a bus or tram from your boarding stop.\n* Get faster access to real-time updates on arrivals and upcoming stops, as well as a shortcut to your tickets.* Shortcut to valid tickets.',
+      'Reisehjelp er ein forenkla modus spesielt tilrettelagt for deg som treng ekstra støtte når du reiser.\n\n* Stopp ein buss eller trikk med appen, frå haldeplassen der du skal på.\n* Raskare tilgang på sanntids-oppdateringar om ankomst og neste haldeplass.\n* Snarveg til gyldige billettar.',
     ),
   },
 
