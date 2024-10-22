4f16cdeafd822073ee604faaeadc0022b687e142
Gøran Dalum <goran.dalum@gmail.com>
2024-10-22 18:28:56 +0200

    feat: Show carnet usage times on details screen

---
+++ src/fare-contracts/details/UsedAccessesSectionItem.tsx
@@ -0,0 +1,32 @@
+import {CarnetTravelRightUsedAccess} from '@atb/ticketing';
+import {FareContractTexts, useTranslation} from '@atb/translations';
+import {View} from 'react-native';
+import {ThemeText} from '@atb/components/text';
+import {fullDateTime} from '@atb/utils/date.ts';
+import React from 'react';
+import {useSectionItem} from '@atb/components/sections';
+
+type Props = {usedAccesses: CarnetTravelRightUsedAccess[]};
+
+export const UsedAccessesSectionItem = ({usedAccesses}: Props) => {
+  const {topContainer} = useSectionItem({});
+  const {t, language} = useTranslation();
+
+  const dateStrings = usedAccesses
+    .map((ua) => ua.startDateTime)
+    .sort((d1, d2) => d2.getTime() - d1.getTime())
+    .map((d) => fullDateTime(d, language));
+
+  return (
+    <View style={topContainer} accessible={true}>
+      <ThemeText type="body__secondary">
+        {t(FareContractTexts.details.usedAccesses)}
+      </ThemeText>
+      {dateStrings.map((dateString) => (
+        <ThemeText type="body__secondary" color="secondary">
+          {dateString}
+        </ThemeText>
+      ))}
+    </View>
+  );
+};
