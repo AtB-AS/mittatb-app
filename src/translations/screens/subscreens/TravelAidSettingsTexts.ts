7ced5baa52c706cd47562e04b9b8736d7dd17468
Jorge Osorio <jorelosorio@gmail.com>
2024-10-28 12:07:23 +0100

    Update TravelAidSettingsTexts.ts: Update Norwegian translation

--- src/translations/screens/subscreens/TravelAidSettingsTexts.ts
+++ src/translations/screens/subscreens/TravelAidSettingsTexts.ts
@@ -15,7 +15,7 @@ const TravelAidSettingsTexts = {
     subText: _(
       'Reisehjelp er en forenklet visning av buss- og trikkeavganger, tilpasset for deg som trenger ekstra støtte på reisen. Den gir rask tilgang til sanntidsoppdateringer om ankomsttid og neste holdeplass.\n\n* Aktiver Reisehjelp ved å trykke på bryteren øverst til høyre.\n* Velg holdeplass under «Avganger»-fanen.\n* Velg riktig buss eller trikk fra listen.\n* Trykk på knappen for Reisehjelp øverst i visningen.\n\nVi jobber fortsatt med funksjonen, og ny funksjonalitet vil komme fortløpende. Del gjerne din tilbakemelding!',
       'Journey Aid is a simplified view of bus and tram departures, designed for those who need extra support when traveling. It provides quick access to real-time updates on arrival times and the next stop.\n\n* Activate Journey Aid by tapping the switch at the top right.\n* Select your stop under the "Departures" tab.\n* Choose the correct bus or tram from the list.\n* Tap the Journey Aid button at the top of the screen.\n\nWe are still working on this feature, and new functionality will be added continuously. Please share your feedback with us!',
-      'Reisehjelp er ei forenkla visning av buss- og trikkeavgangar, tilpassa for deg som trenger ekstra støtte på reisa. Den gir rask tilgang til sanntidsoppdateringer om framkomsttid og neste haldeplass.\n\n* Aktivér Reisehjelp ved å trykke på brytaren øvst til høgre.\n* Velg haldeplass under «Avgangar»-fanen.\n* Velg riktig buss eller trikk frå lista.\n* Trykk på knappen for Reisehjelp øvst i visninga.\n\nVi jobbar fortsatt med funksjonen, og ny funksjonalitet vil komme fortløpande. Del gjerne din tilbakemelding!,
+      'Reisehjelp er ei forenkla visning av buss- og trikkeavgangar, tilpassa for deg som trenger ekstra støtte på reisa. Den gir rask tilgang til sanntidsoppdateringer om framkomsttid og neste haldeplass.\n\n* Aktivér Reisehjelp ved å trykke på brytaren øvst til høgre.\n* Velg haldeplass under «Avgangar»-fanen.\n* Velg riktig buss eller trikk frå lista.\n* Trykk på knappen for Reisehjelp øvst i visninga.\n\nVi jobbar fortsatt med funksjonen, og ny funksjonalitet vil komme fortløpande. Del gjerne din tilbakemelding!',
     ),
   },
 
@@ -59,7 +59,7 @@ const TravelAidSettingsTexts = {
       title: _('Husk:', 'Keep in mind:', 'Husk:'),
       content: _(
         '* Følg retningslinjer, som å stå på ledelinjen eller lengst frem på holdeplassen, for å vise at du skal på.\n* Du kan bare stoppe én buss/trikk om gangen.\n* Du kan ikke angre et stoppsignal som er sendt.',
-        '* Follow guidelines, such as standing on the tactile guiding line or at the front of the stop, to signal that you\'re boarding.\n* You can only stop one bus/tram at a time.\n * You cannot cancel a stop signal once it has been sent.',
+        "* Follow guidelines, such as standing on the tactile guiding line or at the front of the stop, to signal that you're boarding.\n* You can only stop one bus/tram at a time.\n * You cannot cancel a stop signal once it has been sent.",
         '* Følg retningslinjer, som å stå på ledelinja eller lengst fram på haldeplassen, for å vise at du skal på.\n* Du kan berre stoppe ein buss/trikk om gongen.\n* Du kan ikkje angre eit stoppsignal som er sendt.',
       ),
     },
