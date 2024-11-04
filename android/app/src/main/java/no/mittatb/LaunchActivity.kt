64af72079a72703ed7900fe0820d5061d015b412
Ridwan Aditama <reidzeibel@gmail.com>
2024-11-04 13:03:07 +0100

    fix: sort out issues with splash screen

--- android/app/src/main/java/no/mittatb/LaunchActivity.kt
+++ android/app/src/main/java/no/mittatb/LaunchActivity.kt
@@ -3,11 +3,13 @@ package no.mittatb
 import com.facebook.react.ReactActivity
 import android.content.Intent
 import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 
 class LaunchActivity : ReactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        RNBootSplash.init(this, R.style.AppTheme);
         super.onCreate(savedInstanceState)
-        val intent: Intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
