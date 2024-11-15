b1c7ff8adafe79cd8e8d1ef6b829241404bbaa52
Gøran Dalum <goran.dalum@gmail.com>
2024-11-15 11:27:42 +0100

    one more test

--- src/travel-aid/__tests__/is-applicable-transport-mode.test.ts
+++ src/travel-aid/__tests__/is-applicable-transport-mode.test.ts
@@ -45,15 +45,23 @@ describe('isApplicableTransportMode', () => {
 
   it('should give true if modes array includes service journey transport mode, and submodes array includes service journey submode', () => {
     const res = isApplicableTransportMode(
-      [{mode: 'bus', submodes: ['postBus']}, {mode: 'rail'}],
+      [{mode: 'bus', submodes: ['postBus', 'nightBus']}, {mode: 'rail'}],
       withModes(TransportMode.Bus, TransportSubmode.PostBus),
     );
     expect(res).toBe(true);
   });
 
+  it('should give false if modes array includes service journey transport mode, and submodes array includes some submodes while service journey has another submode', () => {
+    const res = isApplicableTransportMode(
+      [{mode: 'bus', submodes: ['postBus', 'nightBus']}, {mode: 'rail'}],
+      withModes(TransportMode.Bus, TransportSubmode.LocalBus),
+    );
+    expect(res).toBe(false);
+  });
+
   it('should give false if modes array includes service journey transport mode, and submodes array includes some submodes while service journey submode is empty', () => {
     const res = isApplicableTransportMode(
-      [{mode: 'bus', submodes: ['postBus']}, {mode: 'rail'}],
+      [{mode: 'bus', submodes: ['postBus', 'nightBus']}, {mode: 'rail'}],
       withModes(TransportMode.Bus),
     );
     expect(res).toBe(false);
