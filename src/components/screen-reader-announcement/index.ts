ae4de0f9d14bef4c1f4cdb36fee22962f4350ef4
Johannes Røsvik <j.rosvik@gmail.com>
2024-11-19 09:43:30 +0100

    feat: use live regions to announce travel aid updates (#4826)

--- src/components/screen-reader-announcement/index.ts
+++ src/components/screen-reader-announcement/index.ts
@@ -1,2 +1,4 @@
 export {ScreenReaderAnnouncement} from './ScreenReaderAnnouncement';
 export {useScreenReaderAnnouncement} from './use-screen-reader-announcement';
+export {LiveRegionWrapper} from './LiveRegionWrapper';
+export {useLiveRegionAnnouncement} from './use-live-region-announcement';
