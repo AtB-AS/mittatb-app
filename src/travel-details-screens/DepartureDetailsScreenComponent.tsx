6ea52578052efac3660364146bf5cc050a0ea44d
Johannes Røsvik <j.rosvik@gmail.com>
2024-11-29 14:37:41 +0100

    refactor: use useQuery for fetching live bus status
    This also fixes an exhaustive deps issue :)

--- src/travel-details-screens/DepartureDetailsScreenComponent.tsx
+++ src/travel-details-screens/DepartureDetailsScreenComponent.tsx
@@ -17,7 +17,7 @@ import {AccessibleText, ThemeText} from '@atb/components/text';
 import {ThemeIcon} from '@atb/components/theme-icon';
 import {CancelledDepartureMessage} from '@atb/travel-details-screens/components/CancelledDepartureMessage';
 import {SituationMessageBox} from '@atb/situations';
-import {useGetServiceJourneyVehicles} from '@atb/travel-details-screens/use-get-service-journey-vehicles';
+import {useGetServiceJourneyVehiclesQuery} from '@atb/travel-details-screens/use-get-service-journey-vehicles';
 import {StyleSheet, useTheme} from '@atb/theme';
 import {
   DepartureDetailsTexts,
@@ -125,8 +125,9 @@ export const DepartureDetailsScreenComponent = ({
     isRealtimeMapEnabled,
   );
 
-  const {vehiclePositions} = useGetServiceJourneyVehicles(
-    shouldShowLive ? [activeItem.serviceJourneyId] : undefined,
+  const {data: vehiclePositions} = useGetServiceJourneyVehiclesQuery(
+    [activeItem.serviceJourneyId],
+    shouldShowLive,
   );
 
   const translatedModeName = getTranslatedModeName(mode);
