426fe6416359c8734ee033eb6137d1efa6faa275
Ridwan Aditama <reidzeibel@gmail.com>
2024-12-03 13:15:42 +0100

    run prettier

--- src/mobile-token/hooks/use-load-native-token-query.tsx
+++ src/mobile-token/hooks/use-load-native-token-query.tsx
@@ -1,8 +1,6 @@
 import {useQuery} from '@tanstack/react-query';
 import {storage} from '@atb/storage';
-import {
-  ActivatedToken,
-} from '@entur-private/abt-mobile-client-sdk';
+import {ActivatedToken} from '@entur-private/abt-mobile-client-sdk';
 import {mobileTokenClient} from '@atb/mobile-token/mobileTokenClient';
 import {tokenService} from '@atb/mobile-token/tokenService';
 import {
