41e7e45ee89cd903d458476a9f3a3c01872ea2e9
Ridwan Aditama <reidzeibel@gmail.com>
2024-12-05 11:13:58 +0100

    prettier

--- src/purchase-selection/use-selectable-user-profiles.ts
+++ src/purchase-selection/use-selectable-user-profiles.ts
@@ -2,7 +2,9 @@ import {PreassignedFareProduct, UserProfile} from '@atb-as/config-specs';
 import {useFirestoreConfiguration} from '@atb/configuration';
 import {isSelectableProfile} from './utils';
 
-export function useSelectableUserProfiles(product: PreassignedFareProduct): UserProfile[] {
+export function useSelectableUserProfiles(
+  product: PreassignedFareProduct,
+): UserProfile[] {
   const {userProfiles} = useFirestoreConfiguration();
   return userProfiles.filter((profile) =>
     isSelectableProfile(product, profile),
