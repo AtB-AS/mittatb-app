f582c816bf5d9338af03924b4f2d01316fbc9e4f
Gøran Dalum <goran.dalum@gmail.com>
2024-12-11 13:16:32 +0100

    feat: Only reserving reservations gives login warning
    Also reservations in rejected state made the user get the login
    warning about active tickets. This should not be the case, so the
    logic was changed so only reserving reservations gives login
    warning.

--- src/ticketing/use-has-reservation-or-active-fare-contract.ts
+++ src/ticketing/use-has-reservation-or-active-fare-contract.ts
@@ -1,6 +1,7 @@
 import {useTicketingState} from '@atb/ticketing/TicketingContext';
 import {useTimeContextState} from '@atb/time';
 import {filterActiveOrCanBeUsedFareContracts} from '@atb/ticketing/utils';
+import {getReservationStatus} from '@atb/fare-contracts/utils';
 
 export const useHasReservationOrActiveFareContract = () => {
   const {fareContracts, reservations} = useTicketingState();
@@ -10,7 +11,9 @@ export const useHasReservationOrActiveFareContract = () => {
     serverNow,
   );
   const hasActiveFareContracts = activeFareContracts.length > 0;
-  const hasReservations = reservations.length > 0;
+  const hasReservingReservations = reservations
+    .map(getReservationStatus)
+    .some((status) => status === 'reserving');
 
-  return hasActiveFareContracts || hasReservations;
+  return hasActiveFareContracts || hasReservingReservations;
 };
