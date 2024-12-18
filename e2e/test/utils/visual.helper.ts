2de517a5b5b0ed37c71a2b1f07877cc518d67fc0
tormoseng <tor@moseng.net>
2024-12-18 14:33:39 +0100

    test: helper functions

--- e2e/test/utils/visual.helper.ts
+++ e2e/test/utils/visual.helper.ts
@@ -3,9 +3,40 @@ import {driver} from '@wdio/globals';
 import {Result} from '@wdio/visual-service/dist/types.js';
 
 /**
- * Different helper methods related to the app in general
+ * Different helper methods related to visual tests in the app
  */
 class VisualHelper {
+  async visualTestScreen(tag: string, testOptions: any, newBaseline: boolean) {
+    if (newBaseline) {
+      await driver.saveScreen(tag, testOptions);
+    } else {
+      const res: Result = await driver.checkScreen(tag, testOptions);
+      // console.log(s.misMatchPercentage);
+      await expect(res.misMatchPercentage).toEqual(0);
+    }
+  }
+
+  async visualTestElement(
+    elemId: string,
+    tag: string,
+    testOptions: any,
+    newBaseline: boolean,
+  ) {
+    if (newBaseline) {
+      const el = `//*[@resource-id="${elemId}"]`;
+      const res: Result = await driver.checkElement(
+        await $(el),
+        tag,
+        testOptions,
+      );
+      // console.log(s.misMatchPercentage);
+      await expect(res.misMatchPercentage).toEqual(0);
+    } else {
+      const el = `//*[@resource-id="${elemId}"]`;
+      await driver.saveElement(await $(el), tag, testOptions);
+    }
+  }
+
   async saveVisualTestScreen(tag: string, testOptions: any) {
     await driver.saveScreen(tag, testOptions);
   }
@@ -15,13 +46,13 @@ class VisualHelper {
     await driver.saveElement(await $(el), tag, testOptions);
   }
 
-  async visualTestScreen(tag: string, testOptions: any) {
+  async visualTestScreen_orig(tag: string, testOptions: any) {
     const res: Result = await driver.checkScreen(tag, testOptions);
     // console.log(s.misMatchPercentage);
     await expect(res.misMatchPercentage).toEqual(0);
   }
 
-  async visualTestElement(elemId: string, tag: string, testOptions: any) {
+  async visualTestElement_orig(elemId: string, tag: string, testOptions: any) {
     const el = `//*[@resource-id="${elemId}"]`;
     const res: Result = await driver.checkElement(
       await $(el),
