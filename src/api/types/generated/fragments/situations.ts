17c771e4329d71c13a07b6001e873ceec96dd12b
Gøran Dalum <goran.dalum@mittatb.no>
2024-12-19 14:44:32 +0100

    refactor: Use generated bff types with inline fragments (#4894)

--- src/api/types/generated/fragments/situations.ts
+++ src/api/types/generated/fragments/situations.ts
@@ -1,12 +1,17 @@
 import * as Types from '@atb/api/types/generated/journey_planner_v3_types';
+import type {
+  InfoLinkFragment,
+  MultilingualStringFragment,
+  ValidityPeriodFragment,
+} from '@atb/api/types/generated/fragments/shared';
 
 export type SituationFragment = {
   id: string;
   situationNumber?: string;
   reportType?: Types.ReportType;
-  summary: Array<{language?: string; value: string}>;
-  description: Array<{language?: string; value: string}>;
-  advice: Array<{language?: string; value: string}>;
-  infoLinks?: Array<{uri: string; label?: string}>;
-  validityPeriod?: {startTime?: any; endTime?: any};
+  summary: Array<MultilingualStringFragment>;
+  description: Array<MultilingualStringFragment>;
+  advice: Array<MultilingualStringFragment>;
+  infoLinks?: Array<InfoLinkFragment>;
+  validityPeriod?: ValidityPeriodFragment;
 };
