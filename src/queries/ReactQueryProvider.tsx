e56edbecd207600879e50305afc7744f94877c65
Håkon Strandlie <hakon.strandlie@atb.no>
2025-01-13 15:22:13 +0100

    fix: Disable retries on response code 429

--- src/queries/ReactQueryProvider.tsx
+++ src/queries/ReactQueryProvider.tsx
@@ -1,7 +1,20 @@
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactNode} from 'react';
+import type {AxiosError} from 'axios';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        const axiosError = error as AxiosError;
+        if (axiosError.response?.status === 429) {
+          return false;
+        }
+        return failureCount < 3; // Default is 3
+      },
+    },
+  },
+});
 
 export const ReactQueryProvider = ({children}: {children: ReactNode}) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
