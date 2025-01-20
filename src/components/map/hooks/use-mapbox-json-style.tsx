6cdba78847a8859093e8c2d2aa2d33465ef8afcf
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-01-20 15:15:40 +0100

    Use sprite url from remote config

--- src/components/map/hooks/use-mapbox-json-style.tsx
+++ src/components/map/hooks/use-mapbox-json-style.tsx
@@ -2,17 +2,19 @@ import {useThemeContext} from '@atb/theme';
 import {useMemo} from 'react';
 import {mapboxLightStyle} from '../mapbox-styles/mapbox-light-style';
 import {mapboxDarkStyle} from '../mapbox-styles/mapbox-dark-style';
+import {useRemoteConfigContext} from '@atb/RemoteConfigContext';
 
 export const useMapboxJsonStyle: () => string | undefined = () => {
   const {themeName} = useThemeContext();
+  const {mapbox_sprite_url} = useRemoteConfigContext();
 
   const mapboxJsonStyle = useMemo(
     () =>
       JSON.stringify({
         ...(themeName === 'dark' ? mapboxDarkStyle : mapboxLightStyle),
-        sprite: 'http://localhost:3000/sprite/v1/' + themeName, // todo: replace with remote config url
+        sprite: mapbox_sprite_url + themeName,
       }),
-    [themeName],
+    [themeName, mapbox_sprite_url],
   );
 
   return mapboxJsonStyle;
