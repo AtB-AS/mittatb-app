cee62b61d9da150b591d2378e6065b955cd83970
Ridwan Aditama <reidzeibel@gmail.com>
2025-01-22 15:28:49 +0100

    feat: add more breadcrumbs and remove log filtering

--- src/mobile-token/abtClientLogger.ts
+++ src/mobile-token/abtClientLogger.ts
@@ -1,9 +1,7 @@
 import Bugsnag, {Event} from '@bugsnag/react-native';
 import {
   ClientConfig,
-  TokenFactoryError,
 } from '@entur-private/abt-mobile-client-sdk';
-import {isRemoteTokenStateError} from './utils';
 
 export const localLogger: ClientConfig['localLogger'] = {
   debug: (msg, metadata?) => {
@@ -13,10 +11,12 @@ export const localLogger: ClientConfig['localLogger'] = {
     Bugsnag.leaveBreadcrumb('Mobiletoken sdk info message: ' + msg, metadata);
   },
   warn: (msg, err, metadata?) => {
+    Bugsnag.leaveBreadcrumb('Mobiletoken sdk info message: ' + msg, metadata);
     const onError = toOnErrorCallback('warning', msg, metadata);
     if (err) Bugsnag.notify(err, onError);
   },
   error: (msg, err, metadata?) => {
+    Bugsnag.leaveBreadcrumb('Mobiletoken sdk info message: ' + msg, metadata);
     const onError = toOnErrorCallback('error', msg, metadata);
     if (err) Bugsnag.notify(err, onError);
   },
@@ -27,9 +27,7 @@ export const remoteLogger: ClientConfig['remoteLogger'] = (err) => {
     'error',
     'Mobiletoken sdk remote logger error',
   );
-  if (!isRemoteTokenStateError(err) && !(err instanceof TokenFactoryError)) {
-    Bugsnag.notify(err, onError);
-  }
+  Bugsnag.notify(err, onError);
 };
 
 const toOnErrorCallback =
