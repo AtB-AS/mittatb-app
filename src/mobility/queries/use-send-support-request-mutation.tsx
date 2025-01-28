6ed8d57eaf5b3097ef7fe83d632ba1c0f30e6d83
jorunnl <jorunn.leithe@kantega.no>
2025-01-28 15:25:49 +0100

    fix: remove unused import

--- src/mobility/queries/use-send-support-request-mutation.tsx
+++ src/mobility/queries/use-send-support-request-mutation.tsx
@@ -2,7 +2,6 @@ import {useMutation} from '@tanstack/react-query';
 import {sendSupportRequest} from '@atb/api/mobility';
 import {SendSupportRequestBody} from '@atb/api/types/mobility';
 import {useAcceptLanguage} from '@atb/api/use-accept-language';
-import {useEffect} from 'react';
 
 export const useSendSupportRequestMutation = (
   operatorId: string,
