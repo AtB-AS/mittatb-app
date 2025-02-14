1554d0cbdda96b3d453c2b414303c8d3f890826e
Magnus Dahl <magnus52@hotmail.com>
2025-02-14 12:50:11 +0100

    added disabled functionallity and small fixes

--- src/mobility/components/ShmoTripCard.tsx
+++ src/mobility/components/ShmoTripCard.tsx
@@ -6,18 +6,20 @@ import {useTimeContext} from '@atb/time';
 import {LineWithVerticalBars} from '@atb/fare-contracts/components/ValidityLine';
 import {useTransportColor} from '@atb/utils/use-transport-color';
 import {useShmoBookingQuery} from '../queries/use-shmo-booking-query';
+import {ShmoBooking} from '@atb/api/types/mobility';
 
 type Props = {
-  bookingId: string;
+  bookingId: ShmoBooking['bookingId'];
+  isDisabled: boolean;
 };
 
-export const TripCard = ({bookingId}: Props) => {
+export const ShmoTripCard = ({bookingId, isDisabled}: Props) => {
   const styles = useStyles();
   const {serverNow} = useTimeContext();
   const {theme} = useThemeContext();
   const lineColor = theme.color.background.neutral[0].background;
   const backgroundColor = useTransportColor('scooter', 'escooter');
-  const {data: booking} = useShmoBookingQuery(bookingId, 30000);
+  const {data: booking} = useShmoBookingQuery(bookingId, 15000);
 
   if (!booking) {
     return null;
@@ -26,17 +28,23 @@ export const TripCard = ({bookingId}: Props) => {
   return (
     <Section style={styles.container}>
       <LineWithVerticalBars
-        backgroundColor={backgroundColor.primary.background}
+        backgroundColor={
+          isDisabled
+            ? backgroundColor.primary.foreground.disabled
+            : backgroundColor.primary.background
+        }
         lineColor={lineColor}
-        style={{
-          borderTopLeftRadius: theme.border.radius.regular,
-          borderTopRightRadius: theme.border.radius.regular,
-        }}
+        style={styles.LineBars}
+        animate={!isDisabled}
       />
 
       <ShmoTripDetailsSectionItem
         startDateTime={booking.departureTime ?? new Date()}
-        endDateTime={new Date(serverNow)}
+        endDateTime={
+          isDisabled
+            ? new Date(booking.arrivalTime ?? booking.departureTime ?? '')
+            : new Date(serverNow)
+        }
         totalAmount={booking?.pricing.currentAmount.toString() ?? ''}
         withHeader={false}
       />
@@ -49,6 +57,10 @@ const useStyles = StyleSheet.createThemeHook((theme) => {
     container: {
       paddingHorizontal: theme.spacing.medium,
       marginBottom: theme.spacing.medium,
+    },
+    LineBars: {
+      borderTopLeftRadius: theme.border.radius.regular,
+      borderTopRightRadius: theme.border.radius.regular,
     },
   };
 });
