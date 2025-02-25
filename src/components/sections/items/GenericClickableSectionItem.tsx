41288ea8513307d033e3b68aae85be82a0ffadf4
jorunnl <jorunn.leithe@kantega.no>
2025-02-25 12:23:35 +0100

    fix: use active GenericClickableSectionItem

--- src/components/sections/items/GenericClickableSectionItem.tsx
+++ src/components/sections/items/GenericClickableSectionItem.tsx
@@ -1,16 +1,21 @@
 import React, {PropsWithChildren, forwardRef} from 'react';
-import {AccessibilityProps, View} from 'react-native';
+import {AccessibilityProps, View, PressableProps} from 'react-native';
 import {useSectionItem} from '../use-section-item';
 import {SectionItemProps} from '../types';
-import {PressableOpacity} from '@atb/components/pressable-opacity';
+import {
+  PressableOpacity,
+  PressableOpacityProps,
+} from '@atb/components/pressable-opacity';
 
 type Props = PropsWithChildren<
   SectionItemProps<
     {
       onPress?(): void;
-    } & AccessibilityProps
+    } & AccessibilityProps &
+      PressableOpacityProps
   >
 >;
+
 export const GenericClickableSectionItem = forwardRef<any, Props>(
   ({children, ...props}, focusRef) => {
     const {topContainer} = useSectionItem(props);
--- src/mobility/components/PayWithBonusPointsCheckbox.tsx
+++ src/mobility/components/PayWithBonusPointsCheckbox.tsx
@@ -1,8 +1,11 @@
 import React, {ComponentProps} from 'react';
 import {BonusPriceTag} from '@atb/bonus';
 import {Checkbox} from '@atb/components/checkbox';
-import {PressableOpacity} from '@atb/components/pressable-opacity';
-import {GenericSectionItem} from '@atb/components/sections';
+import {
+  GenericClickableSectionItem,
+  Section,
+  SectionProps,
+} from '@atb/components/sections';
 import {ThemeText} from '@atb/components/text';
 import {ThemeIcon} from '@atb/components/theme-icon';
 import {BonusProductType} from '@atb/configuration/types';
@@ -15,7 +18,7 @@ import {
 } from '@atb/translations';
 import {View} from 'react-native';
 
-type Props = ComponentProps<typeof View> & {
+type Props = SectionProps & {
   userBonusPoints: number;
   bonusProduct: BonusProductType;
   isChecked: boolean;
@@ -32,66 +35,51 @@ export const PayWithBonusPointsCheckbox = ({
   const styles = useStyles();
   const {t, language} = useTranslation();
   const {theme} = useThemeContext();
-  const background = theme.color.background.neutral[0].background;
-  const selectedBackground = theme.color.interactive[2].active.background;
-  const selectedBorder = theme.color.background.accent[3].background;
 
   const disabled = userBonusPoints < bonusProduct.price.amount;
 
   return (
-    <GenericSectionItem
-      style={[
-        props.style,
-        styles.sectionItem,
-        {
-          backgroundColor: isChecked ? selectedBackground : background,
-          borderColor: isChecked ? selectedBorder : background,
-        },
-      ]}
-    >
-      <PressableOpacity
+    <Section {...props}>
+      <GenericClickableSectionItem
+        active={isChecked}
         onPress={onPress}
         accessibilityRole="button"
         accessibilityLabel={t(BonusProgramTexts.checkbox.a11yLabel)}
         accessibilityHint={t(BonusProgramTexts.checkbox.a11yHint)}
         disabled={disabled}
-        style={[styles.container, disabled && {opacity: 0.5}]}
       >
-        <Checkbox checked={isChecked} />
-        <View style={styles.textContainer}>
-          <ThemeText>
-            {getTextForLanguage(bonusProduct.paymentDescription, language) ??
-              ''}
-          </ThemeText>
-          <View style={styles.currentPointsRow}>
-            <ThemeText typography="body__secondary" color="secondary">
-              {t(BonusProgramTexts.youHave) + userBonusPoints}
+        <View style={[styles.container, disabled && {opacity: 0.5}]}>
+          <Checkbox checked={isChecked} />
+          <View style={styles.textContainer}>
+            <ThemeText>
+              {getTextForLanguage(bonusProduct.paymentDescription, language) ??
+                ''}
             </ThemeText>
-            <ThemeIcon
-              color={theme.color.foreground.dynamic.secondary}
-              svg={StarFill}
-              size="small"
-              accessibilityHint={t(BonusProgramTexts.bonuspoints)}
-            />
+            <View style={styles.currentPointsRow}>
+              <ThemeText typography="body__secondary" color="secondary">
+                {t(BonusProgramTexts.youHave) + userBonusPoints}
+              </ThemeText>
+              <ThemeIcon
+                color={theme.color.foreground.dynamic.secondary}
+                svg={StarFill}
+                size="small"
+                accessibilityHint={t(BonusProgramTexts.bonuspoints)}
+              />
+            </View>
           </View>
+          <BonusPriceTag
+            price={bonusProduct.price.amount}
+            style={{alignSelf: 'flex-start'}}
+          />
         </View>
-        <BonusPriceTag
-          price={bonusProduct.price.amount}
-          style={{alignSelf: 'flex-start'}}
-        />
-      </PressableOpacity>
-    </GenericSectionItem>
+      </GenericClickableSectionItem>
+    </Section>
   );
 };
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
-  sectionItem: {
-    borderWidth: 1,
-    borderRadius: theme.border.radius.regular,
-  },
   container: {
     width: '100%',
-    height: '100%',
     display: 'flex',
     flexDirection: 'row',
     gap: theme.spacing.medium,
