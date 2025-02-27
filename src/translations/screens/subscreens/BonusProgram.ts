944f1a5e2e682bc9659a3b40337b15a1c9a418a1
Jorunn Leithe <jorunn@selbu.org>
2025-02-27 08:58:52 +0100

    feat: pay with bonus point checkbox (#5033)
    * feat: add py with bonus points checkbox
    
    * fix: add tweaks to checkbox and BonusPriceTag
    
    * fix: add accessibility hint and label
    
    * fix: bonus accessibility
    
    * fix: use active GenericClickableSectionItem
    
    * remove unused imports
    
    * fix: rename stuff based on feedback
    
    * fix: fix some accessibility
    
    * fix: accessiibility BonusPriceTag, remove accessibility from checkbox
    
    * fix: genericClickableSectionItem disabled prop
    
    * fix: disabled is optional prop

--- src/translations/screens/subscreens/BonusProgram.ts
+++ src/translations/screens/subscreens/BonusProgram.ts
@@ -1,36 +1,49 @@
 import {translation as _} from '../../commons';
 const BonusProgramTexts = {
   bonuspoints: _('Bonuspoeng', 'Bonus points', 'Bonuspoeng'),
-  header: {
-    title: _('Bonus', 'Bonus', 'Bonus'),
-  },
-  yourBonusPoints: _(
-    'Dine bonuspoeng',
-    'Your bonus points',
-    'Bonuspoenga dine',
-  ),
-  spendPoints: {
-    heading: _('Bruk poeng på', 'Spend points on', 'Bruk poeng på'),
-  },
-  noProfile: _(
-    'Du må logge inn for å få tilgang til Bonus.',
-    'You must log in to get access to Bonus.',
-    'Du må logga inn for å få tilgang til Bonus.',
-  ),
-  readMore: {
-    heading: _('Les mer', 'Read more', 'Les meir'),
-    info: {
-      title: _(
-        'Hvordan funker Bonus?',
-        'How does the Bonus work?',
-        'Korleis fungerer bonus?',
-      ),
-      description: _(
-        'Tjen poeng hver gang du kjøper en billett for buss eller trikk i sone A.',
-        'Earn points every time you buy a bus or tram ticket in zone A.',
-        'Ten poeng kvar gong du kjøper ein billett for buss eller trikk i sone A.',
-      ),
+  costA11yLabel: (amount: number) =>
+    _(
+      `Koster ${amount} bonuspoeng`,
+      `Costs ${amount} bonus points`,
+      `Kostar ${amount} bonuspoeng`,
+    ),
+
+  bonusProfile: {
+    header: {
+      title: _('Bonus', 'Bonus', 'Bonus'),
+    },
+    yourBonusPoints: _(
+      'Dine bonuspoeng',
+      'Your bonus points',
+      'Bonuspoenga dine',
+    ),
+    yourBonusPointsA11yLabel: (amount: number) =>
+      _(`Du har ${amount}`, `You have ${amount}`, `Du har ${amount}`),
+
+    spendPoints: {
+      heading: _('Bruk poeng på', 'Spend points on', 'Bruk poeng på'),
+    },
+    noProfile: _(
+      'Du må logge inn for å få tilgang til Bonus.',
+      'You must log in to get access to Bonus.',
+      'Du må logga inn for å få tilgang til Bonus.',
+    ),
+    readMore: {
+      heading: _('Les mer', 'Read more', 'Les meir'),
+      info: {
+        title: _(
+          'Hvordan funker Bonus?',
+          'How does the Bonus work?',
+          'Korleis fungerer bonus?',
+        ),
+        description: _(
+          'Tjen poeng hver gang du kjøper en billett for buss eller trikk i sone A.',
+          'Earn points every time you buy a bus or tram ticket in zone A.',
+          'Ten poeng kvar gong du kjøper ein billett for buss eller trikk i sone A.',
+        ),
+      },
     },
   },
+  youHave: _('Du har ', 'You have ', 'Du har '),
 };
 export default BonusProgramTexts;
