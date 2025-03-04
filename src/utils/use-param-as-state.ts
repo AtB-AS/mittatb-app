2facbdc036b3eac501c160dc968f40b35f1bc24e
Gøran Dalum <goran.dalum@gmail.com>
2025-03-04 10:32:43 +0100

    fix

--- src/utils/use-param-as-state.ts
+++ src/utils/use-param-as-state.ts
@@ -2,8 +2,8 @@ import {type Dispatch, type SetStateAction, useEffect, useState} from 'react';
 import isEqual from 'lodash.isequal';
 
 /**
- * Utility hook to sync a React state with a parameter. If the parameter changes
- * it will update the React state.
+ * Utility hook to sync a parameter to React state. If the parameter changes it
+ * will update the React state.
  *
  * @param param the param which will cause state change when updated
  * @param checkEquality set to true if the state should remain unchanged if the
