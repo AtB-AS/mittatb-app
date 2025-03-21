778ccc6e906884327150180f699b59aa7e04857c
jorunnl <jorunn.leithe@kantega.no>
2025-03-21 09:20:49 +0100

    fix: remove export

--- src/bonus/index.tsx
+++ src/bonus/index.tsx
@@ -1,5 +1,3 @@
 export {BonusPriceTag, PayWithBonusPointsCheckbox} from './components';
 
 export {isActive, findRelevantBonusProduct} from './utils';
-
-export {getBonusBalanceQuery} from './queries/get-bonus-balance-query';
