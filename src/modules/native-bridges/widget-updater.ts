2ce1f54903daafe9dc84adcab6531b7bc23853c4
Ridwan Aditama <reidzeibel@gmail.com>
2025-05-14 09:19:56 +0200

    fix: prevent crash on Android when selecting favorite departure

--- src/modules/native-bridges/widget-updater.ts
+++ src/modules/native-bridges/widget-updater.ts
@@ -1,4 +1,4 @@
-import {NativeModules} from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
 // Found in /ios/BridgeModules/WidgetUpdaterBridge.m
 interface WidgetUpdaterBridge {
@@ -7,7 +7,7 @@ interface WidgetUpdaterBridge {
 
 const WidgetUpdaterBridge =
   NativeModules.WidgetUpdaterBridge as WidgetUpdaterBridge;
-if (!WidgetUpdaterBridge) {
+if (Platform.OS === 'ios' && !WidgetUpdaterBridge) {
   throw new Error(
     'WidgetUpdaterBridge module is not linked. Please check your native module setup.',
   );
