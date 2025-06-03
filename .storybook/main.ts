d70692af29fa24767ecb23b80a5a94de8f0304c9
Johannes Røsvik <j.rosvik@gmail.com>
2025-06-03 11:12:54 +0200

    fix: update path to storybook stories (#5226)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type {StorybookConfig} from '@storybook/react-native';
 
 const main: StorybookConfig = {
-  stories: ['../src/storybook/stories/**/*.stories.?(ts|tsx|js|jsx)'],
+  stories: ['../src/modules/storybook/stories/**/*.stories.?(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/addon-ondevice-controls',
     '@storybook/addon-ondevice-actions',
