17545d121d9a9b1f2e32b054f1079acc0dc53d31
Jorge Osorio <jorelosorio@gmail.com>
2025-06-12 14:57:05 +0200

    Create shared.ts

---
+++ src/translations/shared.ts
@@ -0,0 +1,8 @@
+import {translation as _} from '@atb/translations/commons';
+
+const SharedTexts = {
+  from: _('Fra', 'From', 'Frå'),
+  to: _('Til', 'To', 'Til'),
+};
+
+export default SharedTexts;
