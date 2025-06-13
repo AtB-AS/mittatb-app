7a730f7e05d7ee44a42efc2ee70760c51ecf0d79
Jorge Osorio <jorelosorio@gmail.com>
2025-06-13 10:47:43 +0200

    Update sales.ts

--- src/api/types/sales.ts
+++ src/api/types/sales.ts
@@ -1,5 +1,4 @@
 import type {UserProfile} from '@atb-as/config-specs';
-import type {Offer} from '@atb/modules/ticketing';
 import {TransportSubmode} from '@atb/api/types/generated/journey_planner_v3_types';
 import type {TicketOffer} from '@atb-as/utils';
 
