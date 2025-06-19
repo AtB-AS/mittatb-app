008fa8802040fbc1e59f4f99995016870eb85c5b
Ridwan Aditama <reidzeibel@gmail.com>
2025-06-19 09:36:35 +0200

    fix: fix the token renewal check version

--- src/modules/mobile-token/hooks/use-validate-token.tsx
+++ src/modules/mobile-token/hooks/use-validate-token.tsx
@@ -15,7 +15,7 @@ import {LOAD_NATIVE_TOKEN_QUERY_KEY} from './use-load-native-token-query';
 import {useAuthContext} from '@atb/modules/auth';
 
 const RETRY_MAX_COUNT = 3;
-const MOBILE_TOKEN_LIBRARY_VERSION = '3.3.14';
+const MOBILE_TOKEN_LIBRARY_VERSION = '3.3.14-hotfix1';
 
 /**
  * This hook is used to check if the token needs renewal/reset.
