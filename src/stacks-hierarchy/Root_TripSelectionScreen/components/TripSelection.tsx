6d23385a29a3c02da8edde232b00809593f895f1
Håkon Strandlie <hakon.strandlie@atb.no>
2025-06-22 00:58:38 +0200

    refactor: Refactor usage of Tag such that only one Tag is shown at a time

--- src/stacks-hierarchy/Root_TripSelectionScreen/components/TripSelection.tsx
+++ src/stacks-hierarchy/Root_TripSelectionScreen/components/TripSelection.tsx
@@ -86,15 +86,11 @@ export function BookingTrip({tripPattern, onSelect}: BookingTripProps) {
   const styles = useBookingTripStyles();
   const {t, language} = useTranslation();
 
-  const availableSeats = tripPattern.booking?.offer?.available ?? 0;
-
   const onPress = () => {
     onSelect(tripPattern.legs);
   };
 
-  const isDisabled =
-    tripPattern.booking.availability === 'closed' ||
-    tripPattern.booking.availability === 'sold_out';
+  const isAvailable = tripPattern.booking.availability === 'available';
 
   const notices = findAllNotices(tripPattern);
   const situations = findAllSituations(tripPattern);
@@ -105,29 +101,15 @@ export function BookingTrip({tripPattern, onSelect}: BookingTripProps) {
       onPress={onPress}
       style={[
         styles.container,
-        styles.containerAvailable,
-        isDisabled && styles.containerDisabled,
+        isAvailable ? styles.containerAvailable : styles.containerDisabled,
       ]}
     >
       <View style={styles.mainContent}>
-        {tripPattern.booking.availability === 'closed' && (
-          <Tag labels={[t(TicketingTexts.booking.closed)]} tagType="warning" />
-        )}
-        {tripPattern.booking.availability === 'sold_out' && (
-          <Tag labels={[t(TicketingTexts.booking.soldOut)]} tagType="warning" />
-        )}
-        {!!availableSeats && availableSeats <= SEAT_TAG_LIMIT && (
-          <Tag
-            labels={[
-              t(TicketingTexts.booking.numAvailableTickets(availableSeats)),
-            ]}
-            tagType="info"
-          />
-        )}
+        <TripSelectionTag bookingInfo={tripPattern.booking} />
         <MemoizedResultItem
           tripPattern={tripPattern}
           key={tripPattern.compressedQuery}
-          state={isDisabled ? 'disabled' : 'enabled'}
+          state={isAvailable ? 'enabled' : 'disabled'}
         />
         <View
           accessibilityLabel={getSituationOrNoticeA11yLabel(
@@ -154,15 +136,13 @@ export function BookingTrip({tripPattern, onSelect}: BookingTripProps) {
           ))}
         </View>
       </View>
-      {tripPattern.booking.availability === 'available' && (
+      {isAvailable && (
         <>
           <SectionSeparator />
           <View
             style={[
               styles.footer,
-              styles.footerDisabled,
-              tripPattern.booking.availability === 'available' &&
-                styles.footerAvailable,
+              isAvailable ? styles.footerAvailable : styles.footerDisabled,
             ]}
           >
             <Button
@@ -204,10 +184,38 @@ function EmptyState() {
   );
 }
 
+/**
+ * We only want to show one tag at a time
+ */
+function TripSelectionTag({
+  bookingInfo,
+}: {
+  bookingInfo: TripPatternWithBooking['booking'];
+}) {
+  const {t} = useTranslation();
+  const availableSeats = bookingInfo?.offer?.available ?? 0;
+  if (bookingInfo.availability === 'closed')
+    return (
+      <Tag labels={[t(TicketingTexts.booking.closed)]} tagType="warning" />
+    );
+  if (bookingInfo.availability === 'sold_out')
+    return (
+      <Tag labels={[t(TicketingTexts.booking.soldOut)]} tagType="warning" />
+    );
+  if (!!availableSeats && availableSeats <= SEAT_TAG_LIMIT)
+    return (
+      <Tag
+        labels={[t(TicketingTexts.booking.numAvailableTickets(availableSeats))]}
+        tagType="info"
+      />
+    );
+}
+
 const useBookingTripStyles = StyleSheet.createThemeHook((theme) => {
   return {
     container: {
       borderRadius: theme.border.radius.regular,
+      overflow: 'hidden',
     },
     containerAvailable: {
       backgroundColor: theme.color.interactive[2].default.background,
@@ -240,7 +248,6 @@ const useBookingTripSelectionStyles = StyleSheet.createThemeHook((theme) => {
     container: {
       width: '100%',
       rowGap: theme.spacing.medium,
-      paddingVertical: theme.spacing.medium,
     },
   };
 });
