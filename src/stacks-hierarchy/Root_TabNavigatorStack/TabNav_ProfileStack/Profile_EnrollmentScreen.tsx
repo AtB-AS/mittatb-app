07739e2cbacd6d6784ee9ded06be7d503defcd23
Jorunn Leithe <jorunn@selbu.org>
2025-06-23 16:28:27 +0200

    feat: update enrollment screen

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_EnrollmentScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_EnrollmentScreen.tsx
@@ -3,17 +3,20 @@ import {ActivityIndicator, View} from 'react-native';
 import {Button} from '@atb/components/button';
 import {StyleSheet, useThemeContext} from '@atb/theme';
 import {EnrollmentTexts, useTranslation} from '@atb/translations';
+import {MessageInfoBox} from '@atb/components/message-info-box';
 import {
-  MessageInfoBox,
-  MessageInfoBoxProps,
-} from '@atb/components/message-info-box';
-import {TextInputSectionItem} from '@atb/components/sections';
-import {Confirm} from '@atb/assets/svg/mono-icons/actions';
+  GenericSectionItem,
+  Section,
+  TextInputSectionItem,
+} from '@atb/components/sections';
 import {useRemoteConfigContext} from '@atb/modules/remote-config';
 import {enrollIntoBetaGroups} from '@atb/api/enrollment';
 import analytics from '@react-native-firebase/analytics';
 import {FullScreenView} from '@atb/components/screen-view';
 import {ScreenHeading} from '@atb/components/heading';
+import {ThemedBeacons} from '@atb/theme/ThemedAssets';
+import {ThemeText} from '@atb/components/text';
+import {useFontScale} from '@atb/utils/use-font-scale';
 
 export const Profile_EnrollmentScreen = () => {
   const styles = useStyles();
@@ -21,15 +24,9 @@ export const Profile_EnrollmentScreen = () => {
   const {theme} = useThemeContext();
   const interactiveColor = theme.color.interactive[0];
   const [inviteCode, setInviteCode] = useState<string>('');
+  const fontScale = useFontScale();
 
   const {onEnroll, hasError, isLoading, isEnrolled} = useEnroll();
-
-  const messageType: MessageInfoBoxProps['type'] = isEnrolled
-    ? 'valid'
-    : hasError
-    ? 'warning'
-    : 'info';
-  const messageText = useMessageText(messageType);
 
   return (
     <FullScreenView
@@ -42,24 +39,47 @@ export const Profile_EnrollmentScreen = () => {
       )}
     >
       <View style={styles.contentContainer}>
-        <MessageInfoBox type={messageType} message={messageText} />
-        {isEnrolled ? null : isLoading ? (
+        <Section>
+          <Section>
+            <GenericSectionItem>
+              <View style={styles.horizontalContainer}>
+                <ThemedBeacons
+                  height={61 * fontScale}
+                  width={61 * fontScale}
+                  style={{
+                    alignSelf: 'flex-start',
+                  }}
+                />
+                <View style={styles.headerTextContainer}>
+                  <ThemeText typography="body__secondary" color="secondary">
+                    {t(EnrollmentTexts.info)}
+                  </ThemeText>
+                </View>
+              </View>
+            </GenericSectionItem>
+          </Section>
+        </Section>
+        {isEnrolled ? (
+          <MessageInfoBox type="valid" message={t(EnrollmentTexts.success)} />
+        ) : isLoading ? (
           <ActivityIndicator />
         ) : (
           <>
             <TextInputSectionItem
               radius="top-bottom"
+              inlineLabel={false}
               label={t(EnrollmentTexts.label)}
+              placeholder={t(EnrollmentTexts.placeholder)}
               value={inviteCode}
               onChangeText={setInviteCode}
               autoCapitalize="none"
+              errorText={hasError ? t(EnrollmentTexts.warning) : undefined}
             />
             <Button
               expanded={true}
               onPress={() => onEnroll(inviteCode)}
               text={t(EnrollmentTexts.button)}
-              leftIcon={{svg: Confirm}}
-              disabled={isLoading || !inviteCode}
+              disabled={isLoading}
               interactiveColor={interactiveColor}
             />
           </>
@@ -110,22 +130,19 @@ const useEnroll = () => {
   };
 };
 
-function useMessageText(type: MessageInfoBoxProps['type']) {
-  const {t} = useTranslation();
-  switch (type) {
-    case 'valid':
-      return t(EnrollmentTexts.success);
-    case 'warning':
-      return t(EnrollmentTexts.warning);
-    case 'info':
-    default:
-      return t(EnrollmentTexts.info);
-  }
-}
-
 const useStyles = StyleSheet.createThemeHook((theme) => ({
   contentContainer: {
     padding: theme.spacing.medium,
     rowGap: theme.spacing.medium,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: theme.spacing.medium,
+  },
+  headerTextContainer: {
+    flex: 1,
+    gap: theme.spacing.xSmall,
   },
 }));
