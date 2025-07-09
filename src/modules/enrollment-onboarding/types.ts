74fa1825f39ac428d6504814e3b6c0ab71e5d247
Jorunn Leithe <jorunn@selbu.org>
2025-07-09 10:16:38 +0200

    fix: reuse bonusPilotEnrollmentId for type definition

--- src/modules/enrollment-onboarding/types.ts
+++ src/modules/enrollment-onboarding/types.ts
@@ -1,10 +1,12 @@
+import {bonusPilotEnrollmentId} from '../bonus';
+
 export type EnrollmentOnboardingScreenName =
   | 'BonusOnboarding_WelcomeScreen'
   | 'BonusOnboarding_BuyTicketsScreen'
   | 'BonusOnboarding_MoreTravelMethodsScreen'
   | 'BonusOnboarding_DownloadScreen';
 
-export type EnrollmentOnboardingConfigId = 'bonus-pilot';
+export type EnrollmentOnboardingConfigId = typeof bonusPilotEnrollmentId;
 
 export type EnrollmentOnboardingConfig = {
   id: EnrollmentOnboardingConfigId;
