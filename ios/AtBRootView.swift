ddf063b5a869c3a6e66520b033af8e598ec3671f
Johannes Røsvik <j.rosvik@gmail.com>
2025-07-14 15:41:00 +0200

    refactor: rewrite AtBRootView to swift

---
+++ ios/AtBRootView.swift
@@ -0,0 +1,27 @@
+import React
+
+@objc(AtBRootView)
+class AtBRootView: RCTRootView {
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if #available(iOS 13.0, *) {
+            if traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
+                setBackgroundByTrait()
+            }
+        }
+    }
+
+    func setBackgroundByTrait() {
+        if #available(iOS 13.0, *) {
+            if traitCollection.userInterfaceStyle == .dark {
+                backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+            } else {
+                backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            }
+        } else {
+            backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
