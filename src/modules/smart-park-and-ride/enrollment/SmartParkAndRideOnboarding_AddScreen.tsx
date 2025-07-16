93edfa9cc4465e6c24fbeca365376a44e8163c24
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-07-17 01:11:19 +0200

    Update SmartParkAndRideOnboarding_AddScreen.tsx

--- src/modules/smart-park-and-ride/enrollment/SmartParkAndRideOnboarding_AddScreen.tsx
+++ src/modules/smart-park-and-ride/enrollment/SmartParkAndRideOnboarding_AddScreen.tsx
@@ -1,8 +1,5 @@
 import {Root_SmartParkAndRideAddScreenComponent} from '@atb/screen-components/smart-park-and-ride';
-import {RootStackScreenProps} from '@atb/stacks-hierarchy';
 
-type Props = RootStackScreenProps<'Root_SmartParkAndRideAddScreen'>;
-
-export const SmartParkAndRideOnboarding_AddScreen = (props: Props) => (
-  <Root_SmartParkAndRideAddScreenComponent {...props} />
+export const SmartParkAndRideOnboarding_AddScreen = () => (
+  <Root_SmartParkAndRideAddScreenComponent />
 );
