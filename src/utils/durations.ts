e8ec1e396f8bf955fed06a5e6fdb4d2265961ecf
Johannes Røsvik <j.rosvik@gmail.com>
2025-07-18 13:03:58 +0200

    fix: correctly set duration consts and preemptiveRefreshPercent (#5348)

--- src/utils/durations.ts
+++ src/utils/durations.ts
@@ -1,5 +1,5 @@
 export const ONE_SECOND_MS = 1000;
 export const ONE_MINUTE_MS = 60 * ONE_SECOND_MS;
 export const ONE_HOUR_MS = 60 * ONE_MINUTE_MS;
-export const HALF_DAY_MS = 12 * ONE_MINUTE_MS;
-export const ONE_DAY_MS = 24 * ONE_MINUTE_MS;
+export const HALF_DAY_MS = 12 * ONE_HOUR_MS;
+export const ONE_DAY_MS = 24 * ONE_HOUR_MS;
