3999eaa8e8b38d390ec384a0c253b30dffb176af
Jorunn Leithe <jorunn@selbu.org>
2025-08-19 16:16:57 +0200

    fix: enrollment text update and remove code on enrollment

--- src/translations/screens/subscreens/Enrollment.ts
+++ src/translations/screens/subscreens/Enrollment.ts
@@ -2,15 +2,11 @@ import {translation as _} from '../../commons';
 const EnrollmentTexts = {
   header: _('Nye konsepter', 'New concepts', 'Nye konsept'),
   info: _(
-    'Vi tester nye konsepter i appen, på et mindre antall brukere. Har du fått en kode for å bli med? Skriv den inn her!',
-    'We are trying out new concepts in the app with small groups of users. Got a code to join? Enter it here!',
-    'Me testar nye konsept i appen, på nokre få brukarar. Har du fått ein kode for å bli med? Skriv den inn her!',
+    'Vi tester ny funksjonalitet i appen på utvalgte brukere. Har du fått en kode for å bli med? Skriv den inn her.',
+    'We are trying out new features in the app on a select group of users. Have you received a code to join? Enter it here.',
+    'Me testar ny funksjonalitet i appen på utvalte brukarar. Har du fått ein kode for å bli med? Skriv den inn her.',
   ),
-  success: _(
-    'Du ble innrullert i den lukkede gruppen!',
-    'You were enrolled in the closed group!',
-    'Du vart lagt til i den lukka gruppa!',
-  ),
+  success: _('Du er inne!', 'You are in!', 'Du er inne!'),
   label: _('Kode', 'Code', 'Kode'),
   placeholder: _('Skriv inn kode ', 'Enter code', 'Skriv inn kode'),
   button: _('Bli med', 'Join', 'Bli med'),
