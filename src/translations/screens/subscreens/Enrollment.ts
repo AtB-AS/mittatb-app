bdb2f01b2c1529126c81847e6f8422952a88e0f9
Jorunn Leithe <jorunn@selbu.org>
2025-08-20 14:06:04 +0200

    fix: updating enrollment text to say with users

--- src/translations/screens/subscreens/Enrollment.ts
+++ src/translations/screens/subscreens/Enrollment.ts
@@ -2,9 +2,9 @@ import {translation as _} from '../../commons';
 const EnrollmentTexts = {
   header: _('Nye konsepter', 'New concepts', 'Nye konsept'),
   info: _(
-    'Vi tester ny funksjonalitet i appen på utvalgte brukere. Har du fått en kode for å bli med? Skriv den inn her.',
-    'We are trying out new features in the app on a select group of users. Have you received a code to join? Enter it here.',
-    'Me testar ny funksjonalitet i appen på utvalde brukarar. Har du fått ein kode for å bli med? Skriv den inn her.',
+    'Vi tester ny funksjonalitet i appen med utvalgte brukere. Har du fått en kode for å bli med? Skriv den inn her.',
+    'We are trying out new features in the app with a selected group of users. Have you received a code to join? Enter it here.',
+    'Me testar ny funksjonalitet i appen med utvalde brukarar. Har du fått ein kode for å bli med? Skriv den inn her.',
   ),
   success: _('Du er inne!', 'You are in!', 'Du er inne!'),
   label: _('Kode', 'Code', 'Kode'),
