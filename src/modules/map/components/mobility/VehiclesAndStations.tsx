f1fcb504ae50aed2429ada1569555fd87fa8cae5
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-08-21 11:29:08 +0200

    Reduce maxzoom (#5393)

--- src/modules/map/components/mobility/VehiclesAndStations.tsx
+++ src/modules/map/components/mobility/VehiclesAndStations.tsx
@@ -196,7 +196,7 @@ export const useVehiclesAndStationsVectorSource: () => {
         type: 'vector',
         tiles: [tileUrlTemplate || ''],
         minzoom: 14,
-        maxzoom: 19,
+        maxzoom: 17,
         volatile: true,
       },
     }),
