9b181516673b69d7e8afe7d41b52cd22791180d4
tormoseng <tor@moseng.net>
2025-09-05 14:58:16 +0200

    test: more tests for measuring performance

--- e2e/scripts/run-flashlight-tests.sh
+++ e2e/scripts/run-flashlight-tests.sh
@@ -16,8 +16,19 @@ else
     #flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=debug yarn test:android:local:firebase --spec test/flashlight/performanceMeasures_travelsearch.e2e.ts" --resultsTitle performance_measures_travelsearch --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
     ./scripts/handle-flashlight-results.sh 30000 travelsearch
 
+    echo "Run test for Departures"
+    /home/runner/.flashlight/bin/flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=staging yarn test:android --spec test/flashlight/performanceMeasures_departures.e2e.ts" --resultsTitle performance_measures_departures --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
+    #flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=debug yarn test:android:local:firebase --spec test/flashlight/performanceMeasures_departures.e2e.ts" --resultsTitle performance_measures_departures --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
+    ./scripts/handle-flashlight-results.sh 30000 departures
+
     echo "Run test for Map"
     /home/runner/.flashlight/bin/flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=staging yarn test:android --spec test/flashlight/performanceMeasures_map.e2e.ts" --resultsTitle performance_measures_map --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
     #flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=debug yarn test:android:local:firebase --spec test/flashlight/performanceMeasures_map.e2e.ts" --resultsTitle performance_measures_map --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
     ./scripts/handle-flashlight-results.sh 30000 map
+
+    echo "Run test for Tickets"
+    /home/runner/.flashlight/bin/flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=staging yarn test:android --spec test/flashlight/performanceMeasures_tickets.e2e.ts" --resultsTitle performance_measures_tickets --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
+    #flashlight test --bundleId no.mittatb.staging --testCommand "APP_ENV=debug yarn test:android:local:firebase --spec test/flashlight/performanceMeasures_tickets.e2e.ts" --resultsTitle performance_measures_tickets --iterationCount ${TEST_ITERATIONS} --maxRetries ${MAX_RETRIES}
+    ./scripts/handle-flashlight-results.sh 30000 tickets
+
 fi
