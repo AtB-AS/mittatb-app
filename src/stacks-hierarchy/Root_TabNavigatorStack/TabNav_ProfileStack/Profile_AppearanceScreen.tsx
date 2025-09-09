a3bb01fcf5fa4986bd43b1d3c7c2e4935ef02ec0
Johannes Røsvik <j.rosvik@gmail.com>
2025-09-09 09:15:47 +0200

    feat: update appearance settings page

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_AppearanceScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_AppearanceScreen.tsx
@@ -1,19 +1,22 @@
-import {Section, ToggleSectionItem} from '@atb/components/sections';
+import {
+  RadioGroupSection,
+  Section,
+  ToggleSectionItem,
+} from '@atb/components/sections';
 import {StyleSheet, useThemeContext} from '@atb/theme';
 import {AppearanceSettingsTexts, useTranslation} from '@atb/translations';
 import React from 'react';
 import {Platform, View} from 'react-native';
 import {FullScreenView} from '@atb/components/screen-view';
-import {ScreenHeading} from '@atb/components/heading';
+import {ContentHeading, ScreenHeading} from '@atb/components/heading';
+import {AppearanceSelection} from '@atb/theme/ThemeContext';
 
 export const Profile_AppearanceScreen = () => {
   const {
-    storedColorScheme,
-    overrideSystemAppearance,
-    updateThemePreference,
-    overrideOSThemePreference,
-    useAndroidSystemFont,
-    updateAndroidFontOverride,
+    appearanceSelection,
+    setAppearanceSelection,
+    androidSystemFont,
+    setAndroidSystemFont,
   } = useThemeContext();
   const {t} = useTranslation();
   const styles = useStyles();
@@ -31,32 +34,41 @@ export const Profile_AppearanceScreen = () => {
         />
       )}
     >
-      <View>
-        <Section style={styles.section}>
-          <ToggleSectionItem
-            text={t(AppearanceSettingsTexts.actions.usePhoneTheme)}
-            value={!overrideSystemAppearance}
-            onValueChange={(checked) => overrideOSThemePreference(!checked)}
-          />
-
-          {overrideSystemAppearance && (
-            <ToggleSectionItem
-              text={t(AppearanceSettingsTexts.actions.darkMode)}
-              value={storedColorScheme === 'dark'}
-              onValueChange={(checked) =>
-                updateThemePreference(checked ? 'dark' : 'light')
+      <View style={styles.container}>
+        <ContentHeading text={t(AppearanceSettingsTexts.theme.heading)} />
+        <Section>
+          <RadioGroupSection<AppearanceSelection>
+            items={[
+              AppearanceSelection.SYSTEM,
+              AppearanceSelection.LIGHT,
+              AppearanceSelection.DARK,
+            ]}
+            itemToText={(item) => {
+              switch (item) {
+                case AppearanceSelection.SYSTEM:
+                  return t(AppearanceSettingsTexts.theme.system);
+                case AppearanceSelection.LIGHT:
+                  return t(AppearanceSettingsTexts.theme.light);
+                case AppearanceSelection.DARK:
+                  return t(AppearanceSettingsTexts.theme.dark);
               }
-            />
-          )}
+            }}
+            selected={appearanceSelection}
+            keyExtractor={(item) => item}
+            onSelect={(selection) => setAppearanceSelection(selection)}
+          />
         </Section>
         {Platform.OS === 'android' && (
-          <Section style={styles.section}>
-            <ToggleSectionItem
-              text={t(AppearanceSettingsTexts.actions.useSystemFont)}
-              value={useAndroidSystemFont}
-              onValueChange={(checked) => updateAndroidFontOverride(checked)}
-            />
-          </Section>
+          <>
+            <ContentHeading text={t(AppearanceSettingsTexts.font.heading)} />
+            <Section>
+              <ToggleSectionItem
+                text={t(AppearanceSettingsTexts.font.toggle)}
+                value={androidSystemFont}
+                onValueChange={(checked) => setAndroidSystemFont(checked)}
+              />
+            </Section>
+          </>
         )}
       </View>
     </FullScreenView>
@@ -64,9 +76,8 @@ export const Profile_AppearanceScreen = () => {
 };
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
-  section: {
-    marginHorizontal: theme.spacing.medium,
-    marginBottom: theme.spacing.small,
-    marginTop: theme.spacing.large,
+  container: {
+    gap: theme.spacing.small,
+    margin: theme.spacing.medium,
   },
 }));
