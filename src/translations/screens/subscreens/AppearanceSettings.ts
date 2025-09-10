c742b3aceb41d52d209d70aaa79e43e1dd5d5f3e
Johannes Røsvik <j.rosvik@gmail.com>
2025-09-10 14:05:33 +0200

    Update src/translations/screens/subscreens/AppearanceSettings.ts

--- src/translations/screens/subscreens/AppearanceSettings.ts
+++ src/translations/screens/subscreens/AppearanceSettings.ts
@@ -4,7 +4,7 @@ const AppearanceSettingsTexts = {
     title: _('Utseende', 'Appearance', 'Utsjånad'),
   },
   theme: {
-    heading: _('Fargetema', 'Color scheme', 'Fargetema'),
+    heading: _('Fargetema', 'Theme', 'Fargetema'),
     system: _('Automatisk', 'Automatic', 'Automatisk'),
     light: _('Lyst', 'Light', 'Lyst'),
     dark: _('Mørkt', 'Dark', 'Mørkt'),
