affb2b9f319e3c6ea7f415078155d8c7f540fae5
Christian Brevik <christian.brevik@gmail.com>
2025-09-18 15:55:58 +0200

    chore: Upgrade marked to latest version

--- src/components/text/strip-markdown.tsx
+++ src/components/text/strip-markdown.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {marked} from 'marked';
+import {marked, Token, Tokens} from 'marked';
 import {Text} from 'react-native';
 
 export function stripMarkdown(markdown: string): React.ReactElement[] {
-  const tree = marked.lexer(markdown, {smartypants: true});
+  const tree = marked.lexer(markdown);
   return tree.map(stripToken);
 }
 
-const stripToken = (token: marked.Token, index: number) => {
+const stripToken = (token: Token, index: number) => {
   switch (token.type) {
     case 'text':
     case 'heading':
@@ -19,7 +19,7 @@ const stripToken = (token: marked.Token, index: number) => {
     case 'paragraph':
       return (
         <React.Fragment key={index}>
-          {token.tokens.map(stripToken)}
+          {token.tokens?.map(stripToken)}
         </React.Fragment>
       );
     case 'br':
@@ -27,7 +27,9 @@ const stripToken = (token: marked.Token, index: number) => {
       return <Text key={index}> </Text>;
     case 'list':
       return (
-        <Text key={index}>{token.items.map((item) => `${item.text} `)}</Text>
+        <Text key={index}>
+          {token.items?.map((item: Tokens.ListItem) => `${item.text} `)}
+        </Text>
       );
     default:
       return <Text key={index}>{token.raw}</Text>;
