15288aaf57c95d91ad3824cbcf5697b12bd4f63f
Jorunn Leithe <jorunn@selbu.org>
2025-09-22 15:52:11 +0200

    fix: use onboardingScreenComponent in extended onboarding screens

--- src/modules/extended-onboarding/ExtendedOnboarding_AlsoGoodToKnowScreen.tsx
+++ src/modules/extended-onboarding/ExtendedOnboarding_AlsoGoodToKnowScreen.tsx
@@ -1,80 +1,30 @@
 import {Onboarding5} from '@atb/assets/svg/color/images';
-import {Button} from '@atb/components/button';
-import {ThemeText} from '@atb/components/text';
-import {StyleSheet, Theme, useThemeContext} from '@atb/theme';
 import {ExtendedOnboardingTexts, useTranslation} from '@atb/translations';
 import React from 'react';
-import {ScrollView, useWindowDimensions, View} from 'react-native';
-import {useOnboardingNavigation} from '@atb/modules/onboarding';
-
-const getThemeColor = (theme: Theme) => theme.color.background.accent[0];
+import {
+  OnboardingScreenComponent,
+  useOnboardingNavigation,
+} from '@atb/modules/onboarding';
+import {Confirm} from '@atb/assets/svg/mono-icons/actions';
 
 export const ExtendedOnboarding_AlsoGoodToKnowScreen = () => {
   const {t} = useTranslation();
-  const {theme} = useThemeContext();
-  const themeColor = getThemeColor(theme);
-  const interactiveColor = theme.color.interactive[0];
-  const styles = useThemeStyles();
-  const {width: windowWidth} = useWindowDimensions();
 
   const {continueFromOnboardingSection} = useOnboardingNavigation();
 
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <View style={styles.mainView}>
-        <ThemeText
-          typography="body__primary--jumbo--bold"
-          color={themeColor}
-          style={styles.header}
-        >
-          {t(ExtendedOnboardingTexts.alsoGoodToKnow.title)}
-        </ThemeText>
-        <Onboarding5 width={windowWidth} height={windowWidth * (4 / 5)} />
-        <ThemeText style={styles.description} color={themeColor}>
-          {t(ExtendedOnboardingTexts.alsoGoodToKnow.description)}
-        </ThemeText>
-      </View>
-      <View style={styles.bottomView}>
-        <Button
-          expanded={true}
-          interactiveColor={interactiveColor}
-          //onPress={navigateToNextScreen}
-          onPress={() => continueFromOnboardingSection('extendedOnboarding')}
-          text={t(ExtendedOnboardingTexts.alsoGoodToKnow.mainButton)}
-          testID="nextButtonAlsoGoodToKnowOnboarding"
-        />
-      </View>
-    </ScrollView>
+    <OnboardingScreenComponent
+      illustration={<Onboarding5 width={240} height={240} />}
+      title={t(ExtendedOnboardingTexts.alsoGoodToKnow.title)}
+      description={t(ExtendedOnboardingTexts.alsoGoodToKnow.description)}
+      footerButton={{
+        onPress: () => continueFromOnboardingSection('extendedOnboarding'),
+        text: t(ExtendedOnboardingTexts.goodToKnow.mainButton),
+        expanded: true,
+        rightIcon: {svg: Confirm},
+        testID: 'nextButtonAlsoGoodToKnowOnboarding',
+      }}
+      testID="goodToKnowExtendedOnboarding"
+    />
   );
 };
-
-const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
-  contentContainer: {
-    flexGrow: 1,
-    paddingTop: theme.spacing.xLarge,
-  },
-  container: {
-    backgroundColor: getThemeColor(theme).background,
-    paddingTop: theme.spacing.xLarge,
-  },
-  mainView: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
-  header: {
-    textAlign: 'center',
-    paddingHorizontal: theme.spacing.xLarge,
-  },
-  description: {
-    textAlign: 'center',
-    paddingHorizontal: theme.spacing.xLarge,
-    paddingBottom: theme.spacing.xLarge,
-  },
-  bottomView: {
-    paddingHorizontal: theme.spacing.xLarge,
-    paddingBottom: theme.spacing.xLarge,
-  },
-}));
--- src/modules/extended-onboarding/ExtendedOnboarding_GoodToKnowScreen.tsx
+++ src/modules/extended-onboarding/ExtendedOnboarding_GoodToKnowScreen.tsx
@@ -1,22 +1,13 @@
 import {Onboarding4} from '@atb/assets/svg/color/images';
-import {Button} from '@atb/components/button';
-import {ThemeText} from '@atb/components/text';
 import {useNavigateToNextOnboardingCarouselScreen} from '@atb/modules/onboarding-carousel';
-import {StyleSheet, Theme, useThemeContext} from '@atb/theme';
 import {ExtendedOnboardingTexts, useTranslation} from '@atb/translations';
 import React from 'react';
-import {ScrollView, useWindowDimensions, View} from 'react-native';
 import {extendedOnboardingId} from '.';
-
-const getThemeColor = (theme: Theme) => theme.color.background.accent[0];
+import {ArrowRight} from '@atb/assets/svg/mono-icons/navigation';
+import {OnboardingScreenComponent} from '../onboarding';
 
 export const ExtendedOnboarding_GoodToKnowScreen = () => {
   const {t} = useTranslation();
-  const {theme} = useThemeContext();
-  const themeColor = getThemeColor(theme);
-  const interactiveColor = theme.color.interactive[0];
-  const styles = useThemeStyles();
-  const {width: windowWidth} = useWindowDimensions();
 
   const navigateToNextScreen = useNavigateToNextOnboardingCarouselScreen(
     extendedOnboardingId,
@@ -24,60 +15,18 @@ export const ExtendedOnboarding_GoodToKnowScreen = () => {
   );
 
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <View style={styles.mainView}>
-        <ThemeText
-          typography="body__primary--jumbo--bold"
-          color={themeColor}
-          style={styles.header}
-        >
-          {t(ExtendedOnboardingTexts.goodToKnow.title)}
-        </ThemeText>
-        <Onboarding4 width={windowWidth} height={windowWidth * (4 / 5)} />
-        <ThemeText style={styles.description} color={themeColor}>
-          {t(ExtendedOnboardingTexts.goodToKnow.description)}
-        </ThemeText>
-      </View>
-      <View style={styles.bottomView}>
-        <Button
-          expanded={true}
-          interactiveColor={interactiveColor}
-          onPress={navigateToNextScreen}
-          text={t(ExtendedOnboardingTexts.goodToKnow.mainButton)}
-          testID="nextButtonGoodToKnowOnboarding"
-        />
-      </View>
-    </ScrollView>
+    <OnboardingScreenComponent
+      illustration={<Onboarding4 height={170} />}
+      title={t(ExtendedOnboardingTexts.goodToKnow.title)}
+      description={t(ExtendedOnboardingTexts.goodToKnow.description)}
+      footerButton={{
+        onPress: navigateToNextScreen,
+        text: t(ExtendedOnboardingTexts.goodToKnow.mainButton),
+        expanded: true,
+        rightIcon: {svg: ArrowRight},
+        testID: 'nextButtonGoodToKnowOnboarding',
+      }}
+      testID="goodToKnowExtendedOnboarding"
+    />
   );
 };
-
-const useThemeStyles = StyleSheet.createThemeHook((theme) => ({
-  contentContainer: {
-    flexGrow: 1,
-    paddingTop: theme.spacing.xLarge,
-  },
-  container: {
-    backgroundColor: getThemeColor(theme).background,
-    paddingTop: theme.spacing.xLarge,
-  },
-  mainView: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
-  header: {
-    textAlign: 'center',
-    paddingHorizontal: theme.spacing.xLarge,
-  },
-  description: {
-    textAlign: 'center',
-    paddingHorizontal: theme.spacing.xLarge,
-    paddingBottom: theme.spacing.xLarge,
-  },
-  bottomView: {
-    paddingHorizontal: theme.spacing.xLarge,
-    paddingBottom: theme.spacing.xLarge,
-  },
-}));
