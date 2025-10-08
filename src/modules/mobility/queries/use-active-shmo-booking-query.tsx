d5903e129e508e91764c21073a001e26aa89932f
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-10-08 16:43:30 +0200

    Remove legacy useEffect

--- src/modules/mobility/queries/use-active-shmo-booking-query.tsx
+++ src/modules/mobility/queries/use-active-shmo-booking-query.tsx
@@ -1,10 +1,8 @@
-import {useQuery, useQueryClient} from '@tanstack/react-query';
+import {useQuery} from '@tanstack/react-query';
 import {getActiveShmoBooking} from '@atb/api/mobility';
 import {ONE_MINUTE_MS} from '@atb/utils/durations';
 import {useAcceptLanguage} from '@atb/api/use-accept-language';
 import {useFeatureTogglesContext} from '@atb/modules/feature-toggles';
-import {useEffect} from 'react';
-import {getShmoBookingQueryKey} from './use-shmo-booking-query';
 
 export const getActiveShmoBookingQueryKey = (acceptLanguage: string) => [
   'GET_ACTIVE_SHMO_BOOKING_QUERY_KEY',
@@ -15,7 +13,6 @@ export const useActiveShmoBookingQuery = (
   refetchInterval: number | false = false,
 ) => {
   const acceptLanguage = useAcceptLanguage();
-  const queryClient = useQueryClient();
   const {isShmoDeepIntegrationEnabled} = useFeatureTogglesContext();
   const res = useQuery({
     enabled: isShmoDeepIntegrationEnabled,
@@ -27,15 +24,6 @@ export const useActiveShmoBookingQuery = (
     refetchOnWindowFocus: 'always',
     refetchOnReconnect: true,
   });
-
-  useEffect(() => {
-    if (res.status === 'success' && res.data) {
-      queryClient.setQueryData(
-        getShmoBookingQueryKey(res.data.bookingId, acceptLanguage),
-        res.data,
-      );
-    }
-  }, [res.status, res.data, queryClient, acceptLanguage]);
 
   return res;
 };
