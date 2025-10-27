13715a4e225a5f791d563147f47d55440c9faaea
Håkon Strandlie <hakon.strandlie@atb.no>
2025-10-27 15:51:02 +0100

    fix: Removed orgId from app. Added it to BFF instead

--- types/app-orgs.ts
+++ types/app-orgs.ts
@@ -11,10 +11,3 @@ export enum FareZoneAuthorities {
   fram = 'MOR',
   troms = 'TRO',
 }
-
-export const EnturOrgIDs: Record<AppOrgs, string> = {
-  [AppOrgs.nfk]: '18',
-  [AppOrgs.atb]: '3',
-  [AppOrgs.fram]: '71',
-  [AppOrgs.troms]: '72',
-};
