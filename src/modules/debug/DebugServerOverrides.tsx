68bbb23200c9bed0a5a51b44c954d4186ab52adf
Håkon Strandlie <hakon.strandlie@atb.no>
2025-10-28 15:20:33 +0100

    feat: Second commit, a bit closer

--- src/modules/debug/DebugServerOverrides.tsx
+++ src/modules/debug/DebugServerOverrides.tsx
@@ -1,6 +1,9 @@
 import {AxiosRequestHeaders} from 'axios';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {View} from 'react-native';
+import {ThemeText} from '@atb/components/text';
+import {storage} from '@atb/modules/storage';
+import {Button} from '@atb/components/button';
 
 type ServerOverride = {
   match: RegExp;
@@ -9,6 +12,33 @@ type ServerOverride = {
 };
 
 export function DebugServerOverrides() {
-  const [];
-  return <View />;
+  const [overrides, setOverrides] = React.useState<ServerOverride[]>([]);
+  const [newMatch, setNewMatch] = React.useState<string>('');
+  const [newValue, setNewValue] = React.useState<string>('');
+  const [newHeaders, setNewHeaders] = React.useState<string>('');
+
+  useCallback(async () => {
+    const fromStorage = await storage.get('@ATB_debug_server_overrides');
+    const overrides = JSON.parse(fromStorage || '[]') as ServerOverride[];
+    setOverrides(overrides);
+  }, [])();
+
+  function onAddOverride() {}
+
+  return (
+    <View style={{gap: 12}}>
+      {overrides.length ? (
+        overrides.map((o) => (
+          <ThemeText>
+            {`${o.match}`} {o.newValue}
+          </ThemeText>
+        ))
+      ) : (
+        <ThemeText typography="body__primary--big">
+          No server overrides set
+        </ThemeText>
+      )}
+      <Button text="Add server override" expanded={true} onPress={() => {}} />
+    </View>
+  );
 }
