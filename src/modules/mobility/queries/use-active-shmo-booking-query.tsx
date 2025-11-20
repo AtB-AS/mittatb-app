b39c00874cba29eae30ecd6de43bf569af797174
Adrian Santana Berg <adrian_berg96@hotmail.com>
2025-11-20 09:02:39 +0100

    refactor: remove acceptLanguage parameter from event stream handling and query key functions

--- src/modules/mobility/queries/use-active-shmo-booking-query.tsx
+++ src/modules/mobility/queries/use-active-shmo-booking-query.tsx
@@ -4,7 +4,7 @@ import {ONE_MINUTE_MS} from '@atb/utils/durations';
 import {useAcceptLanguage} from '@atb/api/use-accept-language';
 import {useFeatureTogglesContext} from '@atb/modules/feature-toggles';
 
-export const getActiveShmoBookingQueryKey = (acceptLanguage: string) => [
+export const getActiveShmoBookingQueryKey = (acceptLanguage?: string) => [
   'GET_ACTIVE_SHMO_BOOKING_QUERY_KEY',
   acceptLanguage,
 ];
