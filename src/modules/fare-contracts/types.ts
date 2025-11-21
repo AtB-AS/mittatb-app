c23b263961b0c32956ce2fd59118ae2a6db8f6e9
Christian Brevik <christian.brevik@gmail.com>
2025-11-21 15:07:13 +0100

    nit

--- src/modules/fare-contracts/types.ts
+++ src/modules/fare-contracts/types.ts
@@ -1,17 +1,8 @@
-import {UserProfile, SupplementProduct} from '@atb/modules/configuration';
+import {UserProfile, BaggageProduct} from '@atb/modules/configuration';
 import type {Unit} from 'humanize-duration';
 import {UniqueWithCount} from '@atb/utils/array-map-unique-with-count';
 
 export type UserProfileWithCount = UniqueWithCount<UserProfile>;
-
-// BaggageProduct is a SupplementProduct where isBaggageProduct is true and baggageType is not null.
-export type BaggageProduct = Omit<
-  SupplementProduct,
-  'isBaggageProduct' | 'baggageType'
-> & {
-  baggageType: string;
-  isBaggageProduct: true;
-};
 
 export type BaggageProductWithCount = UniqueWithCount<BaggageProduct>;
 export type Range = {low: number; high: number};
