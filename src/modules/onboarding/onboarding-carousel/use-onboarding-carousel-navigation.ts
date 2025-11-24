956c0ba25cf08ab8d2389ac31aee5a9f30c8d614
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-11-24 09:54:07 +0100

    Complete onboarding section when closing carousel

--- src/modules/onboarding/onboarding-carousel/use-onboarding-carousel-navigation.ts
+++ src/modules/onboarding/onboarding-carousel/use-onboarding-carousel-navigation.ts
@@ -7,6 +7,7 @@ import {
   OnboardingCarouselScreenName,
 } from './types';
 import {useOnboardingNavigation} from '../use-onboarding-navigation';
+import {useOnboardingContext} from '../OnboardingContext';
 
 function getAdjacentOnboardingCarouselScreenName(
   configId: OnboardingCarouselConfigId,
@@ -35,6 +36,8 @@ function useNavigateToAdjacentOnboardingCarouselScreen(
   offset: number,
 ) {
   const navigation = useNavigation<OnboardingCarouselNavigationProps>();
+  const {continueFromOnboardingSection} = useOnboardingNavigation();
+  const {onboardingSections} = useOnboardingContext();
 
   return useCallback(() => {
     const adjacentScreenName = getAdjacentOnboardingCarouselScreenName(
@@ -45,9 +48,27 @@ function useNavigateToAdjacentOnboardingCarouselScreen(
     if (adjacentScreenName) {
       navigation.navigate(adjacentScreenName);
     } else {
-      navigation.getParent()?.goBack();
+      const comingFromOnboardingSectionId = onboardingSections.find(
+        (onboardingSection) =>
+          onboardingSection.initialScreen?.params?.configId === configId,
+      )?.onboardingSectionId;
+
+      if (comingFromOnboardingSectionId) {
+        continueFromOnboardingSection(comingFromOnboardingSectionId);
+      } else {
+        // should never happen
+        console.error('UNKNOWN ONBOARDING SECTION');
+        navigation.getParent()?.goBack();
+      }
     }
-  }, [configId, currentScreenName, offset, navigation]);
+  }, [
+    configId,
+    currentScreenName,
+    offset,
+    navigation,
+    onboardingSections,
+    continueFromOnboardingSection,
+  ]);
 }
 
 export function useOnboardingCarouselNavigation(
