177c0d6d4b93d0339cef1321d30bd841afc331b6
Christian Brevik <christian.brevik@gmail.com>
2025-11-27 12:33:14 +0100

    chore: Move to use config-specs for BaggageProduct

--- src/modules/configuration/types.ts
+++ src/modules/configuration/types.ts
@@ -1,6 +1,3 @@
-import {SupplementProduct} from '@atb-as/config-specs';
-import {z} from 'zod';
-
 export * from '@atb-as/config-specs';
 export * from '@atb-as/config-specs/lib/mobility';
 
@@ -10,13 +7,3 @@ export type PointToPointValidity = {
   fromPlace: string;
   toPlace: string;
 };
-
-export const BaggageType = z.enum(['BICYCLE']);
-export type BaggageType = z.infer<typeof BaggageType>;
-
-// BaggageProduct is a SupplementProduct where isBaggageProduct is true and baggageType is not null.
-export const BaggageProduct = SupplementProduct.extend({
-  isBaggageProduct: z.literal(true),
-  baggageType: BaggageType,
-});
-export type BaggageProduct = z.infer<typeof BaggageProduct>;
