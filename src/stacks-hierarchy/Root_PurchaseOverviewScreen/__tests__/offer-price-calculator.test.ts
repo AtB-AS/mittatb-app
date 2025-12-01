9bb5e2b6b979eaaffaec1cda151279f33ee8d3c1
Christian Brevik <christian.brevik@gmail.com>
2025-12-01 17:31:42 +0100

    more realistic prices

--- src/stacks-hierarchy/Root_PurchaseOverviewScreen/__tests__/offer-price-calculator.test.ts
+++ src/stacks-hierarchy/Root_PurchaseOverviewScreen/__tests__/offer-price-calculator.test.ts
@@ -11,7 +11,7 @@ import {
   getCheapestOffer,
 } from '../offer-price-calculator';
 
-const TEST_USER_PROFILE: UserProfile = {
+const TEST_ADULT_USER_PROFILE: UserProfile = {
   id: 'A',
   name: {lang: 'no', value: 'Test User'},
   version: 'v1',
@@ -19,7 +19,7 @@ const TEST_USER_PROFILE: UserProfile = {
   userTypeString: 'ADULT',
 };
 
-const TEST_BAGGAGE_PRODUCT: BaggageProduct = {
+const TEST_BICYCLE_PRODUCT: BaggageProduct = {
   id: 'B',
   name: {lang: 'no', value: 'Test Baggage'},
   version: 'v1',
@@ -30,7 +30,7 @@ const TEST_BAGGAGE_PRODUCT: BaggageProduct = {
   description: [],
 };
 
-const TEST_TICKET_OFFER: TicketOffer = {
+const TEST_ADULT_SINGLE_TICKET_OFFER: TicketOffer = {
   offerId: 'A',
   available: 1,
   shouldStartNow: false,
@@ -49,11 +49,11 @@ const TEST_TICKET_OFFER: TicketOffer = {
     ],
   },
   price: {
-    amount: '100',
-    amountFloat: 100,
+    amount: '45',
+    amountFloat: 45,
     currency: 'NOK',
-    originalAmount: '100',
-    originalAmountFloat: 100,
+    originalAmount: '45',
+    originalAmountFloat: 45,
     vatGroup: 'A',
   },
   validFrom: '2021-01-01',
@@ -63,7 +63,7 @@ const TEST_TICKET_OFFER: TicketOffer = {
   supplementProducts: [],
 };
 
-const TEST_BAGGAGE_TICKET_OFFER: BaggageTicketOffer = {
+const TEST_BICYCLE_TICKET_OFFER: BaggageTicketOffer = {
   offerId: 'A',
   available: 1,
   shouldStartNow: false,
@@ -93,132 +93,126 @@ const TEST_BAGGAGE_TICKET_OFFER: BaggageTicketOffer = {
       id: 'A',
       selectableId: 'A',
       price: {
-        amount: '100',
-        amountFloat: 100,
+        amount: '22',
+        amountFloat: 22,
         currency: 'NOK',
-        originalAmount: '100',
-        originalAmountFloat: 100,
+        originalAmount: '22',
+        originalAmountFloat: 22,
         vatGroup: 'A',
       },
     },
   ],
 };
 
-const TEST_USER_PROFILE_WITH_COUNT_AND_OFFER: UserProfileWithCountAndOffer = {
-  ...TEST_USER_PROFILE,
-  count: 1,
-  offer: TEST_TICKET_OFFER,
-};
-
-const TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER: BaggageProductWithCountAndOffer =
+const TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER: UserProfileWithCountAndOffer =
   {
-    ...TEST_BAGGAGE_PRODUCT,
+    ...TEST_ADULT_USER_PROFILE,
     count: 1,
-    offer: TEST_BAGGAGE_TICKET_OFFER,
+    offer: TEST_ADULT_SINGLE_TICKET_OFFER,
   };
 
+const TEST_BICYCLE_WITH_COUNT_AND_OFFER: BaggageProductWithCountAndOffer = {
+  ...TEST_BICYCLE_PRODUCT,
+  count: 1,
+  offer: TEST_BICYCLE_TICKET_OFFER,
+};
+
 describe('offer-price-calculator', () => {
-  it('calculateTotalPrice with user profile and baggage product should calculate the total price', () => {
+  it('calculateTotalPrice with adult + bicycle should be 67', () => {
     const totalPrice = calculateTotalPrice(
-      [TEST_USER_PROFILE_WITH_COUNT_AND_OFFER],
-      [TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER],
+      [TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(totalPrice).toBe(200);
+    expect(totalPrice).toBe(67);
   });
 
-  it('calculateTotalPrice with no user profiles and one baggage products should calculate the total price', () => {
+  it('calculateTotalPrice with no user profiles and one bicycle should be 22', () => {
     const totalPrice = calculateTotalPrice(
       [],
-      [TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(totalPrice).toBe(100);
+    expect(totalPrice).toBe(22);
   });
 
-  it('calculateTotalPrice with one user profile and no baggage products should calculate the total price', () => {
+  it('calculateTotalPrice with one adult user profile and no baggage products should be 45', () => {
     const totalPrice = calculateTotalPrice(
-      [TEST_USER_PROFILE_WITH_COUNT_AND_OFFER],
+      [TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER],
       [],
     );
 
-    expect(totalPrice).toBe(100);
+    expect(totalPrice).toBe(45);
   });
 
-  it('calculateTotalPrice with no user profiles and no baggage products should calculate the total price', () => {
+  it('calculateTotalPrice with no user profiles and no baggage products should be 0', () => {
     const totalPrice = calculateTotalPrice([], []);
 
     expect(totalPrice).toBe(0);
   });
 
-  it('calculateTotalPrice with multiple user profiles and baggage products should calculate the total price', () => {
+  it('calculateTotalPrice with 2x adults and 2x bicycles should be 134', () => {
     const totalPrice = calculateTotalPrice(
       [
-        TEST_USER_PROFILE_WITH_COUNT_AND_OFFER,
-        TEST_USER_PROFILE_WITH_COUNT_AND_OFFER,
+        TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER,
+        TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER,
       ],
-      [
-        TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER,
-        TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER,
-      ],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER, TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(totalPrice).toBe(400);
+    expect(totalPrice).toBe(134);
   });
 
-  it('calculateOriginalPrice with user profile and baggage product should calculate the original price', () => {
+  it('calculateOriginalPrice with adult + bicycle should be 67', () => {
     const originalPrice = calculateOriginalPrice(
-      [TEST_USER_PROFILE_WITH_COUNT_AND_OFFER],
-      [TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER],
+      [TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(originalPrice).toBe(200);
+    expect(originalPrice).toBe(67);
   });
 
-  it('calculateOriginalPrice with no user profiles and one baggage products should calculate the original price', () => {
+  it('calculateOriginalPrice with no user profiles and one bicycle should be 22', () => {
     const originalPrice = calculateOriginalPrice(
       [],
-      [TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(originalPrice).toBe(100);
+    expect(originalPrice).toBe(22);
   });
 
-  it('calculateOriginalPrice with one user profile and no baggage products should calculate the original price', () => {
+  it('calculateOriginalPrice with one adult user profile and no baggage products should be 45', () => {
     const originalPrice = calculateOriginalPrice(
-      [TEST_USER_PROFILE_WITH_COUNT_AND_OFFER],
+      [TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER],
       [],
     );
 
-    expect(originalPrice).toBe(100);
+    expect(originalPrice).toBe(45);
   });
 
-  it('calculateOriginalPrice with no user profiles and no baggage products should calculate the original price', () => {
+  it('calculateOriginalPrice with no user profiles and no baggage products should be 0', () => {
     const originalPrice = calculateOriginalPrice([], []);
 
     expect(originalPrice).toBe(0);
   });
 
-  it('calculateOriginalPrice with multiple user profiles and baggage products should calculate the original price', () => {
+  it('calculateOriginalPrice with 2x adults and 2x bicycles should be 134', () => {
     const originalPrice = calculateOriginalPrice(
       [
-        TEST_USER_PROFILE_WITH_COUNT_AND_OFFER,
-        TEST_USER_PROFILE_WITH_COUNT_AND_OFFER,
+        TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER,
+        TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER,
       ],
-      [
-        TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER,
-        TEST_BAGGAGE_PRODUCT_WITH_COUNT_AND_OFFER,
-      ],
+      [TEST_BICYCLE_WITH_COUNT_AND_OFFER, TEST_BICYCLE_WITH_COUNT_AND_OFFER],
     );
 
-    expect(originalPrice).toBe(400);
+    expect(originalPrice).toBe(134);
   });
 
-  it('calculateTotalPrice should differ from calculateOriginalPrice when there is a discount', () => {
+  it('calculateTotalPrice and calculateOriginalPrice should differ when there is a discount', () => {
     const testUserProfileWithCountAndOffer = {
-      ...TEST_USER_PROFILE_WITH_COUNT_AND_OFFER,
+      ...TEST_ADULT_USER_PROFILE_WITH_COUNT_AND_OFFER,
       offer: {
-        ...TEST_TICKET_OFFER,
+        ...TEST_ADULT_SINGLE_TICKET_OFFER,
         price: {
           amount: '80',
           amountFloat: 80,
@@ -247,9 +241,9 @@ describe('offer-price-calculator', () => {
   it('getCheapestOffer with ticket offers should return the cheapest offer', () => {
     const cheapestOffer = getCheapestOffer(
       [
-        TEST_TICKET_OFFER,
+        TEST_ADULT_SINGLE_TICKET_OFFER,
         {
-          ...TEST_TICKET_OFFER,
+          ...TEST_ADULT_SINGLE_TICKET_OFFER,
           price: {
             amount: '200',
             amountFloat: 200,
@@ -263,18 +257,18 @@ describe('offer-price-calculator', () => {
       (o) => o.price,
     );
 
-    expect(cheapestOffer).toBe(TEST_TICKET_OFFER);
+    expect(cheapestOffer).toBe(TEST_ADULT_SINGLE_TICKET_OFFER);
   });
 
   it('getCheapestOffer with baggage offers should return the cheapest offer', () => {
     const cheapestOffer = getCheapestOffer(
       [
-        TEST_BAGGAGE_TICKET_OFFER,
+        TEST_BICYCLE_TICKET_OFFER,
         {
-          ...TEST_BAGGAGE_TICKET_OFFER,
+          ...TEST_BICYCLE_TICKET_OFFER,
           supplementProducts: [
             {
-              ...TEST_BAGGAGE_TICKET_OFFER.supplementProducts[0],
+              ...TEST_BICYCLE_TICKET_OFFER.supplementProducts[0],
               price: {
                 amount: '200',
                 amountFloat: 200,
@@ -290,6 +284,6 @@ describe('offer-price-calculator', () => {
       (o) => o.supplementProducts[0].price,
     );
 
-    expect(cheapestOffer).toBe(TEST_BAGGAGE_TICKET_OFFER);
+    expect(cheapestOffer).toBe(TEST_BICYCLE_TICKET_OFFER);
   });
 });
