7809ec7f80e5ec2842d4d7168be95421bf54e3c3
Ridwan Aditama <reidzeibel@gmail.com>
2025-12-01 12:38:09 +0100

    fix: changed some links to use external browser (#5617)
    * fix: trip section provider should use external browser
    
    * fix: contacting customer service should go to external browser

--- src/screen-components/travel-details-screens/components/TripSection.tsx
+++ src/screen-components/travel-details-screens/components/TripSection.tsx
@@ -24,7 +24,7 @@ import {
 import {useTransportColor} from '@atb/utils/use-transport-color';
 import {useBottomSheetContext} from '@atb/components/bottom-sheet';
 import React from 'react';
-import {View} from 'react-native';
+import {Linking, View} from 'react-native';
 import {
   getLineName,
   getNoticesForLeg,
@@ -60,7 +60,6 @@ import {ExternalLink} from '@atb/assets/svg/mono-icons/navigation';
 import {AUTHORITY} from '@env';
 import {AuthorityFragment} from '@atb/api/types/generated/fragments/authority';
 import {getRealtimeState, type TimeValues} from '@atb/utils/realtime';
-import {openInAppBrowser} from '@atb/modules/in-app-browser';
 
 type TripSectionProps = {
   isLast?: boolean;
@@ -533,7 +532,7 @@ const AuthorityRow = ({id, name, url}: AuthorityFragment) => {
             TripDetailsTexts.trip.leg.buyTicketFromA11yLabel(name),
           )}
           leftIcon={{svg: ExternalLink}}
-          onPress={() => url && openInAppBrowser(url, 'close')}
+          onPress={() => url && Linking.openURL(url)}
           mode="primary"
           type="small"
           expanded={false}
