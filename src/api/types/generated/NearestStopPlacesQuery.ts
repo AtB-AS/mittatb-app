7c2a5e99d04a1c33cd660e69b22bc7eec6200e38
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-12-03 15:56:15 +0100

    Simplify NearestStopPlacesQueryVariables type

--- src/api/types/generated/NearestStopPlacesQuery.ts
+++ src/api/types/generated/NearestStopPlacesQuery.ts
@@ -1,13 +1,14 @@
 import * as Types from '@atb/api/types/generated/journey_planner_v3_types';
 import type {SituationFragment} from '@atb/api/types/generated/fragments/situations';
 
-export type NearestStopPlacesQueryVariables = Types.Exact<{
-  count?: Types.InputMaybe<Types.Scalars['Int']['input']>;
-  distance: Types.Scalars['Float']['input'];
-  longitude: Types.Scalars['Float']['input'];
-  latitude: Types.Scalars['Float']['input'];
-  after?: Types.InputMaybe<Types.Scalars['String']['input']>;
-}>;
+// copied from https://github.com/AtB-AS/atb-bff/blob/main/src/service/impl/departures/journey-gql/stops-nearest.graphql-gen.ts#L7-L13
+export type NearestStopPlacesQueryVariables = {
+  count?: number;
+  distance: number;
+  longitude: number;
+  latitude: number;
+  after?: string;
+};
 
 export type NearestStopPlacesQuery = {
   nearest?: {
