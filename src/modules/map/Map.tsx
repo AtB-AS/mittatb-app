b95ee8973feda551cdf751e46c18c4d209f103d2
Magnus Dahl <magnus52@hotmail.com>
2025-12-10 09:17:37 +0100

    Made some constraint to moving the map

--- src/modules/map/Map.tsx
+++ src/modules/map/Map.tsx
@@ -342,6 +342,9 @@ export const Map = (props: MapProps) => {
           onDidFinishLoadingMap={() => {
             setInitMapLoaded(true);
           }}
+          gestureSettings={{
+            panDecelerationFactor: 0.994,
+          }}
         >
           <Viewport
             onStatusChanged={(status) => {
