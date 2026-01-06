680486351436dd22f7ec324d6d6349c1b2953922
Ridwan Aditama <reidzeibel@gmail.com>
2026-01-06 15:33:36 +0100

    feat: show cancellation message on quay list view (#5686)
    * feat: show cancellation message on quay list view
    
    * update english text

--- src/translations/components/CancelledDeparture.ts
+++ src/translations/components/CancelledDeparture.ts
@@ -6,6 +6,11 @@ const CancelledDepartureTexts = {
     'The departure from this stop has been cancelled.',
     'Avgangen frå denne haldeplassen er kansellert.',
   ),
+  someDeparturesCancelled: _(
+    'Noen avganger fra denne holdeplassen er kansellert.',
+    'Some departures from this stop have been cancelled.',
+    'Nokre avgangar frå denne haldeplassen er kansellert.',
+  ),
   cancelled: _('Kansellert', 'Cancelled', 'Kansellert'),
 };
 
