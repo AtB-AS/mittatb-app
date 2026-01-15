2f60d717a0eefb0b08c3fccc428fd1ebfb870507
Ridwan Aditama <reidzeibel@gmail.com>
2026-01-15 11:29:55 +0100

    update config file

--- scripts/android/override-config-files.sh
+++ scripts/android/override-config-files.sh
@@ -28,6 +28,6 @@ xmlstarlet edit --inplace --omit-decl \
 
 echo "Set Intercom API key and App ID in Intercom.xml"
 xmlstarlet edit --inplace --omit-decl \
-  -u "//resources/string[@name='IntercomApiKey']" -v "$INTERCOM_ANDROID_API_KEY" \
-  -u "//resources/string[@name='IntercomAppId']" -v "$INTERCOM_APP_ID" \
+  -u //resources/string[@name='IntercomApiKey'] -v "$INTERCOM_ANDROID_API_KEY" \
+  -u //resources/string[@name='IntercomAppId'] -v "$INTERCOM_APP_ID" \
    android/app/src/main/res/values/Intercom.xml
