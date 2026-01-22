645b8dcd38f2b029f1bd640e44a384e35b0f3614
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2026-01-22 09:52:16 +0100

    fix: Refactor useHasReservationOrAvailableFareContract (#5726)
    * Refactor useHasReservationOrAvailableFareContract to only update on click
    
    * Simplify useGetFareContractsQuery usage
    
    * Move const outside render
    
    * Memoize hasReservationOrAvailableFareContract

--- src/modules/ticketing/use-fare-contracts.ts
+++ src/modules/ticketing/use-fare-contracts.ts
@@ -7,6 +7,7 @@ import {useAuthContext} from '@atb/modules/auth';
 import {getAvailabilityStatus, AvailabilityStatus} from '@atb-as/utils';
 import {isDefined} from '@atb/utils/presence';
 import {ONE_WEEK_MS} from '@atb/utils/durations';
+import {getServerNowGlobal} from '../time';
 
 type AvailabilityStatusInput = {
   availability: Exclude<AvailabilityStatus['availability'], 'invalid'>;
@@ -55,16 +56,11 @@ export const useFareContracts = (
     });
   };
 
-  const filteredFareContracts = fareContracts.filter((fc) => {
-    const as = getAvailabilityStatus(fc, now);
-    if (as.availability === availabilityStatus.availability) {
-      return availabilityStatus.status
-        ? as.status === availabilityStatus.status
-        : true;
-    }
-
-    return false;
-  });
+  const filteredFareContracts = getFilterdFareContracts(
+    fareContracts,
+    availabilityStatus,
+    now,
+  );
 
   return {fareContracts: filteredFareContracts, refetch, isRefetching};
 };
@@ -82,9 +78,26 @@ export const useGetFareContractsQuery = (props: {
     refetchOnReconnect: true,
     refetchOnMount: false,
     retry: 0,
+    staleTime: ONE_WEEK_MS,
     gcTime: ONE_WEEK_MS,
     meta: {
       persistInAsyncStorage: true,
     },
   });
 };
+
+export const getFilterdFareContracts = (
+  fareContracts: FareContractType[],
+  availabilityStatus: AvailabilityStatusInput,
+  now = getServerNowGlobal(),
+) =>
+  fareContracts.filter((fc) => {
+    const as = getAvailabilityStatus(fc, now);
+    if (as.availability === availabilityStatus.availability) {
+      return availabilityStatus.status
+        ? as.status === availabilityStatus.status
+        : true;
+    }
+
+    return false;
+  });
