ea8c328616010eded5e8a1d2c729593e6ed21eb8
Håkon Strandlie <hakon.strandlie@atb.no>
2026-02-11 10:20:55 +0100

    feat: add swap button (#5788)
    * feat: add swap button
    
    This reverts commit 8a620dcbbc2e378254ef77f9a00d598adbee4feb.
    
    * feat: Added swap-button planner-web style
    
    * fix: Removed possibility to hide swap button
    
    * fix: Removed unnecessary &&

--- src/components/swap-button/SwapButton.tsx
+++ src/components/swap-button/SwapButton.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import {Swap} from '@atb/assets/svg/mono-icons/actions';
 import type {ButtonProps} from '@atb/components/button';
 import {PressableOpacity} from '@atb/components/pressable-opacity';
+import {TripSearchTexts, useTranslation} from '@atb/translations';
+import {screenReaderPause} from '@atb/components/text';
+import {ThemeIcon} from '@atb/components/theme-icon';
 
 export function SwapButton(props: ButtonProps) {
+  const {t} = useTranslation();
   return (
-    <PressableOpacity {...props}>
-      <Swap />
+    <PressableOpacity
+      style={props.style}
+      {...props}
+      accessibilityLabel={
+        t(TripSearchTexts.location.swapButton.a11yLabel) + screenReaderPause
+      }
+    >
+      <ThemeIcon svg={Swap} size="normal" />
     </PressableOpacity>
   );
 }
---
+++ src/components/swap-button/WithSwapButton.tsx
@@ -0,0 +1,65 @@
+import type {PropsWithChildren} from 'react';
+import {View, type ViewStyle} from 'react-native';
+import {StyleSheet} from '@atb/theme';
+import {SwapButton} from './SwapButton';
+import type {ContrastColor} from '@atb-as/theme';
+
+type Props = PropsWithChildren<{
+  onPress: () => void;
+  backgroundColor: ContrastColor;
+  horizontalPosition: 'left' | 'right';
+  swapButtonStyleOverride?: ViewStyle;
+}>;
+
+export function WithSwapButton({
+  children,
+  horizontalPosition,
+  onPress,
+  backgroundColor,
+  swapButtonStyleOverride,
+}: Props) {
+  const style = useStyle();
+  return (
+    <View>
+      {children}
+      <SwapButton
+        style={[
+          style.common,
+          style.circle,
+          style.verticalCenter,
+          style[horizontalPosition],
+          swapButtonStyleOverride,
+        ]}
+        onPress={onPress}
+        expanded={false}
+        backgroundColor={backgroundColor}
+        pointerEvents="box-only"
+      />
+    </View>
+  );
+}
+
+const useStyle = StyleSheet.createThemeHook((theme) => ({
+  circle: {
+    paddingVertical: theme.spacing.medium,
+    paddingHorizontal: theme.spacing.medium,
+    borderRadius: theme.border.radius.circle,
+    borderWidth: theme.border.width.slim,
+    borderColor: theme.color.border.secondary.foreground.secondary,
+  },
+  common: {
+    position: 'absolute',
+    zIndex: 100,
+    backgroundColor: theme.color.background.neutral['0'].background,
+  },
+  verticalCenter: {
+    top: '50%',
+    transform: [{translateY: '-50%'}],
+  },
+  right: {
+    right: theme.spacing.medium,
+  },
+  left: {
+    left: theme.spacing.medium,
+  },
+}));
