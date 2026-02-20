5b3aedbba7db1b1a60e7cfc4b1b17c879805d0c9
Ridwan Aditama <reidzeibel@gmail.com>
2026-02-20 06:00:17 +0100

    test with dummy manifest

--- scripts/android/upload-sourcemaps.sh
+++ scripts/android/upload-sourcemaps.sh
@@ -17,6 +17,19 @@ else
     VARIANT="$APP_FLAVOR${APP_ENVIRONMENT^}"
     SOURCEMAP_ARGS=""
     if [[ "${BUNDLE_ONLY}" == "true" ]]; then
+      MANIFEST_DIR="android/app/build/intermediates/merged_manifests/${VARIANT}"
+      mkdir -p "${MANIFEST_DIR}"
+      cat > "${MANIFEST_DIR}/AndroidManifest.xml" << EOF
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.your.package.name"
+    android:versionCode="${BUILD_ID}"
+    android:versionName="${APP_VERSION}">
+    <application>
+        <meta-data android:name="com.bugsnag.android.API_KEY" android:value="${BUGSNAG_API_KEY}"/>
+    </application>
+</manifest>
+EOF
       echo "Uploading sourcemaps from replaced bundle"
       SOURCEMAP_ARGS="--bundle=${BUNDLE_PATH} --source-map=${SOURCEMAP_PATH}"
       echo "${SOURCEMAP_ARGS}"
