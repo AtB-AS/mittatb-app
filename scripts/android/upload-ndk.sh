380d751a6703600b1eb2e07ac2a88a9983547e28
Ridwan Aditama <reidzeibel@gmail.com>
2026-02-23 11:25:51 +0100

    upload ndk store

---
+++ scripts/android/upload-ndk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[
+    -z "${BUGSNAG_API_KEY}"
+   ]]; then
+    echo "Argument error!"
+    echo "Expected one env variables: 
+  - BUGSNAG_API_KEY"
+
+    exit 1
+else 
+    echo "Uploading Android NDK symbols (so files)"
+    bugsnag-cli upload android-ndk \
+        android/ \
+        --api-key="${BUGSNAG_API_KEY}" \
+        --application-id="${ANDROID_APPLICATION_ID}" \
+        --version-name="${APP_VERSION}" \
+        --version-code="${BUILD_ID}" \
+        --project-root=/Users/runner/work/mittatb-app/mittatb-app \
+        --verbose
+fi
