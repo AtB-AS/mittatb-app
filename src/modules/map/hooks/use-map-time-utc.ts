9aa1fd9313b2ca8f5a0055bc34a959840ed17763
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2026-03-04 10:38:53 +0100

    Simplify useMapTimeUtc

--- src/modules/map/hooks/use-map-time-utc.ts
+++ src/modules/map/hooks/use-map-time-utc.ts
@@ -1,39 +1,22 @@
 import {getServerNowGlobal} from '@atb/modules/time';
-import {ONE_SECOND_MS} from '@atb/utils/durations';
+import {ONE_MINUTE_MS} from '@atb/utils/durations';
 import {useInterval} from '@atb/utils/use-interval';
-import {useState, useCallback, useEffect} from 'react';
+import {useState, useCallback} from 'react';
 
-// this should not be too low, as it is not good for performance to change this often
-const MAP_TIME_UTC_UPDATE_INTERVAL = 30 * ONE_SECOND_MS;
-
-// timestamp used for filtering tiles with outdated data
+// Use case: filtering tiles with expired data
 export const useMapTimeUtc = (isFocusedAndActive: boolean) => {
   const [mapTimeUtc, setMapTimeUtc] = useState(getServerNowGlobal());
 
-  const updateMapTimeUtc = useCallback(
-    (isFocusedAndActive: boolean) => {
-      const now = getServerNowGlobal();
-      if (
-        isFocusedAndActive &&
-        now > mapTimeUtc + MAP_TIME_UTC_UPDATE_INTERVAL
-      ) {
-        setMapTimeUtc(now);
-      }
-    },
-    [mapTimeUtc],
-  );
+  const updateMapTimeUtc = useCallback((isFocusedAndActive: boolean) => {
+    isFocusedAndActive && setMapTimeUtc(getServerNowGlobal());
+  }, []);
 
-  // update when isFocusedAndActive changes
-  useEffect(
-    () => updateMapTimeUtc(isFocusedAndActive),
-    [isFocusedAndActive, updateMapTimeUtc],
-  );
-
-  // update every 30s
   useInterval(
     () => updateMapTimeUtc(isFocusedAndActive),
-    [updateMapTimeUtc, isFocusedAndActive],
-    MAP_TIME_UTC_UPDATE_INTERVAL,
+    [updateMapTimeUtc, isFocusedAndActive], // the isFocusedAndActive dependency triggers updates when important
+    5 * ONE_MINUTE_MS, // frequent updates are bad for performance, and should only make a difference when tile requests fail, so keep the interval long
+    false,
+    true,
   );
 
   return mapTimeUtc;
