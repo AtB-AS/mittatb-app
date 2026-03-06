d133a335181b8b19b1e60010be51ea8eb6a8f56b
Ridwan Aditama <reidzeibel@gmail.com>
2026-03-06 12:04:41 +0100

    remove reanimated from use-scroll-border

--- src/utils/use-scroll-border.ts
+++ src/utils/use-scroll-border.ts
@@ -1,24 +1,21 @@
+import {useCallback, useState} from 'react';
+import type {NativeScrollEvent, NativeSyntheticEvent, ViewStyle} from 'react-native';
 import {useThemeContext} from '@atb/theme';
-import {
-  useAnimatedScrollHandler,
-  useSharedValue,
-  useAnimatedStyle,
-} from 'react-native-reanimated';
 
 export function useScrollBorder() {
   const {theme} = useThemeContext();
-  const scrollY = useSharedValue(0);
+  const [isScrolled, setIsScrolled] = useState(false);
   const color = theme.color.background.neutral[3].background;
-  const scrollHandler = useAnimatedScrollHandler({
-    onScroll: (event) => {
-      scrollY.value = event.contentOffset.y;
-    },
-  });
 
-  const borderStyle = useAnimatedStyle(() => ({
+  const onScroll = useCallback((event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const y = event.nativeEvent.contentOffset.y;
+    setIsScrolled(y > 0);
+  }, []);
+
+  const borderStyle: ViewStyle = {
     borderBottomWidth: 1,
-    borderBottomColor: scrollY.value > 0 ? color : 'transparent',
-  }));
+    borderBottomColor: isScrolled ? color : 'transparent',
+  };
 
-  return {scrollHandler, borderStyle} as const;
+  return {onScroll, borderStyle} as const;
 }
