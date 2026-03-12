250f2fda4e50f02a550ea2c531cf8f23005fa40b
Ridwan Aditama <reidzeibel@gmail.com>
2026-03-12 08:25:00 +0100

    refactor: improvements

--- scripts/android/create-sourcemaps.sh
+++ scripts/android/create-sourcemaps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir -p bundle
+BUNDLE_DIR="${GITHUB_WORKSPACE:-.}/bundle"
+mkdir -p "$BUNDLE_DIR"
 
-npx react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output bundle/index.android.bundle --sourcemap-output bundle/index.android.bundle.map
+npx react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output "$BUNDLE_DIR/index.android.bundle" --sourcemap-output "$BUNDLE_DIR/index.android.bundle.map"
--- scripts/ios/create-sourcemaps.sh
+++ scripts/ios/create-sourcemaps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p bundle
+BUNDLE_DIR="${GITHUB_WORKSPACE:-.}/bundle"
+mkdir -p "$BUNDLE_DIR"
 
-# iOS doesn't output source maps when bundling normally 
-npx react-native bundle --platform ios --dev false --reset-cache --entry-file index.js --bundle-output bundle/main.jsbundle --sourcemap-output bundle/main.jsbundle.map
+# iOS doesn't output source maps when bundling normally
+npx react-native bundle --platform ios --dev false --reset-cache --entry-file index.js --bundle-output "$BUNDLE_DIR/main.jsbundle" --sourcemap-output "$BUNDLE_DIR/main.jsbundle.map"
