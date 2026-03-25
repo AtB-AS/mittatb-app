a491b76c15e1db926e6fb9cea1c576673170807f
Håkon Strandlie <hakon.strandlie@atb.no>
2026-03-25 16:29:08 +0100

    feat: Implemented WithNotificationBadge for TravelCard

--- src/screen-components/travel-card/legs/TransportationLeg.tsx
+++ src/screen-components/travel-card/legs/TransportationLeg.tsx
@@ -4,16 +4,25 @@ import {isLineFlexibleTransport} from '@atb/screen-components/travel-details-scr
 import {useTranslation, TravelCardTexts} from '@atb/translations';
 import {getTranslatedModeName} from '@atb/utils/transportation-names';
 import {View} from 'react-native';
+import {useThemeContext} from '@atb/theme';
+import React from 'react';
+import {getNotificationSvgForLeg, getA11yLabelForLeg} from '@atb/modules/situations';
+import {isDefined} from '@atb/utils/presence';
 
 export const TransportationLeg = ({leg}: {leg: Leg}) => {
   const {t} = useTranslation();
+  const {themeName} = useThemeContext();
   const modeName = t(
     getTranslatedModeName(leg.mode, leg.line?.transportSubmode),
   );
   const publicCode = leg.line?.publicCode;
-  const a11yLabel = t(
-    TravelCardTexts.legs.transportation.a11yLabel(modeName, publicCode ?? ''),
-  );
+  const a11yLabel = [
+    t(TravelCardTexts.legs.transportation.a11yLabel(modeName, publicCode ?? '')),
+    getA11yLabelForLeg(leg, t),
+  ]
+    .filter(isDefined)
+    .join('. ');
+  const notification = getNotificationSvgForLeg(leg, themeName);
   return (
     <View accessible={true} accessibilityLabel={a11yLabel}>
       <TransportationIconBox
@@ -23,6 +32,7 @@ export const TransportationLeg = ({leg}: {leg: Leg}) => {
         lineNumber={leg.line?.publicCode}
         type="standard"
         testID={`${leg.mode}Leg`}
+        notification={notification}
       />
     </View>
   );
