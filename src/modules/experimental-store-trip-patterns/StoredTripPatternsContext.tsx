1b4595a53b316a2a937019074368bd658ddd7413
Håkon Strandlie <hakon.strandlie@atb.no>
2026-04-09 12:03:28 +0200

    feat: Replaced ExperimentalFeature with feature toggle for SaveTrip (#5953)

--- src/modules/experimental-store-trip-patterns/StoredTripPatternsContext.tsx
+++ src/modules/experimental-store-trip-patterns/StoredTripPatternsContext.tsx
@@ -8,7 +8,6 @@ import React, {
 import {StoredTripPattern, storedTripPatterns} from './storage';
 import {TripPattern} from '@atb/api/types/trips';
 import {getTripPatternKey} from '../trip-patterns/utils';
-import {wrapWithExperimentalFeatureToggledComponent} from '@atb/modules/experimental';
 import {ONE_MINUTE_MS} from '@atb/utils/durations';
 import {useTimeContext} from '@atb/modules/time';
 
@@ -24,87 +23,79 @@ const StoredTripPatternsContext = createContext<
   StoredTripPatternsContextState | undefined
 >(undefined);
 
-export const StoredTripPatternsContextProvider =
-  wrapWithExperimentalFeatureToggledComponent<React.PropsWithChildren>(
-    'render-children-if-disabled',
-    ({children}) => {
-      const [tripPatterns, setTripPatternsState] = useState<
-        StoredTripPattern[]
-      >([]);
-      const {serverNow} = useTimeContext(ONE_MINUTE_MS);
+export const StoredTripPatternsContextProvider: React.FC<
+  React.PropsWithChildren
+> = ({children}) => {
+  const [tripPatterns, setTripPatternsState] = useState<StoredTripPattern[]>(
+    [],
+  );
+  const {serverNow} = useTimeContext(ONE_MINUTE_MS);
 
-      const populateTripPatterns = useCallback(async () => {
-        const tripPatterns = await storedTripPatterns.getTripPatterns();
-        setTripPatternsState(tripPatterns ?? []);
-      }, []);
+  const populateTripPatterns = useCallback(async () => {
+    const tripPatterns = await storedTripPatterns.getTripPatterns();
+    setTripPatternsState(tripPatterns ?? []);
+  }, []);
 
-      useEffect(() => {
-        populateTripPatterns();
-      }, [populateTripPatterns]);
+  useEffect(() => {
+    populateTripPatterns();
+  }, [populateTripPatterns]);
 
-      const addTripPattern = useCallback(async (tripPattern: TripPattern) => {
-        const newTripPatterns =
-          await storedTripPatterns.addTripPattern(tripPattern);
-        setTripPatternsState(newTripPatterns);
-      }, []);
+  const addTripPattern = useCallback(async (tripPattern: TripPattern) => {
+    const newTripPatterns =
+      await storedTripPatterns.addTripPattern(tripPattern);
+    setTripPatternsState(newTripPatterns);
+  }, []);
 
-      const removeTripPattern = useCallback(
-        async (tripPattern: TripPattern) => {
-          const newTripPatterns =
-            await storedTripPatterns.removeTripPattern(tripPattern);
-          setTripPatternsState(newTripPatterns);
-        },
-        [],
-      );
+  const removeTripPattern = useCallback(async (tripPattern: TripPattern) => {
+    const newTripPatterns =
+      await storedTripPatterns.removeTripPattern(tripPattern);
+    setTripPatternsState(newTripPatterns);
+  }, []);
 
-      const updateTripPattern = useCallback(
-        async (tripPattern: TripPattern) => {
-          const newTripPatterns =
-            await storedTripPatterns.updateTripPattern(tripPattern);
-          setTripPatternsState(newTripPatterns);
-        },
-        [],
-      );
+  const updateTripPattern = useCallback(async (tripPattern: TripPattern) => {
+    const newTripPatterns =
+      await storedTripPatterns.updateTripPattern(tripPattern);
+    setTripPatternsState(newTripPatterns);
+  }, []);
 
-      const isTripPatternStored = useCallback(
-        (tripPattern: TripPattern) => {
-          return tripPatterns.some(
-            (tp) => tp.key === getTripPatternKey(tripPattern),
-          );
-        },
-        [tripPatterns],
-      );
-
-      const canAddTripPattern = useCallback((tripPattern: TripPattern) => {
-        return tripPattern && tripPattern.legs.filter((l) => l.id).length > 0;
-      }, []);
-
-      const removeTripPatternsOlderThan = useCallback(async (date: Date) => {
-        const newTripPatterns =
-          await storedTripPatterns.removeTripPatternsOlderThan(date);
-        setTripPatternsState(newTripPatterns);
-      }, []);
-
-      useEffect(() => {
-        removeTripPatternsOlderThan(new Date(serverNow));
-      }, [serverNow, removeTripPatternsOlderThan]);
-
-      const contextValue: StoredTripPatternsContextState = {
-        tripPatterns,
-        addTripPattern,
-        removeTripPattern,
-        updateTripPattern,
-        isTripPatternStored,
-        canAddTripPattern,
-      };
-
-      return (
-        <StoredTripPatternsContext.Provider value={contextValue}>
-          {children}
-        </StoredTripPatternsContext.Provider>
+  const isTripPatternStored = useCallback(
+    (tripPattern: TripPattern) => {
+      return tripPatterns.some(
+        (tp) => tp.key === getTripPatternKey(tripPattern),
       );
     },
+    [tripPatterns],
   );
+
+  const canAddTripPattern = useCallback((tripPattern: TripPattern) => {
+    return tripPattern && tripPattern.legs.filter((l) => l.id).length > 0;
+  }, []);
+
+  const removeTripPatternsOlderThan = useCallback(async (date: Date) => {
+    const newTripPatterns =
+      await storedTripPatterns.removeTripPatternsOlderThan(date);
+    setTripPatternsState(newTripPatterns);
+  }, []);
+
+  useEffect(() => {
+    removeTripPatternsOlderThan(new Date(serverNow));
+  }, [serverNow, removeTripPatternsOlderThan]);
+
+  const contextValue: StoredTripPatternsContextState = {
+    tripPatterns,
+    addTripPattern,
+    removeTripPattern,
+    updateTripPattern,
+    isTripPatternStored,
+    canAddTripPattern,
+  };
+
+  return (
+    <StoredTripPatternsContext.Provider value={contextValue}>
+      {children}
+    </StoredTripPatternsContext.Provider>
+  );
+};
 
 export function useStoredTripPatterns() {
   const context = useContext(StoredTripPatternsContext);
