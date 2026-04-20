ecd2a8683efeece1cce9d1b7ff54620a3e6ab534
Jorunn Leithe <jorunn@selbu.org>
2026-04-20 14:57:22 +0200

    feat: update design for Point product

--- src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_BonusScreen.tsx
+++ src/stacks-hierarchy/Root_TabNavigatorStack/TabNav_ProfileStack/Profile_BonusScreen.tsx
@@ -7,8 +7,6 @@ import {StyleSheet, useThemeContext} from '@atb/theme';
 import {
   BonusProgramTexts,
   dictionary,
-  getTextForLanguage,
-  Language,
   ProfileTexts,
   useTranslation,
 } from '@atb/translations';
@@ -16,7 +14,7 @@ import React, {useEffect, useRef, useState} from 'react';
 import {Platform, View} from 'react-native';
 import {openUrl} from '@atb/utils/open-url';
 import {FullScreenView} from '@atb/components/screen-view';
-import {screenReaderPause, ThemeText} from '@atb/components/text';
+import {ThemeText} from '@atb/components/text';
 import {
   ThemedBonusBag,
   ThemedBonusTransaction,
@@ -24,10 +22,10 @@ import {
 } from '@atb/theme/ThemedAssets';
 import {ContentHeading, ScreenHeading} from '@atb/components/heading';
 import {
-  BonusPriceTag,
   UserBonusBalanceContent,
   isActive,
   useBonusBalanceQuery,
+  BonusProductList,
 } from '@atb/modules/bonus';
 import {
   useIsEnrolled,
@@ -36,11 +34,7 @@ import {
 } from '@atb/modules/enrollment';
 import {useAuthContext} from '@atb/modules/auth';
 import {MessageInfoBox} from '@atb/components/message-info-box';
-import {
-  BonusProductType,
-  useFirestoreConfigurationContext,
-} from '@atb/modules/configuration';
-import {BrandingImage, findOperatorBrandImageUrl} from '@atb/modules/mobility';
+import {useFirestoreConfigurationContext} from '@atb/modules/configuration';
 import {isDefined} from '@atb/utils/presence';
 import {Chat} from '@atb/assets/svg/mono-icons/actions';
 import {LogIn} from '@atb/assets/svg/mono-icons/profile';
@@ -51,8 +45,6 @@ import {ExternalLink} from '@atb/assets/svg/mono-icons/navigation';
 import {Button} from '@atb/components/button';
 import {useFocusOnLoad} from '@atb/utils/use-focus-on-load';
 import {ProfileScreenProps} from './navigation-types';
-import {useFontScale} from '@atb/utils/use-font-scale';
-import {TFunc} from '@leile/lobo-t';
 import {formatToDate} from '@atb/utils/date';
 import {MessageInfoText} from '@atb/components/message-info-text';
 import {
@@ -73,8 +65,7 @@ export const Profile_BonusScreen = ({navigation}: Props) => {
   const {authenticationType} = useAuthContext();
   const {enable_vipps_login} = useRemoteConfigContext();
   const isLoggedIn = authenticationType === 'phone';
-  const {bonusProducts, mobilityOperators} = useFirestoreConfigurationContext();
-  const fontScale = useFontScale();
+  const {bonusProducts} = useFirestoreConfigurationContext();
 
   const isEnrolled = useIsEnrolled(KnownProgramId.BONUS);
   const wasEnrolledRef = useRef(isEnrolled);
@@ -227,54 +218,20 @@ export const Profile_BonusScreen = ({navigation}: Props) => {
             />
           </View>
         ) : (
-          <View style={styles.bonusProductsContainer}>
-            <Section>
-              {activeBonusProducts?.map((bonusProduct) => (
-                <GenericSectionItem
-                  key={bonusProduct.id}
-                  style={{gap: theme.spacing.medium}}
-                  accessibility={{
-                    accessible: true,
-                    accessibilityLabel: bonusProductA11yLabel(
-                      bonusProduct,
-                      language,
-                      t,
-                    ),
-                  }}
-                >
-                  <View style={styles.horizontalContainer}>
-                    <BrandingImage
-                      logoUrl={findOperatorBrandImageUrl(
-                        bonusProduct.operatorId,
-                        mobilityOperators,
-                      )}
-                      logoSize={
-                        theme.typography['heading__xl'].fontSize * fontScale
-                      }
-                      style={styles.logo}
-                    />
-                    <ThemeText style={{flex: 1}}>
-                      {getTextForLanguage(
-                        bonusProduct.productDescription.title,
-                        language,
-                      ) ?? ''}
-                    </ThemeText>
-                    <BonusPriceTag amount={bonusProduct.price.amount} />
-                  </View>
-                  <ThemeText
-                    isMarkdown={true}
-                    typography="body__s"
-                    color="secondary"
-                  >
-                    {getTextForLanguage(
-                      bonusProduct.productDescription.description,
-                      language,
-                    ) ?? ''}
-                  </ThemeText>
-                </GenericSectionItem>
-              ))}
-            </Section>
-          </View>
+          activeBonusProducts && (
+            <BonusProductList
+              bonusProducts={activeBonusProducts}
+              onNavigateToMap={(initialFilters) =>
+                navigation.navigate('Root_TabNavigatorStack', {
+                  screen: 'TabNav_MapStack',
+                  params: {
+                    screen: 'Map_RootScreen',
+                    params: {initialFilters},
+                  },
+                })
+              }
+            />
+          )
         )}
         <ContentHeading
           text={t(BonusProgramTexts.bonusProfile.feedback.heading)}
@@ -336,24 +293,6 @@ const useStyles = StyleSheet.createThemeHook((theme) => ({
     marginTop: theme.spacing.small,
   },
 }));
-
-const bonusProductA11yLabel = (
-  bonusProduct: BonusProductType,
-  language: Language,
-  t: TFunc<typeof Language>,
-) => {
-  return (
-    (getTextForLanguage(bonusProduct.productDescription.title, language) ??
-      '') +
-    screenReaderPause +
-    t(BonusProgramTexts.costA11yLabel(bonusProduct.price.amount)) +
-    screenReaderPause +
-    (getTextForLanguage(
-      bonusProduct.productDescription.description,
-      language,
-    ) ?? '')
-  );
-};
 
 const HowPointsWork = () => {
   const {t} = useTranslation();
