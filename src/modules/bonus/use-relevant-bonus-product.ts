2aa353942673570b06c64269d112365469e60c1c
Jorunn Leithe <jorunn@selbu.org>
2026-05-05 16:30:20 +0200

    fix: remove unnecessary call to useIsBonusActiveForUser in useRelevantBonusProduct

--- src/modules/bonus/use-relevant-bonus-product.ts
+++ src/modules/bonus/use-relevant-bonus-product.ts
@@ -1,14 +1,12 @@
 import {FormFactor} from '@atb/api/types/generated/mobility-types_v2';
-import {useIsBonusActiveForUser} from './use-is-bonus-active-for-user';
 import {useActiveBonusProductsQuery} from './queries';
 
 export const useRelevantBonusProduct = (
   operatorId: string | undefined,
   formFactor: FormFactor,
 ) => {
-  const isBonusActiveForUser = useIsBonusActiveForUser();
   const {data: activeBonusProducts} = useActiveBonusProductsQuery(
-    Boolean(operatorId) && isBonusActiveForUser,
+    Boolean(operatorId),
   );
   if (!operatorId) return undefined;
   return activeBonusProducts?.find(
