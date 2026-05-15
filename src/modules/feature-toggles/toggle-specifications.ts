b9efd333d2479502671f9c6a1f06d826d8c009ed
Håkon Strandlie <hakon.strandlie@atb.no>
2026-05-15 15:49:41 +0200

    feat: added NewTravelCardInTripSearch feature toggle (#6073)

--- src/modules/feature-toggles/toggle-specifications.ts
+++ src/modules/feature-toggles/toggle-specifications.ts
@@ -82,6 +82,10 @@ export const toggleSpecifications = [
     remoteConfigKey: 'enable_new_travel_card_booking',
   },
   {
+    name: 'isNewTravelCardInTripSearchEnabled',
+    remoteConfigKey: 'enable_new_travel_card_in_trip_search',
+  },
+  {
     name: 'isNynorskEnabled',
     remoteConfigKey: 'enable_nynorsk',
   },
