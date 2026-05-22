42aa4c1a1c2c3ccfdefaa76940f1702921ce9bd5
Ridwan Aditama <reidzeibel@gmail.com>
2026-05-22 12:20:21 +0200

    refactor: add legs fragment

--- src/api/types/generated/fragments/stop-places.ts
+++ src/api/types/generated/fragments/stop-places.ts
@@ -1,6 +1,9 @@
+import * as Types from '@atb/api/types/generated/journey_planner_v3_types';
+
 export type StopPlaceFragment = {
   id: string;
   name: string;
   latitude?: number;
   longitude?: number;
+  transportMode?: Array<Types.TransportMode>;
 };
