784370d949311bca8a64b54d756dd847b31e6603
Johannes Røsvik <j.rosvik@gmail.com>
2026-06-25 13:35:26 +0200

    feat: update DebugServerOverrides bottom sheet (#6202)

--- src/modules/debug/HeaderOverride.tsx
+++ src/modules/debug/HeaderOverride.tsx
@@ -1,8 +1,9 @@
-import {TextInputSectionItem} from '@atb/components/sections';
+import {Section, TextInputSectionItem} from '@atb/components/sections';
 import React from 'react';
 import {View} from 'react-native';
 import {StyleSheet, useThemeContext} from '@atb/theme';
 import {Button} from '@atb/components/button';
+import {BottomSheetTextInput} from '@gorhom/bottom-sheet';
 
 type Props = {
   headerKey: string;
@@ -24,8 +25,9 @@ export function HeaderOverride({
 
   return (
     <View style={style.container}>
-      <View style={style.inputs}>
+      <Section>
         <TextInputSectionItem
+          InputComponent={BottomSheetTextInput}
           label="Key"
           placeholder="Authorization"
           onChangeText={onKeyChange}
@@ -33,33 +35,28 @@ export function HeaderOverride({
           autoCapitalize="none"
         />
         <TextInputSectionItem
+          InputComponent={BottomSheetTextInput}
           label="Value"
           placeholder="Bearer ..."
           onChangeText={onValueChange}
           value={headerValue}
           autoCapitalize="none"
         />
-      </View>
-      <View style={style.button}>
-        <Button
-          onPress={onDelete}
-          expanded={false}
-          type="small"
-          text="Remove"
-          mode="secondary"
-          backgroundColor={theme.color.background.neutral[1]}
-        />
-      </View>
+      </Section>
+      <Button
+        onPress={onDelete}
+        expanded={false}
+        type="small"
+        text="Remove"
+        mode="secondary"
+        backgroundColor={theme.color.background.neutral[1]}
+      />
     </View>
   );
 }
 
 const useStyles = StyleSheet.createThemeHook((theme) => ({
   container: {
-    gap: theme.spacing.medium,
-  },
-  inputs: {
     gap: theme.spacing.small,
   },
-  button: {},
 }));
