d3bb924c0e5a1b203808d5f5bbd366b8d8bec16c
Johannes Røsvik <j.rosvik@gmail.com>
2026-06-26 13:27:45 +0200

    fix: avoid resub on rerender

--- src/modules/event-stream/use-event-stream-listener.ts
+++ src/modules/event-stream/use-event-stream-listener.ts
@@ -1,10 +1,10 @@
-import {useEffect, useId} from 'react';
+import {useEffect, useId, useRef} from 'react';
 import {useEventStreamContext} from './EventStreamContext';
 import {EventKind, StreamEventOfKind} from './types';
 
 /**
  * Listens for events of a specific kind from the event stream, and calls the
- * provided callback when such events are received.
+ * provided callback when events of that kind are received.
  */
 export function useEventStreamListener<K extends EventKind>(
   eventKind: K,
@@ -13,15 +13,25 @@ export function useEventStreamListener<K extends EventKind>(
   const {subscribe, unsubscribe} = useEventStreamContext();
   const id = useId();
 
+  // Keep the callback in a ref to avoid resubscribes on rerenders.
+  const callbackRef = useRef(callback);
   useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    // The event kind isn't unique when several listeners listen to the same
+    // event kind, so we combine it with an ID that is unique to this hook
+    // instance.
     const listenerId = `${eventKind}-${id}`;
+
     subscribe({
       id: listenerId,
       eventKind,
-      callback,
+      callback: (streamEvent) => callbackRef.current(streamEvent),
     });
     return () => {
       unsubscribe(listenerId);
     };
-  }, [subscribe, unsubscribe, callback, eventKind, id]);
+  }, [subscribe, unsubscribe, eventKind, id]);
 }
