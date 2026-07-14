9405efc225c6bcf7e196adbb96b1b42d088c4b74
Johannes Røsvik <j.rosvik@gmail.com>
2026-07-14 15:53:34 +0200

    feat: show alert on fare contract screenshots (#6235)

--- src/modules/fare-contracts/details/Barcode.tsx
+++ src/modules/fare-contracts/details/Barcode.tsx
@@ -11,7 +11,7 @@ import Bugsnag from '@bugsnag/react-native';
 import {renderAztec} from '@entur-private/abt-mobile-barcode-javascript-lib';
 import QRCode from 'qrcode';
 import React, {RefObject, useEffect, useRef, useState} from 'react';
-import {View} from 'react-native';
+import {Alert, View} from 'react-native';
 import {NativeBlockButton} from '@atb/components/native-button';
 import {SvgXml} from 'react-native-svg';
 import {GenericSectionItem} from '@atb/components/sections';
@@ -31,6 +31,7 @@ import {
   BottomSheetModalMethods,
 } from '@atb/components/bottom-sheet';
 import {Loading} from '@atb/components/loading';
+import {useScreenshotAware} from 'react-native-screenshot-aware';
 
 type Props = {
   validityStatus: ValidityStatus;
@@ -38,9 +39,22 @@ type Props = {
 };
 
 export function Barcode({validityStatus, fc}: Props): React.JSX.Element | null {
+  const {t} = useTranslation();
   const {mobileTokenStatus} = useMobileTokenContext();
   const {enable_new_token_barcode} = useRemoteConfigContext();
+  const isFocusedAndActive = useIsFocusedAndActive();
+
   useScreenBrightnessIncrease();
+
+  useScreenshotAware(() => {
+    if (isFocusedAndActive) {
+      Alert.alert(
+        t(FareContractTexts.details.screenshotWarning.title),
+        t(FareContractTexts.details.screenshotWarning.description),
+      );
+    }
+  });
+
   if (validityStatus !== 'valid') return null;
 
   switch (mobileTokenStatus) {
