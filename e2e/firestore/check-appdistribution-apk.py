e4b3f87520926bd995fe1f87b5b93ed0bd910497
tormoseng <tor@moseng.net>
2026-08-14 07:57:41 +0200

    test: handle results in yml

--- e2e/firestore/check-appdistribution-apk.py
+++ e2e/firestore/check-appdistribution-apk.py
@@ -12,6 +12,7 @@ from google.oauth2 import service_account
 CREDENTIALS_APITESTER = base64.b64decode(os.environ.get('CREDENTIALS_APITESTER', '{}'))
 if not CREDENTIALS_APITESTER:
     raise ValueError("CREDENTIALS_APITESTER environment variable is not set")
+BUILD_ID = os.environ.get('BUILD_ID', '')
 # AtB Firebase IDs
 PROJECT_ID = "939812594010"
 APP_ID = "1:939812594010:android:e6721549bec728c3069363"
@@ -69,19 +70,29 @@ def list_releases(access_token: str):
             print("[WARN] No releases found")
             sys.exit()
         else:
-            for release in releases:
-                # Only use master branch from today
-                if 'Branch: master' in (release.get('releaseNotes', {}).get('text', '')):
-                    ts = release.get('createTime', '')
-                    dt = datetime.fromisoformat(ts.replace("Z", "+00:00"))
-                    # Only check for new releases, i.e. releases today
-                    if dt.date() == datetime.now(timezone.utc).date():
+            # Only use master branch from today - OR given build id
+            if BUILD_ID != '':
+                for release in releases:
+                    if BUILD_ID == release.get('buildVersion', ''):
                         print("[INFO] Release found")
                         print("[INFO] App version: {}".format(release.get('displayVersion', '')))
                         print("[INFO] App build ID: {}".format(release.get('buildVersion', '')))
                         print("[INFO] App creation time: {}".format(release.get('createTime', '')))
                         MY_RELEASE = release
                         break
+            else:
+                for release in releases:
+                    if 'Branch: master' in (release.get('releaseNotes', {}).get('text', '')):
+                        ts = release.get('createTime', '')
+                        dt = datetime.fromisoformat(ts.replace("Z", "+00:00"))
+                        # Only check for new releases, i.e. releases today
+                        if dt.date() == datetime.now(timezone.utc).date():
+                            print("[INFO] Release found")
+                            print("[INFO] App version: {}".format(release.get('displayVersion', '')))
+                            print("[INFO] App build ID: {}".format(release.get('buildVersion', '')))
+                            print("[INFO] App creation time: {}".format(release.get('createTime', '')))
+                            MY_RELEASE = release
+                            break
     else:
         print(f"[ERROR] Failed to list releases")
         print(f"[ERROR] HTTP {response.status_code}. Response: {response.text}")
