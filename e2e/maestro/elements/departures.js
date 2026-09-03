6a70fa6dd676f7a8721371fb30999c0fdc4c86b0
tormoseng <tor@moseng.net>
2026-09-03 12:38:21 +0200

    test: travel search with time changes

--- e2e/maestro/elements/departures.js
+++ e2e/maestro/elements/departures.js
@@ -1,28 +1,31 @@
 output.departures = {
-    departure: departure
+  departure: departure,
+  timePicker: 'setDateButton',
+  previousDay: 'previousDayButton',
+  nextDay: 'nextDayButton',
 };
 
 output.departure = {
-    view: 'departureDetailsContentView',
-    publicCode: 'publicCode',
-    lineName: 'lineName',
-    leg: 'legType_trip',
-    quayName: 'quayName',
+  view: 'departureDetailsContentView',
+  publicCode: 'publicCode',
+  lineName: 'lineName',
+  leg: 'legType_trip',
+  quayName: 'quayName',
 };
 
 output.nearby = {
-    stopPlace: stopPlace,
-    stopPlaceName: stopPlaceName
+  stopPlace: stopPlace,
+  stopPlaceName: stopPlaceName,
 };
 
-function departure(index   = 0) {
-    return 'quay' + index + 'CallItem';
+function departure(index = 0) {
+  return 'quay' + index + 'CallItem';
 }
 
-function stopPlace(index   = 0) {
-    return 'stopPlaceItem' + index;
+function stopPlace(index = 0) {
+  return 'stopPlaceItem' + index;
 }
 
-function stopPlaceName(index   = 0) {
-    return 'stopPlaceItem' + index + 'Name';
+function stopPlaceName(index = 0) {
+  return 'stopPlaceItem' + index + 'Name';
 }
